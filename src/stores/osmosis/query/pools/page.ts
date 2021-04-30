9650dcb08cb5f92207fa8eb3925cbb3af2b5eb91
Thunnini <yunjh1994@gmail.com>
2021-04-30 19:26:35 +0900

    Add initial query for paginated pools

---
+++ src/stores/osmosis/query/pools/page.ts
@@ -0,0 +1,39 @@
+import { ObservableChainQuery } from "@keplr-wallet/stores/build/query/chain-query";
+import { Pools } from "./types";
+import { ChainGetter } from "@keplr-wallet/stores";
+import { KVStore } from "@keplr-wallet/common";
+import { computed, makeObservable } from "mobx";
+import { ObservablePool } from "../pool";
+
+export class ObservableQueryPoolsPagination extends ObservableChainQuery<
+  Pools
+> {
+  constructor(
+    kvStore: KVStore,
+    chainId: string,
+    chainGetter: ChainGetter,
+    itemsPerPage: number,
+    page: number
+  ) {
+    super(
+      kvStore,
+      chainId,
+      chainGetter,
+      `/osmosis/gamm/v1beta1/pools/all?pagination.offset=${(page - 1) *
+        itemsPerPage}&pagination.limit=${itemsPerPage}`
+    );
+
+    makeObservable(this);
+  }
+
+  @computed
+  get pools(): ObservablePool[] {
+    if (!this.response) {
+      return [];
+    }
+
+    return this.response.data.pools.map(pool => {
+      return new ObservablePool(this.chainId, this.chainGetter, pool);
+    });
+  }
+}
--- src/stores/osmosis/query/pools/pools.ts
+++ src/stores/osmosis/query/pools/pools.ts
@@ -2,14 +2,42 @@ import { ObservableChainQuery } from "@keplr-wallet/stores/build/query/chain-que
 import { Pools } from "./types";
 import { ChainGetter } from "@keplr-wallet/stores";
 import { KVStore } from "@keplr-wallet/common";
-import { computed, makeObservable } from "mobx";
+import { computed, makeObservable, observable, runInAction } from "mobx";
 import { ObservablePool } from "../pool";
+import { ObservableQueryPoolsPagination } from "./page";
 
 export class ObservableQueryPools extends ObservableChainQuery<Pools> {
+  @observable.shallow
+  protected map: Map<string, ObservableQueryPoolsPagination> = new Map();
+
   constructor(kvStore: KVStore, chainId: string, chainGetter: ChainGetter) {
     super(kvStore, chainId, chainGetter, "/osmosis/gamm/v1beta1/pools/all");
 
     makeObservable(this);
+  }
+
+  getPoolsPagenation(
+    itemsPerPage: number,
+    page: number
+  ): ObservableQueryPoolsPagination {
+    const key = `${itemsPerPage}/${page}`;
+
+    if (!this.map.has(key)) {
+      const query = new ObservableQueryPoolsPagination(
+        this.kvStore,
+        this.chainId,
+        this.chainGetter,
+        itemsPerPage,
+        page
+      );
+
+      runInAction(() => {
+        this.map.set(key, query);
+      });
+    }
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    return this.map.get(key)!;
   }
 
   @computed
