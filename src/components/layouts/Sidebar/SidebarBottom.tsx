52b587d1e482bd828617bacc6c9c294bb3e65fd3
Thunnini <yunjh1994@gmail.com>
2021-05-04 00:19:25 +0900

    Implement the feature that initializes Keplr account

--- src/components/layouts/Sidebar/SidebarBottom.tsx
+++ src/components/layouts/Sidebar/SidebarBottom.tsx
@@ -2,24 +2,32 @@ import React, { FunctionComponent } from 'react';
 import cn from 'clsx';
 import { LINKS, MISC } from '../../../constants';
 import { Img } from '../../common/Img';
+import { observer } from 'mobx-react-lite';
+import { useStore } from '../../../stores';
 
-export const SidebarBottom: FunctionComponent<TSidebarBottom> = ({ openSidebar }) => {
+export const SidebarBottom: FunctionComponent<TSidebarBottom> = observer(({ openSidebar }) => {
+	const { chainStore, accountStore } = useStore();
+
+	const account = accountStore.getAccount(chainStore.current.chainId);
+
 	const buttonClick = React.useCallback(() => {
-		// TODO : @Thunnini Not sure what to add here since wallet is automatically connected
-		alert('not implemented yet');
+		account.init();
 	}, []);
 	return (
 		<div>
-			<button
-				onClick={buttonClick}
-				className="bg-primary-200 h-9 w-full rounded-md py-2 px-1 flex items-center justify-center mb-8">
-				<Img className="w-5 h-5" src={`${MISC.ASSETS_BASE}/Icons/Wallet.svg`} />
-				<p
-					style={{ maxWidth: openSidebar ? '105px' : '0px', marginLeft: `${openSidebar ? '12px' : '0px'}` }}
-					className="text-sm text-white-high font-semibold overflow-x-hidden truncate transition-all">
-					Connect Wallet
-				</p>
-			</button>
+			{/* TODO: 계정이 불러와져 있으면 뭘 표시하지? (아마 주소?) and 계정 불러오는 중의 짧은 시간 동안 로딩 인디케이터 표시? */}
+			{account.bech32Address ? null : (
+				<button
+					onClick={buttonClick}
+					className="bg-primary-200 h-9 w-full rounded-md py-2 px-1 flex items-center justify-center mb-8">
+					<Img className="w-5 h-5" src={`${MISC.ASSETS_BASE}/Icons/Wallet.svg`} />
+					<p
+						style={{ maxWidth: openSidebar ? '105px' : '0px', marginLeft: `${openSidebar ? '12px' : '0px'}` }}
+						className="text-sm text-white-high font-semibold overflow-x-hidden truncate transition-all">
+						Connect Wallet
+					</p>
+				</button>
+			)}
 			<div className={cn('flex items-center transition-all justify-center')}>
 				<Img className="w-9 h-9" src={`${MISC.ASSETS_BASE}/Icons/${openSidebar ? 'Menu-in' : 'Menu'}.svg`} />
 				<div
@@ -35,7 +43,7 @@ export const SidebarBottom: FunctionComponent<TSidebarBottom> = ({ openSidebar }
 			</div>
 		</div>
 	);
-};
+});
 interface TSidebarBottom {
 	openSidebar: boolean;
 }
