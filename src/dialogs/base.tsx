1791b1edc50860590b31d87571a8957caa31274f
Thunnini <yunjh1994@gmail.com>
2021-05-22 01:28:05 +0900

    `BaseDialog`가 open 상태일 때만 렌더링 되도록 수정

--- src/dialogs/base.tsx
+++ src/dialogs/base.tsx
@@ -11,14 +11,18 @@ export interface BaseModalProps {
 // TODO : has issues with FocusTrap -> fork and fix maybe?
 export const BaseDialog: FunctionComponent<BaseModalProps> = ({ isOpen, close, children, style }) => {
 	return (
-		<Dialog as="div" className="fixed inset-0 z-100 overflow-y-auto" open={isOpen} onClose={close}>
-			<div className="flex items-center justify-center min-h-screen">
-				<Dialog.Overlay className="fixed inset-0 bg-black opacity-20 z-0" />
-				<div style={style} className="min-w-modal p-8 bg-surface shadow-elevation-24dp rounded-2xl z-10">
-					{children}
-				</div>
-			</div>
-		</Dialog>
+		<React.Fragment>
+			{isOpen ? (
+				<Dialog as="div" className="fixed inset-0 z-100 overflow-y-auto" open={isOpen} onClose={close}>
+					<div className="flex items-center justify-center min-h-screen">
+						<Dialog.Overlay className="fixed inset-0 bg-black opacity-20 z-0" />
+						<div style={style} className="min-w-modal p-8 bg-surface shadow-elevation-24dp rounded-2xl z-10">
+							{children}
+						</div>
+					</div>
+				</Dialog>
+			) : null}
+		</React.Fragment>
 	);
 };
 
