1b9f891778d1e5dd90e96b7f48e302a1c05b7018
Thunnini <yunjh1994@gmail.com>
2021-07-15 23:31:17 +0900

    Query the pools for swap when users enter from other tab

--- src/pages/main/components/SwapButton.tsx
+++ src/pages/main/components/SwapButton.tsx
@@ -7,14 +7,35 @@ import { useAccountConnection } from '../../../hooks/account/useAccountConnectio
 import { useStore } from '../../../stores';
 import { isSlippageError } from '../../../utils/tx';
 import { TradeConfig } from '../stores/trade/config';
+import { useEffect, useMemo, useRef } from 'react';
 
 interface Props {
 	config: TradeConfig;
 }
 
+const useVisibilitychangeVisible = (fn: () => void) => {
+	const fnRef = useRef(fn);
+	fnRef.current = fn;
+
+	useEffect(() => {
+		const handler = () => {
+			if (document.visibilityState === 'visible') {
+				fnRef.current();
+			}
+		};
+
+		document.addEventListener('visibilitychange', handler);
+
+		return () => {
+			document.removeEventListener('visibilitychange', handler);
+		};
+	}, []);
+};
+
 export const SwapButton = observer(({ config }: Props) => {
-	const { chainStore, accountStore } = useStore();
+	const { chainStore, accountStore, queriesStore } = useStore();
 	const account = accountStore.getAccount(chainStore.current.chainId);
+	const queries = queriesStore.get(chainStore.current.chainId);
 	const { isAccountConnected, connectAccount } = useAccountConnection();
 
 	const toast = useToast();
@@ -30,6 +51,33 @@ export const SwapButton = observer(({ config }: Props) => {
 			/>
 		);
 	}
+
+	const currentSwapPools = useMemo(() => {
+		return config.optimizedRoutes?.swaps.map(swap => swap.poolId) ?? [];
+	}, [config.optimizedRoutes]);
+
+	const isSwapPoolsFetching =
+		currentSwapPools.find(poolId => {
+			return queries.osmosis.queryGammPools.getObservableQueryPool(poolId).isFetching;
+		}) != null;
+
+	useEffect(() => {
+		currentSwapPools.forEach(poolId => {
+			const pool = queries.osmosis.queryGammPools.getObservableQueryPool(poolId);
+			if (!pool.isFetching) {
+				pool.fetch();
+			}
+		});
+	}, []);
+
+	useVisibilitychangeVisible(() => {
+		currentSwapPools.forEach(poolId => {
+			const pool = queries.osmosis.queryGammPools.getObservableQueryPool(poolId);
+			if (!pool.isFetching) {
+				pool.fetch();
+			}
+		});
+	});
 
 	return (
 		<button
@@ -130,7 +178,26 @@ export const SwapButton = observer(({ config }: Props) => {
 					/>
 				</svg>
 			) : (
-				<p className="font-body tracking-wide">{config.showWarningOfSlippage ? 'Swap Anyway' : 'Swap'}</p>
+				<p className="font-body tracking-wide">
+					{isSwapPoolsFetching ? (
+						<svg
+							style={{
+								display: 'inline-block',
+							}}
+							xmlns="http://www.w3.org/2000/svg"
+							fill="none"
+							className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+							viewBox="0 0 24 24">
+							<circle cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4" className="opacity-25" />
+							<path
+								fill="currentColor"
+								d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								className="opacity-75"
+							/>
+						</svg>
+					) : null}
+					{config.showWarningOfSlippage ? 'Swap Anyway' : 'Swap'}
+				</p>
 			)}
 		</button>
 	);
