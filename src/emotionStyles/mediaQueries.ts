3442f96db85a3f2333641aaf792da17a80cf6b31
sko-kr <vxxcko@gmail.com>
2021-07-28 14:45:08 +0900

    define commonly used color and mediaQueries

---
+++ src/emotionStyles/mediaQueries.ts
@@ -0,0 +1,5 @@
+export const onXlWidth = `@media (max-width: 1920px)`;
+export const onLWidth = `@media (max-width: 1680px)`;
+export const onMWidth = `@media (max-width: 1380px)`;
+export const onSWidth = `@media (max-width: 1200px)`;
+export const onXsWidth = `@media (max-width: 800px)`;
