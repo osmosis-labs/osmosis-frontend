4234574c1218c6d03bb158a4c9855d320be1e819
sko-kr <vxxcko@gmail.com>
2021-07-29 13:35:33 +0900

    stop propagation so window event listener won't trigger.

--- src/components/common/TokenListDisplay.tsx
+++ src/components/common/TokenListDisplay.tsx
@@ -1,7 +1,7 @@
 import styled from '@emotion/styled';
 import { AppCurrency } from '@keplr-wallet/types';
 import { observer } from 'mobx-react-lite';
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useMemo, useState } from 'react';
 import { colorPrimary, colorPrimaryLight } from '../../emotionStyles/colors';
 import { useStore } from '../../stores';
 import { Img } from './Img';
@@ -11,20 +11,20 @@ export const TokenListDisplay: FunctionComponent<{
 	onSelect: (minimalDenom: string) => void;
 	close: () => void;
 }> = observer(({ currencies, onSelect, close }) => {
-	const [input, setInput] = React.useState<string>('');
+	const [input, setInput] = useState<string>('');
 	const { chainStore, accountStore, queriesStore } = useStore();
 
 	const account = accountStore.getAccount(chainStore.current.chainId);
 	const queries = queriesStore.get(chainStore.current.chainId);
 
-	const filteredCurrencies = React.useMemo(() => {
+	const filteredCurrencies = useMemo(() => {
 		return currencies.filter(cur => {
 			return cur.coinDenom.toUpperCase().includes(input.toUpperCase());
 		});
 	}, [currencies, input]);
 
 	return (
-		<div className="pr-5 pl-4 pt-8 pb-8">
+		<div className="pr-5 pl-4 pt-8 pb-8" onClick={e => e.stopPropagation()}>
 			<div className="w-full h-9 rounded-2xl bg-card pl-4.5 flex items-center">
 				<Img className="w-4.5 h-4.5" src="/public/assets/Icons/Search.svg" />
 				<input
