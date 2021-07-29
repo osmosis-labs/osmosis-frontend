beba234b5c60fe5e442a481ee6486ffe639626b8
sko-kr <vxxcko@gmail.com>
2021-07-29 13:31:16 +0900

    implement boolean state hook with window event option

---
+++ src/hooks/useBooleanStateWithWindowEvent.ts
@@ -0,0 +1,34 @@
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+interface Options {
+	/** @default: click */
+	windowEventName?: string;
+	/** @description: return next boolean state*/
+	onWindowEvent?: (prevBool?: boolean) => boolean;
+}
+
+export function useBooleanStateWithWindowEvent(
+	defaultBool: boolean,
+	{ windowEventName = 'click', onWindowEvent }: Options = {}
+) {
+	const listenerRef = useRef<Options['onWindowEvent'] | null>(onWindowEvent ?? null);
+
+	const [bool, setBool] = useState<boolean>(defaultBool);
+
+	const windowListener = useCallback(() => {
+		setBool(prevBool => listenerRef.current?.(prevBool) ?? !prevBool);
+	}, []);
+
+	useEffect(() => {
+		if (bool) {
+			window.addEventListener(windowEventName, windowListener);
+		} else {
+			window.removeEventListener(windowEventName, windowListener);
+		}
+		return () => {
+			window.removeEventListener(windowEventName, windowListener);
+		};
+	}, [bool, windowEventName, windowListener]);
+
+	return [bool, setBool] as const;
+}
