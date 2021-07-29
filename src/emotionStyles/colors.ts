79f65129193b4f73dfa4b98506218b3ee6360a40
sko-kr <vxxcko@gmail.com>
2021-07-29 12:41:12 +0900

    define primary variant colors

--- src/emotionStyles/colors.ts
+++ src/emotionStyles/colors.ts
@@ -1,4 +1,6 @@
-export const colorPrimary = `rgb(23, 15, 52)`;
+export const colorPrimaryDark = `rgb(23, 15, 52)`;
+export const colorPrimary = `rgb(45, 39, 85)`;
+export const colorPrimaryLight = `rgb(60, 53, 109)`;
 
 export const colorWhiteHigh = `rgba(255, 255, 255, 0.95)`;
 export const colorWhiteEmphasis = `rgba(255, 255, 255, 0.87)`;
