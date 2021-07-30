57bc33577579c4a87afdabd7cd613c91dcd90c4c
sko-kr <vxxcko@gmail.com>
2021-07-30 16:01:11 +0900

    add delegated osmo amount

--- src/pages/assets/AssetsOverview.tsx
+++ src/pages/assets/AssetsOverview.tsx
@@ -3,7 +3,7 @@ import { OverviewLabelValue } from '../../components/common/OverviewLabelValue';
 import { observer } from 'mobx-react-lite';
 import { useStore } from '../../stores';
 import { PricePretty } from '@keplr-wallet/unit/build/price-pretty';
-import { Dec } from '@keplr-wallet/unit';
+import { CoinPretty, Dec } from '@keplr-wallet/unit';
 
 export const AssetsOverview: FunctionComponent<{ title: string }> = observer(({ title }) => {
 	const { chainStore, accountStore, queriesStore, priceStore } = useStore();
@@ -11,50 +11,18 @@ export const AssetsOverview: FunctionComponent<{ title: string }> = observer(({ 
 	const account = accountStore.getAccount(chainStore.current.chainId);
 	const queries = queriesStore.get(chainStore.current.chainId);
 
-	const queryBalances = queries.queryBalances.getQueryBech32Address(account.bech32Address);
+	const availableBalanceList = queries.queryBalances
+		.getQueryBech32Address(account.bech32Address)
+		.balances.map(bal => bal.balance);
+	const availableBalancePrice = calcTotalFiatValue(availableBalanceList);
 
-	let availableBalancePrice = new PricePretty(priceStore.getFiatCurrency('usd')!, new Dec(0));
-	for (const bal of queryBalances.balances) {
-		if (bal.balance.currency.coinMinimalDenom.startsWith('gamm/pool/')) {
-			const poolId = bal.balance.currency.coinMinimalDenom.replace('gamm/pool/', '');
-			const pool = queries.osmosis.queryGammPools.getPool(poolId);
-			if (pool) {
-				const tvl = pool.computeTotalValueLocked(priceStore, priceStore.getFiatCurrency('usd')!);
-				const totalShare = pool.totalShare;
-				if (tvl.toDec().gt(new Dec(0)) && totalShare.toDec().gt(new Dec(0))) {
-					const value = tvl.mul(bal.balance.quo(totalShare));
-					availableBalancePrice = availableBalancePrice.add(value);
-				}
-			}
-		} else {
-			const price = priceStore.calculatePrice('usd', bal.balance);
-			if (price) {
-				availableBalancePrice = availableBalancePrice.add(price);
-			}
-		}
-	}
-
-	let lockedBalancePrice = new PricePretty(priceStore.getFiatCurrency('usd')!, new Dec(0));
 	const lockedCoins = queries.osmosis.queryLockedCoins.get(account.bech32Address).lockedCoins;
-	for (const locked of lockedCoins) {
-		if (locked.currency.coinMinimalDenom.startsWith('gamm/pool/')) {
-			const poolId = locked.currency.coinMinimalDenom.replace('gamm/pool/', '');
-			const pool = queries.osmosis.queryGammPools.getPool(poolId);
-			if (pool) {
-				const tvl = pool.computeTotalValueLocked(priceStore, priceStore.getFiatCurrency('usd')!);
-				const totalShare = pool.totalShare;
-				if (tvl.toDec().gt(new Dec(0)) && totalShare.toDec().gt(new Dec(0))) {
-					const value = tvl.mul(locked.quo(totalShare));
-					lockedBalancePrice = lockedBalancePrice.add(value);
-				}
-			}
-		} else {
-			const price = priceStore.calculatePrice('usd', locked);
-			if (price) {
-				lockedBalancePrice = lockedBalancePrice.add(price);
-			}
-		}
-	}
+	const lockedBalancePrice = calcTotalFiatValue(lockedCoins);
+
+	const delegatedBalance = queries.cosmos.queryDelegations
+		.getQueryBech32Address(account.bech32Address)
+		.delegationBalances.map(delegation => delegation.balance);
+	const delegatedBalancePrice = calcTotalFiatValue(delegatedBalance);
 
 	return (
 		<section className="w-full">
@@ -63,7 +31,12 @@ export const AssetsOverview: FunctionComponent<{ title: string }> = observer(({ 
 			</div>
 			<div className="flex items-center gap-21.5">
 				<OverviewLabelValue label="Total Assets">
-					<h4 className="inline">{availableBalancePrice.add(lockedBalancePrice).toString()}</h4>
+					<h4 className="inline">
+						{availableBalancePrice
+							.add(lockedBalancePrice)
+							.add(delegatedBalancePrice)
+							.toString()}
+					</h4>
 				</OverviewLabelValue>
 				<OverviewLabelValue label="Available Assets">
 					<h4 className="inline">{availableBalancePrice.toString()}</h4>
@@ -71,7 +44,34 @@ export const AssetsOverview: FunctionComponent<{ title: string }> = observer(({ 
 				<OverviewLabelValue label="Bonded Assets">
 					<h4 className="inline">{lockedBalancePrice.toString()}</h4>
 				</OverviewLabelValue>
+				<OverviewLabelValue label="Delegated OSMO">
+					<h4 className="inline">{delegatedBalancePrice.toString()}</h4>
+				</OverviewLabelValue>
 			</div>
 		</section>
 	);
+
+	function calcTotalFiatValue(balanceList: CoinPretty[]) {
+		let fiatValue = new PricePretty(priceStore.getFiatCurrency('usd')!, new Dec(0));
+		for (const balance of balanceList) {
+			if (balance.currency.coinMinimalDenom.startsWith('gamm/pool/')) {
+				const poolId = balance.currency.coinMinimalDenom.replace('gamm/pool/', '');
+				const pool = queries.osmosis.queryGammPools.getPool(poolId);
+				if (pool) {
+					const tvl = pool.computeTotalValueLocked(priceStore, priceStore.getFiatCurrency('usd')!);
+					const totalShare = pool.totalShare;
+					if (tvl.toDec().gt(new Dec(0)) && totalShare.toDec().gt(new Dec(0))) {
+						const value = tvl.mul(balance.quo(totalShare));
+						fiatValue = fiatValue.add(value);
+					}
+				}
+			} else {
+				const price = priceStore.calculatePrice('usd', balance);
+				if (price) {
+					fiatValue = fiatValue.add(price);
+				}
+			}
+		}
+		return fiatValue;
+	}
 });
