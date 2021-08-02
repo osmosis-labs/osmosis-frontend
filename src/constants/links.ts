4a1f1dc0f9b7d160a84bdeafa2f1d77e4f71bcc0
Thunnini <yunjh1994@gmail.com>
2021-08-02 14:27:52 +0900

    Add more links to the bottom of sidebar

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,6 @@
 export const LINKS = {
 	TWITTER: 'https://twitter.com/osmosiszone',
 	MEDIUM: 'https://medium.com/@Osmosis',
+	DISCORD: 'https://discord.gg/osmosis',
+	TELEGRAM: 'https://t.me/osmosis_chat',
 };
