f78b4f1fb4014d9870a8bd55f415e1f26ba2da8f
sko-kr <vxxcko@gmail.com>
2021-08-04 15:52:11 +0900

    commonly used containers and sections.

---
+++ src/components/layouts/Containers.tsx
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const FullWidthContainer = styled.div`
+	width: 100%;
+	max-width: 1920px;
+`;
+
+export const CenterSection = styled.section`
+	display: flex;
+	justify-content: center;
+	align-items: flex-start;
+	padding: 40px;
+`;
