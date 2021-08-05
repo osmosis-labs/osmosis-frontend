25cafc618cb3396e0d43561e61a8f5e8edb4faba
sko-kr <vxxcko@gmail.com>
2021-08-05 15:05:19 +0900

    commonly used texts

---
+++ src/components/Texts.tsx
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+
+export const TitleText = styled.h5`
+	margin-bottom: 30px;
+`;
+
+export const SubTitleText = styled.h6`
+	margin-bottom: 20px;
+	font-weight: 400;
+`;
+
+export const MediumTextWhite = styled.p`
+	color: rgba(255, 255, 255, 0.6);
+	font-weight: 500;
+`;
