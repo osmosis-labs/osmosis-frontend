8659cb030c12a635af3a362cb62408ef862e18ee
sko-kr <vxxcko@gmail.com>
2021-08-05 17:00:05 +0900

    texts and color

--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -1,4 +1,6 @@
 import styled from '@emotion/styled';
+import { colorSecondary } from 'src/emotionStyles/colors';
+import { onLgWidth } from 'src/emotionStyles/mediaQueries';
 
 export const TitleText = styled.h5`
 	margin-bottom: 30px;
@@ -12,4 +14,18 @@ export const SubTitleText = styled.h6`
 export const MediumTextWhite = styled.p`
 	color: rgba(255, 255, 255, 0.6);
 	font-weight: 500;
+`;
+
+export const H4 = styled.h4`
+	font-weight: 400;
+	font-size: 24px;
+	${onLgWidth} {
+		font-size: 36px;
+	}
+	margin-bottom: 16px;
+`;
+
+export const H6Secondary = styled.h6`
+	font-weight: 400;
+	color: ${colorSecondary};
 `;
--- src/emotionStyles/colors.ts
+++ src/emotionStyles/colors.ts
@@ -1,6 +1,8 @@
 export const colorPrimaryDark = `rgb(23, 15, 52)`;
-export const colorPrimary = `rgb(45, 39, 85)`;
+export const colorPrimary = `#2d2755`;
 export const colorPrimaryLight = `rgb(60, 53, 109)`;
+
+export const colorSecondary = `#c4a46a`;
 
 export const colorWhiteHigh = `rgba(255, 255, 255, 0.95)`;
 export const colorWhiteEmphasis = `rgba(255, 255, 255, 0.87)`;
