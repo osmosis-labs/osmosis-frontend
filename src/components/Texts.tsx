410d96c154e0901ab36db95c7d0990e9a5be1174
sko-kr <vxxcko@gmail.com>
2021-08-09 11:12:01 +0900

    common text

--- src/components/Texts.tsx
+++ src/components/Texts.tsx
@@ -2,8 +2,70 @@ import styled from '@emotion/styled';
 import { colorSecondary } from 'src/emotionStyles/colors';
 import { onLgWidth } from 'src/emotionStyles/mediaQueries';
 
-export const TitleText = styled.h5`
+const FontSizeBySize = {
+	'2xl': 48,
+	xl: 36,
+	lg: 24,
+	md: 16,
+	sm: 12,
+	xs: 10,
+};
+
+const OpacityByEmphasis = {
+	high: 0.95,
+	medium: 0.87,
+	regular: 0.6,
+	low: 0.38,
+	/**flat/dead/disabled?: 0.12*/
+};
+
+const FontWeightByBoldness = {
+	bold: 700,
+	semiBold: 600,
+	medium: 500,
+	regular: 400,
+	light: 300,
+} as const;
+
+const RgbByColor = {
+	white: `255, 255, 255`,
+	/**TODO: add colors to be used in Texts */
+};
+
+interface OsmosisTextProps {
+	size?: keyof typeof FontSizeBySize | number | string;
+	weight?: keyof typeof FontWeightByBoldness;
+	emphasis?: keyof typeof OpacityByEmphasis;
+	color?: keyof typeof RgbByColor;
+	type?: 'Inter' | 'Poppins';
+}
+
+export const Text = styled.p<OsmosisTextProps>`
+	${mapTextPropsToCssProps};
+`;
+
+function mapTextPropsToCssProps({
+	size = 'md',
+	weight = 'regular',
+	emphasis = 'regular',
+	color = 'white',
+	type = 'Inter',
+}: OsmosisTextProps) {
+	return {
+		fontFamily: `${type}, ui-sans-serif, system-ui`,
+		fontSize: FontSizeBySize[size as keyof typeof FontSizeBySize] ?? size,
+		fontWeight: FontWeightByBoldness[weight],
+		color: `rgba(${RgbByColor[color]}, ${OpacityByEmphasis[emphasis]})`,
+	};
+}
+
+export const SectionTitleText = styled.h5`
+	line-height: 1;
 	margin-bottom: 30px;
+	font-size: 24px;
+	font-weight: 600;
+	font-family: Poppins, ui-sans-serif, system-ui;
+	color: rgba(255, 255, 255, 0.95);
 `;
 
 export const SubTitleText = styled.h6`
@@ -11,6 +73,7 @@ export const SubTitleText = styled.h6`
 	font-weight: 400;
 `;
 
+/** Paragraph */
 export const MediumTextWhite = styled.p`
 	color: rgba(255, 255, 255, 0.6);
 	font-weight: 500;
@@ -19,9 +82,11 @@ export const MediumTextWhite = styled.p`
 export const H4 = styled.h4`
 	font-weight: 400;
 	font-size: 24px;
+
 	${onLgWidth} {
 		font-size: 36px;
 	}
+
 	margin-bottom: 16px;
 `;
 
