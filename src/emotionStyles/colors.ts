43e707a25aafca94dca7a60ffd77f025d7e00864
sko-kr <vxxcko@gmail.com>
2021-08-10 12:20:17 +0900

    refactor: hex color

--- src/emotionStyles/colors.ts
+++ src/emotionStyles/colors.ts
@@ -1,13 +1,18 @@
-export const colorPrimaryDarker = `rgb(23, 15, 52)`;
-export const colorPrimaryDark = `#231d4b`;
+/** 60, 53, 109 */
+export const colorPrimaryLight = `#3c356d`;
 /** 45, 39, 85 */
 export const colorPrimary = `#2d2755`;
-export const colorPrimaryLight = `rgb(60, 53, 109)`;
+/** 35, 29, 75*/
+export const colorPrimaryDark = `#231d4b`;
+/** 23, 15, 52 */
+export const colorPrimaryDarker = `#170f34`;
+
+/** 50, 45, 194 */
 export const colorPrimary200 = `#322dc2`;
 
-export const colorSecondary = `#c4a46a`;
+/** 196, 164, 106 */
+export const colorGold = `#c4a46a`;
 
 export const colorWhiteHigh = `rgba(255, 255, 255, 0.95)`;
 export const colorWhiteEmphasis = `rgba(255, 255, 255, 0.87)`;
-
-export const colorGrey = `rgba(255, 255, 255, 0.6)`;
+export const colorWhiteRegular = `rgba(255, 255, 255, 0.6)`;
