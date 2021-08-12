5733ad56e834fabe89e4e46b07298618a83bb7f5
sko-kr <vxxcko@gmail.com>
2021-08-12 19:06:27 +0900

    misc: pass props to Spinner

--- src/components/Spinners.tsx
+++ src/components/Spinners.tsx
@@ -1,13 +1,13 @@
 import styled from '@emotion/styled';
-import React from 'react';
+import React, { SVGAttributes } from 'react';
 
-interface Props {
+interface Props extends SVGAttributes<SVGSVGElement> {
 	size?: number;
 }
 
-export function Spinner({ size = 24 }: Props) {
+export function Spinner({ size = 24, ...props }: Props) {
 	return (
-		<SpinnerSvg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${size} ${size}`}>
+		<SpinnerSvg xmlns="http://www.w3.org/2000/svg" viewBox={`0 0 ${size} ${size}`} {...props}>
 			<circle cx="12" cy="12" r="10" />
 			<path d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z" />
 		</SpinnerSvg>
