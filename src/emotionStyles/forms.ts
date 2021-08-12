390ed517af40c2eacb7337207a08456e1a68dc51
sko-kr <vxxcko@gmail.com>
2021-08-12 19:04:29 +0900

    raise button shadow css

--- src/emotionStyles/forms.ts
+++ src/emotionStyles/forms.ts
@@ -19,3 +19,8 @@ export const cssNumberTextInput = css`
 		-moz-appearance: textfield;
 	}
 `;
+
+export const cssRaiseButtonShadow = css`
+	box-shadow: rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0) 0 0 0 0, rgba(0, 0, 0, 0.14) 0 4px 5px 0,
+		rgba(0, 0, 0, 0.12) 0 1px 10px 0, rgba(0, 0, 0, 0.2) 0 2px 4px 0;
+`;
