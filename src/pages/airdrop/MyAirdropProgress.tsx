9fe99b279f0adadc02933bb549e81fdae9ab2e43
sko-kr <vxxcko@gmail.com>
2021-08-31 12:08:11 +0900

    refactor airdrop page

--- src/pages/airdrop/MyAirdropProgress.tsx
+++ src/pages/airdrop/MyAirdropProgress.tsx
@@ -1,9 +1,12 @@
-import React, { FunctionComponent } from 'react';
-import { useStore } from '../../stores';
+import styled from '@emotion/styled';
 import { Dec, IntPretty } from '@keplr-wallet/unit';
 import { observer } from 'mobx-react-lite';
+import React from 'react';
+import { TitleText } from 'src/components/Texts';
+import { colorPrimaryDarker } from 'src/emotionStyles/colors';
+import { useStore } from 'src/stores';
 
-export const MyAirdropProgress: FunctionComponent = observer(() => {
+export const MyAirdropProgress = observer(function MyAirdropProgress() {
 	const { chainStore, queriesStore, accountStore } = useStore();
 
 	const queries = queriesStore.get(chainStore.current.chainId);
@@ -26,28 +29,46 @@ export const MyAirdropProgress: FunctionComponent = observer(() => {
 	})();
 
 	// Actually 1/5 of airdrop is distributes from the genesis.
-	// If there is no intial claimable, assume that the user is not deserved to get the airdrop.
+	// If there is no initial claimable, assume that the user is not deserved to get the airdrop.
 	// Else, add the 20% to the percentage
 	let percent = totalClaimable.toDec().equals(new Dec(0)) ? new IntPretty(new Dec(0)) : new IntPretty(new Dec(20));
 	percent = percent.add(new Dec(20 * countOfCompleted));
 
 	return (
-		<React.Fragment>
-			<h5 className="mb-2 font-normal">My Progress</h5>
-			<div className="w-full mb-4.5">
-				<div className="w-full flex justify-end items-end">
-					<h4>{percent.maxDecimals(0).toString()}%</h4>
-				</div>
-				<div className="w-full relative h-3 rounded-2xl bg-background mt-2.5">
-					<div
-						style={{
-							width: `${percent.maxDecimals(0).toString()}%`,
-							background: 'linear-gradient(270deg, #89EAFB 0%, #1377B0 100%)',
-						}}
-						className="absolute rounded-2xl h-3 left-0 top-0"
-					/>
-				</div>
-			</div>
-		</React.Fragment>
+		<>
+			<TitleText pb={4}>My Progress</TitleText>
+			<ProgressBarSection>
+				<TitleText size="2xl" pb={0} style={{ textAlign: 'right' }}>
+					{percent.maxDecimals(0).toString()}%
+				</TitleText>
+				<ProgressBar>
+					<ProgressBarValue percent={percent.maxDecimals(0).toString()} />
+				</ProgressBar>
+			</ProgressBarSection>
+		</>
 	);
 });
+
+const ProgressBarSection = styled.div`
+	width: 100%;
+	margin-bottom: 18px;
+`;
+
+const ProgressBar = styled.div`
+	width: 100%;
+	position: relative;
+	height: 0.75rem;
+	border-radius: 1rem;
+	background-color: ${colorPrimaryDarker};
+	margin-top: 10px;
+`;
+
+const ProgressBarValue = styled.div<{ percent: string | number }>`
+	width: ${({ percent }) => percent}%;
+	position: absolute;
+	border-radius: 1rem;
+	height: 0.75rem;
+	left: 0;
+	top: 0;
+	background: linear-gradient(270deg, #89eafb 0%, #1377b0 100%);
+`;
