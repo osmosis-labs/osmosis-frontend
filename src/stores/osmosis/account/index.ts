573d9b82c0a44b6049f4811d55b1284a2b377c94
Thunnini <yunjh1994@gmail.com>
2021-09-16 00:38:51 +0900

    Increase the gas limit for add/remove liquidity

--- src/stores/osmosis/account/index.ts
+++ src/stores/osmosis/account/index.ts
@@ -50,12 +50,12 @@ export class AccountWithCosmosAndOsmosis extends AccountSetBase<
 		},
 		joinPool: {
 			type: 'osmosis/gamm/join-pool',
-			gas: 750000,
+			gas: 950000,
 			shareCoinDecimals: 18,
 		},
 		exitPool: {
 			type: 'osmosis/gamm/exit-pool',
-			gas: 750000,
+			gas: 950000,
 			shareCoinDecimals: 18,
 		},
 		swapExactAmountIn: {
