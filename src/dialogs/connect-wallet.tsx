e3e00f38ee880df6c375ce5c0b5887562e6dd4b8
delivan <delivan@chainapsis.com>
2021-10-14 18:07:23 +0900

    Improve wallet-connect function

--- src/dialogs/connect-wallet.tsx
+++ src/dialogs/connect-wallet.tsx
@@ -3,7 +3,11 @@ import ReactDOM from 'react-dom';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import QRCode from 'qrcode.react';
-import { isMobile, isAndroid, saveMobileLinkInfo } from '@walletconnect/browser-utils';
+import {
+	isMobile as checkIsMobile,
+	isAndroid as checkIsAndroid,
+	saveMobileLinkInfo,
+} from '@walletconnect/browser-utils';
 
 import { observer } from 'mobx-react-lite';
 import { makeObservable, observable, flow, action } from 'mobx';
@@ -48,55 +52,16 @@ async function sendTx(chainId: string, stdTx: StdTx, mode: BroadcastMode): Promi
 	return Buffer.from(result.data.txhash, 'hex');
 }
 
-declare global {
-	interface Window {
-		opera: any;
-	}
-}
-
-export type ModalUIOptions = {
-	backdrop?: {
-		className?: string;
-		style?: CSSProperties;
-		disableDefaultStyle?: boolean;
-	};
-	modalContainer?: {
-		className?: string;
-		style?: CSSProperties;
-		disableDefaultStyle?: boolean;
-	};
-	modalHeader?: {
-		className?: string;
-		style?: CSSProperties;
-		disableDefaultStyle?: boolean;
-	};
-	qrCodeContainer?: {
-		className?: string;
-		style?: CSSProperties;
-	};
-	qrCodeSize?: number;
-	appButtonContainer?: {
-		className?: string;
-		style?: CSSProperties;
-	};
-	appButton?: {
-		className?: string;
-		style?: CSSProperties;
-	};
-};
-
-export const Modal: FunctionComponent<{
-	uiOptions?: ModalUIOptions;
-
+export const WalletConnectQRCodeModal: FunctionComponent<{
 	uri: string;
 	close: () => void;
-}> = ({ uiOptions, uri, close }) => {
-	const [checkMobile] = useState(() => isMobile());
-	const [checkAndroid] = useState(() => isAndroid());
+}> = ({ uri, close }) => {
+	const [isMobile] = useState(() => checkIsMobile());
+	const [isAndroid] = useState(() => checkIsAndroid());
 
 	const navigateToAppURL = useMemo(() => {
-		if (checkMobile) {
-			if (checkAndroid) {
+		if (isMobile) {
+			if (isAndroid) {
 				// Save the mobile link.
 				saveMobileLinkInfo({
 					name: 'Keplr',
@@ -114,116 +79,55 @@ export const Modal: FunctionComponent<{
 				return `keplrwallet://wcV1?${uri}`;
 			}
 		}
-	}, [checkAndroid, checkMobile, uri]);
+	}, [isMobile, isAndroid, uri]);
 
 	useEffect(() => {
-		// Try opening the app without interaction.
 		if (navigateToAppURL) {
 			window.location.href = navigateToAppURL;
 		}
 	}, [navigateToAppURL]);
 
+	if (isMobile) {
+		return null;
+	}
+
 	return (
-		<React.Fragment>
-			<div
-				className={uiOptions?.backdrop?.className}
-				style={{
-					...(uiOptions?.backdrop?.disableDefaultStyle
-						? {}
-						: {
-								position: 'fixed',
-								top: 0,
-								bottom: 0,
-								left: 0,
-								right: 0,
-								backgroundColor: 'rgba(0,0,0,0.1)',
-
-								display: 'flex',
-								justifyContent: 'center',
-								alignItems: 'center',
-						  }),
-					...uiOptions?.backdrop?.style,
-				}}
-				onClick={e => {
-					e.preventDefault();
-					e.stopPropagation();
-
-					close();
-				}}>
+		<div className="fixed inset-0 z-100 overflow-y-auto">
+			<div className="p-5 flex items-center justify-center min-h-screen">
 				<div
-					className={uiOptions?.modalContainer?.className}
-					style={{
-						...(uiOptions?.modalContainer?.disableDefaultStyle
-							? {}
-							: {
-									padding: 20,
-									borderRadius: 10,
-									backgroundColor: '#DDDDDD',
-							  }),
-						...uiOptions?.modalContainer?.style,
+					className="fixed inset-0 bg-black opacity-20 flex justify-center items-center"
+					onClick={e => {
+						e.preventDefault();
+						e.stopPropagation();
+
+						close();
 					}}
+				/>
+
+				<div
+					className="relative md:max-w-modal px-4 py-5 md:p-8 bg-surface shadow-elevation-24dp rounded-2xl z-10"
 					onClick={e => {
 						e.preventDefault();
 						e.stopPropagation();
 					}}>
-					{!checkMobile ? (
-						<React.Fragment>
-							<h3
-								className={uiOptions?.modalHeader?.className}
-								style={{
-									...(uiOptions?.modalHeader?.disableDefaultStyle
-										? {}
-										: {
-												fontSize: 20,
-												margin: 0,
-												marginBottom: 10,
-										  }),
-									...uiOptions?.modalHeader?.style,
-								}}>
-								Scan QR Code
-							</h3>
-							<div className={uiOptions?.qrCodeContainer?.className} style={uiOptions?.qrCodeContainer?.style}>
-								<QRCode size={uiOptions?.qrCodeSize || 500} value={uri} />
-							</div>
-						</React.Fragment>
-					) : (
-						<React.Fragment>
-							<h3
-								className={uiOptions?.modalHeader?.className}
-								style={{
-									...(uiOptions?.modalHeader?.disableDefaultStyle
-										? {}
-										: {
-												fontSize: 20,
-												margin: 0,
-												marginBottom: 10,
-										  }),
-									...uiOptions?.modalHeader?.style,
-								}}>
-								Open App
-							</h3>
-							<div className={uiOptions?.appButtonContainer?.className} style={uiOptions?.appButtonContainer?.style}>
-								<button
-									className={uiOptions?.appButton?.className}
-									style={uiOptions?.appButton?.style}
-									onClick={() => {
-										if (navigateToAppURL) {
-											window.location.href = navigateToAppURL;
-										}
-									}}>
-									Open App
-								</button>
-							</div>
-						</React.Fragment>
-					)}
+					<h4 className="text-lg md:text-xl mb-3 md:mb-5 text-white-high">Scan QR Code</h4>
+					<div className="p-3.5 bg-background">
+						<QRCode size={500} value={uri} />
+					</div>
+
+					<img
+						onClick={() => close()}
+						className="absolute cursor-pointer top-3 md:top-5 right-3 md:right-5 w-8 md:w-10"
+						src="/public/assets/Icons/Close.svg"
+					/>
 				</div>
 			</div>
-		</React.Fragment>
+		</div>
 	);
 };
 
-class WalletConnectQRCodeModalV1 {
-	constructor(protected readonly uiOptions?: ModalUIOptions) {}
+class WalletConnectQRCodeModalV1Renderer {
+	constructor() {}
 
 	open(uri: string, cb: any) {
 		const wrapper = document.createElement('div');
@@ -231,13 +135,12 @@ class WalletConnectQRCodeModalV1 {
 		document.body.appendChild(wrapper);
 
 		ReactDOM.render(
-			<Modal
+			<WalletConnectQRCodeModal
 				uri={uri}
 				close={() => {
 					this.close();
 					cb();
 				}}
-				uiOptions={this.uiOptions}
 			/>,
 			wrapper
 		);
@@ -251,52 +154,67 @@ class WalletConnectQRCodeModalV1 {
 	}
 }
 
-type WalletType = 'true' | 'extension' | 'wallet-connect';
+type WalletType = 'true' | 'extension' | 'wallet-connect' | null;
 const KeyConnectingWalletType = 'connecting_wallet_type';
 const KeyAutoConnectingWalletType = 'account_auto_connect';
 
 export class ConnectWalletManager {
-	constructor() {}
+	protected walletConnector: WalletConnect;
 
-	getKeplr(): Promise<Keplr | undefined> {
+	@observable
+	autoConnectingWalletType: WalletType;
+
+	constructor() {
+		this.walletConnector = new WalletConnect({
+			bridge: 'https://bridge.walletconnect.org',
+			signingMethods: [
+				'keplr_enable_wallet_connect_v1',
+				'keplr_get_key_wallet_connect_v1',
+				'keplr_sign_amino_wallet_connect_v1',
+			],
+			qrcodeModal: new WalletConnectQRCodeModalV1Renderer(),
+		});
+		this.autoConnectingWalletType = localStorage?.getItem(KeyAutoConnectingWalletType) as WalletType;
+		makeObservable(this);
+	}
+
+	getKeplr = (): Promise<Keplr | undefined> => {
 		const connectingWalletType =
 			localStorage?.getItem(KeyAutoConnectingWalletType) || localStorage?.getItem(KeyConnectingWalletType);
 		if (connectingWalletType === 'wallet-connect') {
-			const connector = new WalletConnect({
-				bridge: 'https://bridge.walletconnect.org',
-				signingMethods: [
-					'keplr_enable_wallet_connect_v1',
-					'keplr_get_key_wallet_connect_v1',
-					'keplr_sign_amino_wallet_connect_v1',
-				],
-				qrcodeModal: new WalletConnectQRCodeModalV1(),
-			});
-
-			// Check if connection is already established
-			if (!connector.connected) {
-				// create new session
-				connector.createSession();
+			if (!this.walletConnector.connected) {
+				this.walletConnector.createSession();
 
 				return new Promise<Keplr>((resolve, reject) => {
-					connector.on('connect', error => {
+					this.walletConnector.on('connect', error => {
 						if (error) {
 							reject(error);
 						} else {
 							localStorage?.removeItem(KeyConnectingWalletType);
 							localStorage?.setItem(KeyAutoConnectingWalletType, 'wallet-connect');
+							this.autoConnectingWalletType = 'wallet-connect';
+
 							resolve(
-								new KeplrWalletConnectV1(connector, {
+								new KeplrWalletConnectV1(this.walletConnector, {
 									sendTx,
 								})
 							);
 						}
 					});
+					this.walletConnector.on('disconnect', error => {
+						if (error) {
+							reject(error);
+						}
+						this.disableAutoConnect();
+					});
 				});
 			} else {
 				localStorage?.removeItem(KeyConnectingWalletType);
 				localStorage?.setItem(KeyAutoConnectingWalletType, 'wallet-connect');
+				this.autoConnectingWalletType = 'wallet-connect';
+
 				return Promise.resolve(
-					new KeplrWalletConnectV1(connector, {
+					new KeplrWalletConnectV1(this.walletConnector, {
 						sendTx,
 					})
 				);
@@ -304,33 +222,54 @@ export class ConnectWalletManager {
 		} else {
 			localStorage?.removeItem(KeyConnectingWalletType);
 			localStorage?.setItem(KeyAutoConnectingWalletType, 'extension');
+			this.autoConnectingWalletType = 'extension';
+
 			return getKeplrFromWindow();
 		}
-	}
+	};
+
+	disconnectWalletConnect = () => {
+		this.walletConnector.killSession();
+	};
+
+	@action
+	disableAutoConnect = () => {
+		localStorage?.removeItem(KeyAutoConnectingWalletType);
+		this.autoConnectingWalletType = null;
+	};
 }
 
 export const ConnectWalletDialog = wrapBaseDialog(
-	observer(({ initialFocus }: { initialFocus: React.RefObject<HTMLDivElement> }) => {
+	observer(({ initialFocus, close }: { initialFocus: React.RefObject<HTMLDivElement>; close: () => void }) => {
 		const { connectAccount } = useAccountConnection();
+		const [isMobile] = useState(() => checkIsMobile());
 
 		return (
 			<div ref={initialFocus}>
 				<h4 className="text-lg md:text-xl text-white-high">Connect Wallet</h4>
-				{walletList.map(wallet => (
-					<button
-						key={wallet.name}
-						className="w-full text-left p-3 md:p-5 rounded-2xl bg-background flex items-center mt-4 md:mt-5"
-						onClick={() => {
-							localStorage.setItem(KeyConnectingWalletType, wallet.type);
-							connectAccount();
-						}}>
-						<img src={wallet.logoUrl} className="w-12 mr-3 md:w-16 md:mr-5" />
-						<div>
-							<h5 className="text-base md:text-lg mb-1 text-white-high">{wallet.name}</h5>
-							<p className="text-xs md:text-sm text-iconDefault">{wallet.description}</p>
-						</div>
-					</button>
-				))}
+				{walletList
+					.filter(wallet => {
+						if (isMobile && wallet.type == 'extension') {
+							return false;
+						}
+						return true;
+					})
+					.map(wallet => (
+						<button
+							key={wallet.name}
+							className="w-full text-left p-3 md:p-5 rounded-2xl bg-background flex items-center mt-4 md:mt-5"
+							onClick={() => {
+								localStorage.setItem(KeyConnectingWalletType, wallet.type);
+								connectAccount();
+								close();
+							}}>
+							<img src={wallet.logoUrl} className="w-12 mr-3 md:w-16 md:mr-5" />
+							<div>
+								<h5 className="text-base md:text-lg mb-1 text-white-high">{wallet.name}</h5>
+								<p className="text-xs md:text-sm text-iconDefault">{wallet.description}</p>
+							</div>
+						</button>
+					))}
 			</div>
 		);
 	})
--- src/hooks/account/useAccountConnection.ts
+++ src/hooks/account/useAccountConnection.ts
@@ -10,16 +10,17 @@ export function useAccountConnection() {
 
 	const account = accountStore.getAccount(chainStore.current.chainId);
 
-	const shouldAutoConnectAccount = localStorage?.getItem(KeyAccountAutoConnect) != null;
 	// Even though the wallet is not loaded, if `shouldAutoConnectAccount` is true, set the `isAccountConnected` as true.
 	// Because the initing the wallet is asyncronous, when users enter the site the wallet is seen as not loaded.
 	// To reduce this problem, if the wallet is connected when users enter the site, just assume that the wallet is already connected.
-	const isAccountConnected = account.walletStatus === WalletStatus.Loaded || shouldAutoConnectAccount;
+	const isAccountConnected =
+		account.walletStatus === WalletStatus.Loaded || connectWalletManager.autoConnectingWalletType;
 
-	const disconnectAccount = useCallback(() => {
-		localStorage?.removeItem(KeyAccountAutoConnect);
+	const disconnectAccount = useCallback(async () => {
 		account.disconnect();
-	}, [account]);
+		connectWalletManager.disableAutoConnect();
+		await connectWalletManager.disconnectWalletConnect();
+	}, [account, connectWalletManager]);
 
 	const connectAccount = useCallback(() => {
 		account.init();
@@ -30,10 +31,15 @@ export function useAccountConnection() {
 
 	useEffect(() => {
 		// 이전에 로그인한 후에 sign out을 명시적으로 하지 않았으면 자동으로 로그인한다.
-		if (shouldAutoConnectAccount && account.walletStatus === WalletStatus.NotInit) {
+		if (!!connectWalletManager.autoConnectingWalletType && account.walletStatus === WalletStatus.NotInit) {
 			account.init();
 		}
-	}, [account, shouldAutoConnectAccount]);
+
+		// 다른 앱에서 WalletConnect를 disconnect했으면 자동으로 로그아웃한다.
+		if (account.walletStatus === WalletStatus.Loaded && !connectWalletManager.autoConnectingWalletType) {
+			account.disconnect();
+		}
+	}, [account, connectWalletManager.autoConnectingWalletType]);
 
 	return useMemo(() => {
 		return {
