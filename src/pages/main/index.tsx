9b1be9f9d70e69c47175f9757af44115832cb833
Thunnini <yunjh1994@gmail.com>
2021-10-26 23:02:18 +0900

    Initial implementation of halloween background

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,14 +1,65 @@
 import styled from '@emotion/styled';
-import React, { FunctionComponent, ReactNode } from 'react';
-import ProgressiveImage from 'react-progressive-image';
+import React, { FunctionComponent, ReactNode, useEffect, useState } from 'react';
 import { colorPrimaryDarker } from 'src/emotionStyles/colors';
 import { TradeClipboard } from './components/TradeClipboard';
 import useWindowSize from 'src/hooks/useWindowSize';
 
+const Background: FunctionComponent = () => {
+	const sidebarWidth = 206;
+	const [ratio, setRatio] = useState((document.body.clientWidth - sidebarWidth) / document.body.clientHeight);
+
+	useEffect(() => {
+		const calcAspectRatio = () => {
+			setRatio((document.body.clientWidth - sidebarWidth) / document.body.clientHeight);
+		};
+
+		window.addEventListener('resize', calcAspectRatio);
+
+		return () => {
+			window.removeEventListener('resize', calcAspectRatio);
+		};
+	}, []);
+
+	const { isMobileView } = useWindowSize();
+
+	return (
+		<svg
+			className="absolute w-full h-full"
+			viewBox="0 0 2936 2590"
+			height="2590"
+			preserveAspectRatio={ratio > 1.1336 ? 'xMinYMid meet' : 'xMidYMid slice'}>
+			<g>
+				{/*
+					TODO: 
+					<image
+						xlinkHref={require('../../../public/assets/halloween-web.png').default}
+						x="0"
+						y="0"
+						width="3923"
+						height="2127"
+					/>
+					 */}
+				{!isMobileView ? (
+					<React.Fragment>
+						<rect x="-3000" y="2127" width="8660" height="463" fill="#120644" />
+						<image
+							xlinkHref={require('../../../public/assets/wosmongton-halloween.png').default}
+							x="0"
+							y="0"
+							width="2936"
+							height="2590"
+						/>
+					</React.Fragment>
+				) : null}
+			</g>
+		</svg>
+	);
+};
+
 export const MainPage: FunctionComponent = () => {
 	return (
 		<PageContainer>
-			<BgImageContainer />
+			<Background />
 			<TradeClipboardWrapper>
 				<TradeClipboard />
 			</TradeClipboardWrapper>
@@ -56,19 +107,10 @@ const TradePosition = styled.div`
 	position: absolute;
 	z-index: 3;
 	height: 100%;
-	--tradePositionLeft: calc(75vh * 193 / 242 + 100px);
-	left: min(1100px, var(--tradePositionLeft));
-	@media (min-aspect-ratio: 6/4) and (max-height: 880px) {
-		left: calc(100vh * 0.62);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 790px) {
-		left: calc(100vh * 0.58);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 700px) {
-		left: calc(100vh * 0.45);
-	}
-	@media (max-aspect-ratio: 6/4) {
-		left: min(calc(95vw - 206px - 540px), 1100px);
+	--tradePositionLeft: calc((100vw - 206px) * 0.8 - 520px);
+	left: min(1020px, var(--tradePositionLeft));
+	@media (max-width: 1280px) {
+		left: calc((100vw - 520px) / 2);
 	}
 	@media (max-width: 800px) {
 		position: static;
@@ -81,109 +123,5 @@ const TradeContainer = styled.div`
 
 	@media (min-width: 768px) {
 		margin-bottom: 130px;
-	}
-`;
-
-function BgImageContainer() {
-	return (
-		<ImageGroupDiv>
-			<ProgressiveImage
-				placeholder="/public/assets/backgrounds/osmosis-home-bg-low.png"
-				src="/public/assets/backgrounds/osmosis-home-bg.png">
-				{(src: string) => <ImgBgOsmoGuy src={src} />}
-			</ProgressiveImage>
-			<OsmoTableDiv />
-			<ProgressiveImage
-				placeholder="/public/assets/backgrounds/osmosis-home-fg-low.png"
-				src="/public/assets/backgrounds/osmosis-home-fg.png">
-				{(src: string) => <ImgBgScienceTools src={src} />}
-			</ProgressiveImage>
-		</ImageGroupDiv>
-	);
-}
-
-const ImgBgOsmoGuy = styled.div<{ src: string }>`
-	background-image: ${({ src }) => `url(${src})`};
-	height: 80vh;
-	margin-top: -75vh;
-	max-width: 1000px;
-	background-position: left bottom;
-	background-size: contain;
-	background-repeat: no-repeat;
-	@media (min-aspect-ratio: 6/4) and (max-height: 880px) {
-		width: calc(100vh * 0.62);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 790px) {
-		width: calc(100vh * 0.58);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 700px) {
-		width: calc(100vh * 0.45);
-	}
-	@media (max-aspect-ratio: 6/4) {
-		--widthOsmo: calc(85vw - 206px - 540px);
-		--heightOsmo: calc(var(--widthOsmo) * 242 / 193);
-		width: var(--widthOsmo);
-		height: var(--heightOsmo);
-		margin-top: calc(var(--heightOsmo) * -0.94);
-	}
-	@media (max-width: 800px) {
-		display: none;
-	}
-`;
-
-const ImgBgScienceTools = styled.div<{ src: string }>`
-	background-image: ${({ src }) => `url(${src})`};
-	position: absolute;
-	background-position: left bottom;
-	background-size: contain;
-	background-repeat: no-repeat;
-	z-index: 3;
-	--heightScienceTools: min(25vh, 500px);
-	--widthScienceTools: calc(var(--heightScienceTools) * 1351 / 857);
-	width: var(--widthScienceTools);
-	height: var(--heightScienceTools);
-	bottom: 30%;
-	left: 5%;
-	@media (min-aspect-ratio: 6/4) and (max-height: 880px) {
-		width: calc(100vh * 0.62 * 0.7);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 790px) {
-		width: calc(100vh * 0.58 * 0.7);
-	}
-	@media (min-aspect-ratio: 6/4) and (max-height: 700px) {
-		width: calc(100vh * 0.45 * 0.7);
-	}
-	@media (max-aspect-ratio: 6/4) {
-		--widthOsmo: calc(100vw - 206px - 540px);
-		--widthScienceTools: calc(var(--widthOsmo) * 0.7);
-		--heightScienceTools: calc(var(--widthScienceTools) * 857 / 1351);
-		width: var(--widthScienceTools);
-		height: var(--heightScienceTools);
-	}
-`;
-
-const OsmoTableDiv = styled.div`
-	position: absolute;
-	top: 0;
-	left: 0;
-	width: 100%;
-	height: 100%;
-	background-color: #120644;
-	z-index: 1;
-`;
-
-const ImageGroupDiv = styled.div`
-	position: absolute;
-	bottom: 0;
-	left: 0;
-	width: 100%;
-	height: min(20vh, 180px);
-	@media (max-aspect-ratio: 5/4) {
-		--widthOsmo: calc(100vw - 206px - 540px);
-		--heightOsmo: calc(var(--widthOsmo) * 242 / 193);
-		height: calc(var(--heightOsmo) * 0.2);
-	}
-	@media (max-width: 800px) {
-		display: none;
 	}
 `;
