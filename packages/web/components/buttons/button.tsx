70369afe456b42cb4a24dcf659ff8fc67ddca96a
Jon Ator <jonathanator0@gmail.com>
2022-01-19 15:49:45 -0600

    Improve button

--- packages/web/components/buttons/button.tsx
+++ packages/web/components/buttons/button.tsx
@@ -1,29 +1,47 @@
+import Image from "next/image";
 import { FunctionComponent } from "react";
 import classNames from "classnames";
 
-const colorToClassName = {
-  primary: "bg-primary-200",
-};
+interface Props {
+  color?: "primary" | "secondary";
+  size?: "xs" | "md" | "lg";
+  style?: "block" | "arrow" | "border";
+  className?: string;
+}
+
+const colorToClassName = (location: "bg" | "border") => ({
+  primary: `${location}-primary-200`,
+  secondary: `${location}-secondary-200`,
+});
 const sizeToClassName = {
-  sm: "px-4 py-3",
+  xs: "px-3 py-2",
+  md: "px-4 py-3",
+  lg: "px-5 py-4",
 };
 
-export const Button: FunctionComponent<{
-  color: "primary";
-  size: "sm";
-  block?: boolean;
-  className?: string;
-}> = ({ color, size, block, className, children }) => {
-  return (
-    <button
-      className={classNames(
-        "flex justify-center items-center rounded-lg text-white-full text-base ",
-        colorToClassName[color],
-        sizeToClassName[size],
-        className
-      )}
-    >
-      {children}
-    </button>
-  );
-};
+export const Button: FunctionComponent<Props> = ({
+  color = "primary",
+  size = "md",
+  style = "block",
+  className,
+  children,
+}) => (
+  <button
+    className={classNames(
+      "flex justify-center items-center rounded-lg text-base",
+      style === "arrow" ? "text-secondary-200" : "text-white-full",
+      style === "block"
+        ? colorToClassName("bg")[color]
+        : style === "border"
+        ? `border-solid border-2 ${colorToClassName("border")[color]}`
+        : null,
+      sizeToClassName[size],
+      className
+    )}
+  >
+    {children}
+    {style === "arrow" && (
+      <Image alt="" src="/icons/chevron-right.svg" height={32} width={32} />
+    )}
+  </button>
+);
