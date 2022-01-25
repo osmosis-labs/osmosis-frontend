19896466055976bb19dc1fdb61bd8cad418ae937
Jon Ator <jonathanator0@gmail.com>
2022-01-25 15:42:07 -0600

    Make menu-toggle keyboard a11y

--- packages/web/components/control/menu-toggle.tsx
+++ packages/web/components/control/menu-toggle.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from "react";
+import { FunctionComponent } from "react";
 import classNames from "classnames";
 import { CustomClasses } from "../types";
 
@@ -21,25 +21,34 @@ export const MenuToggle: FunctionComponent<Props> = ({
 }) => (
   <div className="flex rounded-full bg-card">
     {options.map(({ id, display }) => (
-      <div
+      <label
         key={id}
+        htmlFor={"menu-radio"}
         className={classNames(
-          "h-12 px-4 cursor-pointer",
+          "relative h-12 px-3 select-none cursor-pointer",
           {
             "bg-secondary-200 rounded-full": id === selectedOptionId,
           },
           className
         )}
-        onClick={() => onSelect(id)}
       >
-        <div
-          className={classNames("relative top-3 select-none", {
-            "text-iconDefault": id !== selectedOptionId,
-          })}
-        >
-          {display}
-        </div>
-      </div>
+        <input
+          id="menu-radio"
+          type="radio"
+          className={classNames(
+            "absolute w-full h-full appearance-none cursor-pointer z-20",
+            "after:absolute after:w-full after:h-full",
+            {
+              "text-iconDefault": id !== selectedOptionId,
+            }
+          )}
+          value={id}
+          radioGroup={options.reduce((ids, { id }) => ids + id, "")}
+          checked={id === selectedOptionId}
+          onChange={() => onSelect(id)}
+        ></input>
+        <span className="relative top-3 z-10">{display}</span>
+      </label>
     ))}
   </div>
 );
