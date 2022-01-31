6143158d49766c25948db3ee32969bf2c252a11e
Sunny Aggarwal <sunnya97@gmail.com>
2022-01-30 18:52:52 -0800

    Update src/stores/root.ts

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -687,21 +687,6 @@ export class RootStore {
 				],
 			},
 			{
-				poolId: '562',
-				currencies: [
-					{
-						coinMinimalDenom: DenomHelper.ibcDenom([{ portId: 'transfer', channelId: 'channel-72' }], 'uluna'),
-						coinDenom: 'LUNA',
-						coinDecimals: 6,
-					},
-					{
-						coinMinimalDenom: DenomHelper.ibcDenom([{ portId: 'transfer', channelId: 'channel-72' }], 'uusd'),
-						coinDenom: 'UST',
-						coinDecimals: 6,
-					},
-				],
-			},
-			{
 				poolId: '565',
 				currencies: [
 					{
