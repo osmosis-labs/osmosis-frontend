787dcff3339dd557d866d5bb85c73429ca72d4ec
Thunnini <yunjh1994@gmail.com>
2022-02-04 19:57:56 +0900

    Change the order of the assets

--- src/config.ts
+++ src/config.ts
@@ -579,6 +579,13 @@ export const IBCAssetInfos: {
 		coinMinimalDenom: 'ujuno',
 	},
 	{
+		counterpartyChainId: 'juno-1',
+		sourceChannelId: 'channel-169',
+		destChannelId: 'channel-47',
+		coinMinimalDenom: 'cw20:juno168ctmpyppk90d34p3jjy658zf5a5l3w8wk35wht6ccqj4mr0yv8s4j5awr',
+		ics20ContractAddress: 'juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn',
+	},
+	{
 		counterpartyChainId: 'stargaze-1',
 		sourceChannelId: 'channel-75',
 		destChannelId: 'channel-0',
@@ -733,13 +740,6 @@ export const IBCAssetInfos: {
 		sourceChannelId: 'channel-165',
 		destChannelId: 'channel-0',
 		coinMinimalDenom: 'usomm',
-	},
-	{
-		counterpartyChainId: 'juno-1',
-		sourceChannelId: 'channel-169',
-		destChannelId: 'channel-47',
-		coinMinimalDenom: 'cw20:juno168ctmpyppk90d34p3jjy658zf5a5l3w8wk35wht6ccqj4mr0yv8s4j5awr',
-		ics20ContractAddress: 'juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn',
 	},
 ];
 
