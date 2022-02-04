f6eb76637ffaadac2c33898e93ad4ec8fd6c0d41
Jon Ator <jonathanator0@gmail.com>
2022-02-04 11:14:01 -0600

    Add demo page

--- packages/web/pages/assets/index.tsx
+++ packages/web/pages/assets/index.tsx
@@ -1,30 +1,69 @@
 import type { NextPage } from "next";
-import { Overview } from "../../components/overview";
+import { useFilteredData } from "../../hooks/data";
 
-const Assets: NextPage = () => (
-  <main>
-    <Overview
-      title="My Osmosis Assets"
-      primaryOverviewLabels={[
-        {
-          label: "Total Assets",
-          value: "$12,530.00",
-        },
-        {
-          label: "Available Assets",
-          value: "$2,896.00",
-        },
-        {
-          label: "Bonded Assets",
-          value: "$9,544.00",
-        },
-        {
-          label: "Staked OSMO",
-          value: "$9,544.00",
-        },
-      ]}
-    />
-  </main>
-);
+type Fruit = {
+  name: string;
+  attributes: { color: string; shape: string; size: number };
+};
 
+const Assets: NextPage = () => {
+  const data: Fruit[] = [
+    {
+      name: "Orange",
+      attributes: {
+        color: "orange",
+        shape: "round",
+        size: 42,
+      },
+    },
+    {
+      name: "Pineapple",
+      attributes: {
+        color: "orange",
+        shape: "oval",
+        size: 44,
+      },
+    },
+    {
+      name: "Kiwi",
+      attributes: {
+        color: "green",
+        shape: "round",
+        size: 999,
+      },
+    },
+  ];
+
+  const [query, setQuery, filteredData] = useFilteredData(data, [
+    "name",
+    "attributes.color",
+    "attributes.shape",
+    "attributes.size",
+  ]);
+
+  return (
+    <main className="min-w-container flex flex-col place-content-evenly text-black">
+      <input
+        className="border border-black p-2"
+        type="text"
+        placeholder="Search"
+        value={query}
+        onInput={(e: any) => setQuery(e.target.value)}
+      />
+      <ol>
+        {filteredData.map((data, i) => {
+          const { color, shape, size } = data.attributes;
+          return (
+            <li className="flex flex-col" key={i}>
+              {data.name}
+              <span className="pl-10">Color: {color}</span>
+              <span className="pl-10">Shape: {shape}</span>
+              <span className="pl-10">Size: {size}</span>
+            </li>
+          );
+        })}
+      </ol>
+    </main>
+  );
+};
 export default Assets;
