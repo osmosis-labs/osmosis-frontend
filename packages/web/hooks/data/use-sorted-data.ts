05ff6a42c6aa5a05bb3c67d1959a3dd8954a499a
Jon Ator <jonathanator0@gmail.com>
2022-02-07 10:59:27 -0600

    Improve read

--- packages/web/hooks/data/use-sorted-data.ts
+++ packages/web/hooks/data/use-sorted-data.ts
@@ -3,12 +3,19 @@ import { DataProcessor } from "./types";
 import { useUserProcessedData } from "./use-user-processed-data";
 import { DataSorter } from "./data-sorter";
 
+/**
+ * Manages the use of user input to sort arbitrary data given a 'dot' key path (i.e. `"attributes.color"`).
+ *
+ * @param data Data to sort on. Reference will be copied and sorted inplace.
+ * @param sorter Optional object that can sort user data.
+ * @returns [keypath, setKeypath, results]
+ */
 export function useSortedData<TData>(
   data: TData[],
-  filter?: DataProcessor<TData[]>
+  sorter?: DataProcessor<TData[]>
 ): [string, (terms: string) => void, TData[]] {
   const processor = useMemo(
-    () => filter ?? new DataSorter<TData>(data),
+    () => sorter ?? new DataSorter<TData>(data),
     [data]
   );
 
