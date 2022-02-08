3713d7c45eda6fe76c3dd61f658f2d3032b8f4b6
Jon Ator <jonathanator0@gmail.com>
2022-02-07 20:23:38 -0600

    Try to sort numerically in `DataSorter`

--- packages/web/hooks/data/data-sorter.ts
+++ packages/web/hooks/data/data-sorter.ts
@@ -12,8 +12,21 @@ export class DataSorter<TData> implements DataProcessor<TData[]> {
   /** Key is a path of arbitrary length. Example: `"attributes.color"` or `"attributes.color.shade"` */
   process(key: string) {
     this._data.sort((a: any, b: any) => {
-      const aData = get(a, key);
-      const bData = get(b, key);
+      let aData: string | string[] | number = get(a, key);
+      let bData: string | string[] | number = get(b, key);
+
+      // try to sort numerically
+      if (typeof aData === "string" && typeof bData === "string") {
+        const aDataNumerical = parseFloat(aData);
+        const bDataNumerical = parseFloat(bData);
+
+        if (aDataNumerical !== NaN && bDataNumerical !== NaN) {
+          aData = aDataNumerical;
+          bData = bDataNumerical;
+        }
+      }
+      console.log({ aData, bData });
+
       if (aData < bData) return -1;
       if (aData > bData) return 1;
       return 0;
