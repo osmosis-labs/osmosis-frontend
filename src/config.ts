11199ea4e29de73296b00cf6d15589d7693ee56a
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-02-23 01:16:48 -0700

    Add PSTAKE coingecko Id
    Add PSTAKE coingecko Id
    {"id":"persistence-staked-xprt","symbol":"stkxprt","name":"pSTAKE Staked XPRT"}

--- src/config.ts
+++ src/config.ts
@@ -1159,7 +1159,7 @@ export const EmbedChainInfos: ChainInfoWithExplorer[] = [
 				coinDenom: 'PSTAKE',
 				coinMinimalDenom: 'ibc/8061A06D3BD4D52C4A28FFECF7150D370393AF0BA661C3776C54FF32836C3961',
 				coinDecimals: 18,
-				coinGeckoId: 'persistence',
+				coinGeckoId: 'persistence-staked-xprt',
 				coinImageUrl: window.location.origin + '/public/assets/tokens/pstake.svg',
 			},
 		],
