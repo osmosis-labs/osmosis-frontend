0c1bf78e16fd6706b792324566ea8a8985b0c408
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-03-12 18:41:01 -0800

    fix bridge link

--- src/config.ts
+++ src/config.ts
@@ -990,8 +990,8 @@ export const IBCAssetInfos: {
 		sourceChannelId: 'channel-122',
 		destChannelId: 'channel-8',
 		coinMinimalDenom: 'inj',
-		depositUrlOverride: 'https://hub.injective.network/bridge?destination=osmosis&origin=injective&token=inj',
-		withdrawUrlOverride: 'https://hub.injective.network/bridge?destination=injective&origin=osmosis&token=inj',
+		depositUrlOverride: 'https://hub.injective.network/bridge/?destination=osmosis&origin=injective&token=inj',
+		withdrawUrlOverride: 'https://hub.injective.network/bridge/?destination=injective&origin=osmosis&token=inj',
 	},
 ];
 
