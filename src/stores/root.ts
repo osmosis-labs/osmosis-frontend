fde3d148c1c20faefec8fee305bf57e29a076bea
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-03-14 11:00:27 -0600

    Add Pool 651 to Trade
    Add Pool 651 SWTH/OSMO to Trade page

--- src/stores/root.ts
+++ src/stores/root.ts
@@ -1329,6 +1329,21 @@ export class RootStore {
 					},
 				],
 			},
+			{
+				poolId: '651',
+				currencies: [
+					{
+						coinMinimalDenom: DenomHelper.ibcDenom([{ portId: 'transfer', channelId: 'channel-188' }], 'swth'),
+						coinDenom: 'SWTH',
+						coinDecimals: 8,
+					},
+					{
+						coinMinimalDenom: 'uosmo',
+						coinDenom: 'OSMO',
+						coinDecimals: 6,
+					},
+				],
+			},
 		]);
 
 		this.lpCurrencyRegistrar = new LPCurrencyRegistrar(this.chainStore);
