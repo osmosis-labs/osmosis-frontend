ad7b97934cf36d04e25573d0dc524cc3ba7b044e
Sunny Aggarwal <sunny@osmosis.team>
2022-03-26 15:06:52 -0400

    Update src/config.ts

--- src/config.ts
+++ src/config.ts
@@ -979,13 +979,6 @@ export const IBCAssetInfos: {
 		ics20ContractAddress: 'juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn',
 	},
 	{
-		counterpartyChainId: 'juno-1',
-		sourceChannelId: 'channel-169',
-		destChannelId: 'channel-47',
-		coinMinimalDenom: 'cw20:juno1g2g7ucurum66d42g8k5twk34yegdq8c82858gz0tq2fc75zy7khssgnhjl',
-		ics20ContractAddress: 'juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn',
-	},
-	{
 		counterpartyChainId: 'injective-1',
 		sourceChannelId: 'channel-122',
 		destChannelId: 'channel-8',
