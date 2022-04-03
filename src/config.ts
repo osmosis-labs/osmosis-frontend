4780df04293ec3a309b66c2fed3de7f1ef0119d4
Hades Hounds <99146370+cerberus-zone@users.noreply.github.com>
2022-04-03 00:34:03 -0500

    Corrected Gauge IDs on gamm/pool/662 (#329)
    * updated coinGeckoID
    
    * added external incentives to gamm/pool/662
    
    * added external incentives to gamm/pool/662
    
    * Revert "added external incentives to gamm/pool/662"
    
    This reverts commit 5ea606011ba12bf378ac6b30011e8d91cabc7bcb.
    
    * fixed issue: added external incentives to gamm/pool/662
    
    * corrected guage id for gamm/pool/662
    
    * updated coinGeckoId feeCurrencies from pool:ucrbrus -> cerberus-2
    
    Co-authored-by: Sunny Aggarwal <sunny@osmosis.team>
    Co-authored-by: JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>

--- src/config.ts
+++ src/config.ts
@@ -709,22 +709,22 @@ export const ExtraGaugeInPool: {
 		{
 			gaugeId: '2549',
 			denom: 'ibc/8FEFAE6AECF6E2A255585617F781F35A8D5709A545A804482A261C0C9548A9D3',
-		},
-	],
+    },
+  ],
 	'662': [
 		{
-			gaugeId: '2554',
+			gaugeId: '2591',
 			denom: 'ibc/41999DF04D9441DAC0DF5D8291DF4333FBCBA810FFD63FDCE34FDF41EF37B6F7',
 		},
 		{
-			gaugeId: '2555',
+			gaugeId: '2592',
 			denom: 'ibc/41999DF04D9441DAC0DF5D8291DF4333FBCBA810FFD63FDCE34FDF41EF37B6F7',
 		},
 		{
-			gaugeId: '2556',
+			gaugeId: '2593',
 			denom: 'ibc/41999DF04D9441DAC0DF5D8291DF4333FBCBA810FFD63FDCE34FDF41EF37B6F7',
-		},
-	],
+    },
+  ]
 };
 
 export const PoolsPerPage = 10;
@@ -2611,7 +2611,7 @@ export const EmbedChainInfos: ChainInfoWithExplorer[] = [
 				coinDenom: 'CRBRUS',
 				coinMinimalDenom: 'ucrbrus',
 				coinDecimals: 6,
-				coinGeckoId: 'pool:crbrus',
+				coinGeckoId: 'cerberus-2',
 				coinImageUrl: window.location.origin + '/public/assets/tokens/crbrus.png',
 			},
 		],
