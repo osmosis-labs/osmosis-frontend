0204d79a47458eefca792535c9bfb5f31b4571dc
Jon Ator <jonathanator0@gmail.com>
2022-04-05 14:56:57 -0500

    Add create pool observable ui config

---
+++ packages/stores/src/ui-config/create-pool.ts
@@ -0,0 +1,231 @@
+import { observable, makeObservable, override, action } from "mobx";
+import { computedFn } from "mobx-utils";
+import { TxChainSetter, IFeeConfig } from "@keplr-wallet/hooks";
+import { ObservableQueryBalances, ChainGetter } from "@keplr-wallet/stores";
+import { AppCurrency } from "@keplr-wallet/types";
+import { Dec } from "@keplr-wallet/unit";
+import { ObservableAmountConfig } from "./amount-config";
+import { CREATE_POOL_MAX_ASSETS } from ".";
+
+export class ObservableCreatePoolConfig extends TxChainSetter {
+  @observable
+  protected _sender: string;
+
+  @observable.ref
+  protected _feeConfig: IFeeConfig | undefined = undefined;
+
+  @observable.ref
+  protected _queryBalances: ObservableQueryBalances;
+
+  @observable.shallow
+  protected _assets: {
+    percentage: string;
+    amountConfig: ObservableAmountConfig;
+  }[] = [];
+
+  @observable
+  protected _swapFee: string = "0";
+
+  @observable
+  public acknowledgeFee = false;
+
+  constructor(
+    chainGetter: ChainGetter,
+    initialChainId: string,
+    sender: string,
+    queryBalances: ObservableQueryBalances
+  ) {
+    super(chainGetter, initialChainId);
+
+    this._sender = sender;
+    this._queryBalances = queryBalances;
+
+    makeObservable(this);
+  }
+
+  @override
+  setChain(chainId: string) {
+    super.setChain(chainId);
+
+    for (const asset of this.assets) {
+      asset.amountConfig.setChain(chainId);
+    }
+  }
+
+  @action
+  setFeeConfig(feeConfig: IFeeConfig) {
+    this._feeConfig = feeConfig;
+
+    for (const asset of this.assets) {
+      asset.amountConfig.setFeeConfig(feeConfig);
+    }
+  }
+
+  get feeConfig(): IFeeConfig | undefined {
+    return this._feeConfig;
+  }
+
+  get assets(): {
+    percentage: string;
+    amountConfig: ObservableAmountConfig;
+  }[] {
+    return this._assets;
+  }
+
+  get canAddAsset(): boolean {
+    return this._assets.length < CREATE_POOL_MAX_ASSETS;
+  }
+
+  @action
+  addAsset(currency: AppCurrency) {
+    const config = new ObservableAmountConfig(
+      this.chainGetter,
+      this.chainId,
+      this.sender,
+      currency,
+      this.queryBalances
+    );
+    if (this.feeConfig) {
+      config.setFeeConfig(this.feeConfig);
+    }
+
+    if (this.canAddAsset) {
+      this._assets.push({
+        percentage: "",
+        amountConfig: config,
+      });
+    }
+  }
+
+  @action
+  clearAssets() {
+    this._assets = [];
+  }
+
+  @action
+  setAssetPercentageAt(index: number, percentage: string) {
+    if (percentage.startsWith(".")) {
+      percentage = "0" + percentage;
+    }
+
+    this.assets[index] = {
+      ...this.assets[index],
+      percentage,
+    };
+  }
+
+  @action
+  removeAssetAt(index: number) {
+    this._assets.splice(index, 1);
+  }
+
+  get sender(): string {
+    return this._sender;
+  }
+
+  @action
+  setSender(sender: string) {
+    this._sender = sender;
+
+    for (const asset of this.assets) {
+      asset.amountConfig.setSender(sender);
+    }
+  }
+
+  get queryBalances(): ObservableQueryBalances {
+    return this._queryBalances;
+  }
+
+  @action
+  setQueryBalances(queryBalances: ObservableQueryBalances) {
+    this._queryBalances = queryBalances;
+
+    for (const asset of this.assets) {
+      asset.amountConfig.setQueryBalances(queryBalances);
+    }
+  }
+
+  get sendableCurrencies(): AppCurrency[] {
+    return this._queryBalances
+      .getQueryBech32Address(this._sender)
+      .positiveBalances.filter(
+        (bal) => !bal.currency.coinDenom.toLowerCase().includes("gamm")
+      )
+      .map((bal) => {
+        return bal.currency;
+      });
+  }
+
+  get swapFee(): string {
+    return this._swapFee;
+  }
+
+  @action
+  setSwapFee(swapFee: string): void {
+    this._swapFee = swapFee;
+  }
+
+  /**
+   * sendableCurrencies 중에서 현재 assets에 없는 currency들을 반환한다.
+   * Among the SendableCurrencies, return currencies that are not currently in Assets.
+   */
+  get remainingSelectableCurrencies(): AppCurrency[] {
+    return this.sendableCurrencies.filter((cur) => {
+      return (
+        this.assets.find(
+          (asset) =>
+            asset.amountConfig.currency.coinMinimalDenom ===
+            cur.coinMinimalDenom
+        ) == null
+      );
+    });
+  }
+
+  readonly getErrorOfPercentage = computedFn((): Error | undefined => {
+    if (this.assets.length < 2) {
+      return new Error("Minimum of 2 assets required");
+    }
+    if (this.assets.length > 8) {
+      return new Error("Too many assets");
+    }
+
+    let totalPercentage = new Dec(0);
+    for (const asset of this.assets) {
+      try {
+        const percentage = new Dec(asset.percentage);
+
+        if (percentage.lte(new Dec(0))) {
+          return new Error("Non-positive percentage");
+        }
+
+        totalPercentage = totalPercentage.add(percentage);
+      } catch {
+        return new Error("Invalid number");
+      }
+    }
+    if (!totalPercentage.equals(new Dec(100))) {
+      return new Error("Sum of percentages is not 100%");
+    }
+
+    try {
+      const dec = new Dec(this.swapFee);
+      if (dec.lt(new Dec(0))) {
+        return new Error("Negative swap fee");
+      }
+      if (dec.gte(new Dec(100))) {
+        return new Error("Swap fee too high");
+      }
+    } catch {
+      return new Error("Invalid swap fee");
+    }
+  });
+
+  readonly getErrorOfAmount = computedFn((): Error | undefined => {
+    for (const asset of this.assets) {
+      const error = asset.amountConfig.getError();
+      if (error != null) {
+        return error;
+      }
+    }
+  });
+}
--- packages/stores/src/ui-config/index.ts
+++ packages/stores/src/ui-config/index.ts
@@ -1,2 +1,5 @@
 export * from "./manage-liquidity";
 export * from "./amount-config";
+export * from "./create-pool";
+
+export const CREATE_POOL_MAX_ASSETS = 3;
