72595df0be2931ffe38690b527917807f379a179
Daniel Farina <contact@nevulas.com>
2022-04-07 19:07:17 -0400

    Update vercel.sh

--- vercel.sh
+++ vercel.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
+echo "VERCEL_ENV: $VERCEL_ENV"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "refactor"]] ; then
+if [[ "$VERCEL_ENV" == "refactor" ]] ; then
   # Proceed with the build
-    echo "✅ - Build can proceed"
+  echo "✅ - Build can proceed"
   exit 1;
 
 else
