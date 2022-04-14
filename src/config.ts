cdb01ffbca79090eedf52fc1ebb34ca502f2cf88
Galadrin <david.pierret@gmail.com>
2022-04-14 22:07:21 +0200

    remove MOBIX token

--- src/config.ts
+++ src/config.ts
@@ -1092,12 +1092,6 @@ export const IBCAssetInfos: {
 		destChannelId: 'channel-10',
 		coinMinimalDenom: 'afet',
 	},
-	{
-		counterpartyChainId: 'fetchhub-4',
-		sourceChannelId: 'channel-229',
-		destChannelId: 'channel-10',
-		coinMinimalDenom: 'nanomobx',
-	},
 ];
 
 export const EmbedChainInfos: ChainInfoWithExplorer[] = [
@@ -2970,13 +2964,6 @@ export const EmbedChainInfos: ChainInfoWithExplorer[] = [
 				coinDecimals: 18,
 				coinGeckoId: 'fetch-ai',
 				coinImageUrl: window.location.origin + '/public/assets/tokens/fet.png',
-			},
-			{
-				coinDenom: 'MOBX',
-				coinMinimalDenom: 'nanomobx',
-				coinDecimals: 9,
-				coinGeckoId: 'pool:nanomobix',
-				coinImageUrl: window.location.origin + '/public/assets/tokens/mobx.png',
 			},
 		],
 		feeCurrencies: [
