80f9518ddc4d86991b0e24cac20a05c61216ebcc
Jon Ator <jonathanator0@gmail.com>
2022-04-19 14:31:38 -0500

    Pool gauge cards

--- packages/web/components/cards/pool-gauge.tsx
+++ packages/web/components/cards/pool-gauge.tsx
@@ -2,15 +2,16 @@ import Image from "next/image";
 import { FunctionComponent } from "react";
 import classNames from "classnames";
 import { MetricLoader } from "../loaders";
-import { LoadingProps } from "../types";
+import { LoadingProps, MobileProps } from "../types";
 
 export const PoolGaugeCard: FunctionComponent<
   {
     days?: string;
     apr?: string;
     superfluidApr?: string;
-  } & LoadingProps
-> = ({ days, apr, isLoading = false, superfluidApr }) => (
+  } & LoadingProps &
+    MobileProps
+> = ({ days, apr, isLoading = false, superfluidApr, isMobile = false }) => (
   <div
     className={classNames(
       "w-full p-0.5 rounded-xl ",
@@ -18,7 +19,7 @@ export const PoolGaugeCard: FunctionComponent<
     )}
   >
     <div className="flex flex-col w-full gap-1 bg-card rounded-xlinset py-5 px-7">
-      <h5 className="font-medium">
+      <UnbondingHeader isMobile={isMobile}>
         <MetricLoader className="h-6" isLoading={isLoading}>
           {days ?? "0"} unbonding
           {superfluidApr && (
@@ -33,7 +34,7 @@ export const PoolGaugeCard: FunctionComponent<
             </>
           )}
         </MetricLoader>
-      </h5>
+      </UnbondingHeader>
       <MetricLoader className="h-6" isLoading={isLoading}>
         <p className="font-caption text-lg text-secondary-200">
           APR {apr ?? "0%"} {superfluidApr ? `+ ${superfluidApr}` : null}
@@ -42,3 +43,9 @@ export const PoolGaugeCard: FunctionComponent<
     </div>
   </div>
 );
+
+const UnbondingHeader: FunctionComponent<MobileProps> = ({
+  isMobile = false,
+  children,
+}) =>
+  isMobile ? <h6>{children}</h6> : <h5 className="font-medium">{children}</h5>;
