be74c2b213305e257aa1d672bbb102eef3a149bf
Jon Ator <jonathanator0@gmail.com>
2022-05-06 12:52:44 -0500

    Add next build analysis tool + README explanation

--- packages/web/next.config.js
+++ packages/web/next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
   reactStrictMode: true,
   images: {
     domains: ["app.osmosis.zone"],
   },
-};
+  enabled: process.env.ANALYZE === "true",
+});
+
+module.exports = withBundleAnalyzer({});
