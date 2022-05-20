be871c65bf49b3b941a694c943a50000bee538f6
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-05-19 22:07:25 -0600

    remove duplicate axlUSDC, MNTL verified
    -remove duplicate axlUSDC
    -make MNTL verified

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -395,26 +395,6 @@ export const IBCAssetInfos: (IBCAsset & {
     counterpartyChainId: "axelar-dojo-1",
     sourceChannelId: "channel-208",
     destChannelId: "channel-3",
-    coinMinimalDenom: "uusdc",
-    depositUrlOverride:
-      "https://satellite.money/?source=ethereum&destination=osmosis&token=usdc",
-    withdrawUrlOverride:
-      "https://satellite.money/?source=osmosis&destination=ethereum&token=usdc",
-  },
-  {
-    counterpartyChainId: "axelar-dojo-1",
-    sourceChannelId: "channel-208",
-    destChannelId: "channel-3",
-    coinMinimalDenom: "weth-wei",
-    depositUrlOverride:
-      "https://satellite.money/?source=ethereum&destination=osmosis&token=weth",
-    withdrawUrlOverride:
-      "https://satellite.money/?source=osmosis&destination=ethereum&token=weth",
-  },
-  {
-    counterpartyChainId: "axelar-dojo-1",
-    sourceChannelId: "channel-208",
-    destChannelId: "channel-3",
     coinMinimalDenom: "uusdt",
     depositUrlOverride:
       "https://satellite.money/?source=ethereum&destination=osmosis&token=usdt",
@@ -476,6 +456,7 @@ export const IBCAssetInfos: (IBCAsset & {
     sourceChannelId: "channel-232",
     destChannelId: "channel-0",
     coinMinimalDenom: "umntl",
+	  isVerified: true,
   },
   {
     counterpartyChainId: "juno-1",
