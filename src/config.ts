f30cc9e9b7d1dbb9d288f2d2bdfb2d77d8bdbb65
Angrymouse <rykov.nikas@gmail.com>
2022-05-23 21:58:16 +0300

    Add external incentives to pool 716

--- src/config.ts
+++ src/config.ts
@@ -91,6 +91,7 @@ export const LockupAbledPoolIds: {
 	'669': true,
 	'670': true,
 	'679': true,
+	'716': true,
 };
 
 export const PromotedLBPPoolIds: {
@@ -789,6 +790,16 @@ export const ExtraGaugeInPool: {
 		{
 			gaugeId: '3016',
 			denom: 'uosmo',
+		},
+	],
+	'716': [
+		{
+			gaugeId: '3532',
+			denom: 'ibc/AA1C80225BCA7B32ED1FC6ABF8B8E899BEB48ECDB4B417FD69873C6D715F97E7',
+		},
+		{
+			gaugeId: '3531',
+			denom: 'ibc/AA1C80225BCA7B32ED1FC6ABF8B8E899BEB48ECDB4B417FD69873C6D715F97E7',
 		},
 	],
 };
