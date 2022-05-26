6fb095e15befad7d7c1e62772550f944589f2b1f
Thunnini <yunjh1994@gmail.com>
2022-05-27 00:37:53 +0900

    Trim not worthwhile pools from swap router (with temporal, manual selection standard)

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -4,6 +4,8 @@ import { ProgressiveSvgImage } from "../components/progressive-svg-image";
 import { TradeClipboard } from "../components/trade-clipboard";
 import { useStore } from "../stores";
 import { IS_FRONTIER } from "../config";
+import { Dec } from "@keplr-wallet/unit";
+import { useMemo, useRef } from "react";
 
 const Home: NextPage = observer(function () {
   const { chainStore, queriesStore } = useStore();
@@ -12,7 +14,75 @@ const Home: NextPage = observer(function () {
   const queries = queriesStore.get(chainId);
   const queryPools = queries.osmosis!.queryGammPools;
 
-  const pools = queryPools.getAllPools().map((pool) => pool.pool);
+  // If pool has already passed once, it will be passed immediately without recalculation.
+  const poolsPassed = useRef<Map<string, boolean>>(new Map());
+  const allPools = queryPools.getAllPools();
+  // Pools should be memoized before passing to trade in config
+  const pools = useMemo(
+    () =>
+      allPools
+        .filter((pool) => {
+          // TODO: If not on production environment, this logic should pass all pools (or other selection standard).
+
+          // Trim not useful pools.
+
+          const passed = poolsPassed.current.get(pool.id);
+          if (passed) {
+            return true;
+          }
+
+          // There is currently no good way to pick a pool that is worthwhile.
+          // For now, based on the mainnet, only those pools with assets above a certain value are calculated for swap.
+
+          let hasEnoughAssets = false;
+
+          for (const asset of pool.poolAssets) {
+            // Probably, the pools that include gamm token may be created mistakenly by users.
+            if (
+              asset.amount.currency.coinMinimalDenom.startsWith("gamm/pool/")
+            ) {
+              return false;
+            }
+
+            // Only pools with at least 1000 osmo are dealt with.
+            if (asset.amount.currency.coinMinimalDenom === "uosmo") {
+              if (asset.amount.toDec().gt(new Dec(1000))) {
+                hasEnoughAssets = true;
+                break;
+              }
+            }
+
+            // Only pools with at least 10 ion are dealt with.
+            if (asset.amount.currency.coinMinimalDenom === "uion") {
+              if (asset.amount.toDec().gt(new Dec(10))) {
+                hasEnoughAssets = true;
+                break;
+              }
+            }
+
+            // Only pools with at least 1000 atom are dealt with.
+            if (
+              "originChainId" in asset.amount.currency &&
+              asset.amount.currency.coinMinimalDenom ===
+                "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+            ) {
+              if (asset.amount.toDec().gt(new Dec(1000))) {
+                hasEnoughAssets = true;
+                break;
+              }
+            }
+          }
+
+          if (hasEnoughAssets) {
+            console.log(`${pool.id} will be included to swap router`);
+            poolsPassed.current.set(pool.id, true);
+          }
+
+          return hasEnoughAssets;
+        })
+        .map((pool) => pool.pool),
+    [allPools]
+  );
 
   return (
     <main className="relative bg-background h-screen">
