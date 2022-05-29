c119e33d96bd508aa7f3894bce4031e5b1f3c0df
Sunny Aggarwal <sunnya97@protonmail.ch>
2022-05-28 22:04:43 -0400

    try cc endpoint

--- src/config.ts
+++ src/config.ts
@@ -3435,7 +3435,7 @@ export const EmbedChainInfos: ChainInfoWithExplorer[] = [
 	},
 	{
 		rpc: 'https://lcd.terra.dev/',
-		rest: 'https://terra-rpc.polkachu.com/',
+		rest: 'rpc.terrav2.ccvalidators.com/',
 		chainId: 'phoenix-1',
 		chainName: 'Terra 2.0',
 		stakeCurrency: {
