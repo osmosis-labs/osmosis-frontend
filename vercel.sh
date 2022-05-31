c87db73c13079ab3047934327be47820a4e3869f
Daniel Farina <contact@nevulas.com>
2022-05-31 15:59:25 -0400

    Update vercel.sh
    testing vercel ci for frontier branch

--- vercel.sh
+++ vercel.sh
@@ -4,7 +4,7 @@ echo "VERCEL_ENV: $VERCEL_ENV"
 
 if [[ "$VERCEL_ENV" == "frontier" ]] ; then
   # Proceed with the build
-  echo "✅ - Build can proceed"
+  echo "✅ - Build can proceed -testCI"
   exit 1;
 
 else
