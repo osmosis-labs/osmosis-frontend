3537daeda8752d5c042e29a5d4cabe4b9bfcc49f
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-05-31 13:49:59 -0600

    Add LINK to Axelar chaininfo

--- packages/web/config/chain-infos.ts
+++ packages/web/config/chain-infos.ts
@@ -1464,6 +1464,13 @@ chainInfos.push({
       coinGeckoId: "wrapped-bitcoin",
       coinImageUrl: "/tokens/wbtc.png",
     },
+    {
+      coinDenom: "LINK",
+      coinMinimalDenom: "link-wei",
+      coinDecimals: 18,
+      coinGeckoId: "chainlink",
+      coinImageUrl: "/tokens/link.svg",
+    },
   ],
   feeCurrencies: [
     {
