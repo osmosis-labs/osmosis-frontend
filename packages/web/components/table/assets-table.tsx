8be9637c14f390fb42c631734b3e193c8343b75e
Jon Ator <jonathanator0@gmail.com>
2022-06-14 11:39:42 -0500

    Fix Filter zero balances

--- packages/web/components/table/assets-table.tsx
+++ packages/web/components/table/assets-table.tsx
@@ -181,9 +181,7 @@ export const AssetsTable: FunctionComponent<Props> = ({
     "assets_hide_zero_balances",
     false
   );
-  const canHideZeroBalances = cells.some(
-    (cell) => cell.value && cell.value !== "0"
-  );
+  const canHideZeroBalances = cells.some((cell) => cell.amount !== "0");
 
   // Filter data based on user's input in the search box.
   const [query, setQuery, filteredSortedCells] = useFilteredData(
@@ -281,7 +279,7 @@ export const AssetsTable: FunctionComponent<Props> = ({
             <div className="flex gap-3 items-center place-content-between">
               <Switch
                 isOn={hideZeroBalances}
-                disabled={canHideZeroBalances}
+                disabled={!canHideZeroBalances}
                 onToggle={() => setHideZeroBalances(!hideZeroBalances)}
               >
                 Hide zero balances
@@ -314,7 +312,7 @@ export const AssetsTable: FunctionComponent<Props> = ({
             <div className="flex place-content-between">
               <Switch
                 isOn={hideZeroBalances}
-                disabled={canHideZeroBalances}
+                disabled={!canHideZeroBalances}
                 onToggle={() => setHideZeroBalances(!hideZeroBalances)}
               >
                 Hide zero balances
