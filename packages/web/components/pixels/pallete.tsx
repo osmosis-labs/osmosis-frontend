6139b93f695dfdd04b66c858315d041a3bfc5947
Thunnini <yunjh1994@gmail.com>
2022-06-19 19:29:48 +0900

    WIP

---
+++ packages/web/components/pixels/pallete.tsx
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+
+type PaletteProps = {
+  colorSet: string[];
+
+  maxColors: number;
+  x: number;
+  y: number;
+  setColorIndex: (index: number) => void;
+  doneEnabled: boolean;
+  clickDone: Function;
+};
+
+const Palette = ({
+  colorSet,
+  maxColors,
+  x,
+  y,
+  setColorIndex,
+  doneEnabled,
+  clickDone,
+}: PaletteProps) => {
+  const [selectedColor, setSelectedColor] = useState(0);
+
+  const colorOnClick = (index: number) => {
+    setSelectedColor(index);
+    setColorIndex(index);
+  };
+
+  return (
+    <div className="absolute pointer-events-none w-full h-auto bottom-[40px] z-[11]">
+      <div
+        className="w-[524px] h-[172px] rounded-[16px] bg-card p-[20px] mx-auto border border-2 border-enabledGold"
+        style={{
+          pointerEvents: "all",
+        }}
+      >
+        <div className="flex justify-between w-full h-auto mb-[5px]">
+          <div className="font-subtitle1 text-sm my-auto">
+            {doneEnabled ? `(${x + 1}, ${y + 1})` : " "}
+          </div>
+          <div
+            className={`cursor-pointer ${
+              doneEnabled ? "text-secondary-200" : "text-secondary-600"
+            } font-subtitle1 text-base my-auto`}
+            onClick={() => {
+              if (doneEnabled) {
+                clickDone();
+              }
+            }}
+          >
+            Done
+          </div>
+        </div>
+        <div
+          className="w-full h-auto grid justify-around content-center"
+          style={{
+            gridTemplateRows: "repeat(2, 50px)",
+            gridTemplateColumns: "repeat(8, 1fr)",
+          }}
+        >
+          {colorSet.map((color, idx) => (
+            <React.Fragment key={idx}>
+              {maxColors > idx ? (
+                idx === selectedColor ? (
+                  <div
+                    className="cursor-pointer w-[36px] h-[36px] rounded-full border-0 m-auto flex items-center justify-center"
+                    style={{ backgroundColor: color }}
+                  >
+                    <div className="w-[32px] h-[32px] rounded-full border-2 border-black m-auto" />
+                  </div>
+                ) : (
+                  <div
+                    className="cursor-pointer w-[36px] h-[36px] rounded-full border-0 m-auto"
+                    onClick={() => colorOnClick(idx)}
+                    style={{ backgroundColor: color }}
+                  />
+                )
+              ) : (
+                <div
+                  className="w-[36px] h-[36px] rounded-full bg-transparent m-auto"
+                  style={{
+                    border: "1px dashed rgba(255, 255, 255, 0.4)",
+                  }}
+                />
+              )}
+            </React.Fragment>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Palette;
---
+++ packages/web/pages/pixels/index.tsx
@@ -0,0 +1,236 @@
+import React, { useCallback, useEffect, useRef, useState } from "react";
+import { NextPage } from "next";
+import { TransformComponent, TransformWrapper } from "react-zoom-pan-pinch";
+import Palette from "../../components/pixels/pallete";
+
+export const GAME_CONFIG = {
+  PIXEL_SIZE: 25,
+  PIXEL_WIDTH: 500,
+  PIXEL_HEIGHT: 500,
+  SIDE_BAR_WIDTH: 206,
+  CANVAS_SIZE: 2000,
+};
+
+export const COLOR_SET = [
+  "#FFF",
+  "#D4D7D9",
+  "#898D90",
+  "#000",
+  "#FF4500",
+  "#FFA800",
+  "#FFD635",
+  "#00A368",
+  "#7EED56",
+  "#2450A4",
+  "#3690EA",
+  "#51E9F4",
+  "#811E9F",
+  "#B44AC0",
+  "#FF99AA",
+  "#9C6926",
+];
+
+const Pixels: NextPage = function () {
+  const [pixelList] = useState(() =>
+    Array.from(Array(GAME_CONFIG.PIXEL_WIDTH), () =>
+      Array.from(
+        Array(GAME_CONFIG.PIXEL_HEIGHT),
+        () => COLOR_SET[Math.floor(Math.random() * COLOR_SET.length)]
+      )
+    )
+  );
+  const [pixelIndex, setPixelIndex] = useState([-1, -1]);
+  const [focusScale] = useState(4);
+
+  const [colorIndex, setColorIndex] = useState(0);
+
+  const tempMousePosition = useRef([0, 0]);
+
+  // canvas
+  const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+  const loadImg = useCallback(() => {
+    const pixelCanvas = canvasRef.current?.getContext("2d");
+    if (!pixelCanvas) {
+      return;
+    }
+
+    for (let i = 0; i < pixelList.length; i++) {
+      for (let j = 0; j < pixelList[i].length; j++) {
+        pixelCanvas.fillStyle = pixelList[i][j];
+        pixelCanvas.fillRect(
+          i * GAME_CONFIG.PIXEL_SIZE,
+          j * GAME_CONFIG.PIXEL_SIZE,
+          GAME_CONFIG.PIXEL_SIZE,
+          GAME_CONFIG.PIXEL_SIZE
+        );
+      }
+    }
+  }, []);
+
+  const getZoomCoordinate = () => {
+    let pixel = 8000 / GAME_CONFIG.PIXEL_HEIGHT;
+    const viewWidth = (window.innerWidth - GAME_CONFIG.SIDE_BAR_WIDTH) / 33;
+    const viewHeight = window.innerHeight / 36;
+
+    const x = -(pixelIndex[0] * pixel - viewWidth * pixel);
+    const y = -(pixelIndex[1] * pixel - viewHeight * pixel);
+    return { x: x, y: y };
+  };
+
+  const getMousePos = (e: {
+    readonly clientX: number;
+    readonly clientY: number;
+  }) => {
+    if (canvasRef.current) {
+      let rect = canvasRef.current.getBoundingClientRect();
+      let width = rect.right - rect.left;
+      let height = rect.bottom - rect.top;
+      let userX = e.clientX - rect.x;
+      let userY = e.clientY - rect.y;
+
+      const x = userX * (GAME_CONFIG.PIXEL_WIDTH / width);
+      const y = userY * (GAME_CONFIG.PIXEL_HEIGHT / height);
+
+      return { x: Math.floor(x), y: Math.floor(y) };
+    }
+  };
+
+  // handler
+  const onClickHandler = useCallback((e: MouseEvent) => {
+    e.preventDefault();
+
+    const cursor = getMousePos(e);
+    const beforeX = e.clientX;
+    const beforeY = e.clientY;
+
+    if (canvasRef.current && cursor) {
+      canvasRef.current.addEventListener(
+        "mouseup",
+        (e2: MouseEvent) => {
+          const afterX = e2.clientX;
+          const afterY = e2.clientY;
+
+          if (beforeX === afterX && afterY === beforeY) {
+            const newPixel = [cursor.x, cursor.y];
+            setPixelIndex(newPixel);
+          }
+        },
+        {
+          once: true,
+        }
+      );
+    }
+  }, []);
+
+  // action
+  const handleMouseDown = (e: React.MouseEvent) => {
+    const x = e.clientX;
+    const y = e.clientY;
+
+    tempMousePosition.current = [x, y];
+  };
+
+  const handleClicked = (e: React.MouseEvent, setTransform: Function) => {
+    const cursorX = e.clientX;
+    const cursorY = e.clientY;
+
+    if (
+      tempMousePosition.current[0] === cursorX &&
+      tempMousePosition.current[1] === cursorY
+    ) {
+      const coordinate = getZoomCoordinate();
+      setTransform(coordinate.x, coordinate.y, focusScale);
+    }
+  };
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+
+    if (canvas) {
+      canvas.addEventListener("mousedown", onClickHandler);
+
+      return () => {
+        canvas.removeEventListener("mousedown", onClickHandler);
+      };
+    }
+  }, [onClickHandler]);
+
+  useEffect(() => {
+    canvasRef.current?.setAttribute(
+      "width",
+      String(GAME_CONFIG.PIXEL_WIDTH * GAME_CONFIG.PIXEL_SIZE)
+    );
+    canvasRef.current?.setAttribute(
+      "height",
+      String(GAME_CONFIG.PIXEL_HEIGHT * GAME_CONFIG.PIXEL_SIZE)
+    );
+    if (canvasRef.current) {
+      canvasRef.current.style.backgroundColor = "#fff";
+    }
+
+    loadImg();
+  }, [loadImg]);
+
+  return (
+    <main>
+      <div className="w-full h-screen">
+        <TransformWrapper
+          initialScale={2}
+          centerZoomedOut={true}
+          minScale={0.4}
+          limitToBounds={true}
+          maxScale={10}
+        >
+          {({ setTransform }) => {
+            return (
+              <React.Fragment>
+                <TransformComponent
+                  wrapperStyle={{ width: "100%", height: "100%" }}
+                >
+                  {pixelIndex[0] >= 0 && pixelIndex[1] >= 0 ? (
+                    <div
+                      className="absolute top-0 left-0"
+                      style={{
+                        width: "4px",
+                        height: "4px",
+                        outline: "solid #5D5FEF 1px",
+                        backgroundColor: COLOR_SET[colorIndex],
+                        transform: `translate(${pixelIndex[0] * 100}%, ${
+                          pixelIndex[1] * 100
+                        }%)`,
+                        transition: "all 0.5s",
+                        zIndex: 1000,
+                      }}
+                    />
+                  ) : null}
+                  <canvas
+                    style={{
+                      width: GAME_CONFIG.CANVAS_SIZE,
+                      height: GAME_CONFIG.CANVAS_SIZE,
+                    }}
+                    ref={canvasRef}
+                    onClick={(e) => handleClicked(e, setTransform)}
+                    onMouseDown={(e) => handleMouseDown(e)}
+                    onDoubleClick={() => {}}
+                  />
+                </TransformComponent>
+                <Palette
+                  colorSet={COLOR_SET}
+                  maxColors={16}
+                  x={pixelIndex[0]}
+                  y={pixelIndex[1]}
+                  setColorIndex={setColorIndex}
+                  clickDone={() => {}}
+                  doneEnabled={pixelIndex[0] >= 0 && pixelIndex[1] >= 0}
+                />
+              </React.Fragment>
+            );
+          }}
+        </TransformWrapper>
+      </div>
+    </main>
+  );
+};
+
+export default Pixels;
