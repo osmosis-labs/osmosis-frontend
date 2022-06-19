7f697e00b879475c91538a9fdaa7ff20a947d4e3
Thunnini <yunjh1994@gmail.com>
2022-06-20 01:38:48 +0900

    Minor fix

--- packages/web/pages/pixels/index.tsx
+++ packages/web/pages/pixels/index.tsx
@@ -120,14 +120,12 @@ const ShareModal: FunctionComponent<
               "Copied!"
             ) : (
               <React.Fragment>
-                <div className="flex items-center justify-center mr-1">
-                  <Image
-                    alt=""
-                    src="/icons/copy-white.svg"
-                    height={28}
-                    width={28}
-                  />
-                </div>
+                <Image
+                  alt=""
+                  src="/icons/copy-white.svg"
+                  height={28}
+                  width={28}
+                />
                 Copy link
               </React.Fragment>
             )}
