1f6c1c4c056e9c69bb82105c1fb5b76f9361bf1d
Jon Ator <jonathanator0@gmail.com>
2022-07-06 09:40:09 -0500

    Revert July 4th bg image

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -97,11 +97,15 @@ const Home: NextPage = observer(function () {
           <g>
             {!IS_FRONTIER && (
               <ProgressiveSvgImage
-                xlinkHref="/images/July-4th-web.png"
-                height="100%"
+                lowResXlinkHref="/images/osmosis-home-bg-low.png"
+                xlinkHref="/images/osmosis-home-bg.png"
+                x="56"
+                y="97"
+                width="578.7462"
+                height="725.6817"
               />
             )}
-            {/* {!IS_FRONTIER && (
+            {!IS_FRONTIER && (
               <rect
                 x="-3000"
                 y="778"
@@ -109,8 +113,8 @@ const Home: NextPage = observer(function () {
                 height="244"
                 fill="#120644"
               />
-            )} */}
-            {/* <ProgressiveSvgImage
+            )}
+            <ProgressiveSvgImage
               lowResXlinkHref={
                 IS_FRONTIER
                   ? "/images/osmosis-cowboy-woz-low.png"
@@ -125,7 +129,7 @@ const Home: NextPage = observer(function () {
               y={IS_FRONTIER ? "100" : "602"}
               width={IS_FRONTIER ? "800" : "448.8865"}
               height={IS_FRONTIER ? "800" : "285.1699"}
-            /> */}
+            />
           </g>
         </svg>
       </div>
