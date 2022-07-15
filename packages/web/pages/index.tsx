c9730b2524bfb16d09f09652bcd77cc7fd947feb
Jon Ator <jonathanator0@gmail.com>
2022-07-15 12:00:25 -0500

    Add USDC and DAI pools to router

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -71,6 +71,30 @@ const Home: NextPage = observer(function () {
                 break;
               }
             }
+
+            // only pools with at least 10,000 USDC
+            if (
+              "originChainId" in asset.amount.currency &&
+              asset.amount.currency.coinMinimalDenom ===
+                "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858"
+            ) {
+              if (asset.amount.toDec().gt(new Dec(10_000))) {
+                hasEnoughAssets = true;
+                break;
+              }
+            }
+
+            // only pools with at least 10,000 DAI
+            if (
+              "originChainId" in asset.amount.currency &&
+              asset.amount.currency.coinMinimalDenom ===
+                "ibc/0CD3A0285E1341859B5E86B6AB7682F023D03E97607CCC1DC95706411D866DF7"
+            ) {
+              if (asset.amount.toDec().gt(new Dec(10_000))) {
+                hasEnoughAssets = true;
+                break;
+              }
+            }
           }
 
           if (hasEnoughAssets) {
