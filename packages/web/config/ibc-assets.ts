294c6626dbf1fe23c3e5f6c121c509545a788317
nodebreaker0-0 <zeroday26@gmail.com>
2022-08-09 15:38:07 +0900

    Add Crescent to Frontier

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -780,6 +780,12 @@ export const IBCAssetInfos: (IBCAsset & {
     ics20ContractAddress:
       "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
   },
+  {
+    counterpartyChainId: "crescent-1",
+    sourceChannelId: "channel-297",
+    destChannelId: "channel-9",
+    coinMinimalDenom: "ucre",
+  },
 ].filter((ibcAsset) => (IS_FRONTIER ? true : ibcAsset.isVerified));
 
 export default IBCAssetInfos;
