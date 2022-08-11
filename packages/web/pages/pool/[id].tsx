3164a660b7e8aa90bb1f2fe6d6c192eb0f3f5ed4
Jon Ator <jonathanator0@gmail.com>
2022-08-11 16:12:54 -0500

    Remove gauge check

--- packages/web/pages/pool/[id].tsx
+++ packages/web/pages/pool/[id].tsx
@@ -193,26 +193,13 @@ const Pool: FunctionComponent = observer(() => {
       )
       .flat();
     externalGuages = (ExternalIncentiveGaugeAllowList[pool.id] ?? [])
-      .map(({ gaugeId, denom }, _i, gauges) => {
+      .map(({ gaugeId, denom }) => {
         const observableGauge = queryOsmosis.queryGauge.get(gaugeId);
         const currency = chainStore
           .getChain(chainStore.osmosis.chainId)
           .findCurrency(denom);
 
         if (observableGauge.remainingEpoch < 1) {
-          return;
-        }
-
-        // first gauge is main gauge, and must be longest
-        const mainGauge =
-          gauges.length > 0
-            ? queryOsmosis.queryGauge.get(gauges[0].gaugeId)
-            : undefined;
-        if (
-          mainGauge &&
-          mainGauge.lockupDuration.asMilliseconds() >
-            observableGauge.lockupDuration.asMilliseconds()
-        ) {
           return;
         }
 
@@ -233,6 +220,7 @@ const Pool: FunctionComponent = observer(() => {
           remainingEpochs: number;
         } => gauge !== undefined
       );
+
     const lockableDurations =
       queryOsmosis.queryLockableDurations.lockableDurations;
     guages = lockableDurations.map((duration) => {
