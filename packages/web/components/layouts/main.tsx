f2ae7b39445dc4cd11163b2f4c563ee6890414cb
Loa212 <anobileloris@gmail.com>
2022-08-25 18:15:38 +0200

    drawer component

--- packages/web/components/layouts/main.tsx
+++ packages/web/components/layouts/main.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 import Link from "next/link";
 import Head from "next/head";
 import { useRouter } from "next/router";
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, ReactNode } from "react";
 import classNames from "classnames";
 import { observer } from "mobx-react-lite";
 import {
@@ -46,10 +46,10 @@ export const MainLayout: FunctionComponent<MainLayoutProps> = observer(
             <OsmosisFullLogo onClick={() => router.push("/")} />
           </div>
         )}
-        <div
-          className={`${
-            !showSidebar && isMobile ? "-translate-x-full" : ""
-          } z-40 motion-reduce:transition-non transform transition ease-in-out delay-75 duration-700 fixed w-sidebar h-full bg-card flex flex-col px-5 py-6 overflow-x-hidden overflow-y-auto`}
+        <Drawer
+          // isOpen={showSidebar && isMobile}
+          showSidebar={showSidebar}
+          isMobile={isMobile}
         >
           {showBlockLogo && (
             <div className="z-50 w-sidebar mx-auto">
@@ -131,7 +131,7 @@ export const MainLayout: FunctionComponent<MainLayoutProps> = observer(
             </ul>
             <SidebarBottom />
           </div>
-        </div>
+        </Drawer>
         <div
           className={classNames(
             "fixed flex z-40 h-mobile-header w-screen items-center justify-end px-5",
@@ -179,3 +179,45 @@ const OsmosisFullLogo: FunctionComponent<{
     }}
   />
 );
+
+interface DrawerProps {
+  children: ReactNode;
+  showSidebar: boolean;
+  isMobile: boolean;
+  setIsOpen?: (value: boolean) => void;
+}
+
+const Drawer: React.FC<DrawerProps> = ({
+  children,
+  showSidebar,
+  isMobile,
+  setIsOpen = () => console.log("close"),
+}: DrawerProps) => {
+  return (
+    <main
+      className={
+        " fixed overflow-hidden z-40 bg-backdrop inset-0 transform ease-in-out w-full h-full backdrop-blur-sm" +
+        (showSidebar && isMobile
+          ? " transition-opacity opacity-100 duration-100 translate-x-0 "
+          : " transition-all delay-500 opacity-0 -translate-x-full  ")
+      }
+    >
+      <section
+        className={
+          " max-w-lg left-0 absolute bg-white h-full  delay-75 duration-500 ease-in-out transition-all transform bg-card flex flex-col px-5 py-6 overflow-x-hidden overflow-y-auto   " +
+          (showSidebar && isMobile
+            ? "shadow-xl translate-x-0 "
+            : " -translate-x-full ")
+        }
+      >
+        <article className="relative pt-8 h-full">{children}</article>
+      </section>
+      <section
+        className=" w-screen h-full cursor-pointer "
+        onClick={() => {
+          setIsOpen(false);
+        }}
+      ></section>
+    </main>
+  );
+};
