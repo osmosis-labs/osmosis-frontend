02c9edbc36ea02aa8d55e8b2e32ea6d8f81cab16
Jon Ator <jonathanator0@gmail.com>
2022-09-22 13:30:51 -0400

    Update switch size

--- packages/web/components/control/switch.tsx
+++ packages/web/components/control/switch.tsx
@@ -4,13 +4,19 @@ import { Disableable, CustomClasses } from "../types";
 import { ToggleProps } from "./types";
 
 export const Switch: FunctionComponent<
-  ToggleProps & Disableable & CustomClasses & { containerClassName?: string }
+  ToggleProps &
+    Disableable &
+    CustomClasses & {
+      containerClassName?: string;
+      labelPosition?: "left" | "right";
+    }
 > = ({
   isOn,
   onToggle,
   disabled = false,
   containerClassName,
   className,
+  labelPosition = "left",
   children,
 }) => (
   <label
@@ -22,12 +28,15 @@ export const Switch: FunctionComponent<
       containerClassName
     )}
   >
+    {labelPosition === "left" && (
+      <div className={disabled ? "opacity-30" : undefined}>{children}</div>
+    )}
     <input
       type="checkbox"
       className={classNames(
-        "cursor-pointer w-7 h-[18px] rounded-full appearance-none bg-osmoverse-600 transition ease-inOutBack duration-200 relative",
-        "after:h-4 after:w-4 after:rounded-full after:bg-white-high after:absolute after:left-px after:top-px after:transform after:ease-inOutBack after:scale-100 after:transition after:duration-200", // dot
-        "checked:after:transform checked:after:scale-100 checked:after:translate-x-2.5", // dot on transform
+        "cursor-pointer w-[52px] h-[32px] rounded-full appearance-none bg-osmoverse-600 transition ease-inOutBack duration-200 relative",
+        "after:h-7 after:w-7 after:rounded-full after:bg-white-high after:absolute after:left-[2px] after:top-[2px] after:transform after:ease-inOutBack after:scale-100 after:transition after:duration-200", // dot
+        "checked:after:transform checked:after:scale-100 checked:after:translate-x-[20px]", // dot on transform
         disabled
           ? "cursor-default after:bg-osmoverse-500 bg-osmoverse-600"
           : "checked:bg-wosmongton-500",
@@ -37,6 +46,8 @@ export const Switch: FunctionComponent<
       disabled={disabled}
       onChange={(e) => onToggle(e.target.checked)}
     />
-    <div className={disabled ? "opacity-30" : undefined}>{children}</div>
+    {labelPosition === "right" && (
+      <div className={disabled ? "opacity-30" : undefined}>{children}</div>
+    )}
   </label>
 );
