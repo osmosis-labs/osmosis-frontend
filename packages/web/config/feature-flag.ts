560e2fcdf3b6e0447ad6195d1cf61e4477b26d4b
Daniel Farina <contact@nevulas.com>
2022-10-03 16:16:37 -0400

    Turn off halted network banner

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -2,7 +2,7 @@ import { DenomHelper } from "@keplr-wallet/common";
 import { IS_FRONTIER } from "./ibc-assets";
 
 /** UI will go into "halt mode" if `true`. */
-export const IS_HALTED = true;
+export const IS_HALTED = false;
 
 export const UserAction: { [key: string]: boolean } = {
   CreateNewPool: true,
