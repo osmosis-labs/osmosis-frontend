1f55e33239bce5884f075041a874d703afc0600e
Dorian <dorian@latouche.dev>
2022-10-07 12:56:15 +0200

    Remove unnecessary comments

--- packages/web/components/control/language-select.tsx
+++ packages/web/components/control/language-select.tsx
@@ -12,6 +12,7 @@ export type LanguageOption = {
   display: string;
   image: string;
 };
+
 export type LanguageSelectProps = {
   options: LanguageOption[];
 };
@@ -101,27 +102,3 @@ export const LanguageSelect: FunctionComponent<LanguageSelectProps> = observer(
     );
   }
 );
-
-/*
- <button
-                className={classNames(
-                  "px-[1rem] py-[0.5rem] cursor-pointer hover:bg-osmoverse-700 flex items-center ",
-                  {
-                    "text-rust-200": value === currentLanguage,
-                    "rounded-b-xlinset": index === options.length - 1,
-                    "rounded-t-xlinset": index === 0,
-                  }
-                )}
-                key={value}
-                onClick={onSelect)}
-              >
-                <div className="flex items-center justify-center min-w-[24px]">
-                  <Image
-                    src={image}
-                    width={24}
-                    height={24}
-                    alt={`${display}`}
-                  />
-                </div>
-                <p className="ml-[0.75rem]">{display}</p>
-              </button> */
