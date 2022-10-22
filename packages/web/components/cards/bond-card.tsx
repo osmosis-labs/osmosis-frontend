d2b837082b01cca813fc9308332db681c906bc8f
Jon Ator <jonathanator0@gmail.com>
2022-10-22 13:51:57 -0500

    Fix drawer

--- packages/web/components/cards/bond-card.tsx
+++ packages/web/components/cards/bond-card.tsx
@@ -159,7 +159,7 @@ const Drawer: FunctionComponent<{
   return (
     <div
       className={classNames(
-        "absolute w-full h-[320px] -bottom-[226px] left-1/2 -translate-x-1/2 flex flex-col transition-all duration-300 ease-inOutBack z-50",
+        "absolute w-full h-[320px] -bottom-[234px] left-1/2 -translate-x-1/2 flex flex-col transition-all duration-300 ease-inOutBack z-50",
         {
           "-translate-y-[210px] bg-osmoverse-700 rounded-t-[18px]": drawerUp,
         }
