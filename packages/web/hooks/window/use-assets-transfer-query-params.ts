c61baaacc9c756209c4152e161d164111f14dc27
milesz8 <miles.mclenon@gmail.com>
2022-10-22 13:53:35 -0500

    Config maintains query params for coin transfers

---
+++ packages/web/hooks/window/use-assets-transfer-query-params.ts
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+import { useEffect, useRef } from "react";
+import { ObservableTransferUIConfig, TransferDir } from "../../stores/assets";
+
+/** Bidirectionally sets/gets window query params transfer/direction `transfer=DENOM&direction=TransferDir` and sets in assets config object. */
+export function useTokenTransferQueryParams(
+  transferConfig: ObservableTransferUIConfig,
+  isInModal = false
+) {
+  const router = useRouter();
+  const firstQueryEffectChecker = useRef(false);
+  // Reads query params and sets appropriate asset config state
+  useEffect(() => {
+    if (isInModal || !transferConfig || firstQueryEffectChecker.current) {
+      return;
+    }
+    var directionOfTransfer = router.query.direction;
+    var transferDenom = router.query.transfer;
+    if (
+      directionOfTransfer !== "withdraw" &&
+      directionOfTransfer !== "deposit"
+    ) {
+      return;
+    }
+    if (transferDenom) {
+      transferConfig.transferAsset(
+        directionOfTransfer,
+        transferDenom as string
+      );
+    } else transferConfig.startTransfer(directionOfTransfer);
+    firstQueryEffectChecker.current = true;
+  }, [router.query.transfer, router.query.direction]);
+
+  return {
+    setTransferQueryParams: (direction: TransferDir, coinDenom?: string) => {
+      router.query.direction = direction;
+      if (coinDenom) {
+        router.query.transfer = coinDenom;
+      } else {
+        router.query.transfer = "";
+      }
+      router.push(router);
+    },
+  };
+}
