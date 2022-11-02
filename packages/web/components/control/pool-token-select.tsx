4889ed911ce0da9b402b7cf9f6c1524ef49002f7
Jon Ator <jonathanator0@gmail.com>
2022-11-02 11:07:00 -0500

    Update colors in pool token select

--- packages/web/components/control/pool-token-select.tsx
+++ packages/web/components/control/pool-token-select.tsx
@@ -32,9 +32,9 @@ export const PoolTokenSelect: FunctionComponent<
     <div>
       <div
         className={classNames(
-          "md:p-1 p-3 flex hover:bg-card cursor-pointer",
+          "md:p-1 p-3 flex hover:bg-osmoverse-700 cursor-pointer",
           {
-            "bg-card rounded-t-xl md:w-48 w-64": isToggleOpen,
+            "bg-osmoverse-700 rounded-t-xl md:w-48 w-64": isToggleOpen,
             "rounded-xl": !isToggleOpen,
           },
           className
@@ -85,7 +85,7 @@ const TokensDropdown: FunctionComponent<
     onSelect: (coinDenom: string) => void;
   } & MobileProps
 > = ({ tokens, onSelect, isMobile = false }) => (
-  <div className="absolute flex flex-col bg-card rounded-b-xl z-50 md:w-52 w-64">
+  <div className="absolute flex flex-col bg-osmoverse-700 rounded-b-xl z-50 md:w-52 w-64">
     {tokens.map((token, index) => (
       <button
         className={classNames(
