891c88cb2a6288a99377e89ab3a812831cf19b8c
gejeduck <47668701+gejeduck@users.noreply.github.com>
2022-11-08 05:04:02 +1000

    feat: update testnet contracts

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -7,13 +7,13 @@ export const SUBQUERY_ENDPOINTS: { [chainId: string]: string } = {
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "osmo1t9d3sn68fs3gfl8nqxjhjm7mme4sq0ps6t5ar2umuqshmd75ctxqecu5lp",
+    "osmo1dhkyxu0g9u6zr3wm3d3s875atnffvr60hg2gnsz80s48j33atmxq3ptp4c",
 };
 
 export const WRAPPER_ADDRESSES: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "osmo1zkgtuakfpxw00m7rn0e2aphtxe34znr2ewajr04ch6h4usmcf6tqdhvejw",
+    "osmo16ca3u3l8mrgrmz3u48rrfh7dhf3e9y8d9xrxf3w0lla6xy073v7stz3r4e",
 };
 export const ENABLE_AUTONOMY =
   process.env.NEXT_PUBLIC_ENABLE_AUTONOMY === "enabled";
