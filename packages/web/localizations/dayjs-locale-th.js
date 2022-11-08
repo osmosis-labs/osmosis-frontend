e7ce25a000faf031d0a9f9a2cdd7c2add1f287e5
Daniel Farina <contact@nevulas.com>
2022-11-08 04:21:22 -0500

    Add Thai Language (Work in progress)

---
+++ packages/web/localizations/dayjs-locale-th.js
@@ -0,0 +1,20 @@
+!(function (e, n) {
+  "object" == typeof exports && "undefined" != typeof module
+    ? (module.exports = n())
+    : "function" == typeof define && define.amd
+    ? define(n)
+    : ((e =
+        "undefined" != typeof globalThis
+          ? globalThis
+          : e || self).dayjs_locale_en = n());
+})(this, function () {
+  "use strict";
+  return {
+    name: "th",
+    weekdays: "อาทิตย์_จันทร์_อังคาร_พุธ_พฤหัสบดี_ศุกร์_เสาร์".split("_"),
+    months:
+      "มกราคม_กุมภาพันธ์_มีนาคม_เมษายน_พฤษภาคม_มิถุนายน_กรกฎาคม_สิงหาคม_กันยายน_ตุลาคม_พฤศจิกายน_ธันวาคม".split(
+        "_"
+      ),
+  };
+});
