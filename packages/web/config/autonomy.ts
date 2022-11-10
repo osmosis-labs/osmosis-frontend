8d9ca07019b48b2945c8c6e0f974a3afe1ed81bc
gejeduck <47668701+gejeduck@users.noreply.github.com>
2022-11-11 02:24:38 +1000

    fix: registry address

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -1,13 +1,13 @@
 export const SUBQUERY_ENDPOINTS: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "https://api.subquery.network/sq/0xlook/autonomy-osmosis-subql",
+    "https://api.subquery.network/sq/0xlook/autonomy-osmosis-testnet",
 };
 
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
   "osmosis-1": "",
   "osmo-test-4":
-    "osmo1dhkyxu0g9u6zr3wm3d3s875atnffvr60hg2gnsz80s48j33atmxq3ptp4c",
+    "osmo1ulqdnqjkm6v6kdehh37md4a6fs4ppyrv9jqr0ypcud9p8efygzmsstn2sy",
 };
 
 export const WRAPPER_ADDRESSES: { [chainId: string]: string } = {
