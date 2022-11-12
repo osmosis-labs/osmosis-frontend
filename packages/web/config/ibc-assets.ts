c1f132c499412332a45a14e2e2abbb2ef100fbaa
Assaf Morami <assaf.morami@gmail.com>
2022-11-12 07:38:03 +0200

    Update ibc-assets.ts

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -166,14 +166,14 @@ export const IBCAssetInfos: (IBCAsset & {
   {
     counterpartyChainId: "secret-4",
     sourceChannelId: "channel-476",
-    destChannelId: "channel-39",
+    destChannelId: "channel-44",
     coinMinimalDenom: "cw20:secret1qfql357amn448duf5gvp9gr48sxx9tsnhupu3d:SHD",
     ics20ContractAddress: "secret1tqmms5awftpuhalcv5h5mg76fa0tkdz4jv9ex4",
   },
   {
     counterpartyChainId: "secret-4",
     sourceChannelId: "channel-476",
-    destChannelId: "channel-39",
+    destChannelId: "channel-44",
     coinMinimalDenom:
       "cw20:secret1k6u0cy4feepm6pehnz804zmwakuwdapm69tuc4:stkd-SCRT",
     ics20ContractAddress: "secret1tqmms5awftpuhalcv5h5mg76fa0tkdz4jv9ex4",
@@ -181,7 +181,7 @@ export const IBCAssetInfos: (IBCAsset & {
   {
     counterpartyChainId: "secret-4",
     sourceChannelId: "channel-476",
-    destChannelId: "channel-39",
+    destChannelId: "channel-44",
     coinMinimalDenom:
       "cw20:secret1rgm2m5t530tdzyd99775n6vzumxa5luxcllml4:SIENNA",
     ics20ContractAddress: "secret1tqmms5awftpuhalcv5h5mg76fa0tkdz4jv9ex4",
@@ -189,14 +189,14 @@ export const IBCAssetInfos: (IBCAsset & {
   {
     counterpartyChainId: "secret-4",
     sourceChannelId: "channel-476",
-    destChannelId: "channel-39",
+    destChannelId: "channel-44",
     coinMinimalDenom: "cw20:secret1yxcexylwyxlq58umhgsjgstgcg2a0ytfy4d9lt:BUTT",
     ics20ContractAddress: "secret1tqmms5awftpuhalcv5h5mg76fa0tkdz4jv9ex4",
   },
   {
     counterpartyChainId: "secret-4",
     sourceChannelId: "channel-476",
-    destChannelId: "channel-39",
+    destChannelId: "channel-44",
     coinMinimalDenom:
       "cw20:secret12rcvz0umvk875kd6a803txhtlu7y0pnd73kcej:ALTER",
     ics20ContractAddress: "secret1tqmms5awftpuhalcv5h5mg76fa0tkdz4jv9ex4",
