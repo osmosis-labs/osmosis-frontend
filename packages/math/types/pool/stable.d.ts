0971c5e4b636c523bd3bc4fa9e925fa3cb58c9c4
jonator <jonathanator0@gmail.com>
2022-11-19 16:59:46 -0600

    Update types build product

--- packages/math/types/pool/stable.d.ts
+++ packages/math/types/pool/stable.d.ts
@@ -1,23 +1,29 @@
 import { Coin, Dec } from "@keplr-wallet/unit";
+import { BigDec } from "../big-dec";
 export declare const StableSwapMath: {
     calcOutGivenIn: typeof calcOutGivenIn;
     calcInGivenOut: typeof calcInGivenOut;
     calcSpotPrice: typeof calcSpotPrice;
-    solveCfmm: typeof solveCfmm;
-    compare_checkMultErrorTolerance: typeof compare_checkMultErrorTolerance;
 };
 export declare type StableSwapToken = {
     amount: Dec;
     denom: string;
     scalingFactor: number;
 };
-export declare function solveCalcOutGivenIn(tokens: StableSwapToken[], tokenIn: Coin, tokenOutDenom: string, swapFee: Dec): Dec;
+declare type BigDecStableSwapToken = {
+    amount: BigDec;
+    denom: string;
+    scalingFactor: number;
+};
+export declare function solveCalcOutGivenIn(tokens: StableSwapToken[], tokenIn: Coin, tokenOutDenom: string, swapFee: Dec): BigDec;
 export declare function calcOutGivenIn(tokens: StableSwapToken[], tokenIn: Coin, tokenOutDenom: string, swapFee: Dec): import("@keplr-wallet/unit").Int;
 export declare function calcInGivenOut(tokens: StableSwapToken[], tokenOut: Coin, tokenInDenom: string, swapFee: Dec): import("@keplr-wallet/unit").Int;
-export declare function scaleTokens(tokens: StableSwapToken[]): StableSwapToken[];
+export declare function scaleTokens(tokens: StableSwapToken[]): BigDecStableSwapToken[];
 export declare function calcSpotPrice(tokens: StableSwapToken[], baseDenom: string, quoteDenom: string): Dec;
-export declare function solveCfmm(xReserve: Dec, yReserve: Dec, remReserves: Dec[], yIn: Dec): Dec;
-export declare function solveCfmmBinarySearchMulti(xReserve: Dec, yReserve: Dec, wSumSquares: Dec, yIn: Dec): Dec;
+export declare function solveCfmm(xReserve: BigDec, yReserve: BigDec, remReserves: BigDec[], yIn: BigDec): BigDec;
+export declare function solveCfmmBinarySearchMulti(xReserve: BigDec, yReserve: BigDec, wSumSquares: BigDec, yIn: BigDec): BigDec;
+export declare function targetKCalculator(x0: BigDec, y0: BigDec, w: BigDec, yf: BigDec): BigDec;
+export declare function iterKCalculator(xf: BigDec, x0: BigDec, w: BigDec, yf: BigDec): BigDec;
 /**
  multi-asset CFMM is xyv(x^2 + y^2 + w) = k,
  where u is the product of the reserves of assets
@@ -25,6 +31,9 @@ export declare function solveCfmmBinarySearchMulti(xReserve: Dec, yReserve: Dec,
  of their squares (e.g. v = w^2 + z^2).
  When u = 1 and v = 0, this is equivalent to solidly's CFMM
  */
-export declare function cfmmConstantMultiNoV(xReserve: Dec, yReserve: Dec, wSumSquares: Dec): Dec;
-export declare function binarySearch(makeOutput: (est: Dec) => Dec, lowerBound: Dec, upperBound: Dec, targetOutput: Dec, maxIterations?: number, errorTolerance?: Dec): Dec;
-export declare function compare_checkMultErrorTolerance(expected: Dec, actual: Dec, tolerance: Dec, roundingMode: string): number;
+export declare function cfmmConstantMultiNoV(xReserve: BigDec, yReserve: BigDec, wSumSquares: BigDec): BigDec;
+export declare function binarySearch(makeOutput: (est: BigDec) => BigDec, lowerBound: BigDec, upperBound: BigDec, targetOutput: BigDec, maxIterations?: number, errorTolerance?: BigDec): BigDec;
+export declare function compareBigDec_checkMultErrorTolerance(expected: BigDec, actual: BigDec, tolerance: BigDec, roundingMode: string): number;
+export declare function compareDec_checkMultErrorTolerance(expected: Dec, actual: Dec, tolerance: Dec, roundingMode: string): number;
+export declare function calcWSumSquares(remReserves: BigDec[]): BigDec;
+export {};
