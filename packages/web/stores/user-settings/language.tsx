abf3e2dbffe6fc247dc9a3181c397abd14a22372
PikachuEXE <pikachuexe@gmail.com>
2022-11-23 15:12:44 +0800

    + Add language zh-hk

--- packages/web/stores/user-settings/language.tsx
+++ packages/web/stores/user-settings/language.tsx
@@ -42,6 +42,10 @@ const SUPPORTED_LANGUAGES: MenuDropdownIconItemProps[] = [
     value: "zh-tw",
     display: "正體中文",
   },
+  {
+    value: "zh-hk",
+    display: "香港語",
+  },
 ];
 
 export class LanguageUserSetting implements IUserSetting<LanguageState> {
