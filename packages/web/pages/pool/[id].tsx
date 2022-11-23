178683c6bc1b75a91c7c154bbc10d4dba32dff11
jonator <jonathanator0@gmail.com>
2022-11-23 11:06:02 -0600

    Fix cut off breakdown

--- packages/web/pages/pool/[id].tsx
+++ packages/web/pages/pool/[id].tsx
@@ -364,7 +364,7 @@ const Pool: FunctionComponent = observer(() => {
             className={classNames(
               "flex flex-col gap-10 px-10 pt-10 md:px-5 md:pt-7 transition-height duration-300 ease-inOutBack overflow-hidden",
               showPoolDetails
-                ? "h-[230px] xl:h-[300px] lg:h-[520px]"
+                ? "h-[235px] xl:h-[300px] lg:h-[520px]"
                 : "h-[120px] xl:h-[180px] lg:h-[280px]"
             )}
           >
