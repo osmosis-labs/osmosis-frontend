d26db3136ec9455d7b10ba59f1be2a6b6d8fef88
PikachuEXE <pikachuexe@gmail.com>
2022-11-24 01:58:53 +0800

    Fix zh-tw format (#1043)

--- packages/web/localizations/dayjs-locale-zh-tw.js
+++ packages/web/localizations/dayjs-locale-zh-tw.js
@@ -14,7 +14,7 @@
   }
   var t = n(e),
     i = {
-      name: "zh-cn",
+      name: "zh-tw",
       weekdays: "星期日_星期一_星期二_星期三_星期四_星期五_星期六".split("_"),
       weekdaysShort: "週日_週一_週二_週三_週四_週五_週六".split("_"),
       weekdaysMin: "日_一_二_三_四_五_六".split("_"),
@@ -57,8 +57,8 @@
         hh: "%d 小時",
         d: "1 天",
         dd: "%d 天",
-        M: "1 个月",
-        MM: "%d 个月",
+        M: "1 個月",
+        MM: "%d 個月",
         y: "1 年",
         yy: "%d 年",
       },
