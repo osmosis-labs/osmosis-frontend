be4b7f3abddbddcb7e76ff710bff78bc60dbff37
jonator <jonathanator0@gmail.com>
2022-11-23 16:31:30 -0600

    Add select pool type

--- packages/web/modals/create-pool.tsx
+++ packages/web/modals/create-pool.tsx
@@ -2,6 +2,8 @@ import { FunctionComponent, useState } from "react";
 import { observer } from "mobx-react-lite";
 import { ObservableCreatePoolConfig } from "@osmosis-labs/stores";
 import {
+  PoolType,
+  SelectType,
   Step1SetRatios,
   Step2AddLiquidity,
   Step3Confirm,
@@ -10,16 +12,20 @@ import { ModalBase, ModalBaseProps } from ".";
 
 export const CreatePoolModal: FunctionComponent<
   ModalBaseProps & {
-    step?: 1 | 2 | 3;
     createPoolConfig: ObservableCreatePoolConfig;
     isSendingMsg?: boolean;
     onCreatePool: () => void;
   }
 > = observer((props) => {
-  const { step, createPoolConfig: config, isSendingMsg, onCreatePool } = props;
-  const [curStep, setCurStep] = useState<1 | 2 | 3>(step ?? 1);
-  const advanceStep = () => setCurStep(Math.min(curStep + 1, 3) as 1 | 2 | 3);
-  const backStep = () => setCurStep(Math.max(curStep - 1, 0) as 1 | 2 | 3);
+  const { createPoolConfig: config, isSendingMsg, onCreatePool } = props;
+  const [poolType, setPoolType] = useState<PoolType | null>(null);
+  const [curStep, setCurStep] = useState<1 | 2 | 3 | null>(null);
+  const advanceStep = () => {
+    if (curStep) setCurStep(Math.min(curStep + 1, 3) as 1 | 2 | 3);
+  };
+  const backStep = () => {
+    if (curStep) setCurStep(Math.max(curStep - 1, 0) as 1 | 2 | 3);
+  };
 
   return (
     <ModalBase
@@ -30,6 +36,15 @@ export const CreatePoolModal: FunctionComponent<
       }}
       onRequestBack={curStep && curStep !== 1 ? backStep : undefined}
     >
+      {poolType === null && (
+        <SelectType
+          types={["weighted", "stable"]}
+          selectType={(type) => {
+            setPoolType(type);
+            setCurStep(1);
+          }}
+        ></SelectType>
+      )}
       {curStep === 1 && (
         <Step1SetRatios createPoolConfig={config} advanceStep={advanceStep} />
       )}
