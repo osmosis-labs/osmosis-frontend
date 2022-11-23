20357a78217045a884e10b12bc5807d96e3e7ed6
jonator <jonathanator0@gmail.com>
2022-11-23 15:14:32 -0600

    Support token factory assets

--- packages/web/stores/assets/index.ts
+++ packages/web/stores/assets/index.ts
@@ -65,7 +65,7 @@ export class ObservableAssets {
   @computed
   get nativeBalances(): CoinBalance[] {
     return this.chain.currencies
-      .filter((currency) => !currency.coinMinimalDenom.includes("/"))
+      .filter((currency) => !currency.coinMinimalDenom.includes("gamm"))
       .map((currency) => {
         const bal = this.queries.queryBalances
           .getQueryBech32Address(this.account.bech32Address)
