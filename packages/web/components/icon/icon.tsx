da807aadc5505f656c5a5b8fc507cf16381d7bef
Sehyun Chung <sehyun@berkeley.edu>
2022-12-07 13:39:28 -0800

    fill icon attempt

--- packages/web/components/icon/icon.tsx
+++ packages/web/components/icon/icon.tsx
@@ -1,6 +1,7 @@
 import { useMemo } from "react";
 import { IconProps } from "./types";
 import Star from "./icons/star";
+import Image from "next/image";
 
 const Icon: React.FC<IconProps> = ({
   color = "#fff",
@@ -18,17 +19,18 @@ const Icon: React.FC<IconProps> = ({
     }
   }, [color, name, size]);
   return (
-    <div
-      className="inline-flex"
-      onClick={onClick}
-      style={{
-        cursor: onClick ? "pointer" : "default",
-        height: `${size}px`,
-        width: `${size}px`,
-      }}
-    >
-      {Child}
-    </div>
+    // <div
+    //   className="inline-flex"
+    //   onClick={onClick}
+    //   style={{
+    //     cursor: onClick ? "pointer" : "default",
+    //     height: `${size}px`,
+    //     width: `${size}px`,
+    //   }}
+    // >
+    <Image alt={name} src={`/icons/${name}.svg`} height={size} width={size} />
+    // {/* {Child} */}
+    // </div>
   );
 };
 
