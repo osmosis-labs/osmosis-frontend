0084794c84e7fa195058046ddac6e2ff0644f367
Crypto Assassin <103904125+CryptoAssassin1@users.noreply.github.com>
2022-12-13 01:25:30 +0500

    Unverified DARC
    Governance removed incentives from Darc pool.

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -393,7 +393,6 @@ export const IBCAssetInfos: (IBCAsset & {
     sourceChannelId: "channel-171",
     destChannelId: "channel-0",
     coinMinimalDenom: "udarc",
-    isVerified: true,
   },
   {
     counterpartyChainId: "umee-1",
