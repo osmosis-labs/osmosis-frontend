f87b227f1746651565da620e4217a00820e555ab
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2022-12-13 14:27:55 -0700

    Update IXO chain id to ixo-4

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -289,7 +289,7 @@ export const IBCAssetInfos: (IBCAsset & {
     isVerified: true,
   },
   {
-    counterpartyChainId: "impacthub-3",
+    counterpartyChainId: "ixo-4",
     sourceChannelId: "channel-38",
     destChannelId: "channel-4",
     coinMinimalDenom: "uixo",
