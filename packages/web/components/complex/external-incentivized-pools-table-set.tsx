6d6627abcc3dae1d3c25f009072c275d70e031f2
jonator <jonathanator0@gmail.com>
2023-01-01 11:30:26 +0800

    Remove unused

--- packages/web/components/complex/external-incentivized-pools-table-set.tsx
+++ packages/web/components/complex/external-incentivized-pools-table-set.tsx
@@ -1,4 +1,4 @@
-import { PricePretty, RatePretty } from "@keplr-wallet/unit";
+import { RatePretty } from "@keplr-wallet/unit";
 import { ObservableQueryPool } from "@osmosis-labs/stores";
 import { observer } from "mobx-react-lite";
 import { FunctionComponent, useMemo, useCallback, useState } from "react";
