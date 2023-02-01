c3119ade8cd116c33bbc1b6d7c7391a833ec7f6d
Sehyun Chung <sehyun@berkeley.edu>
2023-02-01 14:27:59 -0800

    add virtualization

--- packages/web/components/complex/paginated-table.tsx
+++ packages/web/components/complex/paginated-table.tsx
@@ -1,9 +1,9 @@
 import { flexRender, Row, Table } from "@tanstack/react-table";
+import { useVirtualizer } from "@tanstack/react-virtual";
 import Image from "next/image";
 import { useRef } from "react";
 import { IS_FRONTIER } from "../../config";
 import { Pool } from "./all-pools-table-set";
-import { useVirtualizer } from "@tanstack/react-virtual";
 
 type Props = {
   paginate: () => void;
@@ -15,94 +15,90 @@ const PaginatedTable = ({ table }: Props) => {
 
   const { rows } = table.getRowModel();
   const rowVirtualizer = useVirtualizer({
-    count: 10000,
+    count: rows.length,
     getScrollElement: () => parentRef.current,
-    estimateSize: () => 35,
+    estimateSize: () => 80,
     overscan: 5,
   });
 
   return (
-    <div
-      ref={parentRef}
-      style={{
-        height: "400px",
-      }}
-      className="my-5 w-full overflow-auto"
-    >
-      <table className="w-full">
-        <thead>
-          {table.getHeaderGroups().map((headerGroup) => (
-            <tr key={headerGroup.id}>
-              {headerGroup.headers.map((header) => (
-                <th
-                  key={header.id}
-                  colSpan={header.colSpan}
-                  style={{ width: header.getSize() }}
-                >
-                  <div
-                    {...{
-                      className: header.column.getCanSort()
-                        ? "cursor-pointer select-none"
-                        : "",
-                      onClick: header.column.getToggleSortingHandler(),
-                    }}
+    <div ref={parentRef} className="my-5 h-screen w-full overflow-auto">
+      <div className={`relative h-[${rowVirtualizer.getTotalSize()}] w-full`}>
+        <table className="w-full">
+          <thead>
+            {table.getHeaderGroups().map((headerGroup) => (
+              <tr key={headerGroup.id}>
+                {headerGroup.headers.map((header) => (
+                  <th
+                    key={header.id}
+                    colSpan={header.colSpan}
+                    className={`w-[${header.getSize()}]px`}
                   >
-                    {flexRender(
-                      header.column.columnDef.header,
-                      header.getContext()
-                    )}
-                    {{
-                      asc: (
-                        <Image
-                          alt="ascending"
-                          src={
-                            IS_FRONTIER
-                              ? "/icons/sort-up-white.svg"
-                              : "/icons/sort-up.svg"
-                          }
-                          height={16}
-                          width={16}
-                        />
-                      ),
-                      desc: (
-                        <Image
-                          alt="descending"
-                          src={
-                            IS_FRONTIER
-                              ? "/icons/sort-down-white.svg"
-                              : "/icons/sort-down.svg"
-                          }
-                          height={16}
-                          width={16}
-                        />
-                      ),
-                    }[header.column.getIsSorted() as string] ?? null}
-                  </div>
-                </th>
-              ))}
-            </tr>
-          ))}
-        </thead>
-        <tbody>
-          {rowVirtualizer.getVirtualItems().map((virtualRow) => {
-            const row = rows[virtualRow.index] as Row<Pool>;
-            return (
-              <tr key={row.id}>
-                {row.getVisibleCells().map((cell) => {
-                  return (
-                    <td key={cell.id}>
+                    <div
+                      {...{
+                        className: header.column.getCanSort()
+                          ? "cursor-pointer select-none"
+                          : "",
+                        onClick: header.column.getToggleSortingHandler(),
+                      }}
+                    >
                       {flexRender(
-                        cell.column.columnDef.cell,
-                        cell.getContext()
+                        header.column.columnDef.header,
+                        header.getContext()
                       )}
-                    </td>
-                  );
-                })}
+                      {{
+                        asc: (
+                          <Image
+                            alt="ascending"
+                            src={
+                              IS_FRONTIER
+                                ? "/icons/sort-up-white.svg"
+                                : "/icons/sort-up.svg"
+                            }
+                            height={16}
+                            width={16}
+                          />
+                        ),
+                        desc: (
+                          <Image
+                            alt="descending"
+                            src={
+                              IS_FRONTIER
+                                ? "/icons/sort-down-white.svg"
+                                : "/icons/sort-down.svg"
+                            }
+                            height={16}
+                            width={16}
+                          />
+                        ),
+                      }[header.column.getIsSorted() as string] ?? null}
+                    </div>
+                  </th>
+                ))}
               </tr>
-            );
-          })}
-        </tbody>
-      </table>
+            ))}
+          </thead>
+          <tbody>
+            {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+              const row = rows[virtualRow.index] as Row<Pool>;
+              return (
+                <tr key={row.id} className={`h-[${virtualRow.size}]`}>
+                  {row.getVisibleCells().map((cell) => {
+                    return (
+                      <td key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </td>
+                    );
+                  })}
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
