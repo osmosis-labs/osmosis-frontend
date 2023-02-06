785ec8114e7a951f77dae2a1cafb4638a2aac884
Aries <43811871+SynergyNodes@users.noreply.github.com>
2023-02-06 11:22:34 +0530

    Update ibc-assets.ts

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1433,6 +1433,14 @@ export const IBCAssetInfos: (IBCAsset & {
     withdrawUrlOverride: "https://app.arable.finance/#/ibc",
   },
   {
+    counterpartyChainId: "acre_9052-1",
+    sourceChannelId: "channel-490",
+    destChannelId: "channel-0",
+    coinMinimalDenom: "erc20/0xAE6D3334989a22A65228732446731438672418F2",
+    depositUrlOverride: "https://app.arable.finance/#/ibc",
+    withdrawUrlOverride: "https://app.arable.finance/#/ibc",
+  },    
+  {
     counterpartyChainId: "planq_7070-2",
     sourceChannelId: "channel-492",
     destChannelId: "channel-1",
