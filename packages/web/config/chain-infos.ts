1bd7b99656bc58982fbcdf6174b0e6c70d3ef1ac
Le Hieu <levanhieuhust@gmail.com>
2023-02-08 11:48:46 +0700

    Fix gasPriceStep

--- packages/web/config/chain-infos.ts
+++ packages/web/config/chain-infos.ts
@@ -2078,9 +2078,9 @@ const chainInfos = (
           isStakeCurrency: true,
           isFeeCurrency: true,
           gasPriceStep: {
-            low: 0.005,
-            average: 0.01,
-            high: 0.03,
+            low: 0.003,
+            average: 0.005,
+            high: 0.007,
           },
         },
       ],
