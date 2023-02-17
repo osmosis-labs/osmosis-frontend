aa0d6b91ae95c6168c87f5ad2133ec6bbe7043b2
Diego <diegochui96@gmail.com>
2023-02-16 23:08:07 -0800

    fix: quickfix until subquery fully syncs

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -1,25 +1,25 @@
 export const SUBQUERY_ENDPOINTS: { [chainId: string]: string } = {
-  "osmosis-1": "",
-  "osmo-test-4":
-    "https://api.subquery.network/sq/Autonomy-Network/osmosis-testnet",
+  'osmosis-1': '',
+  'osmo-test-4':
+    'https://testnet.osmosis.subquery.autonomynetwork.alishahverdi.com/',
 };
 
 export const SUBQUERY_BACKUP_ENDPOINTS: { [chainId: string]: string } = {
-  "osmosis-1": "",
-  "osmo-test-4":
-    "https://testnet.osmosis.subquery.autonomynetwork.alishahverdi.com/",
+  'osmosis-1': '',
+  'osmo-test-4':
+    'https://api.subquery.network/sq/Autonomy-Network/osmosis-testnet',
 };
 
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
-  "osmosis-1": "",
-  "osmo-test-4":
-    "osmo1zynr26u48vdjrcuxkgswfhcx4zh5lw58qshzycykf33p7fp5y32qkydwrp",
+  'osmosis-1': '',
+  'osmo-test-4':
+    'osmo1zynr26u48vdjrcuxkgswfhcx4zh5lw58qshzycykf33p7fp5y32qkydwrp',
 };
 
 export const WRAPPER_ADDRESSES: { [chainId: string]: string } = {
-  "osmosis-1": "",
-  "osmo-test-4":
-    "osmo1dwpdh2clk7c8csf9ql2xj36336xsryyg4j7622jhaert9htp48gsh8u9ve",
+  'osmosis-1': '',
+  'osmo-test-4':
+    'osmo1dwpdh2clk7c8csf9ql2xj36336xsryyg4j7622jhaert9htp48gsh8u9ve',
 };
 export const ENABLE_AUTONOMY = true;
 // process.env.NEXT_PUBLIC_ENABLE_AUTONOMY === "enabled";
