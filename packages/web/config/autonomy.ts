45ce4140c2ee64b283ebfd3438b992c789a08b2b
Diego <diegochui96@gmail.com>
2023-02-21 13:25:19 -0800

    chore: update subquery endpoints

--- packages/web/config/autonomy.ts
+++ packages/web/config/autonomy.ts
@@ -1,13 +1,13 @@
 export const SUBQUERY_ENDPOINTS: { [chainId: string]: string } = {
   'osmosis-1': '',
   'osmo-test-4':
-    'https://testnet.osmosis.subquery.autonomynetwork.alishahverdi.com/',
+    'https://api.subquery.network/sq/Autonomy-Network/osmosis4-testnet',
 };
 
 export const SUBQUERY_BACKUP_ENDPOINTS: { [chainId: string]: string } = {
   'osmosis-1': '',
   'osmo-test-4':
-    'https://api.subquery.network/sq/Autonomy-Network/osmosis-testnet',
+    'https://testnet.osmosis.subquery.autonomynetwork.alishahverdi.com/',
 };
 
 export const REGISTRY_ADDRESSES: { [chainId: string]: string } = {
