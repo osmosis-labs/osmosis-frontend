2db75fab0b1bdb0ea1615a9ea74fd70993c40cc3
Juno Fox <118552854+JunoNetworkFox@users.noreply.github.com>
2023-03-14 00:16:38 +0700

    Add Fox to IBC Assets

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1509,6 +1509,16 @@ export const IBCAssetInfos: (IBCAsset & {
         destChannelId: "channel-2",
         coinMinimalDenom: "uqatom",
       },
+      {
+        //JunoFox
+        counterpartyChainId: "juno-1",
+        sourceChannelId: "channel-169",
+        destChannelId: "channel-47",
+        coinMinimalDenom:
+          "cw20:juno1u8cr3hcjvfkzxcaacv9q75uw9hwjmn8pucc93pmy6yvkzz79kh3qncca8x",
+        ics20ContractAddress:
+          "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
+      },
     ].filter((ibcAsset) => {
       // validate IBC asset config
       if (
