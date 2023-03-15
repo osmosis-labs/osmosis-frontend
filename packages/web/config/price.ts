fc5b2d96424cb91759963357a98b656b7d5fcf35
Juno Fox <118552854+JunoNetworkFox@users.noreply.github.com>
2023-03-16 00:56:25 +0700

    Add Fox Price

--- packages/web/config/price.ts
+++ packages/web/config/price.ts
@@ -1318,4 +1318,13 @@ export const PoolPriceRoutes: IntermediateRoute[] = [
     spotPriceDestDenom: "uosmo",
     destCoinId: "pool:uosmo",
   },
+  {
+    alternativeCoinId: "pool:fox",
+    poolId: "949",
+    spotPriceSourceDenom: DenomHelper.ibcDenom(
+        [{ portId: "transfer", channelId: "channel-169" }],
+        "cw20:juno1u8cr3hcjvfkzxcaacv9q75uw9hwjmn8pucc93pmy6yvkzz79kh3qncca8x"
+    ),
+    spotPriceDestDenom: "uosmo",
+    destCoinId: "pool:uosmo",
 ];
