e5b5f73b9708ae65ec5fb6abb5a21f0810145f06
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-04-01 15:07:19 -0600

    Update active-gauges.ts

--- packages/web/pages/api/active-gauges.ts
+++ packages/web/pages/api/active-gauges.ts
@@ -1,8 +1,8 @@
 import { Gauge } from "@osmosis-labs/stores/build/queries/incentives/types";
 import type { NextApiRequest, NextApiResponse } from "next";
-import { Epochs } from "@osmosis-labs/stores/build/queries/epochs/types";
-import { ChainInfos } from "../../config";
 
+import { ChainInfos } from "../../config";
+import { Epochs } from "@osmosis-labs/stores/build/queries/epochs/types";
 
 type ExternalIncentiveGaugesResponse = {
   data: Gauge[];
