f001781856e84f564f53102668bb54ba56bb2548
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-04-04 19:37:35 +0200

    update config

--- inlang.config.js
+++ inlang.config.js
@@ -6,34 +6,30 @@
  */
 export async function defineConfig(env) {
     // importing inlang-plugin-json from https://github.com/samuelstroschein/inlang-plugin-json
-	const plugin = await env.$import(
-		"https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
-	);
+    const plugin = await env.$import(
+        "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
+    );
+    const { standardLintRules } = await env.$import(
+        "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
+      );
+    const pluginConfig = {
+        pathPattern: "./packages/web/localizations/{language}.json",
+    };
 
-	const pluginConfig = {
-		pathPattern: "./packages/web/localizations/{language}.json",
-	};
-
-	return {
-		referenceLanguage: "en",
-		languages: [
-			"en",
-			"es",
-			"fr",
-			"ko",
-			"pl",
-			"ro",
-			"tr",
-			"zh-cn",
-			"zh-hk",
-			"zh-tw",
-			"fa",
-		],
-		readResources: (args) => {
-			return plugin.readResources({ ...args, ...env, pluginConfig });
-		},
-		writeResources: (args) => {
-			return plugin.writeResources({ ...args, ...env, pluginConfig });
-		},
-	};
+    return {
+        referenceLanguage: "en",
+        languages: await plugin.getLanguages({
+          ...env,
+          pluginConfig,
+        }),
+        readResources: (args) => {
+          return plugin.readResources({ ...args, ...env, pluginConfig });
+        },
+        writeResources: (args) => {
+          return plugin.writeResources({ ...args, ...env, pluginConfig });
+        },
+        lint: {
+          rules: [standardLintRules()],
+        },
+    };
 }
