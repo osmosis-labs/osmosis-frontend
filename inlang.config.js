d8931adebc96a30889078165f6dff5b07306d504
jannesblobel <72493222+jannesblobel@users.noreply.github.com>
2023-04-13 09:28:00 +0200

    remove default from StandardLintRules

--- inlang.config.js
+++ inlang.config.js
@@ -9,7 +9,7 @@ export async function defineConfig(env) {
     const plugin = await env.$import(
         "https://cdn.jsdelivr.net/gh/samuelstroschein/inlang-plugin-json@1/dist/index.js"
     );
-    const { default: standardLintRules } = await env.$import(
+    const {standardLintRules } = await env.$import(
         "https://cdn.jsdelivr.net/gh/inlang/standard-lint-rules@1/dist/index.js"
       );
     const pluginConfig = {
