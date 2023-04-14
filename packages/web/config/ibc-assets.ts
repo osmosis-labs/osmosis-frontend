1efbd586d42236ab580fecbc729e2ee16644a0d0
0xGhastly01 <130753294+0xGhastly01@users.noreply.github.com>
2023-04-14 12:24:36 +0200

    add CLST to ibc-assets

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1576,6 +1576,16 @@ export const IBCAssetInfos: (IBCAsset & {
         ics20ContractAddress:
           "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
       },
+      {
+        //CLST
+        counterpartyChainId: "juno-1",
+        sourceChannelId: "channel-169",
+        destChannelId: "channel-47",
+        coinMinimalDenom:
+          "cw20:juno1ngww7zxak55fql42wmyqrr4rhzpne24hhs4p3w4cwhcdgqgr3hxsmzl9zg",
+        ics20ContractAddress:
+          "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
+      },        
     ].filter((ibcAsset) => {
       // validate IBC asset config
       if (
