b3981a3c108af8d50aa08821e6d8cf6305441654
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-04-16 14:15:58 -0600

    Add missing property name

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1609,6 +1609,7 @@ export const IBCAssetInfos: (IBCAsset & {
         counterpartyChainId: "juno-1",
         sourceChannelId: "channel-169",
         destChannelId: "channel-47",
+        coinMinimalDenom:
           "cw20:juno1jrr0tuuzxrrwcg6hgeqhw5wqpck2y55734e7zcrp745aardlp0qqg8jz06",
         ics20ContractAddress:
           "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
