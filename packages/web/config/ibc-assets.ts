ed0c00d6dbba320ed476004649c5f1aea01aadf6
Spauthix0 <131483350+Spauthix0@users.noreply.github.com>
2023-04-21 20:29:40 +0200

    Add SPACER ibc-assets

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1672,6 +1672,16 @@ export const IBCAssetInfos: (IBCAsset & {
         ics20ContractAddress:
           "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
       },
+      {
+        //SPACER
+        counterpartyChainId: "juno-1",
+        sourceChannelId: "channel-169",
+        destChannelId: "channel-47",
+        coinMinimalDenom:
+          "cw20:juno1dyyf7pxeassxvftf570krv7fdf5r8e4r04mp99h0mllsqzp3rs4q7y8yqg",
+        ics20ContractAddress:
+          "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
+      },        
     ].filter((ibcAsset) => {
       // validate IBC asset config
       if (
