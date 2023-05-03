035043bed0824663dfe2fca879b1f3f8deda23b4
jonator <jonathanator0@gmail.com>
2023-05-03 11:22:11 -0500

    Kado- pre-select OSMO, with OSMO,USDC options

--- packages/web/integrations/kado/index.tsx
+++ packages/web/integrations/kado/index.tsx
@@ -16,7 +16,7 @@ export const Kado: FunctionComponent<
 
   return (
     <iframe
-      src={`https://app.kado.money/?onPayCurrency=USD&onPayAmount=200&onRevCurrency=${assetKey}&offPayCurrency=${assetKey}&offRevCurrency=USD&network=OSMOSIS&onToAddress=${account.bech32Address}&offFromAddress=X&cryptoList=${assetKey}&networkList=OSMOSIS&apiKey=67a2aaad-a2a5-4412-be40-e3a70aa7b53d&product=BUY`}
+      src={`https://app.kado.money/?onPayCurrency=USD&onPayAmount=200&onRevCurrency=OSMO&offPayCurrency=${assetKey}&offRevCurrency=USD&network=OSMOSIS&onToAddress=${account.bech32Address}&offFromAddress=X&cryptoList=OSMO,USDC&networkList=OSMOSIS&apiKey=67a2aaad-a2a5-4412-be40-e3a70aa7b53d&product=BUY`}
       width="420"
       height="700"
     />
