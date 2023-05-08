3bdf619a73848ad6ccf31962c28765a2077fa6d8
Max Millington <maxmillington89@gmail.com>
2023-05-08 16:23:01 -0600

    add zoom on hover

--- packages/web/components/cards/app-display-card.tsx
+++ packages/web/components/cards/app-display-card.tsx
@@ -18,33 +18,43 @@ const AppDisplayCard: React.FC<AppDisplayCardProps> = ({
   externalUrl,
 }) => {
   return (
-    <a href={externalUrl} target="_blank" rel="noopener noreferrer">
-      <div className="bg-white overflow-hidden rounded-lg rounded-lg bg-osmoverse-800 shadow-md">
-        <div
-          className="h-40 bg-cover bg-center"
-          style={{ backgroundImage: `url(${imageUrl})` }}
-        ></div>
-        <div className="p-4">
-          <div className="flex items-center">
-            <h6 className="font-semibold">{title}</h6>
-            <div className="ml-auto">
-              <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
-                <i className="color-osmoverse-400 fab fa-twitter text-xl"></i>
-              </a>
-              <a
-                href={githubUrl}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="color-osmoverse-400 ml-2"
-              >
-                <i className="color-osmoverse-400  fab fa-github text-xl"></i>
-              </a>
+    <>
+      <style jsx>{`
+        .app-display-card:hover .card-image {
+          @apply scale-115 transform transition-all duration-300 ease-in;
+        }
+        .app-display-card {
+          @apply overflow-hidden;
+        }
+      `}</style>
+      <a href={externalUrl} target="_blank" rel="noopener noreferrer">
+        <div className="app-display-card bg-white overflow-hidden rounded-lg rounded-lg bg-osmoverse-800 shadow-md">
+          <div
+            className="card-image h-40 bg-cover bg-center"
+            style={{ backgroundImage: `url(${imageUrl})` }}
+          ></div>
+          <div className="p-4">
+            <div className="flex items-center">
+              <h6 className="font-semibold">{title}</h6>
+              <div className="ml-auto">
+                <a href={twitterUrl} target="_blank" rel="noopener noreferrer">
+                  <i className="color-osmoverse-400 fab fa-twitter text-xl"></i>
+                </a>
+                <a
+                  href={githubUrl}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="color-osmoverse-400 ml-2"
+                >
+                  <i className="color-osmoverse-400  fab fa-github text-xl"></i>
+                </a>
+              </div>
             </div>
+            <p className="body2 text-osmoverse-200">{subtitle}</p>
           </div>
-          <p className="body2 text-osmoverse-200">{subtitle}</p>
         </div>
-      </div>
-    </a>
+      </a>
+    </>
   );
 };
 
