97a7761ae8a655422e0785de04fcc5f7e17cc9ef
Jose Felix <jfelx@outlook.com>
2023-05-19 15:03:54 -0400

    improvement: remove extra testEnvironment

--- packages/stores/jest.config.js
+++ packages/stores/jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   preset: "ts-jest",
-  testEnvironment: "node",
   roots: ["<rootDir>/src/"],
   testMatch: ["**/__tests__/?(*.)+(spec|test).[jt]s?(x)"],
   globalSetup: "./src/__tests__/global-setup.ts",
