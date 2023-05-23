0a78d8e6add22a8671b887ac4d0d6bce42cd03a3
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-05-23 09:18:58 -0600

    Revert "Add IBCX Banner and Background Art"

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -36,14 +36,7 @@ export const Announcement:
         "Chain is halted, transactions are temporarily disabled",
       isWarning: true,
     }
-  : {
-      enTextOrLocalizationPath: "ibcx.description",
-      link: {
-        enTextOrLocalizationKey: "ibcx.link",
-        url: "https://index.ion.wtf/",
-        isExternal: true,
-      },
-    };
+  : undefined;
 
 // Past localstorage keys:
 // * "feedback_wQ1KR7": "Help us shape the future of Osmosis." Give us feedback -> https://tally.so/r/wQ1KR7
