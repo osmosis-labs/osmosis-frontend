09bfa2fcf7ea192a73ffd37e41b193634809c111
Jon Ator <jonathanator0@gmail.com>
2023-05-30 21:56:12 -0500

    WIP: add CL intro modal

---
+++ packages/web/components/funnels/concentrated-liquidity/index.ts
@@ -0,0 +1,3 @@
+export * from "./intro";
+export * from "./learn-more";
+export * from "./supercharge-dai-osmo-pool";
---
+++ packages/web/components/funnels/concentrated-liquidity/learn-more.tsx
@@ -0,0 +1,5 @@
+import { FunctionComponent } from "react";
+
+export const ConcentratedLiquidityLearnMore: FunctionComponent = () => {
+  return <div>Learn more</div>;
+};
