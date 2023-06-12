f0dd42c2e62b3ac00288cc17397031eb6f591d3e
mattupham <mcupham@gmail.com>
2023-06-12 13:14:00 -0700

    Add sentry exception

--- packages/web/sentry.client.config.ts
+++ packages/web/sentry.client.config.ts
@@ -19,6 +19,8 @@ Sentry.init({
   // in development and sample at a lower rate in production
   replaysSessionSampleRate: 0.1,
 
+  environment: process.env.NODE_ENV || "development",
+
   // You can remove this option if you're not planning to use the Sentry Session Replay feature:
   integrations: [
     new Sentry.Replay({
