25c6c859f1a0f1ee915be0c883956b95173d9e21
Serkan Reis <serkanreis@gmail.com>
2023-06-20 15:34:34 +0300

    Update profileStore to include a custom stargaze pfp uri

--- packages/web/stores/profile.ts
+++ packages/web/stores/profile.ts
@@ -8,13 +8,17 @@ import {
   toJS,
 } from "mobx";
 
-type Avatar = "wosmongton" | "ammelia";
+type Avatar = "wosmongton" | "ammelia" | "stargaze-pfp";
 
 export class ProfileStore {
   @observable
   private _currentAvatar: Avatar;
+  @observable
+  private _stargazeAvatarUri: string | undefined;
 
   private readonly _avatarStorageKey = "profile_store_current_avatar";
+  private readonly _stargazeAvatarUriStorageKey =
+    "profile_store_stargaze_avatar_uri";
 
   constructor(protected readonly kvStorage: KVStore) {
     this._currentAvatar = "wosmongton";
@@ -27,6 +31,15 @@ export class ProfileStore {
         this._currentAvatar = value as Avatar;
       });
     });
+    // Need to get previous stargaze avatar uri from storage
+    this.kvStorage
+      .get(this._stargazeAvatarUriStorageKey)
+      .then((value: unknown) => {
+        runInAction(() => {
+          if (!value) return;
+          this._stargazeAvatarUri = value as string;
+        });
+      });
   }
 
   @computed
@@ -34,9 +47,23 @@ export class ProfileStore {
     return this._currentAvatar;
   }
 
+  @computed
+  get stargazeAvatarUri(): string | undefined {
+    return this._stargazeAvatarUri;
+  }
+
   @action
   setCurrentAvatar(value: Avatar) {
     this._currentAvatar = value;
     this.kvStorage.set(this._avatarStorageKey, toJS(this._currentAvatar));
+  }
+
+  @action
+  setStargazeAvatarUri(value: string | undefined) {
+    this._stargazeAvatarUri = value;
+    this.kvStorage.set(
+      this._stargazeAvatarUriStorageKey,
+      toJS(this._stargazeAvatarUri)
+    );
   }
 }
