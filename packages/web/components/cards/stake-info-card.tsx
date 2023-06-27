3a2b9f2292ce80545f1e8b19838ee2c6b476d3c3
Max Millington <maxmillington89@gmail.com>
2023-06-27 11:28:45 -0600

    add earnings card

--- packages/web/components/cards/stake-info-card.tsx
+++ packages/web/components/cards/stake-info-card.tsx
@@ -1,10 +1,13 @@
+import { Dec } from "@keplr-wallet/unit";
 import classNames from "classnames";
 import Image from "next/image";
-import React from "react";
+import React, { useMemo } from "react";
 import { useTranslation } from "react-multi-lang";
 
-import { useWindowSize } from "~/hooks";
+import { IS_FRONTIER, IS_TESTNET } from "~/config";
+import { useTradeTokenInConfig, useWindowSize } from "~/hooks";
 
+import { useStore } from "../../stores";
 import { Button } from "../buttons";
 import { OsmoverseCard } from "./osmoverse-card";
 
@@ -15,6 +18,45 @@ export const StakeInfoCard = () => {
   const isMobile = useWindowSize();
   const outAmountValue = "1,917,227";
   const inAmountValue = "3763470";
+
+  const { chainStore, accountStore, queriesStore, priceStore } = useStore();
+  const { chainId } = chainStore.osmosis;
+  const account = accountStore.getWallet(chainId);
+  const address = account?.address ?? "";
+
+  const queries = queriesStore.get(chainId);
+  const queryPools = queries.osmosis!.queryGammPools;
+  const allPools = queryPools.getAllPools();
+
+  const pools = useMemo(
+    () =>
+      allPools
+        .filter((pool) =>
+          pool
+            .computeTotalValueLocked(priceStore)
+            .toDec()
+            .gte(new Dec(IS_TESTNET ? -1 : IS_FRONTIER ? 1_000 : 10_000))
+        )
+        .sort((a, b) => {
+          // sort by TVL to find routes amongst most valuable pools
+          const aTVL = a.computeTotalValueLocked(priceStore);
+          const bTVL = b.computeTotalValueLocked(priceStore);
+
+          return Number(bTVL.sub(aTVL).toDec().toString());
+        })
+        .map((pool) => pool.pool),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [allPools, priceStore.response]
+  );
+
+  const tradeTokenInConfig = useTradeTokenInConfig(
+    chainStore,
+    chainId,
+    address,
+    queriesStore,
+    pools
+  );
+
   return (
     <OsmoverseCard>
       <div className="flex place-content-between items-center transition-opacity">
@@ -23,7 +65,14 @@ export const StakeInfoCard = () => {
             {t("stake.available")}
           </span>
           <span className="caption ml-1.5 text-sm text-wosmongton-300 md:text-xs">
-            10,000 OSMO
+            {queries.queryBalances
+              .getQueryBech32Address(address)
+              .getBalanceFromCurrency(tradeTokenInConfig.sendCurrency)
+              .trim(true)
+              .hideDenom(true)
+              .maxDecimals(8)
+              .toString()}
+            &nbsp; OSMO
           </span>
         </div>
         <div className="flex items-center gap-1.5">
@@ -48,7 +97,7 @@ export const StakeInfoCard = () => {
         </div>
       </div>
       <div className="flex items-center gap-2 py-3 px-2 text-left">
-        <div className="mr-1 flex h-[50px] w-[50px] shrink-0 overflow-hidden rounded-full md:h-7 md:w-7">
+        <div className="mr-1 flex h-[50px] shrink-0 overflow-hidden rounded-full md:h-7 md:w-7">
           <Image
             src={OSMO_IMG_URL}
             alt="osmosis icon"
@@ -58,7 +107,7 @@ export const StakeInfoCard = () => {
         </div>
         <div className="flex flex-col">
           <div className="flex items-center">
-            {isMobile ? <span className="subtitle1">OSMO</span> : <h5>OSMO</h5>}
+            {isMobile ? <span className="subtitle1">OSMO</span> : <h4>OSMO</h4>}
           </div>
           <div className="subtitle2 md:caption w-32 text-osmoverse-400">
             Osmosis
--- packages/web/pages/stake/index.tsx
+++ packages/web/pages/stake/index.tsx
@@ -1,10 +1,8 @@
 import React, { useState } from "react";
 import { useTranslation } from "react-multi-lang";
 
-import { Icon } from "~/components/assets";
-import IconButton from "~/components/buttons/icon-button";
+import { EstimatedEarningCard } from "~/components/cards/estimated-earnings-card";
 import { StakeInfoCard } from "~/components/cards/stake-info-card";
-import { ValidatorSquad } from "~/components/cards/validator-squad";
 import { StakeTab } from "~/components/control/stake-tab";
 
 import { useWindowSize } from "../../hooks";
@@ -16,36 +14,10 @@ export const Staking: React.FC = () => {
 
   return (
     <main className="flex h-screen items-center justify-center">
-      <div className="relative flex w-[27rem] flex-col gap-8 overflow-hidden rounded-[24px] bg-osmoverse-800 px-1 py-1 lg:mx-auto md:mt-mobile-header md:gap-6 md:px-3 md:pt-4 md:pb-4">
-        <div className="relative flex flex-col gap-8 overflow-hidden rounded-[24px] bg-osmoverse-800 px-6 pt-8 pb-8 md:gap-6 md:px-3 md:pt-4 md:pb-4">
-          <div className="relative flex w-full items-center justify-between">
-            <Icon
-              id="sandbox"
-              width={isMobile ? 20 : 28}
-              height={isMobile ? 20 : 28}
-              className="text-osmoverse-400 hover:text-white-full"
-            />
-
+      <div className="relative flex w-[27rem] flex-col gap-8 overflow-hidden rounded-[24px] bg-osmoverse-800 lg:mx-auto md:mt-mobile-header md:gap-6 md:px-3 md:pt-4 md:pb-4">
+        <div className="relative flex flex-col gap-6 overflow-hidden rounded-[24px] bg-osmoverse-800 px-6 py-10 md:gap-6 md:px-3 md:pt-4 md:pb-4">
+          <div className="relative flex w-full items-center justify-center">
             <h6 className="text-center">{t("stake.title")}</h6>
-            <div className="flex items-center">
-              <IconButton
-                aria-label="Open swap settings"
-                className="z-40 w-fit py-0"
-                size="unstyled"
-                mode="unstyled"
-                onClick={(e) => {
-                  e.stopPropagation();
-                }}
-                icon={
-                  <Icon
-                    id="setting"
-                    width={isMobile ? 20 : 28}
-                    height={isMobile ? 20 : 28}
-                    className="text-osmoverse-400 hover:text-white-full"
-                  />
-                }
-              />
-            </div>
           </div>
           <div className="flex justify-around border-b-2 border-transparent">
             <StakeTab
@@ -62,7 +34,7 @@ export const Staking: React.FC = () => {
             </StakeTab>
           </div>
           <StakeInfoCard />
-          <ValidatorSquad />
+          <EstimatedEarningCard />
         </div>
       </div>
     </main>
