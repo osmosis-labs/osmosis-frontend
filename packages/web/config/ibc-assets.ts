5efb9964c33dadf0e3004812a7baf4fbe7c5d5c9
Jeremy Parish <jeremyparish69@gmail.com>
2023-06-27 16:31:07 -0600

    add CASA

--- packages/web/config/ibc-assets.ts
+++ packages/web/config/ibc-assets.ts
@@ -1868,6 +1868,16 @@ export const IBCAssetInfos: (IBCAsset & {
         destChannelId: "channel-10",
         coinMinimalDenom: "untrn",
       },
+      {
+        //CASA
+        counterpartyChainId: "juno-1",
+        sourceChannelId: "channel-169",
+        destChannelId: "channel-47",
+        coinMinimalDenom:
+          "cw20:juno1ju8k8sqwsqu5k6umrypmtyqu2wqcpnrkf4w4mntvl0javt4nma7s8lzgss",
+        ics20ContractAddress:
+          "juno1v4887y83d6g28puzvt8cl0f3cdhd3y6y9mpysnsp3k8krdm7l6jqgm0rkn",
+      },
     ].filter((ibcAsset) => {
       // validate IBC asset config
       if (
