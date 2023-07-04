adca20c3dde5baf7df075247176e10719ece22b1
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-07-04 12:32:36 -0600

    Update source-chain-infos.ts

--- packages/web/config/generate-chain-infos/source-chain-infos.ts
+++ packages/web/config/generate-chain-infos/source-chain-infos.ts
@@ -3725,41 +3725,6 @@ const mainnetChainInfos: SimplifiedChainInfo[] = [
     features: ["ibc-transfer", "ibc-go"],
     explorerUrlToTx: "https://ping.pub/Centauri/tx/{txHash}",
   },
-  {
-    rpc: "picasso",
-    rest: "picasso",
-    chainId: "picasso",
-    chainName: "Picasso",
-    bip44: {
-      coinType: 118,
-    },
-    bech32Config: Bech32Address.defaultBech32Config("picasso"),
-    currencies: [
-      {
-        coinDenom: "PICA",
-        coinMinimalDenom: "ppica",
-        coinDecimals: 12,
-        coinGeckoId: "pool:ppica",
-        coinImageUrl: "/tokens/composable.png",
-        isStakeCurrency: true,
-        isFeeCurrency: true,
-        gasPriceStep: {
-          low: 0,
-          average: 0,
-          high: 0,
-        },
-      },
-      {
-        coinDenom: "KSM",
-        coinMinimalDenom: "4",
-        coinDecimals: 12,
-        coinGeckoId: "pool:ksm",
-        coinImageUrl: "/tokens/ksm.svg",
-      },
-    ],
-    features: ["ibc-transfer", "ibc-go"],
-    explorerUrlToTx: "https://ping.pub/Centauri/tx/{txHash}",
-  },
 ];
 
 const chainInfos = IS_TESTNET
