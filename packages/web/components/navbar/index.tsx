7419c6954ca34f40722f9b42380d9e0d2606364c
Jose Felix <jfelx@outlook.com>
2023-07-07 16:29:41 -0400

    improvement: match cta btn height to profile btn

--- packages/web/components/navbar/index.tsx
+++ packages/web/components/navbar/index.tsx
@@ -157,7 +157,7 @@ export const NavBar: FunctionComponent<
                 className="h-fit w-[180px] lg:w-fit lg:px-2"
                 mode={index > 0 ? "secondary" : undefined}
                 key={index}
-                size={isMobile ? "sm" : undefined}
+                size="sm"
                 {...button}
               >
                 <span className="subtitle1 mx-auto">{button.label}</span>
