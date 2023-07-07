db7af1d26ccc2af2221ab4d6c7064413b36a80d2
Max Millington <maxmillington89@gmail.com>
2023-07-07 15:06:22 -0600

    update earnings card to make calls to osmosis chain

--- packages/web/components/cards/estimated-earnings-card.tsx
+++ packages/web/components/cards/estimated-earnings-card.tsx
@@ -1,10 +1,49 @@
-import React from "react";
+import { CoinPretty, Dec } from "@keplr-wallet/unit";
+import { observer } from "mobx-react-lite";
+import React, { FunctionComponent } from "react";
 import { useTranslation } from "react-multi-lang";
+
+import { useStore } from "~/stores";
 
 import { OsmoverseCard } from "./osmoverse-card";
 
-export const EstimatedEarningCard = () => {
+export const EstimatedEarningCard: FunctionComponent<{
+  stakeAmount: CoinPretty;
+}> = observer(({}) => {
   const t = useTranslation();
+  const { queriesStore, chainStore, priceStore } = useStore();
+
+  const osmosisChainId = chainStore.osmosis.chainId;
+
+  const osmo = chainStore.osmosis.stakeCurrency;
+  const amount = new CoinPretty(osmo, 1000000000);
+
+  const cosmosQueries = queriesStore.get(osmosisChainId).cosmos;
+
+  // staking APR, despite the name.
+  const inflation = cosmosQueries.queryInflation.inflation.toDec();
+
+  const calculatedInflationAmountPerYear = amount
+    .toDec()
+    .mul(inflation.quo(new Dec(100)));
+
+  const perDayCalculation = calculatedInflationAmountPerYear.quo(new Dec(365));
+  const perMonthCalculation = calculatedInflationAmountPerYear.quo(new Dec(12));
+
+  const prettifiedDailyAmount = new CoinPretty(
+    osmo,
+    perDayCalculation
+  ).moveDecimalPointRight(osmo.coinDecimals);
+  const prettifiedMonthlyAmount = new CoinPretty(
+    osmo,
+    perMonthCalculation
+  ).moveDecimalPointRight(osmo.coinDecimals);
+
+  const calculatedDailyPrice = priceStore.calculatePrice(prettifiedDailyAmount);
+  const calculatedMonthlyPrice = priceStore.calculatePrice(
+    prettifiedMonthlyAmount
+  );
+
   return (
     <OsmoverseCard containerClasses="bg-opacity-50">
       <div className="flex flex-col gap-2 text-left">
@@ -13,13 +52,13 @@ export const EstimatedEarningCard = () => {
         </span>
         <div className="mt-5 mb-2 flex items-center justify-around">
           <span className="caption text-sm text-osmoverse-200 md:text-xs">
-            $32.80/day
+            {calculatedDailyPrice?.toString()}/day
           </span>
           <span className="caption text-sm text-osmoverse-200 md:text-xs">
-            $500.80/month
+            {calculatedMonthlyPrice?.toString()}/month
           </span>
         </div>
       </div>
     </OsmoverseCard>
   );
-};
+});
