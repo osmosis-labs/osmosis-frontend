15d3e83d57022165f0340c25ce2e38707fc30f94
Roman <roman@osmosis.team>
2023-07-08 04:33:06 -0400

    updates

--- packages/stores/src/__tests_e2e__/stop-osmosisd.sh
+++ packages/stores/src/__tests_e2e__/stop-osmosisd.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# # Get container ID from command line arguments
-# CONTAINER_ID=$1
+# Get container ID from command line arguments
+CONTAINER_ID=$1
 
-# # Remove the local osmosisd data directory containing state amongst other stuff
-# rm -rf ~/.osmosisd-local/
+# Remove the local osmosisd data directory containing state amongst other stuff
+rm -rf ~/.osmosisd-local/
 
-# # Stop and remove the osmosisd container
-# docker stop $CONTAINER_ID
-# docker rm --force $CONTAINER_ID
+# Stop and remove the osmosisd container
+docker stop $CONTAINER_ID
+docker rm --force $CONTAINER_ID
