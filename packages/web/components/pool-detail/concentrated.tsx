cddfe00fc9a13f966b01b40d3e19781190364e18
Jon Ator <jonathanator0@gmail.com>
2023-07-14 19:21:30 -0400

    fix price in header

--- packages/web/components/pool-detail/concentrated.tsx
+++ packages/web/components/pool-detail/concentrated.tsx
@@ -371,16 +371,15 @@ const Chart: FunctionComponent<{
       annotations={[]}
       domain={yRange}
       onPointerHover={setHoverPrice}
-      onPointerOut={
-        config.pool.concentratedLiquidityPoolInfo
-          ? () =>
-              setHoverPrice(
-                Number(config.pool.concentratedLiquidityPoolInfo.currentPrice)
-              )
-          : lastChartData
-          ? () => setHoverPrice(Number(lastChartData.close))
-          : undefined
-      }
+      onPointerOut={() => {
+        if (config?.pool?.concentratedLiquidityPoolInfo) {
+          setHoverPrice(
+            Number(config.pool.concentratedLiquidityPoolInfo.currentPrice)
+          );
+        } else if (lastChartData) {
+          setHoverPrice(Number(lastChartData.close));
+        }
+      }}
     />
   );
 });
