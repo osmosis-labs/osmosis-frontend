a605972dcdaca12bdef177cc36632e61fc650e95
Max Millington <maxmillington89@gmail.com>
2023-07-17 08:16:09 -0600

    remove tally feedback banner

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -36,14 +36,7 @@ export const Announcement:
         "Chain is halted, transactions are temporarily disabled",
       isWarning: true,
     }
-  : {
-      enTextOrLocalizationPath: "tally.description",
-      link: {
-        enTextOrLocalizationKey: "tally.link",
-        url: "http://app.osmosis.zone/pool/1066",
-        isExternal: false,
-      },
-    };
+  : undefined;
 
 // Past localstorage keys:
 // * "feedback_wQ1KR7": "Help us shape the future of Osmosis." Give us feedback -> https://tally.so/r/wQ1KR7
