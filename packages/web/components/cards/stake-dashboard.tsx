6758eab7c8f0d649ce0766518054cb8cb845daed
Michael Millington <>
2023-07-25 10:30:30 -0600

    remove link

--- packages/web/components/cards/stake-dashboard.tsx
+++ packages/web/components/cards/stake-dashboard.tsx
@@ -39,12 +39,7 @@ export const StakeDashboard: React.FC<{
     );
 
     return (
-      <GenericMainCard
-        title={t("stake.dashboard")}
-        titleIcon={icon}
-        titleIconAction="www.google.com"
-        width="45"
-      >
+      <GenericMainCard title={t("stake.dashboard")} titleIcon={icon} width="45">
         <div className="flex w-full flex-row justify-between py-10">
           <StakeBalances
             title={t("stake.stakeBalanceTitle")}
