05cb09dcf9ec40fe41817ae22407afe51b52efb2
Jeremy Parish <jeremyparish69@gmail.com>
2023-07-26 14:50:38 -0600

    add WATR

--- packages/web/config/price.ts
+++ packages/web/config/price.ts
@@ -1892,6 +1892,16 @@ const mainnetPoolPriceRoutes: IntermediateRoute[] = [
     spotPriceDestDenom: "uosmo",
     destCoinId: "pool:uosmo",
   },
+  {
+    alternativeCoinId: "pool:watr",
+    poolId: "1080",
+    spotPriceSourceDenom: DenomHelper.ibcDenom(
+      [{ portId: "transfer", channelId: "channel-169" }],
+      "cw20:juno1m4h8q4p305wgy7vkux0w6e5ylhqll3s6pmadhxkhqtuwd5wlxhxs8xklsw"
+    ),
+    spotPriceDestDenom: "uosmo",
+    destCoinId: "pool:uosmo",
+  },
 ];
 
 const testnetPoolPriceRoutes: IntermediateRoute[] = [];
