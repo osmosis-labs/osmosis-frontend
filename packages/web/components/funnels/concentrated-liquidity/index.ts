0c66b0f7c32c789c25a6467a35fa73e698855210
Jon Ator <jonathanator0@gmail.com>
2023-07-26 12:42:28 +0200

    remove unused code

--- packages/web/components/funnels/concentrated-liquidity/index.ts
+++ packages/web/components/funnels/concentrated-liquidity/index.ts
@@ -1,4 +1,3 @@
 export * from "~/components/funnels/concentrated-liquidity/intro";
 export * from "~/components/funnels/concentrated-liquidity/learn-more";
 export * from "~/components/funnels/concentrated-liquidity/supercharge-pool";
-export * from "~/components/funnels/concentrated-liquidity/use-cfmm-to-cl-migration";
