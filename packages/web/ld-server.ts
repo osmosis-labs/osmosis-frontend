888e373229e228d71e3b3316b9fa70d5c9bb47d3
Matt Upham <mcupham@gmail.com>
2023-08-08 12:54:03 -0700

    Clean up

--- packages/web/ld-server.ts
+++ packages/web/ld-server.ts
@@ -1,24 +1,12 @@
-// import * as LaunchDarkly from "@launchdarkly-node-server-sdk";
-
-// let launchDarklyClient;
-
-// async function initialize() {
-//   const client = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY);
-//   await client.waitForInitialization();
-//   return client;
-// }
-
-// export async function getClient() {
-//   if (launchDarklyClient) return launchDarklyClient;
-//   return (launchDarklyClient = await initialize());
-// }
-
 import * as LaunchDarkly from "@launchdarkly/node-server-sdk";
 
 let launchDarklyClient: LaunchDarkly.LDClient | undefined;
 
 async function initialize(): Promise<LaunchDarkly.LDClient> {
-  const client = LaunchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY as string);
+
+  const LAUNCH_DARKLY_SDK_KEY = process.env.LAUNCH_DARKLY_SDK_KEY as string;
+
+  const client = LaunchDarkly.init(LAUNCH_DARKLY_SDK_KEY);
   await client.waitForInitialization();
   return client;
 }
