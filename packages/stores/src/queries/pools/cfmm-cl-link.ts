a49f1da399234fa3254aaab1854a1d2e81b81a00
Jon Ator <jonathanator0@gmail.com>
2023-08-10 09:29:15 -0400

    fix query

--- packages/stores/src/queries/pools/cfmm-cl-link.ts
+++ packages/stores/src/queries/pools/cfmm-cl-link.ts
@@ -3,9 +3,9 @@ import { ChainGetter, ObservableChainQuery } from "@keplr-wallet/stores";
 import { makeObservable } from "mobx";
 import { computedFn } from "mobx-utils";
 
-import { CfmmToClMigratonRecords } from "./types";
+import { MigrationRecords } from "./types";
 
-export class ObservableQueryCfmmConcentratedPoolLinks extends ObservableChainQuery<CfmmToClMigratonRecords> {
+export class ObservableQueryCfmmConcentratedPoolLinks extends ObservableChainQuery<MigrationRecords> {
   constructor(kvStore: KVStore, chainId: string, chainGetter: ChainGetter) {
     super(
       kvStore,
@@ -18,14 +18,13 @@ export class ObservableQueryCfmmConcentratedPoolLinks extends ObservableChainQue
   }
 
   /** Link to concentrated liquidity pool from the given balancer/cfmm pool ID.
-   *  Returns `undefined` if not loaded, `false`, if nonexistent, or the CL pool ID if it exists.
-   */
+   *  Returns `undefined` if not loaded, `false`, if nonexistent, or the CL pool ID if it exists. */
   readonly getLinkedConcentratedPoolId = computedFn(
     (cfmmPooLId): string | false | undefined => {
       if (!this.response) return;
 
       return (
-        this.response.data.migration_records.find(
+        this.response.data.migration_records.balancer_to_concentrated_pool_links.find(
           (record) => record.balancer_pool_id === cfmmPooLId
         )?.cl_pool_id ?? false
       );
@@ -34,13 +33,13 @@ export class ObservableQueryCfmmConcentratedPoolLinks extends ObservableChainQue
 
   /**
    * Link to CFMM pool from the given CL pool ID.
-   */
+   * Returns `undefined` if not loaded, `false`, if nonexistent, or the CFMM pool ID if it exists. */
   readonly getLinkedCfmmPoolId = computedFn(
     (clPoolId): string | false | undefined => {
       if (!this.response) return;
 
       return (
-        this.response?.data.migration_records.find(
+        this.response?.data.migration_records.balancer_to_concentrated_pool_links.find(
           (record) => record.cl_pool_id === clPoolId
         )?.balancer_pool_id ?? false
       );
--- packages/stores/src/queries/pools/types.ts
+++ packages/stores/src/queries/pools/types.ts
@@ -24,9 +24,11 @@ export type NumPools = {
   num_pools: string;
 };
 
-export type CfmmToClMigratonRecords = {
+export type MigrationRecords = {
   migration_records: {
-    balancer_pool_id: string;
-    cl_pool_id: string;
-  }[];
+    balancer_to_concentrated_pool_links: {
+      balancer_pool_id: string;
+      cl_pool_id: string;
+    }[];
+  };
 };
