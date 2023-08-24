f8d9758bb50686a31f743d3dc03c2f5987c92f50
Matt Upham <mcupham@gmail.com>
2023-08-24 15:39:56 -0700

    Update rounded 3xl

--- packages/web/components/cards/card.tsx
+++ packages/web/components/cards/card.tsx
@@ -8,7 +8,7 @@ export const Card: React.FC<{ children: ReactNode; classes?: string }> = ({
   return (
     <div
       className={classNames(
-        `relative flex h-full flex-col rounded-[24px] bg-osmoverse-800 px-1 py-1`,
+        `relative flex h-full flex-col rounded-3xl bg-osmoverse-800 px-1 py-1`,
         classes
       )}
     >
