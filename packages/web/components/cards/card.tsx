653b25621efe5b6005c71bc0a0b5879077573458
Matt Upham <mcupham@gmail.com>
2023-08-24 15:30:43 -0700

    Update classes

--- packages/web/components/cards/card.tsx
+++ packages/web/components/cards/card.tsx
@@ -9,7 +9,7 @@ export const Card: React.FC<{ children: ReactNode; classes?: string }> = ({
     <div
       className={classNames(
         `relative flex h-full flex-col rounded-[24px] bg-osmoverse-800 px-1 py-1`,
-        classes ? classes : ""
+        classes
       )}
     >
       {children}
