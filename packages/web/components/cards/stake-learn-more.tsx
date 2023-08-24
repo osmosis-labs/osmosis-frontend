24476755d88ed1538efd31da87d43f6977b460ff
Matt Upham <mcupham@gmail.com>
2023-08-24 15:33:51 -0700

    Add translation

--- packages/web/components/cards/stake-learn-more.tsx
+++ packages/web/components/cards/stake-learn-more.tsx
@@ -1,11 +1,12 @@
 import React from "react";
-
+import { useTranslation } from "react-multi-lang";
 import { GenericMainCard } from "~/components/cards/generic-main-card";
 import { Step, Stepper, StepsIndicator } from "~/components/stepper";
 
 export const StakeLearnMore: React.FC<{}> = () => {
+  const t = useTranslation();
   return (
-    <GenericMainCard title={"Get started with staking"}>
+    <GenericMainCard title={t("stake.learnMore.getStarted"}>
       <Stepper
         className="relative flex flex-1 flex-col gap-2 text-center text-osmoverse-100"
         autoplay={{ stopOnHover: true, delayInMs: 4000 }}
