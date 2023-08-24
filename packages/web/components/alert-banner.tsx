3dd7d05d454911ac94c213a69cfceeea23f7c878
Matt Upham <mcupham@gmail.com>
2023-08-24 15:37:49 -0700

    Update alert banner rem

--- packages/web/components/alert-banner.tsx
+++ packages/web/components/alert-banner.tsx
@@ -7,7 +7,7 @@ export const AlertBanner: React.FC<{
   subtitle: string;
 }> = ({ image, title, subtitle }) => {
   return (
-    <div className="z-50 flex flex h-[80px] w-full items-center gap-8 rounded-[24px] bg-gradient-alert py-2 px-10">
+    <div className="z-50 flex flex h-[5rem] w-full items-center gap-8 rounded-[24px] bg-gradient-alert py-2 px-10">
       {image && <Image src={image} alt="moving-on-up" width={80} height={80} />}
       <div className="flex w-full flex-col gap-1 py-2.5">
         <h6 className="font-semibold">{title}</h6>
