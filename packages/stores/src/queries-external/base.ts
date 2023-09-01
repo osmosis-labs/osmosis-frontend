7240a78bb6b85922f6f44f06e41b87d3d6b7ce1c
Michael Millington <>
2023-09-01 14:56:25 -0600

    Setup for testing proxy with basic auth

--- packages/stores/src/queries-external/base.ts
+++ packages/stores/src/queries-external/base.ts
@@ -1,12 +1,26 @@
 import { KVStore } from "@keplr-wallet/common";
 import { ObservableQuery } from "@keplr-wallet/stores";
-import Axios from "axios";
+import Axios, { AxiosRequestConfig } from "axios";
 export class ObservableQueryExternalBase<
   T = unknown,
   E = unknown
 > extends ObservableQuery<T, E> {
   constructor(kvStore: KVStore, baseURL: string, urlPath: string) {
-    const instance = Axios.create({ baseURL });
+    const username = process.env.NEXT_PUBLIC_PROXY_API_USERNAME;
+    const password = process.env.NEXT_PUBLIC_PROXY_API_PASSWORD;
+
+    const axiosConfig: AxiosRequestConfig = {
+      baseURL,
+    };
+
+    if (username && password) {
+      axiosConfig.auth = {
+        username,
+        password,
+      };
+    }
+
+    const instance = Axios.create(axiosConfig);
 
     super(kvStore, instance, urlPath);
   }
--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -12,4 +12,4 @@ export * from "./token-pair-historical-chart";
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
   "https://api-osmosis.imperator.co";
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
-  "https://api-osmosis-chain.imperator.co";
+  "https://api-proxy.dev-osmosis.zone/";
