12f5eac069336cdf7c149a652ec08b532c8b0604
Michael Millington <>
2023-09-05 14:05:18 -0600

    update endpoints

--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -10,6 +10,6 @@ export * from "./token-historical-chart";
 export * from "./token-pair-historical-chart";
 
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
-  "https://api-osmosis.imperator.co";
+  "https://proxy-api.dev-osmosis.zone/";
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
-  "https://api-proxy.dev-osmosis.zone/";
+  "https://proxy-indexer.dev-osmosis.zone/";
