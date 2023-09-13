cc7a8dbf8aaa69f0cc205598ecad36a9eab527ce
Jose Felix <jfelx@outlook.com>
2023-09-12 22:26:05 -0400

    fix: remove useRoutablePool

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -1,14 +1,17 @@
+import { Dec } from "@keplr-wallet/unit";
 import { observer } from "mobx-react-lite";
 import type { GetStaticProps, InferGetServerSidePropsType } from "next";
+import { useEffect, useMemo, useRef } from "react";
 
 import { Ad, AdCMS } from "~/components/ad-banner/ad-banner-types";
 import { ProgressiveSvgImage } from "~/components/progressive-svg-image";
 import { SwapTool } from "~/components/swap-tool";
-import { EventName } from "~/config";
+import { EventName, IS_TESTNET } from "~/config";
 import adCMSData from "~/config/ads-banner.json";
-import { useAmplitudeAnalytics } from "~/hooks";
-import { useRoutablePools } from "~/hooks/data/use-routable-pools";
+import { useAmplitudeAnalytics, useFeatureFlags } from "~/hooks";
 import { useWalletSelect } from "~/hooks/wallet-select";
+import { useStore } from "~/stores";
+import { UnverifiedAssetsState } from "~/stores/user-settings";
 
 interface HomeProps {
   ads: Ad[];
@@ -37,9 +40,66 @@ export const getStaticProps: GetStaticProps<HomeProps> = async () => {
 };
 
 const Home = ({ ads }: InferGetServerSidePropsType<typeof getStaticProps>) => {
+  const { chainStore, queriesStore, priceStore, userSettings } = useStore();
+  const { chainId } = chainStore.osmosis;
+
   const { isLoading: isWalletLoading } = useWalletSelect();
 
-  const routablePools = useRoutablePools();
+  const queries = queriesStore.get(chainId);
+  const queryPools = queries.osmosis!.queryPools;
+  const showUnverified =
+    userSettings.getUserSettingById<UnverifiedAssetsState>("unverified-assets")
+      ?.state?.showUnverifiedAssets;
+
+  const allPools = queryPools.getAllPools();
+
+  const flags = useFeatureFlags();
+
+  // Pools should be memoized before passing to trade in config
+  const pools = useMemo(
+    () =>
+      allPools
+        .filter((pool) => {
+          // include all pools on testnet env
+          if (IS_TESTNET) return true;
+
+          if (!IS_TESTNET && pool.id === "895") return false;
+
+          // filter concentrated pools if feature flag is not enabled
+          if (pool.type === "concentrated" && !flags.concentratedLiquidity)
+            return false;
+
+          if (pool.type === "concentrated" && pool.id !== "1066")
+            // TODO: remove, temporary due to tick query bug with v17 with new CL pools
+            return false;
+
+          // some min TVL for balancer pools
+          return pool
+            .computeTotalValueLocked(priceStore)
+            .toDec()
+            .gte(
+              new Dec(
+                showUnverified || pool.type === "concentrated" ? 1_000 : 10_000
+              )
+            );
+        })
+        .sort((a, b) => {
+          // sort by TVL to find routes amongst most valuable pools
+          const aTVL = a.computeTotalValueLocked(priceStore);
+          const bTVL = b.computeTotalValueLocked(priceStore);
+
+          return Number(bTVL.sub(aTVL).toDec().toString());
+        }),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [allPools, priceStore.response, flags.concentratedLiquidity]
+  );
+
+  const requestedRemaining = useRef(false);
+  useEffect(() => {
+    if (requestedRemaining.current) return;
+    queryPools.fetchRemainingPools();
+    requestedRemaining.current = true;
+  }, [queryPools]);
 
   useAmplitudeAnalytics({
     onLoadEvent: [EventName.Swap.pageViewed, { isOnHome: true }],
@@ -70,8 +130,8 @@ const Home = ({ ads }: InferGetServerSidePropsType<typeof getStaticProps>) => {
       <div className="my-auto flex h-auto w-full items-center">
         <div className="ml-auto mr-[15%] flex w-[27rem] flex-col gap-4 lg:mx-auto md:mt-mobile-header">
           <SwapTool
-            memoedPools={routablePools ?? []}
-            isDataLoading={!Boolean(routablePools) || isWalletLoading}
+            memoedPools={pools ?? []}
+            isDataLoading={!Boolean(pools) || isWalletLoading}
             ads={ads}
           />
         </div>
