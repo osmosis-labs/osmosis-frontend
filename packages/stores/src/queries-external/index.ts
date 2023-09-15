9a27b27b19c781023001f7d29ad69d3438b74fd1
Matt Upham <mcupham@gmail.com>
2023-09-15 14:01:33 -0700

    Add URLs hardcoded

--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -10,6 +10,8 @@ export * from "./token-historical-chart";
 export * from "./token-pair-historical-chart";
 
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
-  "https://proxy-api.dev-osmosis.zone/";
+  "https://proxy-api.osmosis.zone/";
+// "https://proxy-api.dev-osmosis.zone/";
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
-  "https://proxy-indexer.dev-osmosis.zone/";
+  "https://proxy-indexer.osmosis.zone/";
+// "https://proxy-indexer.dev-osmosis.zone/";
