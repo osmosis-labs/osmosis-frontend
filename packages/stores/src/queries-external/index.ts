226c10ca191eb1f3f0524f8d3825c2a7425879b9
Matt Upham <mcupham@gmail.com>
2023-09-19 10:40:38 -0700

    https

--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -10,8 +10,7 @@ export * from "./token-historical-chart";
 export * from "./token-pair-historical-chart";
 
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
-  "http://proxy-api.osmosis.zone/";
-// "https://proxy-api.osmosis.zone/";
+  "https://proxy-api.osmosis.zone/";
 // "https://proxy-api.dev-osmosis.zone/";
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
   "https://proxy-indexer.osmosis.zone/";
