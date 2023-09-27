d40b9ed6dde9a1ab6cd32aa6d3bab49644b3e734
Jon Ator <jonathanator0@gmail.com>
2023-09-27 19:47:49 -0400

    add APR to add liquidity flow

--- packages/stores/src/queries-external/concentrated-liquidity/range-aprs.ts
+++ packages/stores/src/queries-external/concentrated-liquidity/range-aprs.ts
@@ -1,5 +1,5 @@
 import { KVStore } from "@keplr-wallet/common";
-import { RatePretty } from "@keplr-wallet/unit";
+import { Int, RatePretty } from "@keplr-wallet/unit";
 import { HasMapStore } from "@osmosis-labs/keplr-stores";
 import { maxTick, minTick } from "@osmosis-labs/math";
 import { computed, makeObservable } from "mobx";
@@ -7,11 +7,12 @@ import { computed, makeObservable } from "mobx";
 import { IMPERATOR_INDEXER_DEFAULT_BASEURL } from "..";
 import { ObservableQueryExternalBase } from "../base";
 
+type Response = {
+  APR: number;
+};
+
 /** Queries Imperator for extrapolated APR of a given position's tick range. */
-export class ObservableQueryPositionRangeApr extends ObservableQueryExternalBase<{
-  spreadFactorApr: string;
-  incentivesApr: string;
-}> {
+export class ObservableQueryPriceRangeApr extends ObservableQueryExternalBase<Response> {
   constructor(
     kvStore: KVStore,
     baseURL: string,
@@ -19,7 +20,11 @@ export class ObservableQueryPositionRangeApr extends ObservableQueryExternalBase
     protected readonly lowerTickIndex: number,
     protected readonly upperTickIndex: number
   ) {
-    super(kvStore, baseURL, `/lp/v1/rewards/estimation/`);
+    super(
+      kvStore,
+      baseURL,
+      `/cl/v1/apr/rewards/${poolId}?lower_tick=${lowerTickIndex}&upper_tick=${upperTickIndex}`
+    );
 
     makeObservable(this);
   }
@@ -28,20 +33,18 @@ export class ObservableQueryPositionRangeApr extends ObservableQueryExternalBase
   get apr(): RatePretty | undefined {
     if (!this.response) return;
 
-    return new RatePretty(this.response.data.spreadFactorApr).add(
-      new RatePretty(this.response.data.incentivesApr)
-    );
+    return new RatePretty(this.response.data.APR / 100);
   }
 }
 
-export class ObservableQueryPositionsRangeApr extends HasMapStore<ObservableQueryPositionRangeApr> {
+export class ObservableQueryPriceRangeAprs extends HasMapStore<ObservableQueryPriceRangeApr> {
   constructor(
     kvStore: KVStore,
     indexerBaseUrl = IMPERATOR_INDEXER_DEFAULT_BASEURL
   ) {
     super((key) => {
       const { poolId, lowerTickIndex, upperTickIndex } = parseKey(key);
-      return new ObservableQueryPositionRangeApr(
+      return new ObservableQueryPriceRangeApr(
         kvStore,
         indexerBaseUrl,
         poolId,
@@ -51,21 +54,14 @@ export class ObservableQueryPositionsRangeApr extends HasMapStore<ObservableQuer
     });
   }
 
-  get(poolId: string, lowerTickIndex?: number, upperTickIndex?: number) {
-    const key = makeKey(
-      poolId,
-      lowerTickIndex ?? Number(minTick.toString()),
-      upperTickIndex ?? Number(maxTick.toString())
-    );
-    return super.get(key) as ObservableQueryPositionRangeApr;
+  /** Defaults to min and max tick if not provided. */
+  get(poolId: string, lowerTickIndex = minTick, upperTickIndex = maxTick) {
+    const key = makeKey(poolId, lowerTickIndex, upperTickIndex);
+    return super.get(key) as ObservableQueryPriceRangeApr;
   }
 }
 
-function makeKey(
-  poolId: string,
-  lowerTickIndex: number,
-  upperTickIndex: number
-) {
+function makeKey(poolId: string, lowerTickIndex: Int, upperTickIndex: Int) {
   return `${poolId}:${lowerTickIndex}:${upperTickIndex}`;
 }
 
