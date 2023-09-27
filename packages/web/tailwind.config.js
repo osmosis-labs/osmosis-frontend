ed8e50f579d23239c76f5ed6ceaafd6a49a1d73f
fabry-js <vivaldifabrizio10@gmail.com>
2023-09-27 12:00:45 +0200

    feat: :wrench: pre-set tailwind vars

--- packages/web/tailwind.config.js
+++ packages/web/tailwind.config.js
@@ -161,6 +161,8 @@ module.exports = {
         "linear-gradient(to bottom,rgba(0, 0, 0, 0),rgba(0, 0, 0, 0.8));",
       "gradient-dummy-notifications":
         "linear-gradient(0deg, #282750 0%, rgba(40, 39, 80, 0.00) 100%)",
+      "gradient-token-details-info":
+        "linear-gradient(0deg, #140F34 6.87%, rgba(20, 15, 52, 0.00) 100%);",
     },
     screens: {
       "3xl": { max: "1792px" },
@@ -223,6 +225,7 @@ module.exports = {
         sidebar: "14.58rem",
         "mobile-header": "6rem",
         0.25: "1px",
+        4.5: "18px",
       },
       maxWidth: {
         container: "70rem",
