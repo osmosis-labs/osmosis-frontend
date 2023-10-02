8466e9a02d37dfc09c337ffbbaca238c07b2239b
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2023-10-03 00:16:34 +0200

    refactor: change props format to match cms

--- packages/web/components/token-details/token-details.tsx
+++ packages/web/components/token-details/token-details.tsx
@@ -7,91 +7,119 @@ import LinkIconButton from "~/components/buttons/link-icon-button";
 
 const TEXT_CHAR_LIMIT = 450;
 
-interface TokenDetailsProps {
-  denom: string;
+export interface TokenDetailsProps {
+  localization?: string;
+  name?: string;
+  coinMinimalDenom?: string;
+  description?: string;
+  coingeckoURL?: string;
+  twitterURL?: string;
+  websiteURL?: string;
 }
 
-function TokenDetails({ denom }: TokenDetailsProps) {
+function TokenDetails({
+  name,
+  description,
+  coingeckoURL,
+  twitterURL,
+  websiteURL,
+}: TokenDetailsProps) {
   const [isExpanded, setIsExpanded] = useState(false);
   const t = useTranslation();
-  const text = `Osmosis is a decentralized exchange (DEX) that uses the Cosmos SDK for optimized functionality with other blockchains. Its automated market makers set it apart, allowing enhanced market responsiveness for liquidity providers.\n\nOsmosis is a multi-chain DEX specifically built to function with other blockchains in the Cosmos ecosystem. Communication withthese other blockchains is achieved via its Inter-Blockchain Communication protocol (IBC). Because Osmosis is linked to other Tendermint blockchains, providing smooth, fast trades.`;
 
-  const isExpandable = useMemo(() => text.length > TEXT_CHAR_LIMIT, [text]);
+  const isExpandable = useMemo(
+    () => description && description.length > TEXT_CHAR_LIMIT,
+    [description]
+  );
 
   const expandedText = useMemo(() => {
     if (isExpandable && !isExpanded) {
-      return text.substring(0, TEXT_CHAR_LIMIT);
+      return description ? description.substring(0, TEXT_CHAR_LIMIT) : "";
     }
 
-    return text;
-  }, [isExpandable, isExpanded, text]);
+    return description;
+  }, [isExpandable, isExpanded, description]);
 
   return (
     <div className="flex flex-col items-start gap-3 self-stretch rounded-5xl border border-osmoverse-800 bg-osmoverse-900 p-10 xl:gap-6 md:p-6 1.5xs:gap-6">
       <TokenStats />
-      <div className="flex flex-col items-start self-stretch">
-        <div className="flex flex-col items-start gap-4.5 self-stretch 1.5xs:gap-6">
-          <div className="flex items-center gap-8 1.5xs:flex-col 1.5xs:gap-4">
-            <h6 className="text-lg font-h6 leading-6 text-osmoverse-100">
-              {t("tokenInfos.aboutDenom", { denom })}
-            </h6>
-            <div className="flex items-center gap-2">
-              <LinkIconButton
-                href="/"
-                mode="icon-social"
-                size="md-icon-social"
-                aria-label={t("tokenInfos.ariaViewOn", { name: "X" })}
-                icon={<Icon className="h-4 w-4 text-osmoverse-400" id="X" />}
-              />
-              <LinkIconButton
-                href="/"
-                mode="icon-social"
-                size="md-icon-social"
-                aria-label={t("tokenInfos.ariaView", { name: "website" })}
-                icon={<Icon className="h-6 w-6 text-osmoverse-400" id="web" />}
-              />
-              <LinkIconButton
-                href="/"
-                mode="icon-social"
-                size="md-icon-social"
-                aria-label={t("tokenInfos.ariaViewOn", { name: "CoinGecko" })}
-                icon={
-                  <Icon
-                    className="h-10.5 w-10.5 text-osmoverse-300"
-                    id="coingecko"
+      {name && description && (
+        <div className="flex flex-col items-start self-stretch">
+          <div className="flex flex-col items-start gap-4.5 self-stretch 1.5xs:gap-6">
+            <div className="flex items-center gap-8 1.5xs:flex-col 1.5xs:gap-4">
+              <h6 className="text-lg font-h6 leading-6 text-osmoverse-100">
+                {t("tokenInfos.aboutDenom", { name })}
+              </h6>
+              <div className="flex items-center gap-2">
+                {twitterURL && (
+                  <LinkIconButton
+                    href={twitterURL}
+                    mode="icon-social"
+                    size="md-icon-social"
+                    aria-label={t("tokenInfos.ariaViewOn", { name: "X" })}
+                    icon={
+                      <Icon className="h-4 w-4 text-osmoverse-400" id="X" />
+                    }
                   />
-                }
-              />
+                )}
+                {websiteURL && (
+                  <LinkIconButton
+                    href={websiteURL}
+                    mode="icon-social"
+                    size="md-icon-social"
+                    aria-label={t("tokenInfos.ariaView", { name: "website" })}
+                    icon={
+                      <Icon className="h-6 w-6 text-osmoverse-400" id="web" />
+                    }
+                  />
+                )}
+                {coingeckoURL && (
+                  <LinkIconButton
+                    href={coingeckoURL}
+                    mode="icon-social"
+                    size="md-icon-social"
+                    aria-label={t("tokenInfos.ariaViewOn", {
+                      name: "CoinGecko",
+                    })}
+                    icon={
+                      <Icon
+                        className="h-10.5 w-10.5 text-osmoverse-300"
+                        id="coingecko"
+                      />
+                    }
+                  />
+                )}
+              </div>
+            </div>
+            <div
+              className={`${
+                !isExpanded && isExpandable && "tokendetailshadow"
+              } relative self-stretch`}
+            >
+              <p className="breakspaces font-base self-stretch font-subtitle1 text-osmoverse-200 transition-all">
+                {expandedText}
+              </p>
+              {isExpandable && (
+                <button
+                  className={`${
+                    !isExpanded && "bottom-0"
+                  } absolute z-10 flex items-center gap-1 self-stretch`}
+                  onClick={() => setIsExpanded((v) => !v)}
+                >
+                  <p className="font-base leading-6 text-wosmongton-300">
+                    {isExpanded
+                      ? t("tokenInfos.collapse")
+                      : t("components.show.more")}
+                  </p>
+                  <div className={`${isExpanded && "rotate-180"}`}>
+                    <CaretDown className="fill-wosmongton-300" />
+                  </div>
+                </button>
+              )}
             </div>
           </div>
-          <div
-            className={`${
-              !isExpanded && isExpandable && "tokendetailshadow"
-            } relative self-stretch`}
-          >
-            <p className="breakspaces font-base self-stretch font-subtitle1 text-osmoverse-200 transition-all">
-              {expandedText}
-            </p>
-            {isExpandable && (
-              <button
-                className={`${
-                  !isExpanded && "bottom-0"
-                } absolute z-10 flex items-center gap-1 self-stretch`}
-                onClick={() => setIsExpanded((v) => !v)}
-              >
-                <p className="font-base leading-6 text-wosmongton-300">
-                  {isExpanded
-                    ? t("tokenInfos.collapse")
-                    : t("components.show.more")}
-                </p>
-                <div className={`${isExpanded && "rotate-180"}`}>
-                  <CaretDown className="fill-wosmongton-300" />
-                </div>
-              </button>
-            )}
-          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
