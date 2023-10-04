c1f0b56befc36149c4e340668b16f0ff5ef2db95
Matt Upham <mcupham@gmail.com>
2023-10-04 15:00:12 +0300

    Test creds

--- packages/stores/src/queries-external/base.ts
+++ packages/stores/src/queries-external/base.ts
@@ -1,34 +1,33 @@
 import { KVStore } from "@keplr-wallet/common";
 import { ObservableQuery } from "@osmosis-labs/keplr-stores";
-import Axios from "axios";
+import Axios, { AxiosRequestConfig } from "axios";
 export class ObservableQueryExternalBase<
   T = unknown,
   E = unknown
 > extends ObservableQuery<T, E> {
   constructor(kvStore: KVStore, baseURL: string, urlPath: string) {
-    const instance = Axios.create({ baseURL });
-    // const username = process.env.PROXY_API_USERNAME;
-    // const password = process.env.PROXY_API_PASSWORD;
+    // const instance = Axios.create({ baseURL });
+    const username = process.env.PROXY_API_USERNAME;
+    const password = process.env.PROXY_API_PASSWORD;
     // test credentials
     // const username = "osmosisfe";
     // const password = "NDJLU0RybmUzazIsdflkjwerWEFn";
 
-    // const axiosConfig: AxiosRequestConfig = {
-    //   headers: {
-    //     Authorization:
-    //       "Basic b3Ntb3Npc2ZlOk5ESkxVMFJ5Ym1VemF6SXNkZmxrandlcldFRm4=",
-    //   },
-    //   baseURL,
-    // };
+    console.log("username: ", username);
+    console.log("password: ", password);
 
-    // if (username && password) {
-    //   axiosConfig.auth = {
-    //     username,
-    //     password,
-    //   };
-    // }
+    const axiosConfig: AxiosRequestConfig = {
+      baseURL,
+    };
 
-    // const instance = Axios.create(axiosConfig);
+    if (username && password) {
+      axiosConfig.auth = {
+        username,
+        password,
+      };
+    }
+
+    const instance = Axios.create(axiosConfig);
 
     super(kvStore, instance, urlPath);
   }
