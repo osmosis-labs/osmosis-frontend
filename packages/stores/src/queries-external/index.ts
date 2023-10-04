27e24f2e6600370bfd5cb9464242b42e56198a4c
Matt Upham <mcupham@gmail.com>
2023-10-04 16:30:45 +0300

    Add /

--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -11,11 +11,11 @@ export * from "./token-historical-chart";
 export * from "./token-pair-historical-chart";
 
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
-  "https://proxyapi.osmosis-labs.workers.dev";
+  "https://proxyapi.osmosis-labs.workers.dev/";
 // "https://proxy-api.osmosis.zone/";
 // "https://proxy-api.dev-osmosis.zone/";
 
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
-  "https://proxy-indexer.osmosis.zone";
+  "https://proxy-indexer.osmosis.zone/";
 // "https://proxy-indexer.osmosis.zone/";
 // "https://proxy-indexer.dev-osmosis.zone/";
