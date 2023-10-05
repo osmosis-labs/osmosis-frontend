e1be51dcd3bfa2d7267aa07ebadef5c7b73d1697
Enrico Barbieri <enrico.barbieri.1997@gmail.com>
2023-10-06 01:49:36 +0200

    refactor UI to use getServerSideProps for tweets

--- packages/web/components/twitter-section/twitter-section.tsx
+++ packages/web/components/twitter-section/twitter-section.tsx
@@ -1,12 +1,17 @@
 import Link from "next/link";
-import React from "react";
+import React, { FunctionComponent } from "react";
 import { useTranslation } from "react-multi-lang";
 
-function TwitterSection() {
-  const t = useTranslation();
-  const tweetContent = `Get ready for Osmocon 2023!\n\nJoin us in Paris for a jam-packed day, connect with some of the greatest industry innovators, and explore the future of Cosmos.\n\nDon't miss out!\n\nGet your tickets now:`;
+import { RichTweet } from "~/services/twitter";
 
-  return (
+interface TwitterSectionProps {
+  tweets: RichTweet[];
+}
+
+const TwitterSection: FunctionComponent<TwitterSectionProps> = ({ tweets }) => {
+  const t = useTranslation();
+
+  return tweets.length ? (
     <section className="flex flex-1 flex-col items-start gap-6 self-stretch rounded-5xl border border-osmoverse-800 bg-osmoverse-900 py-10 px-8 md:py-6 md:px-4">
       <header>
         <h6 className="text-lg font-h6 leading-6">
@@ -14,15 +19,16 @@ function TwitterSection() {
         </h6>
       </header>
       <ul className="flex flex-col items-start gap-4 self-stretch">
-        <Tweet tweetContent={tweetContent} />
-        <Spacer />
-        <Tweet tweetContent={tweetContent} />
-        <Spacer />
-        <Tweet tweetContent={tweetContent} />
+        {tweets.map((tweet, index) => (
+          <React.Fragment key={index}>
+            {index > 0 && <Spacer />}
+            <Tweet {...tweet} />
+          </React.Fragment>
+        ))}
       </ul>
     </section>
-  );
-}
+  ) : null;
+};
 
 export default TwitterSection;
 
@@ -30,45 +36,60 @@ function Spacer() {
   return <div className="self-stretch border border-osmoverse-700" />;
 }
 
-interface TweetProps {
-  tweetContent: string;
-}
-
-function Tweet({ tweetContent }: TweetProps) {
+const Tweet: FunctionComponent<RichTweet> = ({
+  id,
+  text,
+  createdAt,
+  user,
+  previewImage,
+}) => {
   return (
     <li className="flex flex-col items-start gap-4 self-stretch py-3">
       <div className="flex-start flex gap-4 self-stretch 1.5xs:flex-col">
-        <div className="flex h-13 w-13 items-center gap-3 rounded-full bg-white-high 1.5xs:self-center" />
+        <div className="flex h-13 w-13 items-center gap-3 overflow-hidden rounded-full bg-white-high 1.5xs:self-center">
+          <img className="h-full w-full" src={user.profilePictureUrl} alt="" />
+        </div>
         <div className="flex flex-[1_0_0] flex-col items-start gap-3 1.5xs:gap-y-6">
           <div className="flex items-center justify-between self-stretch 1.5xs:flex-col 1.5xs:gap-y-2">
             <div className="flex items-center gap-2">
               <p className="text-base font-subtitle1 leading-6 text-osmoverse-100">
-                Osmosis 🧪
+                {user.name}
               </p>
-              <Link href={"https://x.com/osmosiszone"} passHref>
+              <Link
+                href={user.url ?? `https://x.com/${user.username}`}
+                passHref
+              >
                 <a
                   target="_blank"
                   className="text-sm font-body2 font-medium leading-5 text-osmoverse-300 hover:underline"
                 >
-                  @osmosiszone
+                  @{user.username}
                 </a>
               </Link>
             </div>
             <time className="text-sm font-body2 font-medium leading-5 text-osmoverse-300">
-              July 15th
+              {new Date(createdAt).toDateString()}
             </time>
           </div>
-          <Link href={"https://x.com/osmosiszone"} passHref>
+          <Link href={`https://x.com/${user.username}/status/${id}`} passHref>
             <a
               target="_blank"
               className="breakspaces self-stretch font-body2 font-medium leading-5 text-osmoverse-300"
             >
-              {tweetContent}
-              <div className="mt-4 h-[258px] self-stretch rounded-3xl bg-wosmongton-300" />
+              {text}
+              {previewImage && (
+                <div className="mt-4 max-h-[258px] self-stretch rounded-3xl">
+                  <img
+                    className="max-h-[258px] w-full object-contain object-left"
+                    src={previewImage}
+                    alt=""
+                  />
+                </div>
+              )}
             </a>
           </Link>
         </div>
       </div>
     </li>
   );
-}
+};
