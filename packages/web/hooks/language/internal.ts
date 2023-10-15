1c1690cd0c6d90049fb981d5a609e9f7f1052e70
Davide Segullo <davidesegullo@gmail.com>
2023-10-15 18:15:16 +0200

    fix: :bug: fix translation fallback

--- packages/web/hooks/language/internal.ts
+++ packages/web/hooks/language/internal.ts
@@ -11,25 +11,29 @@ export const internalTranslate: MultiLanguageInternalT = (
   const keys = path.split(".");
   let current: any = translations[language];
 
-  for (const key of keys) {
-    if (current.hasOwnProperty(key)) {
-      current = current[key];
-    } else {
-      current = undefined;
+  try {
+    for (const key of keys) {
+      if (current.hasOwnProperty(key)) {
+        current = current[key];
+      } else {
+        current = undefined;
+      }
     }
-  }
 
-  if (!current) {
-    console.warn(`I couldn't find a translation with the key: ${path}`, {
-      cause: "MultiLanguageProvider: function 't'",
-    });
+    if (!current) {
+      console.warn(`I couldn't find a translation with the key: ${path}`, {
+        cause: "MultiLanguageProvider: function 't'",
+      });
 
+      return path;
+    }
+
+    if (args && typeof current === "string") {
+      current = current.replace(REPLACE_REGEX, (_, match) => args[match]);
+    }
+
+    return current ?? path;
+  } catch {
     return path;
   }
-
-  if (args && typeof current === "string") {
-    current = current.replace(REPLACE_REGEX, (_, match) => args[match]);
-  }
-
-  return current;
 };
