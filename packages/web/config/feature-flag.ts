f6391507ea46c6b5f0f0788729e562558004bab3
Jon Ator <jonathanator0@gmail.com>
2023-10-19 12:07:20 -0400

    lint fix

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -36,8 +36,7 @@ export const Announcement:
         "Chain is halted, transactions are temporarily disabled",
       isWarning: true,
     }
-    : undefined;
-
+  : undefined;
 
 // Past localstorage keys:
 // * "feedback_wQ1KR7": "Help us shape the future of Osmosis." Give us feedback -> https://tally.so/r/wQ1KR7
