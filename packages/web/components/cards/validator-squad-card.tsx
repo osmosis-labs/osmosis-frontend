aa9459f32fcfd107c2891050818c45a1595cc913
Matt Upham <mcupham@gmail.com>
2023-10-23 13:00:23 -0700

    Remove second edit button

--- packages/web/components/cards/validator-squad-card.tsx
+++ packages/web/components/cards/validator-squad-card.tsx
@@ -133,13 +133,14 @@ export const ValidatorSquadCard: React.FC<{
             {t("stake.validatorHeader")}
           </span>
         </div>
-        <OsmoverseCard>
+        <OsmoverseCard containerClasses="!rounded-[28px]">
           <div className="flex-column flex items-center justify-between">
             {validatorBlock}
             <div className="flex items-center">
               <Button
                 mode="bullish-special"
                 size="normal"
+                className="rounded-[19px]"
                 onClick={() => {
                   setShowValidatorModal(true);
                 }}
