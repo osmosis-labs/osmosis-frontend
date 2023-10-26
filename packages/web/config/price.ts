a9d564e23566d076f77a11677fca0b3de507accd
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-10-26 13:39:06 -0600

    bug: Fix IBCX Price (#2311)
    * Update price.ts
    
    * Update price.ts
    
    * Update source-chain-infos.ts

--- packages/web/config/price.ts
+++ packages/web/config/price.ts
@@ -1736,11 +1736,14 @@ const mainnetPoolPriceRoutes: IntermediateRoute[] = [
   },
   {
     alternativeCoinId: "pool:ibcx",
-    poolId: "1022",
+    poolId: "1042",
     spotPriceSourceDenom:
       "factory/osmo14klwqgkmackvx2tqa0trtg69dmy0nrg4ntq4gjgw2za4734r5seqjqm4gm/uibcx",
-    spotPriceDestDenom: "uosmo",
-    destCoinId: "pool:uosmo",
+    spotPriceDestDenom: DenomHelper.ibcDenom(
+      [{ portId: "transfer", channelId: "channel-169" }],
+      "cw20:juno1rws84uz7969aaa7pej303udhlkt3j9ca0l3egpcae98jwak9quzq8szn2l"
+    ),
+    destCoinId: "pool:phmn",
   },
   {
     alternativeCoinId: "pool:cbeth-wei",
