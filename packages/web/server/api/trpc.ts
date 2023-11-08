d377eccddb82521e9b4e58f56caf9dc2dc21b459
Jose Felix <jfelx@outlook.com>
2023-11-08 17:26:21 -0400

    improvement: docs

--- packages/web/server/api/trpc.ts
+++ packages/web/server/api/trpc.ts
@@ -2,11 +2,7 @@
  * YOU PROBABLY DON'T NEED TO EDIT THIS FILE, UNLESS:
  * 1. You want to modify request context (see Part 1).
  * 2. You want to create a new middleware or type of procedure (see Part 3).
- *
- * TL;DR - This is where all the tRPC server stuff is created and plugged in. The pieces you will
- * need to use are documented accordingly near the end.
  */
-
 import { initTRPC } from "@trpc/server";
 import { FetchCreateContextFnOptions } from "@trpc/server/adapters/fetch";
 import { type CreateNextContextOptions } from "@trpc/server/adapters/next";
@@ -18,16 +14,15 @@ import { ZodError } from "zod";
  *
  * This section defines the "contexts" that are available in the backend API.
  *
- * These allow you to access things when processing a request, like the database, the session, etc.
+ * These allow access to resources when processing a request, like the database, the session, etc.
  */
-
 type CreateContextOptions = Record<string, never>;
 
 /**
- * This helper generates the "internals" for a tRPC context. If you need to use it, you can export
+ * This helper generates the "internals" for a tRPC context. If we need to use it, we can export
  * it from here.
  *
- * Examples of things you may need it for:
+ * Examples of things we may need it for:
  * - testing, so we don't have to mock Next.js' req/res
  * - tRPC's `createSSGHelpers`, where we don't have req/res
  *
@@ -38,8 +33,8 @@ const createInnerTRPCContext = (_opts: CreateContextOptions) => {
 };
 
 /**
- * This is the actual context you will use in your router. It will be used to process every request
- * that goes through your tRPC endpoint.
+ * This is the actual context we'll use in our router. It will be used to process every request
+ * that goes through our tRPC endpoint.
  *
  * @see https://trpc.io/docs/context
  */
@@ -54,7 +49,7 @@ export const createEdgeTRPCContext = (_opts: FetchCreateContextFnOptions) => {
  * 2. INITIALIZATION
  *
  * This is where the tRPC API is initialized, connecting the context and transformer. We also parse
- * ZodErrors so that you get typesafety on the frontend if your procedure fails due to validation
+ * ZodErrors to get typesafety on the frontend if our procedure fails due to validation
  * errors on the backend.
  */
 
@@ -75,12 +70,12 @@ const t = initTRPC.context<typeof createTRPCContext>().create({
 /**
  * 3. ROUTER & PROCEDURE (THE IMPORTANT BIT)
  *
- * These are the pieces you use to build your tRPC API. You should import these a lot in the
+ * These are the pieces to use to build our tRPC API. We should import these a lot in the
  * "/src/server/api/routers" directory.
  */
 
 /**
- * This is how you create new routers and sub-routers in your tRPC API.
+ * This is how we create new routers and sub-routers in our tRPC API.
  *
  * @see https://trpc.io/docs/router
  */
@@ -89,8 +84,8 @@ export const createTRPCRouter = t.router;
 /**
  * Public (unauthenticated) procedure
  *
- * This is the base piece you use to build new queries and mutations on your tRPC API. It does not
- * guarantee that a user querying is authorized, but you can still access user session data if they
+ * This is the base piece we use to build new queries and mutations on our tRPC API. It does not
+ * guarantee that a user querying is authorized, but we can still access user session data if they
  * are logged in.
  */
 export const publicProcedure = t.procedure;
