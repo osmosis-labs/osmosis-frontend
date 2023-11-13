e42a368931d70f0bb62af0600ef40458b192647a
Jon Ator <jonathanator0@gmail.com>
2023-11-13 14:32:58 -0500

    add sidecar query server

---
+++ packages/web/integrations/sidecar/router.ts
@@ -0,0 +1,60 @@
+import { Dec, Int } from "@keplr-wallet/unit";
+import {
+  SplitTokenInQuote,
+  Token,
+  TokenOutGivenInRouter,
+} from "@osmosis-labs/pools";
+import { makeIBCMinimalDenom } from "@osmosis-labs/stores";
+
+import { IBCAssetInfos } from "~/config";
+import { apiClient } from "~/utils/api-client";
+
+import { SidecarQuoteResponse } from "./types";
+
+/** Use all IBC denoms from config. */
+const ibcDenoms = IBCAssetInfos.map(({ sourceChannelId, coinMinimalDenom }) =>
+  makeIBCMinimalDenom(sourceChannelId, coinMinimalDenom)
+);
+
+/** Use this as a client for generating quotes from a sidecar query server. */
+export class OsmosisSidecarRemoteRouter implements TokenOutGivenInRouter {
+  protected readonly baseUrl: URL;
+
+  constructor(protected readonly sidecarBaseUrl: string) {
+    this.baseUrl = new URL(sidecarBaseUrl);
+  }
+
+  async getRoutableCurrencyDenoms(): Promise<string[]> {
+    return ibcDenoms;
+  }
+
+  async routeByTokenIn(
+    tokenIn: Token,
+    tokenOutDenom: string
+  ): Promise<SplitTokenInQuote> {
+    const queryUrl = new URL("quote", this.baseUrl.toString());
+    queryUrl.searchParams.append(
+      "tokenIn",
+      `${tokenIn.amount}${tokenIn.denom}`
+    );
+    queryUrl.searchParams.append("tokenOutDenom", tokenOutDenom);
+    const {
+      amount_out,
+      route: routes,
+      effective_fee,
+    } = await apiClient<SidecarQuoteResponse>(queryUrl.toString());
+
+    return {
+      amount: new Int(amount_out),
+      swapFee: new Dec(effective_fee),
+      split: routes.map(({ Route: route, in_amount }) => ({
+        initialAmount: new Int(in_amount),
+        pools: route.pools.map(({ id }) => ({ id: id.toString() })),
+        tokenInDenom: tokenIn.denom,
+        tokenOutDenoms: route.pools.map(
+          ({ token_out_denom }) => token_out_denom
+        ),
+      })),
+    };
+  }
+}
---
+++ packages/web/integrations/sidecar/types.ts
@@ -0,0 +1,25 @@
+export type SidecarQuoteResponse = {
+  amount_in: {
+    denom: string;
+    amount: string;
+  };
+  amount_out: string;
+  effective_fee: string;
+  route: {
+    in_amount: string;
+    out_amount: string;
+    Route: {
+      pools: {
+        id: number;
+        type: number;
+        balances: {
+          denom: string;
+          amount: string;
+        };
+        spread_factor: string;
+        taker_fee: string;
+        token_out_denom: string;
+      }[];
+    };
+  }[];
+};
