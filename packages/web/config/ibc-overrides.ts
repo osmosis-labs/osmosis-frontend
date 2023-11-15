f8509dcb5253b9dd9afd9f35dbfe78fde4fc8a40
Jose Felix <jfelx@outlook.com>
2023-11-15 16:45:33 -0400

    fix: Stage Build (#2441)

--- packages/web/config/ibc-overrides.ts
+++ packages/web/config/ibc-overrides.ts
@@ -178,7 +178,7 @@ const MainnetIBCAdditionalData: Partial<
       sourceChainTokens: [AxelarSourceChainTokenConfigs.busd.ethereum],
     },
   },
-  axlFIL: {
+  FIL: {
     sourceChainNameOverride: "Filecoin",
     originBridgeInfo: {
       bridge: "axelar" as const,
