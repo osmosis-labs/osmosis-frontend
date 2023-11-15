948e1738e03272d4bbcf279c23d14ec17ca05e8a
Matt Upham <mcupham@gmail.com>
2023-11-15 15:24:04 -0800

    Use radix

--- packages/web/components/control/checkbox.tsx
+++ packages/web/components/control/checkbox.tsx
@@ -1,7 +1,8 @@
 import * as CheckboxPrimitive from "@radix-ui/react-checkbox";
-import { CheckIcon } from "@radix-ui/react-icons";
+import classNames from "classnames";
 import { FunctionComponent } from "react";
 
+import { Icon } from "~/components/assets";
 import { ToggleProps } from "~/components/control/types";
 import { CustomClasses, Disableable } from "~/components/types";
 
@@ -31,12 +32,33 @@ export const CheckBox: FunctionComponent<
         checked={isOn}
         onCheckedChange={onToggle}
         disabled={disabled}
-        className={`border-white relative z-10 flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-[0.5rem] border-[0.15rem] border-osmoverse-300 ${className}`}
+        className={classNames(
+          "border-white relative z-10 flex h-[1.5rem] w-[1.5rem] items-center justify-center rounded-[0.5rem] border-[0.15rem] border-osmoverse-300",
+          isOn ? " bg-osmoverse-300" : "bg-transparent",
+          className
+        )}
         {...containerProps}
-        indeterminate={isIndeterminate}
       >
         <CheckboxPrimitive.Indicator asChild>
-          <CheckIcon color="#B0AADC" width={25} className={checkClassName} />
+          {isIndeterminate ? (
+            <Icon
+              id="minus"
+              className={classNames(
+                "absolute z-20 h-[11px] w-[15px] cursor-pointer text-osmoverse-800",
+                disabled ? "cursor-default opacity-50" : null,
+                checkClassName
+              )}
+            />
+          ) : (
+            <Icon
+              id="check-mark"
+              className={classNames(
+                "absolute z-20 h-[11px] w-[15px] cursor-pointer text-osmoverse-800",
+                disabled ? "cursor-default opacity-50" : null,
+                checkClassName
+              )}
+            />
+          )}
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
       <div className="cursor-pointer pl-3 md:pl-1">{children}</div>
