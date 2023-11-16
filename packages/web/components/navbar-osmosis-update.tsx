0bf47c8eaf92fb986bc93e71269d15cbd65c3665
Jose Felix <jfelx@outlook.com>
2023-11-16 17:02:18 -0500

    feat: show osmosis updates

---
+++ packages/web/components/navbar-osmosis-update.tsx
@@ -0,0 +1,99 @@
+import { useQuery } from "@tanstack/react-query";
+import classNames from "classnames";
+import React, { FunctionComponent } from "react";
+
+import { Icon } from "~/components/assets";
+import { ArrowButton } from "~/components/buttons";
+import IconButton from "~/components/buttons/icon-button";
+import Spinner from "~/components/spinner";
+import { useDisclosure, useLocalStorageState } from "~/hooks";
+import { ModalBase } from "~/modals";
+import { queryGithubFile } from "~/server/queries/github";
+
+const NavbarOsmosisUpdates = () => {
+  const { isOpen, onClose, onOpen } = useDisclosure();
+  const [closedUpdateUrl, setClosedUpdateUrl] = useLocalStorageState(
+    "osmosis-updates-closed-url",
+    ""
+  );
+  const { data, isLoading } = useQuery(["osmosis-updates"], async () =>
+    queryGithubFile<{ iframeUrl: string }>({
+      repo: "osmosis-labs/fe-content",
+      filePath: "cms/osmosis-update.json",
+    })
+  );
+
+  const hasNoUpdates = !data?.iframeUrl && !isLoading;
+
+  if (
+    hasNoUpdates ||
+    (closedUpdateUrl.length > 0 && closedUpdateUrl === data?.iframeUrl)
+  ) {
+    return null;
+  }
+
+  return (
+    <>
+      <div
+        className={classNames(
+          "relative rounded-2xl bg-osmoverse-800 p-4 shadow-[0px_6px_8px_0px_#09052433]",
+          "animate-[slideInUpExtreme_0.25s_cubic-bezier(0.46,0.47,0.4,1.5)]",
+          isLoading && "hidden"
+        )}
+      >
+        <IconButton
+          mode="unstyled"
+          className="transition-color absolute right-2 top-2 !h-5 !w-5 !px-0 text-osmoverse-400 hover:text-osmoverse-100"
+          aria-label="Close"
+          icon={<Icon id="close-thin" />}
+          onClick={() => {
+            if (!data?.iframeUrl) return;
+            setClosedUpdateUrl(data?.iframeUrl);
+          }}
+        />
+        <div className="flex flex-col gap-2">
+          <h1 className="body2 text-osmoverse-100">Osmosis updates!</h1>
+          <ArrowButton
+            className="caption gap-4 !text-bullish-300"
+            classes={{
+              arrowRight: "!w-4 !h-4",
+            }}
+            onClick={onOpen}
+          >
+            <div className=" flex items-center gap-1">
+              <Icon id="gift" className="h-4 w-4" />
+              {"See what's new"}
+            </div>
+          </ArrowButton>
+        </div>
+      </div>
+
+      <OsmosisUpdateModal
+        isOpen={isOpen}
+        onClose={onClose}
+        iframeUrl={data?.iframeUrl}
+      />
+    </>
+  );
+};
+
+const OsmosisUpdateModal: FunctionComponent<{
+  iframeUrl?: string;
+  isOpen: boolean;
+  onClose: () => void;
+}> = ({ isOpen, onClose, iframeUrl }) => {
+  if (!iframeUrl) return null;
+
+  return (
+    <ModalBase
+      isOpen={isOpen}
+      onRequestClose={onClose}
+      className="relative flex h-[80vh] w-[80vw] !max-w-[1920px] flex-col items-center justify-center border-4 border-wosmongton-300 !px-0 !py-0 sm:h-[90vh] sm:w-[90vh]"
+    >
+      <Spinner className="absolute" />
+      <iframe className="z-10 h-full w-full" src={iframeUrl}></iframe>
+    </ModalBase>
+  );
+};
+
+export default NavbarOsmosisUpdates;
