a3fbbd0b7b2d18cdb28477b3f0305331be5730bd
Jon Ator <jonathanator0@gmail.com>
2023-11-16 14:28:19 -0500

    use web router

--- packages/web/server/api/edge-routers/swap-router.ts
+++ packages/web/server/api/edge-routers/swap-router.ts
@@ -1,10 +1,12 @@
 import { Int } from "@keplr-wallet/unit";
+import { TokenOutGivenInRouter } from "@osmosis-labs/pools";
 import { z } from "zod";
 
 import { ChainList } from "~/config/generated/chain-list";
 import { OsmosisSidecarRemoteRouter } from "~/integrations/sidecar/router";
 import { TfmRemoteRouter } from "~/integrations/tfm/router";
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { routeTokenOutGivenIn } from "~/server/queries/complex/route-token-out-given-in";
 import { BestRouteTokenInRouter } from "~/utils/routing/best-route-router";
 
 const osmosisChainId = ChainList[0].chain_id;
@@ -23,15 +25,14 @@ const routers = [
       process.env.NEXT_PUBLIC_SIDECAR_BASE_URL ?? "http://157.230.101.80:9092"
     ),
   },
+  {
+    name: "web",
+    router: {
+      routeByTokenIn: async (tokenIn, tokenOutDenom) =>
+        (await routeTokenOutGivenIn(tokenIn, tokenOutDenom)).quote,
+    } as TokenOutGivenInRouter,
+  },
 ];
-
-// const w = {
-//   name: "web",
-//   router: {
-//     routeByTokenIn: async (tokenIn, tokenOutDenom) =>
-//       (await routeTokenOutGivenIn(tokenIn, tokenOutDenom)).quote,
-//   } as TokenOutGivenInRouter,
-// };
 
 export const swapRouter = createTRPCRouter({
   routeTokenOutGivenIn: publicProcedure
@@ -45,12 +46,13 @@ export const swapRouter = createTRPCRouter({
     .query(
       async ({ input: { tokenInDenom, tokenInAmount, tokenOutDenom } }) => {
         const router = new BestRouteTokenInRouter(routers);
-        const tokenIn = {
-          denom: tokenInDenom,
-          amount: new Int(tokenInAmount),
-        };
-
-        return await router.routeByTokenIn(tokenIn, tokenOutDenom);
+        return await router.routeByTokenIn(
+          {
+            denom: tokenInDenom,
+            amount: new Int(tokenInAmount),
+          },
+          tokenOutDenom
+        );
       }
     ),
 });
