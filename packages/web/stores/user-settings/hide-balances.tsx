6989a80de0ea7fb8649af2b7a5a3066f66b5709c
roman <roman@osmosis.team>
2023-11-16 11:19:25 -0500

    fix bug with Fragment

--- packages/web/stores/user-settings/hide-balances.tsx
+++ packages/web/stores/user-settings/hide-balances.tsx
@@ -1,5 +1,5 @@
 import { action, computed, makeObservable, observable } from "mobx";
-import { FunctionComponent } from "react";
+import { Fragment, FunctionComponent } from "react";
 
 import { Icon } from "~/components/assets";
 import { Switch } from "~/components/control";
@@ -18,31 +18,33 @@ export class HideBalancesUserSetting implements UserSetting<HideBalancesState> {
     const { isMobile } = useWindowSize();
 
     return (
-      !isMobile && (
-        <div className="mt-4 flex flex-col gap-[46px] rounded-2xl border-2 border-osmoverse-700 bg-osmoverse-800 p-6">
-          <div className="flex items-center justify-between">
-            <Icon id="zoom-out" className="text-osmoverse-200" />
-            <Switch
-              isOn={hideBalances}
-              onToggle={() => {
-                console.log("Setting balances setting to " + !hideBalances);
-                this.setState({ hideBalances: !hideBalances });
-              }}
-            />
-          </div>
+      <Fragment>
+        {!isMobile && (
+          <div className="mt-4 flex flex-col gap-[46px] rounded-2xl border-2 border-osmoverse-700 bg-osmoverse-800 p-6">
+            <div className="flex items-center justify-between">
+              <Icon id="zoom-out" className="text-osmoverse-200" />
+              <Switch
+                isOn={hideBalances}
+                onToggle={() => {
+                  console.log("Setting balances setting to " + !hideBalances);
+                  this.setState({ hideBalances: !hideBalances });
+                }}
+              />
+            </div>
 
-          <div className="group flex justify-between text-white-full">
-            <div className="flex flex-col gap-1">
-              <span className="subtitle1 text-left tracking-wide">
-                {t("settings.hideBalances")}
-              </span>
-              <span className="caption tracking-wider text-osmoverse-200">
-                {this.getLabel(t)}
-              </span>
+            <div className="group flex justify-between text-white-full">
+              <div className="flex flex-col gap-1">
+                <span className="subtitle1 text-left tracking-wide">
+                  {t("settings.hideBalances")}
+                </span>
+                <span className="caption tracking-wider text-osmoverse-200">
+                  {this.getLabel(t)}
+                </span>
+              </div>
             </div>
           </div>
-        </div>
-      )
+        )}
+      </Fragment>
     );
   };
 
