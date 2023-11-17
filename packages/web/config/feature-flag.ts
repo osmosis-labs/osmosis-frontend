6a0b417ae2228053fe7b7011ce357d5db8aa5b82
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-11-17 16:33:19 -0700

    revert bg image and rm announcement banner (#2454)

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -36,15 +36,7 @@ export const Announcement:
         "Chain is halted, transactions are temporarily disabled",
       isWarning: true,
     }
-  : {
-      localStorageKey: "announcement.margined",
-      enTextOrLocalizationPath: "margined.description",
-      link: {
-        enTextOrLocalizationKey: "margined.link",
-        url: "https://trade.margined.io",
-        isExternal: true,
-      },
-    };
+  : undefined;
 
 // Past localstorage keys:
 // * "feedback_wQ1KR7": "Help us shape the future of Osmosis." Give us feedback -> https://tally.so/r/wQ1KR7
