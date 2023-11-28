d5a996a7b311c0a86ba2aa2361a4b222b4f731c5
Serkan Reis <serkanreis@gmail.com>
2023-11-28 19:14:02 +0300

    Center banner type PFPs

--- packages/web/components/navbar/index.tsx
+++ packages/web/components/navbar/index.tsx
@@ -449,7 +449,7 @@ const WalletInfo: FunctionComponent<
           }}
           className="group flex place-content-between items-center gap-[13px] rounded-xl border border-osmoverse-700 px-1.5 py-1 hover:border-[1.3px] hover:border-wosmongton-300 hover:bg-osmoverse-800 md:w-full"
         >
-          <div className="h-8 w-8 shrink-0 overflow-hidden rounded-[7px] bg-osmoverse-700 group-hover:bg-gradient-positive">
+          <div className="flex h-8 w-8 shrink-0 items-center justify-center overflow-hidden rounded-[7px] bg-osmoverse-700 group-hover:bg-gradient-positive">
             {profileStore.currentAvatar === "ammelia" ? (
               <Image
                 alt="Wosmongton profile"
@@ -474,7 +474,7 @@ const WalletInfo: FunctionComponent<
                 }
                 height={32}
                 width={32}
-                objectFit="scale-down"
+                style={{ objectFit: "scale-down" }}
               />
             ) : null}
           </div>
