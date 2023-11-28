bbfe5733ceafd4d60f5e8b79eb295c822d899c5c
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-29 00:01:56 +0100

    feat: :sparkles: add my/all switch filter

---
+++ packages/web/components/earn/tabs/filters/my-all.tsx
@@ -0,0 +1,43 @@
+import { RadioGroup } from "@headlessui/react";
+import { useState } from "react";
+
+const tokenFilterOptions = [
+  {
+    value: "my",
+    label: "My Tokens",
+  },
+  {
+    value: "all",
+    label: "All Tokens",
+  },
+];
+
+export const MyAllSwitch = () => {
+  const [selected, setSelected] = useState(tokenFilterOptions[0]);
+
+  return (
+    <RadioGroup
+      className={
+        "inline-flex max-h-[52px] min-w-[290px] items-center rounded-xl bg-[#140F34]"
+      }
+      value={selected}
+      onChange={setSelected}
+    >
+      {tokenFilterOptions.map(({ label, value }) => (
+        <RadioGroup.Option
+          key={`${value} radio button`}
+          className={({ checked }) =>
+            `inline-flex h-13 w-full items-center justify-center rounded-lg py-2.5 font-subtitle1 leading-5 opacity-30 hover:cursor-pointer ${
+              checked
+                ? "bg-wosmongton-700 opacity-100"
+                : "hover:bg-[#140F34]/25"
+            }`
+          }
+          value={value}
+        >
+          {label}
+        </RadioGroup.Option>
+      ))}
+    </RadioGroup>
+  );
+};
