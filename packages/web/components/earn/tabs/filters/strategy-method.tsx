90cd3ad911a15d9dc92b116e1c1f47092938eab4
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-29 12:43:31 +0100

    refactor: :recycle: abstract Dropdown

--- packages/web/components/earn/tabs/filters/strategy-method.tsx
+++ packages/web/components/earn/tabs/filters/strategy-method.tsx
@@ -1,7 +1,6 @@
-import { Listbox, Transition } from "@headlessui/react";
-import { Fragment, useState } from "react";
+import { useState } from "react";
 
-import { Icon } from "~/components/assets";
+import { DropdownWithLabel } from "~/components/earn/abstracts/dropdown-with-label";
 
 const strategies = [
   { id: 1, name: "All" },
@@ -13,61 +12,14 @@ const strategies = [
 ];
 
 export const StrategyMethod = () => {
-  const [selectedStrategies, setSelectedStrategies] = useState(strategies[0]);
+  const [selectedStrategy, setSelectedStrategy] = useState(strategies[0]);
 
   return (
-    <div className="flex items-center gap-7">
-      <span className="text-base font-subtitle1 font-bold">
-        Strategy Method
-      </span>
-      <Listbox value={selectedStrategies} onChange={setSelectedStrategies}>
-        <div className="relative">
-          <Listbox.Button className="inline-flex min-w-[200px] items-center justify-between rounded-lg border-2 border-wosmongton-100 border-opacity-20 bg-osmoverse-900 py-3 px-5">
-            <span className="text-base font-subtitle1 leading-6">
-              {selectedStrategies.name}
-            </span>
-            <Icon id="caret-down" />
-          </Listbox.Button>
-          <Transition
-            as={Fragment}
-            enter="transition ease-in duration-150"
-            enterFrom="opacity-0"
-            enterTo="opacity-100"
-            leave="transition ease-in duration-150"
-            leaveFrom="opacity-100"
-            leaveTo="opacity-0"
-          >
-            <Listbox.Options
-              className={
-                "absolute inset-x-0 mt-1 flex flex-col gap-2 rounded-lg bg-osmoverse-800 py-4"
-              }
-            >
-              {strategies.map((person) => (
-                <Listbox.Option
-                  className={({ active }) =>
-                    `relative cursor-default select-none py-2 pl-10 pr-4 ${
-                      active ? "bg-osmoverse-825" : ""
-                    }`
-                  }
-                  key={person.id}
-                  value={person}
-                >
-                  {({ selected }) => (
-                    <>
-                      <span className={`block truncate`}>{person.name}</span>
-                      {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3">
-                          <Icon id="check-mark" className="h-4 w-4" />
-                        </span>
-                      ) : null}
-                    </>
-                  )}
-                </Listbox.Option>
-              ))}
-            </Listbox.Options>
-          </Transition>
-        </div>
-      </Listbox>
-    </div>
+    <DropdownWithLabel
+      label="Strategy Method"
+      values={strategies}
+      value={selectedStrategy}
+      setValue={setSelectedStrategy}
+    />
   );
 };
