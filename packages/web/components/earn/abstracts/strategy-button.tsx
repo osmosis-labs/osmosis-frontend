2d63c9d933d5b00450a264e2624e5fa2741e4c43
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-29 14:46:11 +0100

    feat: :sparkles: add strategy button and strat categories

---
+++ packages/web/components/earn/abstracts/strategy-button.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+
+interface StrategyButtonProps {
+  imageURI: string;
+  label: string;
+  resp: string;
+}
+
+export const StrategyButton = ({ imageURI, label }: StrategyButtonProps) => {
+  return (
+    <div className="flex min-w-[186px] items-center gap-4 py-1 px-4">
+      <button className="inline-flex max-h-11 w-11 items-center justify-center rounded-lg bg-osmoverse-800 px-2 py-3">
+        <Image src={imageURI} alt={`${label} image`} width={28} height={28} />
+      </button>
+      <span className="text-base font-subtitle1 text-osmoverse-200">
+        {label}
+      </span>
+    </div>
+  );
+};
---
+++ packages/web/components/earn/tabs/filters/strategy-categories.tsx
@@ -0,0 +1,29 @@
+import { StrategyButton } from "~/components/earn/abstracts/strategy-button";
+
+const strategiesFilters = [
+  {
+    label: "Stablecoins",
+    imageURI: "/images/table-filters/stablecoins.svg",
+    resp: "stablecoins",
+  },
+  {
+    label: "Correlated",
+    imageURI: "/images/table-filters/correlated.svg",
+    resp: "correlated",
+  },
+  {
+    label: "Blue Chip",
+    imageURI: "/images/table-filters/blue-chip.svg",
+    resp: "bluechip",
+  },
+];
+
+export const StrategyCategories = () => {
+  return (
+    <div className="flex">
+      {strategiesFilters.map((props) => (
+        <StrategyButton key={`${props.label} strategy button`} {...props} />
+      ))}
+    </div>
+  );
+};
