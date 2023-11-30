65af2f85613ac7e6f516389f8827a573c3fee0c6
fabry-js <vivaldifabrizio10@gmail.com>
2023-11-30 14:19:36 +0100

    refactor: :recycle: move all filters one level up

--- packages/web/components/earn/tabs/top-filters.tsx
+++ packages/web/components/earn/tabs/top-filters.tsx
@@ -1,10 +1,10 @@
-import { LockingDurationSwitch } from "~/components/earn/tabs/filters/locking-duration";
-import { MyAllRadio } from "~/components/earn/tabs/filters/my-all";
-import { PlatformsDropdown } from "~/components/earn/tabs/filters/platforms";
-import { RewardTypesRadio } from "~/components/earn/tabs/filters/reward-types";
-import { SearchFilter } from "~/components/earn/tabs/filters/search-filter";
-import { StrategyCategories } from "~/components/earn/tabs/filters/strategy-categories";
-import { StrategyMethodDropdown } from "~/components/earn/tabs/filters/strategy-method";
+import { LockingDurationSwitch } from "~/components/earn/filters/locking-duration";
+import { MyAllRadio } from "~/components/earn/filters/my-all";
+import { PlatformsDropdown } from "~/components/earn/filters/platforms";
+import { RewardTypesRadio } from "~/components/earn/filters/reward-types";
+import { SearchFilter } from "~/components/earn/filters/search-filter";
+import { StrategyCategories } from "~/components/earn/filters/strategy-categories";
+import { StrategyMethodDropdown } from "~/components/earn/filters/strategy-method";
 
 export const TopFilters = () => {
   return (
