808024b51a562fab76810944c9794cd8e08eb8ba
Matt Upham <30577966+mattupham@users.noreply.github.com>
2023-12-04 15:27:28 -0800

    Collect and Reinvest updates - add flash on rewards change, update button disabled states, tooltips, gas limits (#2485)
    * Update buttons + gas forecast
    
    * Add flash
    
    * Apply effect on diff
    
    * Add classNames
    
    * Update copy
    
    * Update calculation to disable button
    
    * Update rewards calc
    
    * Remove key
    
    * Clean up min balance
    
    * Add translations

--- packages/web/components/cards/rewards-card.tsx
+++ packages/web/components/cards/rewards-card.tsx
@@ -19,31 +19,37 @@ export const RewardsCard: React.FC<{
   image = null,
   disabled,
 }) => {
+  const ConditionalWrapper: React.FC<{ children: React.ReactNode }> = ({
+    children,
+  }) =>
+    disabled ? (
+      <Tooltip content={disabledTooltipContent} className="w-full">
+        {children as any}
+      </Tooltip>
+    ) : (
+      <>{children}</>
+    );
+
   return (
-    <Button
-      disabled={disabled}
-      mode="unstyled"
-      className="relative flex min-h-[50px] w-full flex-grow cursor-pointer flex-col !items-end justify-start overflow-hidden rounded-[28px] border-[1px] border-osmoverse-600 bg-osmoverse-800 !p-0 disabled:cursor-not-allowed disabled:opacity-75"
-      onClick={onClick}
-    >
-      {image}
-      <div className="z-10 flex items-center gap-2 p-4">
-        <span className="text-osmoverse-white text-sm">{title}</span>
-        {disabled && (
-          <div className="text-osmoverse-600 sm:hidden">
-            <Tooltip content={disabledTooltipContent}>
-              <Icon id="info" height="14px" width="14px" fill="#EF3456" />
-            </Tooltip>
-          </div>
-        )}
-        {!disabled && (
-          <div className="text-osmoverse-600 sm:hidden">
-            <Tooltip content={tooltipContent}>
-              <Icon id="info" height="14px" width="14px" fill="#958FC0" />
-            </Tooltip>
-          </div>
-        )}
-      </div>
-    </Button>
+    <ConditionalWrapper>
+      <Button
+        disabled={disabled}
+        mode="unstyled"
+        className="relative flex min-h-[50px] w-full flex-grow cursor-pointer flex-col !items-end justify-start overflow-hidden rounded-[28px] border-[1px] border-osmoverse-600 bg-osmoverse-800 !p-0 disabled:cursor-not-allowed disabled:opacity-50"
+        onClick={onClick}
+      >
+        {image}
+        <div className="z-10 flex items-center gap-2 p-4">
+          <span className="text-osmoverse-white text-sm">{title}</span>
+          {!disabled && (
+            <div className="text-osmoverse-600 sm:hidden">
+              <Tooltip content={tooltipContent}>
+                <Icon id="info" height="14px" width="14px" fill="#958FC0" />
+              </Tooltip>
+            </div>
+          )}
+        </div>
+      </Button>
+    </ConditionalWrapper>
   );
 };
