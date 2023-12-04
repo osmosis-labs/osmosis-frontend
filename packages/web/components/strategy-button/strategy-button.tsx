7eff083f60dd0ff5ad9dc09f4f92eef27afeab0e
fabry-js <vivaldifabrizio10@gmail.com>
2023-12-04 14:42:12 +0100

    feat: :sparkles: add active style to StrategyButton

--- packages/web/components/strategy-button/strategy-button.tsx
+++ packages/web/components/strategy-button/strategy-button.tsx
@@ -1,20 +1,37 @@
+import classNames from "classnames";
 import { ReactNode } from "react";
 
 interface StrategyButtonProps {
   icon: ReactNode;
   label: string;
   resp: string;
+  value: boolean;
+  onChange: (resp: string) => void;
 }
 
-export const StrategyButton = ({ icon, label }: StrategyButtonProps) => {
+export const StrategyButton = ({
+  icon,
+  label,
+  onChange,
+  resp,
+  value,
+}: StrategyButtonProps) => {
   return (
-    <div className="flex min-w-strategy-buttons items-center gap-4 py-1 px-4">
-      <button className="inline-flex max-h-11 items-center justify-center rounded-lg bg-osmoverse-800 px-2 py-3">
+    <button
+      onClick={() => onChange(resp)}
+      className={classNames(
+        "mx-1 flex min-w-strategy-buttons items-center gap-4 rounded-lg py-1 px-4",
+        {
+          "bg-osmoverse-800": value === true,
+        }
+      )}
+    >
+      <div className="inline-flex max-h-11 w-11 items-center justify-center rounded-lg bg-osmoverse-800 px-2 py-3">
         {icon}
-      </button>
+      </div>
       <span className="text-base font-subtitle1 text-osmoverse-200">
         {label}
       </span>
-    </div>
+    </button>
   );
 };
