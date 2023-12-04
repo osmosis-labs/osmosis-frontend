871fe79be463a7584a197be552a72396de61ab06
fabry-js <vivaldifabrizio10@gmail.com>
2023-12-04 14:42:42 +0100

    feat: :label: update DropdownWithLabel types

--- packages/web/components/dropdown-with-label/dropdown-with-label.tsx
+++ packages/web/components/dropdown-with-label/dropdown-with-label.tsx
@@ -2,34 +2,28 @@ import { Listbox, Transition } from "@headlessui/react";
 import { Fragment } from "react";
 
 import { Icon } from "~/components/assets";
+import { ListOption } from "~/components/earn/types";
 
-interface Option {
-  id: number;
-  name: string;
-}
-
-interface DropdownWithLabelProps {
+interface DropdownWithLabelProps<T> {
   label: string;
-  values: Option[];
-  value: Option;
-  onChange: (v: Option) => void;
+  options: ListOption<T>[];
+  value: T;
+  onChange: (v: T) => void;
 }
 
-export const DropdownWithLabel = ({
+export const DropdownWithLabel = <T,>({
   label,
   onChange,
   value,
-  values,
-}: DropdownWithLabelProps) => {
+  options,
+}: DropdownWithLabelProps<T>) => {
   return (
     <div className="flex items-center gap-7">
       <span className="text-base font-subtitle1 font-bold">{label}</span>
       <Listbox value={value} onChange={onChange}>
         <div className="relative">
           <Listbox.Button className="inline-flex min-w-dropdown-with-label items-center justify-between rounded-lg border-2 border-wosmongton-100 border-opacity-20 bg-osmoverse-900 py-3 px-5">
-            <span className="text-base font-subtitle1 leading-6">
-              {value.name}
-            </span>
+            <span className="text-base font-subtitle1 leading-6">{value}</span>
             <Icon id="caret-down" />
           </Listbox.Button>
           <Transition
@@ -46,19 +40,19 @@ export const DropdownWithLabel = ({
                 "absolute inset-x-0 z-10 mt-1 flex flex-col gap-2 rounded-lg bg-osmoverse-800 py-4"
               }
             >
-              {values.map((value) => (
+              {options.map(({ id, name }) => (
                 <Listbox.Option
                   className={({ active }) =>
                     `relative cursor-default select-none py-2 pl-10 pr-4 ${
                       active ? "bg-osmoverse-825" : ""
                     }`
                   }
-                  key={value.id}
-                  value={value}
+                  key={id}
+                  value={name}
                 >
                   {({ selected }) => (
                     <>
-                      <span className={`block truncate`}>{value.name}</span>
+                      <span className={`block truncate`}>{name}</span>
                       {selected ? (
                         <span className="absolute inset-y-0 left-0 flex items-center pl-3">
                           <Icon id="check-mark" className="h-4 w-4" />
