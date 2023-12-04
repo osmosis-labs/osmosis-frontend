92f119de5b3967208c6bfd89fedacf26495e49fc
roman <roman@osmosis.team>
2023-12-03 20:51:45 -0800

    fix: transmuter is not being picked up in router

--- packages/pools/src/router/routes.ts
+++ packages/pools/src/router/routes.ts
@@ -194,19 +194,27 @@ export class OptimizedRoutes implements TokenOutGivenInRouter {
     }, [] as Route[]);
 
     // prioritize (pick) routes by preference
+
+    // Maintain order of preferred routes.
+    const preferredRoutes: Route[] = [];
+    // Maintain order of non-preferred routes.
+    const nonPreferredRoutes: Route[] = [];
     if (this._preferredPoolIds && this._preferredPoolIds.length > 0) {
       routes = routes.reduce((routes, route) => {
         if (
           this._preferredPoolIds &&
           route.pools.some((pool) => this._preferredPoolIds?.includes(pool.id))
         ) {
-          routes.unshift(route);
+          preferredRoutes.push(route);
         } else {
-          routes.push(route);
+          nonPreferredRoutes.push(route);
         }
         return routes;
       }, [] as Route[]);
     }
+
+    // Preferred routes first, then non-preferred routes.
+    routes = [...preferredRoutes, ...nonPreferredRoutes];
 
     this._logger?.info(
       "Candidate routes",
