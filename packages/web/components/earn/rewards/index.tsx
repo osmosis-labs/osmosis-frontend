fdb60883da0a9b512be881f11b7e4ac026dbee3e
fabry-js <vivaldifabrizio10@gmail.com>
2023-12-05 14:00:43 +0100

    feat: :lipstick: update Rewards design

--- packages/web/components/earn/rewards/index.tsx
+++ packages/web/components/earn/rewards/index.tsx
@@ -3,14 +3,8 @@ import { Button } from "~/components/buttons";
 
 export const EarnRewards = () => {
   return (
-    <div className="flex min-w-rewards-container flex-col rounded-3x4pxlinset bg-osmoverse-850 px-7 pt-7 pb-9">
+    <div className="flex min-w-rewards-container flex-col justify-between rounded-3x4pxlinset bg-osmoverse-850 px-7 pt-7 pb-9">
       <p className="text-xl font-semibold text-osmoverse-100">Rewards</p>
-      <div className="mt-6 flex flex-col">
-        <h3 className="text-3xl font-semibold text-osmoverse-100">$24.55</h3>
-        <small className="text-xs font-subtitle2 font-medium text-osmoverse-300">
-          available to claim
-        </small>
-      </div>
       <div className="mt-5 flex flex-col gap-1">
         <h5 className="text-xl font-semibold text-bullish-400">$221.64</h5>
         <div className="flex items-center gap-1">
@@ -20,15 +14,15 @@ export const EarnRewards = () => {
           <Icon id="info" className="h-4 w-4" />
         </div>
       </div>
+      <div className="mt-6 flex flex-col">
+        <h3 className="text-3xl font-semibold text-osmoverse-100">$24.55</h3>
+        <small className="text-xs font-subtitle2 font-medium text-osmoverse-300">
+          available to claim
+        </small>
+      </div>
       <div className="mt-8 flex flex-col gap-3">
         <Button mode={"primary"} className="max-h-11">
           Claim all rewards
-        </Button>
-        <Button
-          mode={"quaternary"}
-          className="max-h-11 border-none text-sm font-subtitle1"
-        >
-          View all earning positions
         </Button>
       </div>
     </div>
