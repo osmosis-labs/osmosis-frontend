56feb50e6f59e9621648ba35b0e22260195d07af
Sunny Aggarwal <sunnya97@protonmail.ch>
2023-12-08 15:47:14 -0800

    try resort

--- packages/web/components/table/processor.ts
+++ packages/web/components/table/processor.ts
@@ -3,6 +3,24 @@ import { Dec } from "@keplr-wallet/unit";
 import { AssetCell } from "~/components/table/cells";
 import { DataSorter } from "~/hooks/data/data-sorter";
 
+const linkedDenoms = [
+  ["AXL", "USDC.axl", "USDT.axl", "wstETH.axl", "DOT.axl"],
+  ["GRAV", "USDC.grv", "USDT.grv", "WETH.grv"],
+  [
+    "STRD",
+    "stATOM",
+    "stOSMO",
+    "stSTARS",
+    "stJUNO",
+    "stLUNA",
+    "stEVMOS",
+    "stUMEE",
+    "stSOMM",
+  ],
+  ["QCK", "qATOM", "qOSMO", "qSOMM", "qREGEN", "qSTARS"],
+  ["IBCX", "stIBCX"],
+];
+
 const nonCanonicalDenoms = [
   "WETH.grv",
   "USDT.axl",
@@ -20,8 +38,19 @@ export class AssetsDataSorter extends DataSorter<AssetCell> {
         const aMarketCap: Dec = new Dec(a.marketCapRaw ?? "0");
         const bMarketCap: Dec = new Dec(b.marketCapRaw ?? "0");
 
-        const aCanonical = nonCanonicalDenoms.includes(a.currency.coinDenom);
-        const bCanonical = nonCanonicalDenoms.includes(b.currency.coinDenom);
+        // const aCanonical = nonCanonicalDenoms.includes(a.currency.coinDenom);
+        // const bCanonical = nonCanonicalDenoms.includes(b.currency.coinDenom);
+
+        for (var linkedDenom in linkedDenoms) {
+          if (linkedDenom.includes(a.currency.coinDenom)) {
+            if (linkedDenom.includes(b.currency.coinDenom)) {
+              return linkedDenom.indexOf(a.currency.coinDenom) <
+                linkedDenom.indexOf(b.currency.coinDenom)
+                ? -1
+                : 1;
+            }
+          }
+        }
 
         if (aMarketCap.lt(bMarketCap)) {
           return -1;
@@ -31,7 +60,9 @@ export class AssetsDataSorter extends DataSorter<AssetCell> {
           return 1;
         }
 
-        return aCanonical === bCanonical ? 0 : aCanonical ? -1 : 1;
+        return 0;
+
+        // return aCanonical === bCanonical ? 0 : aCanonical ? -1 : 1;
       });
     }
 
