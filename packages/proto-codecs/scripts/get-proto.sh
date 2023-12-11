eb9ea7b1a5111f1423ea580877cc6f90a8383449
Matt Upham <mcupham@gmail.com>
2023-12-11 07:02:02 -0800

    Test get-proto.sh

--- packages/proto-codecs/scripts/get-proto.sh
+++ packages/proto-codecs/scripts/get-proto.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset -o pipefail
 command -v shellcheck >/dev/null && shellcheck "$0"
 
 PROTO_DIR="./chain-proto"
-OSMOSIS_COMMIT_HASH="42b2aced91f09a640898f44e4402d5321b7dc451"
+OSMOSIS_COMMIT_HASH="249ab294d04bc36d7063dcf01fa0f613f0ce42f2"
 
 ICS23_COMMIT_HASH="f4deb054b697458e7f0aa353c2f45a365361e895"
 
@@ -14,10 +14,11 @@ mkdir -p "$PROTO_DIR"
 
 # OSMOSIS PROTOS
 
-git clone --filter=blob:none --sparse https://github.com/osmosis-labs/osmosis.git .repos/osmosis
+# git clone --filter=blob:none --sparse https://github.com/osmosis-labs/osmosis.git .repos/osmosis
+git clone https://github.com/osmosis-labs/osmosis.git .repos/osmosis
 
 # Checkout to Osmosis hash commit
-git -C .repos/osmosis sparse-checkout set proto
+# git -C .repos/osmosis sparse-checkout set proto
 git fetch --all --tags
 git -C .repos/osmosis checkout $OSMOSIS_COMMIT_HASH
 
@@ -25,10 +26,12 @@ git -C .repos/osmosis checkout $OSMOSIS_COMMIT_HASH
 # SDK PROTOS
 COSMOS_SDK_VERSION=$(awk '/github.com\/cosmos\/cosmos-sdk/ {print $2}' .repos/osmosis/go.mod | tr -d '=> ')
 
-git clone --filter=blob:none --sparse https://github.com/cosmos/cosmos-sdk.git .repos/cosmos-sdk
+# git clone --filter=blob:none --sparse https://github.com/cosmos/cosmos-sdk.git .repos/cosmos-sdk
+git clone https://github.com/cosmos/cosmos-sdk.git .repos/cosmos-sdk
 
 # Checkout to Cosmos hash commit
-git -C .repos/cosmos-sdk sparse-checkout set proto
+# git -C .repos/cosmos-sdk sparse-checkout set proto
+# git -C .repos/cosmos-sdk set proto
 git fetch --all --tags
 git -C .repos/cosmos-sdk checkout $COSMOS_SDK_VERSION  
 
@@ -37,10 +40,11 @@ git -C .repos/cosmos-sdk checkout $COSMOS_SDK_VERSION
 
 IBC_GO_VERSION=$(awk '/github.com\/cosmos\/ibc-go/ {print $2}' .repos/osmosis/go.mod)
 
-git clone --filter=blob:none --sparse https://github.com/cosmos/ibc-go.git .repos/ibc-go
+# git clone --filter=blob:none --sparse https://github.com/cosmos/ibc-go.git .repos/ibc-go
+git clone https://github.com/cosmos/ibc-go.git .repos/ibc-go
 
 # Checkout to IBC hash commit
-git -C .repos/ibc-go sparse-checkout set proto
+# git -C .repos/ibc-go sparse-checkout set proto
 git fetch --all --tags
 git -C .repos/ibc-go checkout $IBC_GO_VERSION
 
@@ -52,19 +56,21 @@ WASMD_VERSION=$(awk '/github.com\/osmosis-labs\/wasmd/ {print $4}' .repos/osmosi
 
 
 
-git clone --filter=blob:none --sparse https://github.com/osmosis-labs/wasmd.git .repos/wasmd
+# git clone --filter=blob:none --sparse https://github.com/osmosis-labs/wasmd.git .repos/wasmd
+git clone https://github.com/osmosis-labs/wasmd.git .repos/wasmd
 
 # Checkout to IBC hash commit
-git -C .repos/wasmd sparse-checkout set proto
+# git -C .repos/wasmd sparse-checkout set proto
 git fetch --all --tags
 git -C .repos/wasmd checkout $WASMD_VERSION
 
 # ICS23 PROTOS
 
-git clone --filter=blob:none --sparse https://github.com/cosmos/ics23.git .repos/ics23
+# git clone --filter=blob:none --sparse https://github.com/cosmos/ics23.git .repos/ics23
+git clone https://github.com/cosmos/ics23.git .repos/ics23
 
 # Checkout to IBC hash commit
-git -C .repos/ics23 sparse-checkout set proto
+# git -C .repos/ics23 sparse-checkout set proto
 git fetch --all --tags
 git -C .repos/ics23 checkout $ICS23_COMMIT_HASH
 
