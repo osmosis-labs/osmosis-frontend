b9184372239307d9096cd443efde18b3752b24a9
Jon Ator <jonathanator0@gmail.com>
2023-12-15 15:43:32 -0500

    add calc market cap rank

---
+++ packages/web/server/queries/complex/assets/__tests__/market-cap.spec.ts
@@ -0,0 +1,18 @@
+import { TokenMarketCap } from "../../../indexer";
+import { calculateRank } from "../market-cap";
+
+describe("calculateRank", () => {
+  it("should correctly calculate ranks based on market cap", () => {
+    const marketCaps: TokenMarketCap[] = [
+      { symbol: "token1", market_cap: 100 },
+      { symbol: "token2", market_cap: 200 },
+      { symbol: "token3", market_cap: 50 },
+    ];
+
+    const rankMap = calculateRank(marketCaps);
+
+    expect(rankMap.get("token1")).toBe(2);
+    expect(rankMap.get("token2")).toBe(1);
+    expect(rankMap.get("token3")).toBe(3);
+  });
+});
---
+++ packages/web/server/queries/complex/assets/market-cap.ts
@@ -0,0 +1,41 @@
+import cachified, { CacheEntry } from "cachified";
+import { LRUCache } from "lru-cache";
+
+import { queryTokenMarketCaps, TokenMarketCap } from "../../indexer";
+
+const marketCapsCache = new LRUCache<string, CacheEntry>({ max: 1 });
+/** Gets the numerical market cap rank based on the available market caps given a token symbol/denom.
+ *  Returns `undefined` if a market cap is not available for the given symbol/denom. */
+export async function getAssetMarketCapRank({
+  coinDenom,
+}: {
+  coinDenom: string;
+}): Promise<number | undefined> {
+  const rankMap: Map<string, number> = await cachified({
+    cache: marketCapsCache,
+    ttl: 1000 * 60 * 15, // 15 minutes
+    key: "marketCapRankMap",
+    getFreshValue: async () => {
+      try {
+        const marketCaps = await queryTokenMarketCaps();
+
+        return calculateRank(marketCaps);
+      } catch (error) {
+        console.error("Could not fetch market caps for ranking", error);
+        return new Map<string, number>();
+      }
+    },
+  });
+
+  return rankMap.get(coinDenom);
+}
+
+export const calculateRank = (marketCaps: TokenMarketCap[]) => {
+  const rankMap = new Map<string, number>();
+  marketCaps.sort((a, b) => b.market_cap - a.market_cap);
+  for (let i = 0; i < marketCaps.length; i++) {
+    const rank = i + 1;
+    rankMap.set(marketCaps[i].symbol, rank);
+  }
+  return rankMap;
+};
