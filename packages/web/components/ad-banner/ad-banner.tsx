d9e60df2035ac4dc5011f67092eb166027bf33d9
Matt Upham <mcupham@gmail.com>
2023-12-18 11:18:11 -0500

    Comment out shuffle array

--- packages/web/components/ad-banner/ad-banner.tsx
+++ packages/web/components/ad-banner/ad-banner.tsx
@@ -4,9 +4,9 @@ import { AdBannerContent } from "~/components/ad-banner/ad-banner-content";
 import { Ad } from "~/components/ad-banner/ad-banner-types";
 import { Step, Stepper } from "~/components/stepper/index";
 
-function shuffleArray<T>(array: T[]): T[] {
-  return array.sort(() => 0.5 - Math.random());
-}
+// function shuffleArray<T>(array: T[]): T[] {
+//   return array.sort(() => 0.5 - Math.random());
+// }
 
 interface AdBannerProps {
   ads: Ad[];
