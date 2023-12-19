a93e570f60b2e41fbc3ecf95a7215569e6fa3f2f
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2023-12-19 09:15:02 -0700

    yarn fixes (#2591)

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -36,10 +36,7 @@ export const Announcement:
         "Chain is halted, transactions are temporarily disabled",
       isWarning: true,
     }
-  : {
-      localStorageKey: "announcement.lvnmilk",
-      enTextOrLocalizationPath: "lvnmilk.description",
-    };
+  : undefined;
 
 // Past localstorage keys:
 // * "feedback_wQ1KR7": "Help us shape the future of Osmosis." Give us feedback -> https://tally.so/r/wQ1KR7
