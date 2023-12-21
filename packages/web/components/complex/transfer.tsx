9a8254ec6079a586a57be19f07144a1e13ec47f6
Jose Felix <jfelx@outlook.com>
2023-12-21 01:58:34 -0400

    fix: nomic network animation

--- packages/web/components/complex/transfer.tsx
+++ packages/web/components/complex/transfer.tsx
@@ -1,4 +1,3 @@
-import { Menu } from "@headlessui/react";
 import { Bech32Address } from "@keplr-wallet/cosmos";
 import { CoinPretty, PricePretty, RatePretty } from "@keplr-wallet/unit";
 import classNames from "classnames";
@@ -9,12 +8,12 @@ import { ReactNode } from "react";
 import { useRef, useState } from "react";
 import { useClickAway } from "react-use";
 
-import { BridgeAnimation } from "~/components/animation/bridge";
 import { Icon } from "~/components/assets";
 import { GradientView } from "~/components/assets/gradient-view";
 import { Button } from "~/components/buttons";
 import IconButton from "~/components/buttons/icon-button";
 import { SwitchWalletButton } from "~/components/buttons/switch-wallet";
+import { BridgeFromToNetwork } from "~/components/complex/bridge-from-to-network";
 import { CheckBox, MenuDropdown, MenuToggle } from "~/components/control";
 import { InputBox } from "~/components/input";
 import SkeletonLoader from "~/components/skeleton-loader";
@@ -25,7 +24,7 @@ import { useWindowSize } from "~/hooks";
 import { truncateEthAddress } from "~/integrations/ethereum/metamask-utils";
 import { WalletDisplay } from "~/integrations/wallets";
 import { useStore } from "~/stores";
-import { formatICNSName, truncateString } from "~/utils/string";
+import { formatICNSName } from "~/utils/string";
 
 type PathSource = "counterpartyAccount" | "account";
 
@@ -273,136 +272,12 @@ export const Transfer = observer(
         )}
 
         <div className="body1 relative flex w-full flex-col gap-12 text-osmoverse-400 transition-opacity duration-300">
-          <BridgeAnimation />
-
-          <div className="flex w-full overflow-hidden text-center">
-            {/* From Network */}
-            <div className="z-10 flex flex-1 flex-col items-center gap-4 pl-4 md:pl-8 sm:pl-0">
-              <span
-                className={classNames(
-                  "whitespace-nowrap text-osmoverse-100 transition-opacity duration-300",
-                  "md:subtitle2"
-                )}
-              >
-                {t("assets.transfer.from")}{" "}
-                {truncateString(from.networkName, 22)}
-              </span>
-
-              {from.iconUrl && (
-                <div
-                  className="transition-opacity duration-300"
-                  style={overlayedIconSize}
-                >
-                  <Image
-                    alt="token icon"
-                    src={from.iconUrl}
-                    {...overlayedIconSize}
-                  />
-                </div>
-              )}
-            </div>
-
-            {/* Provider select */}
-            {filteredBridgeProviders && selectedProvider && (
-              <div
-                className="absolute left-1/2 z-20 flex -translate-x-[33%] transform place-content-between items-center"
-                title={t("assets.ibcTransfer.provider")}
-              >
-                {filteredBridgeProviders?.length === 0 ? (
-                  <p className="subtitle-2 flex flex-col items-center gap-4">
-                    <span className="rounded-lg bg-osmoverse-700 px-2 text-osmoverse-200">
-                      {selectedProvider.name}
-                    </span>
-                    <Image
-                      src={selectedProvider.logo}
-                      alt={`${selectedProvider.name} logo`}
-                      {...overlayedIconSize}
-                    />
-                  </p>
-                ) : (
-                  <Menu>
-                    {({ open }) => (
-                      <div className="relative">
-                        <Menu.Button className="flex flex-col items-center gap-4">
-                          <div className="subtitle-2 flex items-center gap-1.5 rounded-lg bg-osmoverse-700 px-2 text-osmoverse-200">
-                            {selectedProvider.name}
-                            <Icon
-                              className="flex shrink-0 items-center"
-                              id={open ? "chevron-up" : "chevron-down"}
-                              height={10}
-                              width={10}
-                            />
-                          </div>
-                          <Image
-                            src={selectedProvider.logo}
-                            alt={`${selectedProvider.name} logo`}
-                            {...overlayedIconSize}
-                          />
-                        </Menu.Button>
-
-                        <Menu.Items className="absolute top-1/3 -right-px mb-2 flex w-max select-none flex-col overflow-hidden rounded-xl border border-osmoverse-700 bg-osmoverse-700">
-                          {filteredBridgeProviders.map((provider, index) => (
-                            <Menu.Item key={provider.id}>
-                              {({ active }) => (
-                                <button
-                                  onClick={() =>
-                                    onSelectBridgeProvider?.(provider)
-                                  }
-                                  className={classNames(
-                                    "flex cursor-pointer items-center gap-2 py-1 pl-2  pr-4 transition-colors",
-                                    {
-                                      "bg-osmoverse-600": active,
-                                      "rounded-b-xlinset":
-                                        index ===
-                                        filteredBridgeProviders.length - 1,
-                                    }
-                                  )}
-                                >
-                                  <div className="flex flex-shrink-0">
-                                    <Image
-                                      src={provider.logo}
-                                      alt={`${provider.name} logo`}
-                                      width={16}
-                                      height={16}
-                                    />
-                                  </div>
-                                  {provider.name}
-                                </button>
-                              )}
-                            </Menu.Item>
-                          ))}
-                        </Menu.Items>
-                      </div>
-                    )}
-                  </Menu>
-                )}
-              </div>
-            )}
-
-            {/* To Network */}
-            <div className="z-10 flex flex-1 flex-col items-center gap-4 pr-5 text-center md:pr-8 sm:-mr-1 sm:pr-0">
-              <span
-                className={classNames(
-                  "w-fit whitespace-nowrap text-osmoverse-100 transition-opacity duration-300",
-                  "md:subtitle2"
-                )}
-              >
-                {t("assets.transfer.to")} {truncateString(to.networkName, 22)}
-              </span>
-              {to.iconUrl && (
-                <div
-                  className="transition-opacity duration-300"
-                  style={overlayedIconSize}
-                >
-                  <Image
-                    alt="token icon"
-                    src={to.iconUrl}
-                    {...overlayedIconSize}
-                  />
-                </div>
-              )}
-            </div>
-          </div>
+          <BridgeFromToNetwork
+            transferPath={[from, to]}
+            bridgeProviders={bridgeProviders}
+            onSelectBridgeProvider={onSelectBridgeProvider}
+            selectedBridgeProvidersId={selectedBridgeProvidersId}
+          />
 
           <div className="z-10 flex w-full gap-4 pr-7 pl-6 text-center md:pr-9 sm:pr-0 sm:pl-0">
             {/* From Address */}
@@ -437,7 +312,7 @@ export const Transfer = observer(
             {/* To Address */}
             <div
               className={classNames(
-                "flex h-full w-full rounded-2xl border border-white-faint py-2.5 text-center transition-width",
+                "flex w-full rounded-2xl border border-white-faint py-2.5 text-center transition-width",
                 {
                   "w-3/4": isEditingWithdrawAddr,
                 }
