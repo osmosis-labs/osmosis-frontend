f278cafa27d56c2bb1ee1d87dd00921286367fa3
Davide Segullo <davidesegullo@gmail.com>
2024-01-05 22:15:28 +0100

    feat: :sparkles: add balance stats query

--- packages/web/components/your-balance/your-balance.tsx
+++ packages/web/components/your-balance/your-balance.tsx
@@ -1,4 +1,4 @@
-import { IntPretty } from "@keplr-wallet/unit";
+import { Dec, IntPretty, PricePretty } from "@keplr-wallet/unit";
 import { observer } from "mobx-react-lite";
 import Image from "next/image";
 import Link from "next/link";
@@ -13,6 +13,8 @@ import {
 } from "~/hooks";
 import { TokenCMSData } from "~/server/queries/external";
 import { useStore } from "~/stores";
+import { formatPretty } from "~/utils/formatter";
+import { api } from "~/utils/trpc";
 
 interface YourBalanceProps {
   denom: string;
@@ -52,11 +54,12 @@ const YourBalance = observer(
 
     return (
       <section
-        className={`${
-          details?.stakingURL ? "flex" : "hidden"
-        } ${className} flex flex-col items-start gap-12 self-stretch rounded-5xl bg-osmoverse-850 p-8`}
+        className={`${className} flex flex-col items-start gap-12 self-stretch rounded-5xl bg-osmoverse-850 p-8`}
       >
-        {/* <BalanceStats /> */}
+        <BalanceStats
+          denom={denom}
+          tokenDetailsByLanguage={tokenDetailsByLanguage}
+        />
         {details?.stakingURL && (
           <div className="flex flex-col gap-6 self-stretch">
             <header>
@@ -170,70 +173,38 @@ const ActionButton = ({
   );
 };
 
-/* const BalanceStats = () => {
+const BalanceStats = (props: YourBalanceProps) => {
+  const { denom } = props;
   const { t } = useTranslation();
+  const { chainStore, accountStore, priceStore } = useStore();
+  const account = accountStore.getWallet(chainStore.osmosis.chainId);
+
+  const fiat = priceStore.getFiatCurrency(priceStore.defaultVsCurrency);
+
+  const { data } = api.edge.assets.getAssetInfo.useQuery({
+    findMinDenomOrSymbol: denom,
+    userOsmoAddress: account?.address,
+  });
 
   return (
     <div className="flex items-start justify-between gap-12 self-stretch 2xl:flex-col 2xl:items-stretch 2xl:gap-6">
       <div className="flex flex-col items-start gap-3">
-        <h6 className="text-lg font-h6 leading-6">
+        <h6 className="text-subtitle1 font-subtitle1 leading-6">
           {t("tokenInfos.yourBalance")}
         </h6>
         <div className="flex flex-col items-start gap-1">
-          <h4 className="text-2xl font-h4 leading-9 text-osmoverse-100">
-            $52,967.23
+          <h4 className="text-h4 font-h4 leading-9 text-osmoverse-100">
+            {data?.currentPrice && data.amount && fiat
+              ? formatPretty(
+                  new PricePretty(fiat, data.currentPrice.mul(data.amount))
+                )
+              : formatPretty(new PricePretty(fiat!, new Dec(0)))}
           </h4>
-          <p className="text-base font-subtitle1 leading-6 text-osmoverse-300">
-            104,941 OSMO
+          <p className="text-subtitle1 font-subtitle1 leading-6 text-osmoverse-300">
+            {data?.amount ? formatPretty(data?.amount) : `0 ${denom}`}
           </p>
-        </div>
-      </div>
-      <div className="mr-10 flex flex-1 flex-wrap items-start justify-between gap-12 2xl:mr-0 2xl:grid 2xl:grid-cols-tokenStats md:gap-8 1.5xs:flex-col">
-        <div className="flex flex-grow flex-col gap-2">
-          <p className="text-base font-subtitle1 leading-6 tracking-wide">
-            {t("tokenInfos.pastDaysReturns")}
-          </p>
-          <div className="flex items-center gap-1">
-            <RightArrowIcon
-              classes={{
-                container: "text-bullish-500 -rotate-45",
-              }}
-            />
-            <h5 className="text-xl font-h5 leading-8 tracking-[0.18px] text-bullish-500">
-              $184,17
-            </h5>
-          </div>
-        </div>
-        <div className="flex flex-grow flex-col gap-2">
-          <p className="text-base font-subtitle1 leading-6 tracking-wide">
-            {t("tokenInfos.allTimeReturns")}
-          </p>
-          <div className="flex items-center gap-1">
-            <RightArrowIcon
-              classes={{
-                container: "text-error rotate-45",
-              }}
-            />
-            <h5 className="text-xl font-h5 leading-8 tracking-[0.18px] text-error">
-              $4,829.10
-            </h5>
-          </div>
-        </div>
-        <div className="flex flex-grow flex-col gap-2">
-          <p className="text-base font-subtitle1 leading-6 tracking-wide">
-            {t("tokenInfos.averagePrice")}
-          </p>
-          <h5 className="text-xl font-h5 leading-8 tracking-[0.18px]">$1.36</h5>
-        </div>
-        <div className="flex flex-grow flex-col gap-2">
-          <p className="text-base font-subtitle1 leading-6 tracking-wide">
-            Portfolio %
-          </p>
-          <h5 className="text-xl font-h5 leading-8 tracking-[0.18px]">
-            51.28%
-          </h5>
         </div>
       </div>
     </div>
   );
-} */
+};
