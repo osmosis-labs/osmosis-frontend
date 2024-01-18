5d784cb59a45ca168b6b164a764f3fa0c9e5321d
Matt Upham <mcupham@gmail.com>
2024-01-18 15:38:52 -0800

    clean up mobile

--- packages/web/components/cards/stake-dashboard.tsx
+++ packages/web/components/cards/stake-dashboard.tsx
@@ -136,7 +136,7 @@ export const StakeDashboard: React.FC<{
         titleIcon={LearnMoreIconText}
         titleIconAction={() => setShowStakeLearnMoreModal(true)}
       >
-        <div className="flex w-full flex-row place-content-around items-center space-y-0 py-10 sm:flex-col sm:space-y-4 sm:py-4">
+        <div className="flex w-full flex-row gap-2 py-10 sm:flex-col sm:gap-6 sm:py-4">
           <StakeBalances
             title={t("stake.stakeBalanceTitle")}
             dollarAmount={fiatBalance}
@@ -208,7 +208,7 @@ const StakeBalances: React.FC<{
   }, [osmoAmount]);
 
   return (
-    <div className="flex flex-col items-start justify-center gap-1 text-left">
+    <div className="flex w-full flex-col items-start justify-center gap-1 text-left xl:items-center">
       <span className="caption text-sm text-osmoverse-200 md:text-xs">
         {title}
       </span>
