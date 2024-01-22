6445ae67b920aa770f48da7328388dcc3e6728ba
Jon Ator <jonathanator0@gmail.com>
2024-01-22 12:41:59 -0500

    break table controls sooner

--- packages/web/components/complex/all-pools-table-v2.tsx
+++ packages/web/components/complex/all-pools-table-v2.tsx
@@ -377,10 +377,10 @@ const TableControls: FunctionComponent<{
   useEffect(() => setSearchQuery(queryInput), [setSearchQuery, queryInput]);
 
   return (
-    <div className="flex w-full place-content-between items-center gap-5 1.5lg:flex-col 1.5lg:items-start">
+    <div className="flex w-full place-content-between items-center gap-5 xl:flex-col xl:items-start">
       <h5>{t("pools.allPools.title")}</h5>
 
-      <div className="flex h-12 flex-wrap gap-3 1.5lg:h-fit">
+      <div className="flex h-12 flex-wrap gap-3 xl:h-fit">
         <CheckboxSelect
           label={t("components.pool.title")}
           selectedOptionIds={poolTypesFilter as string[]}
