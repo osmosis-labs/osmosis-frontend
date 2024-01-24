ce1b6b4be5227aa3940be4e66c82fad40d381fb1
Davide Segullo <davidesegullo@gmail.com>
2024-01-24 22:40:42 +0100

    feat: :sparkles: add search filter inside url queries (#2720)
    added search filter inside pools page query filters.

--- packages/web/hooks/input/use-search-query-input.ts
+++ packages/web/hooks/input/use-search-query-input.ts
@@ -1,5 +1,6 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useEffect, useMemo } from "react";
 
+import { useQueryParamState } from "~/hooks/window/use-query-param-state";
 import type { Search } from "~/utils/search";
 
 import { useDebouncedState } from "../use-debounced-state";
@@ -8,19 +9,29 @@ import { useDebouncedState } from "../use-debounced-state";
  *  to perform a search remotely (is debounced). */
 export function useSearchQueryInput(debounceMs = 500) {
   // get selectable currencies for trading, including user balances if wallect connected
-  const [searchInput, setSearchInput_] = useState<string>("");
+  const [searchInput, setSearchInput_, isReady] = useQueryParamState<string>(
+    "s",
+    ""
+  );
 
   // generate debounced search from user inputs
   const [debouncedSearchInput, setDebouncedSearchInput] =
     useDebouncedState<string>("", debounceMs);
+
   const setSearchInput = useCallback(
     (input: string) => {
       const sanitizedSearchInput = input.replace(/#/g, "").trim();
       setSearchInput_(sanitizedSearchInput);
       setDebouncedSearchInput(sanitizedSearchInput);
     },
-    [setDebouncedSearchInput]
+    [setDebouncedSearchInput, setSearchInput_]
   );
+
+  useEffect(() => {
+    if (searchInput && isReady) {
+      setSearchInput(searchInput);
+    }
+  }, [isReady]);
 
   const queryInput: Search | undefined = useMemo(
     () => (debouncedSearchInput ? { query: debouncedSearchInput } : undefined),
