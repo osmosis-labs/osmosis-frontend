58dcaad59a36e82e36c2ea34976d08ad28af466c
Davide Segullo <davidesegullo@gmail.com>
2024-01-24 23:42:47 +0100

    fix: :mag: fix app base path

--- packages/web/next-seo.config.tsx
+++ packages/web/next-seo.config.tsx
@@ -6,6 +6,7 @@ import spriteSVGURL from "~/public/icons/sprite.svg";
 
 const SEO_VALUES = {
   SITE_URL: "https://osmosis.zone",
+  APP_URL: "https://app.osmosis.zone",
   TWITTER_HANDLE: "@osmosiszone",
   IMAGE_PREVIEW: "/images/preview.jpg",
   FAVICON: "/favicon.ico",
@@ -52,7 +53,7 @@ const SEO: React.FC = () => {
       description: t("seo.default.description"),
       images: [
         {
-          url: `${SEO_VALUES.SITE_URL}${SEO_VALUES.IMAGE_PREVIEW}`,
+          url: `${SEO_VALUES.APP_URL}${SEO_VALUES.IMAGE_PREVIEW}`,
           width: 1920,
           height: 1080,
           alt: "Osmosis",
