35fafe730b1bc321dfe3d38c60d12bc739f3d302
Jose Felix <jfelx@outlook.com>
2024-01-24 22:13:59 -0400

    feat: add feature flag and introduction component

---
+++ packages/web/components/one-click-trading/introducing-one-click.tsx
@@ -0,0 +1,41 @@
+import { Button, buttonCVA } from "~/components/buttons";
+import { Pill } from "~/components/indicators/pill";
+import { IntroSvg } from "~/components/one-click-trading/intro-one-click-svg";
+import { useTranslation } from "~/hooks";
+
+export const IntroducingOneClick = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex flex-col items-center gap-8 text-center">
+      <Pill className="!px-3 py-1 normal-case">
+        {t("oneClickTrading.introduction.newFeature")}
+      </Pill>
+      <h1 className="text-h5 font-h5">
+        {t("oneClickTrading.introduction.introducingTitle")}
+      </h1>
+      <h2 className="text-body2 font-body2 text-osmoverse-200">
+        {t("oneClickTrading.introduction.introducingSubtitle")}{" "}
+        <a
+          className={buttonCVA({
+            mode: "text",
+            className: "!inline w-auto px-0 text-body2 font-body2",
+          })}
+          // TODO: Add link
+        >
+          {t("oneClickTrading.introduction.learnMore")} ↗️
+        </a>
+      </h2>
+      <IntroSvg />
+      <Button className="w-fit px-10">
+        {t("oneClickTrading.introduction.startTradingButton")}
+      </Button>
+      <p className="text-caption text-osmoverse-300">
+        {t("oneClickTrading.introduction.activeHourLimit")} –{" "}
+        <Button mode="text" className="!inline">
+          {t("oneClickTrading.introduction.changeButton")}
+        </Button>
+      </p>
+    </div>
+  );
+};
