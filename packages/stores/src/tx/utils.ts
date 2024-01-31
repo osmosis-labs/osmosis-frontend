d093db2fbf0703ce7cc61c08aa27f6e82c30404d
Roman <roman@osmosis.team>
2024-01-31 00:54:19 -0700

    detect pool manager price impact

--- packages/stores/src/tx/utils.ts
+++ packages/stores/src/tx/utils.ts
@@ -1,9 +1,11 @@
 export function isSlippageError(tx: any): boolean {
   if (
     tx &&
-    tx.code === 7 &&
-    tx.codespace === "gamm" &&
-    tx.rawLog?.includes("token is lesser than min amount")
+    ((tx.code === 7 &&
+      tx.codespace === "gamm" &&
+      tx.rawLog?.includes("token is lesser than min amount")) ||
+      // https://github.com/osmosis-labs/osmosis/blob/14078febf2c1dd50c00110f2c2ac00d1fe9defb2/x/poolmanager/types/errors.go#L65-L67
+      tx.rawLog?.includes("price impact protection"))
   ) {
     return true;
   }
