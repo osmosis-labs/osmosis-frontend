cc0419ff781ff85d3261e03c784c0826b28fff89
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-02 12:03:21 +0100

    feat: replace background with osmoverse 1000

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -3,7 +3,6 @@ import { observer } from "mobx-react-lite";
 
 import { AdBanner } from "~/components/ad-banner";
 import ErrorBoundary from "~/components/error/error-boundary";
-import { ProgressiveSvgImage } from "~/components/progressive-svg-image";
 import { SwapTool } from "~/components/swap-tool";
 import { EventName } from "~/config";
 import { useAmplitudeAnalytics, useFeatureFlags } from "~/hooks";
@@ -18,34 +17,7 @@ const Home = () => {
 
   return (
     <main className="relative flex h-full items-center overflow-auto bg-osmoverse-900 py-2">
-      <div className="pointer-events-none fixed h-full w-full bg-home-bg-pattern bg-cover bg-repeat-x">
-        <svg
-          className="absolute h-full w-full lg:hidden"
-          pointerEvents="none"
-          viewBox="0 0 1300 900"
-          height="900"
-          preserveAspectRatio="xMidYMid slice"
-        >
-          <g>
-            <ProgressiveSvgImage
-              lowResXlinkHref="/images/osmosis-home-bg-low.png"
-              xlinkHref="/images/osmosis-home-bg.png"
-              x="56"
-              y="220"
-              width="578.7462"
-              height="725.6817"
-            />
-            <ProgressiveSvgImage
-              lowResXlinkHref={"/images/osmosis-home-fg-low.png"}
-              xlinkHref={"/images/osmosis-home-fg.png"}
-              x={"61"}
-              y={"682"}
-              width={"448.8865"}
-              height={"285.1699"}
-            />
-          </g>
-        </svg>
-      </div>
+      <div className="fixed h-full w-full bg-osmoverse-1000"></div>
       <div className="my-auto flex h-auto w-full items-center">
         <div className="ml-auto mr-[15%] flex w-[27rem] flex-col gap-4 lg:mx-auto md:mt-mobile-header">
           {featureFlags.swapsAdBanner && <SwapAdsBanner />}
