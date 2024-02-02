231bfd1c1253754e7f047bb13cfcdaa47b909a0a
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-02 13:57:07 +0100

    feat: add static layout without chart

--- packages/web/pages/index.tsx
+++ packages/web/pages/index.tsx
@@ -17,7 +17,66 @@ const Home = () => {
 
   return (
     <main className="relative flex h-full items-center overflow-auto bg-osmoverse-900 py-2">
-      <div className="fixed h-full w-full bg-osmoverse-1000"></div>
+      <div className="fixed flex h-full w-full items-center bg-osmoverse-1000">
+        <div className="flex max-w-5xl flex-1 flex-col gap-4 pl-8">
+          <div className="flex flex-col gap-3">
+            <span className="text-subtitle1 text-osmoverse-300">
+              Your total balance
+            </span>
+            <h3>$3,920,849.61</h3>
+            <span className="inline-flex items-center gap-3">
+              <span className="text-subititle1 text-bullish-500">
+                ↗️ $1,863.96 &#40;5.6%&#41;
+              </span>
+              <span className="text-subititle1 inline-flex items-center gap-1 text-wosmongton-200">
+                View assets
+                <Icon id="arrow-right" color="#B3B1FD" className="h-4 w-4" />
+              </span>
+            </span>
+          </div>
+          <div className="flex items-center rounded-5xl bg-osmoverse-900 p-8">
+            <div className="flex w-full justify-between">
+              <div className="flex items-center gap-16">
+                <div className="flex flex-col gap-1">
+                  <span className="inline-flex items-center gap-1">
+                    <h6 className="text-wosmongton-200">OSMO</h6>
+                    <Icon
+                      id="chevron-right"
+                      color="#B3B1FD"
+                      className="h-4 w-4"
+                    />
+                  </span>
+                  <h4>$1.51</h4>
+                  <span className="text-subititle1 text-bullish-500">
+                    ↗️ 10.28%
+                  </span>
+                </div>
+                <div className="flex flex-col gap-1">
+                  <span className="inline-flex items-center gap-1">
+                    <h6 className="text-wosmongton-200">ATOM</h6>
+                    <Icon
+                      id="chevron-right"
+                      color="#B3B1FD"
+                      className="h-4 w-4"
+                    />
+                  </span>
+                  <h4>$10.12</h4>
+                  <span className="text-subititle1 text-bullish-500">
+                    ↗️ 7.71%
+                  </span>
+                </div>
+              </div>
+              <div className="flex items-center gap-1">
+                <button className="text-caption text-osmoverse-400">1H</button>
+                <button className="text-caption text-osmoverse-400">1D</button>
+                <button className="text-caption text-osmoverse-400">7D</button>
+                <button className="text-caption text-osmoverse-400">30D</button>
+                <button className="text-caption text-osmoverse-400">1Y</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
       <div className="my-auto flex h-auto w-full items-center">
         <div className="ml-auto mr-[15%] flex w-[27rem] flex-col gap-4 lg:mx-auto md:mt-mobile-header">
           {featureFlags.swapsAdBanner && <SwapAdsBanner />}
