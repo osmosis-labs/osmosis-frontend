f56d51d3a802a90b3e00d8825d25da917b4a1cc6
Jon Ator <jonathanator0@gmail.com>
2024-02-04 02:55:20 -0500

    Use sidecar pool provider (#2748)
    * use sidecar pool provider
    
    * Optimize pools page query (#2750)

--- packages/web/server/queries/complex/pools/market.ts
+++ packages/web/server/queries/complex/pools/market.ts
@@ -6,7 +6,6 @@ import { DEFAULT_LRU_OPTIONS } from "~/config/cache";
 
 import { queryPoolsFees } from "../../imperator";
 import { DEFAULT_VS_CURRENCY } from "../assets/config";
-import { getPools, Pool, PoolFilter } from "./index";
 
 export type PoolMarketMetrics = Partial<{
   volume7dUsd: PricePretty;
@@ -16,35 +15,9 @@ export type PoolMarketMetrics = Partial<{
   feesSpent7dUsd: PricePretty;
 }>;
 
-/** Get metrics for individual pool. */
-export async function getPoolMarketMetric(
-  poolId: string
-): Promise<PoolMarketMetrics | undefined> {
-  const poolMetrics = await getCachedPoolMarketMetricsMap();
-  return poolMetrics.get(poolId);
-}
-
-/** Maps and adds general supplementary market data such as current price and market cap to the given type.
- *  If no pools provided, they will be fetched and passed the given search params. */
-export async function mapGetPoolMarketMetrics<TPool extends Pool>({
-  pools,
-  ...params
-}: {
-  pools?: TPool[];
-} & PoolFilter = {}): Promise<(PoolMarketMetrics & TPool)[]> {
-  if (!pools) pools = (await getPools(params)) as TPool[];
-
-  const metrics = await getCachedPoolMarketMetricsMap();
-
-  return pools.map((pool) => ({
-    ...pool,
-    ...metrics.get(pool.id),
-  }));
-}
-
 const metricPoolsCache = new LRUCache<string, CacheEntry>(DEFAULT_LRU_OPTIONS);
 /** Get a cached Map with pool IDs mapped to market metrics for that pool. */
-function getCachedPoolMarketMetricsMap(): Promise<
+export function getCachedPoolMarketMetricsMap(): Promise<
   Map<string, PoolMarketMetrics>
 > {
   return cachified({
