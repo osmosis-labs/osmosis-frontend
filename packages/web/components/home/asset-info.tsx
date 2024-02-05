d85332128c6a7f86ca3684699c0c67b48ca1aa99
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-05 21:39:06 +0100

    feat: create asset info component

---
+++ packages/web/components/home/asset-info.tsx
@@ -0,0 +1,36 @@
+import { Dec } from "@keplr-wallet/unit";
+import Link from "next/link";
+import React from "react";
+
+import { Icon } from "~/components/assets";
+import { formatPretty } from "~/utils/formatter";
+
+interface AssetInfoProps {
+  assetPrice: any;
+  denom: string;
+}
+
+export const AssetInfo = ({ assetPrice, denom }: AssetInfoProps) => {
+  const isNumberPositive = assetPrice?.priceChange24h?.toDec().isPositive();
+  return (
+    <div className="flex flex-col gap-1">
+      <Link
+        href={`/assets/${denom}`}
+        className="inline-flex items-center gap-1"
+      >
+        <h6 className="text-wosmongton-200">{denom}</h6>
+        <Icon id="chevron-right" color="#B3B1FD" className="h-4 w-4" />
+      </Link>
+      <h4>{formatPretty(assetPrice?.currentPrice ?? new Dec(0))}</h4>
+      <span
+        className={isNumberPositive ? "text-bullish-400" : "text-osmoverse-500"}
+      >
+        {isNumberPositive ? "↗️ " : "↘ "}
+        {assetPrice?.priceChange24h
+          ?.maxDecimals(2)
+          .inequalitySymbol(false)
+          .toString() ?? ""}
+      </span>
+    </div>
+  );
+};
