35aadec9f5f9ebeeeb62c57c3291064272b0e9c1
Matt Upham <mcupham@gmail.com>
2024-02-08 16:39:08 -0800

    lint

--- packages/web/components/control/checkbox-shadcn.tsx
+++ packages/web/components/control/checkbox-shadcn.tsx
@@ -12,7 +12,7 @@ const Checkbox = React.forwardRef<
   <CheckboxPrimitive.Root
     ref={ref}
     className={classNames(
-      "focus-visible:ring-ring peer h-4 w-4 shrink-0 rounded-sm border border-primary shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground",
+      "focus-visible:ring-ring border-primary data-[state=checked]:bg-primary data-[state=checked]:text-primary-foreground peer h-4 w-4 shrink-0 rounded-sm border shadow focus-visible:outline-none focus-visible:ring-1 disabled:cursor-not-allowed disabled:opacity-50",
       className
     )}
     {...props}
--- packages/web/components/ui/switch.tsx
+++ packages/web/components/ui/switch.tsx
@@ -10,7 +10,7 @@ const Switch = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <SwitchPrimitives.Root
     className={classNames(
-      "focus-visible:ring-ring peer inline-flex h-8 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:ring-offset-background disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-osmoverse-600 data-[state=checked]:bg-wosmongton-500",
+      "focus-visible:ring-ring focus-visible:ring-offset-background peer inline-flex h-8 w-12 shrink-0 cursor-pointer items-center rounded-full border-2 border-transparent shadow-sm transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 data-[state=unchecked]:bg-osmoverse-600 data-[state=checked]:bg-wosmongton-500",
       className
     )}
     {...props}
