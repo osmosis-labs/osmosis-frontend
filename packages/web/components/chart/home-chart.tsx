80e2aacc0ab1a51b65c0d0ce62f946ffcbf63644
Davide Segullo <davidesegullo@gmail.com>
2024-02-11 23:33:08 +0100

    feat: :sparkles: add chart tooltip

--- packages/web/components/chart/home-chart.tsx
+++ packages/web/components/chart/home-chart.tsx
@@ -1,12 +1,17 @@
+import { Dec } from "@keplr-wallet/unit";
 import {
+  AreaData,
   ColorType,
   createChart,
   CrosshairMode,
   Time,
 } from "lightweight-charts";
+import { observer } from "mobx-react-lite";
 import { useEffect, useRef } from "react";
 
+import { useStore } from "~/stores";
 import { theme } from "~/tailwind.config";
+import { formatPretty } from "~/utils/formatter";
 
 type Data = {
   time: number;
@@ -19,16 +24,20 @@ interface HomeChartProps {
   data: Data[][];
 }
 
-const _getTransformedData = (data?: Data[]) =>
+const _getTransformedData = (data?: Data[]): AreaData[] =>
   data
     ? data.map((originalData) => ({
         time: originalData.time as Time,
         value: originalData.close,
+        customValues: {
+          denom: originalData.denom,
+        },
       }))
     : [];
 
-export const HomeChart = ({ data }: HomeChartProps) => {
+export const HomeChart = observer(({ data }: HomeChartProps) => {
   const [data1, data2] = data;
+  const { priceStore } = useStore();
   const firstSeriesData = _getTransformedData(data1);
   const secondSeriesData = _getTransformedData(data2);
 
@@ -53,6 +62,7 @@ export const HomeChart = ({ data }: HomeChartProps) => {
       handleScale: false,
       timeScale: {
         timeVisible: true,
+        secondsVisible: false,
       },
     });
 
@@ -74,8 +84,92 @@ export const HomeChart = ({ data }: HomeChartProps) => {
     firstSeries.setData(firstSeriesData);
     secondSeries.setData(secondSeriesData);
 
-    return () => chart.remove();
+    const toolTipWidth = 80;
+    const toolTipHeight = 80;
+    const toolTipMargin = 15;
+
+    const toolTip = document.createElement("div");
+    toolTip.className =
+      "rounded-xl bg-osmoverse-1000 absolute hidden p-2 left-3 top-3 pointer-events-none z-[1000] drop-shadow-xl";
+    chartContainer.current?.appendChild(toolTip);
+
+    chart.subscribeCrosshairMove((param) => {
+      if (!chartContainer.current) {
+        return;
+      }
+
+      if (
+        param.point === undefined ||
+        !param.time ||
+        param.point.x < 0 ||
+        param.point.x > chartContainer.current.clientWidth ||
+        param.point.y < 0 ||
+        param.point.y > chartContainer.current.clientHeight
+      ) {
+        toolTip.style.display = "none";
+      } else {
+        toolTip.style.display = "block";
+        const firstSeriesData = param.seriesData.get(firstSeries) as AreaData;
+        const secondSeriesData = param.seriesData.get(secondSeries) as AreaData;
+
+        toolTip.innerHTML = `
+        <div class="flex flex-row gap-6">
+          <div>
+            <div class="text-wosmongton-300 text-body2 font-body2 font-medium">${
+              firstSeriesData.customValues?.denom
+            }</div>
+            <div class="text-white text-h6 font-h6 whitespace-nowrap">
+            ${priceStore.getFiatCurrency(priceStore.defaultVsCurrency)?.symbol}
+              ${formatPretty(new Dec(firstSeriesData.value), {
+                maxDecimals: 2,
+                notation: "compact",
+              })}
+            </div>
+          </div>
+          ${
+            secondSeriesData
+              ? `
+              <div>
+                <div class="text-ammelia-400 text-body2 font-body2 font-medium">${
+                  secondSeriesData.customValues?.denom
+                }</div>
+                <div class="text-white text-h6 font-h6 whitespace-nowrap">
+                  ${
+                    priceStore.getFiatCurrency(priceStore.defaultVsCurrency)
+                      ?.symbol
+                  }
+                  ${formatPretty(new Dec(secondSeriesData.value), {
+                    maxDecimals: 2,
+                    notation: "compact",
+                  })}
+                </div>
+              </div>
+              `
+              : ""
+          }
+        </div>
+        `;
+
+        const y = param.point.y;
+        let left = param.point.x + toolTipMargin;
+        if (left > chartContainer.current.clientWidth - toolTipWidth) {
+          left = param.point.x - toolTipMargin - toolTipWidth;
+        }
+
+        let top = y + toolTipMargin;
+        if (top > chartContainer.current.clientHeight - toolTipHeight) {
+          top = y - toolTipHeight - toolTipMargin;
+        }
+        toolTip.style.left = left + "px";
+        toolTip.style.top = top + "px";
+      }
+    });
+
+    return () => {
+      chart.remove();
+      toolTip.remove();
+    };
   }, [firstSeriesData, secondSeriesData]);
 
   return <div className="z-0" ref={chartContainer}></div>;
-};
+});
