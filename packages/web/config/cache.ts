7ca8008b678a659a9e993957f00653a314379bc1
Jose Felix <jfelx@outlook.com>
2024-02-13 14:29:17 -0400

    feat: Add My Pools TRPC Query and Individually Cache Pools (#2800)
    * feat: add my pools to trpc
    
    * improvement: increase cache values
    
    * feat: cache pools individually for imperator
    
    * improvement: user pools query optimization
    
    * improvement: remove unused variable
    
    * improvement: do not display my pools on disconnected wallet
    
    * test: fix tests
    
    * fix: remove unused translations
    
    * Revert "fix: remove unused translations"
    
    This reverts commit 72caa47dd3f1050638712a97439e8f6d297969cc.
    
    * Revert "test: fix tests"
    
    This reverts commit 8b8577235eb11feaafdae6d572735ef39918c8f1.
    
    * feat: revert my pools in asset page
    
    * feat: co locate my pools section component to pools page
    
    * feat: add weighted pool information
    
    * feat: better error message from api failures
    
    * improvement: error message for fetch errors
    
    * fix: handle previous pools with no shares
    
    * fix: scroll into view when closing show more pools
    
    * fix: do not show positions sections if account is not connected
    
    * fix: do not show pools section if account is not connected
    
    * feat: remove wrong address for pools
    
    * feat: add stale while revalidate for pool providers

--- packages/web/config/cache.ts
+++ packages/web/config/cache.ts
@@ -14,5 +14,5 @@ export const LARGE_LRU_OPTIONS: LRUCache.Options<
   CacheEntry<unknown>,
   unknown
 > = {
-  max: 1_500,
+  max: 2_000,
 };
--- packages/web/utils/coin.ts
+++ packages/web/utils/coin.ts
@@ -1,5 +1,7 @@
 import { CoinPretty } from "@keplr-wallet/unit";
 
+import { sum } from "~/utils/math";
+
 /** Combines coins of the same denom into the same CoinPretty object by sum. */
 export function aggregateCoinsByDenom<Coin extends CoinPretty>(
   coins: Coin[]
@@ -16,5 +18,23 @@ export function aggregateCoinsByDenom<Coin extends CoinPretty>(
     }
     return coinMap;
   }, new Map<string, Coin>());
+  return Array.from(aggregate.values());
+}
+
+export function aggregateRawCoinsByDenom(
+  coins: { denom: string; amount: string }[]
+): { denom: string; amount: string }[] {
+  const aggregate = coins.reduce((coinMap, coin) => {
+    const existingCoin = coinMap.get(coin.denom);
+    if (existingCoin) {
+      coinMap.set(coin.denom, {
+        denom: coin.denom,
+        amount: sum([existingCoin.amount, coin.amount]).toString(),
+      });
+    } else {
+      coinMap.set(coin.denom, coin);
+    }
+    return coinMap;
+  }, new Map<string, { denom: string; amount: string }>());
   return Array.from(aggregate.values());
 }
