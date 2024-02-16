09328e0c9b22faa6c7981a37b9e5684b3289926a
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-16 12:10:31 +0100

    feat: add wallet not connected state

--- packages/web/pages/earn/index.tsx
+++ packages/web/pages/earn/index.tsx
@@ -1,9 +1,11 @@
+import classNames from "classnames";
 import { observer } from "mobx-react-lite";
 import Image from "next/image";
 import { useRouter } from "next/router";
 import { useMemo } from "react";
 import { useEffect } from "react";
 
+import { Button } from "~/components/buttons";
 import {
   FilterProvider,
   Filters,
@@ -21,7 +23,12 @@ import {
   Tabs,
 } from "~/components/earn/tabs";
 import { Spinner } from "~/components/loaders";
-import { useFeatureFlags, useNavBar, useTranslation } from "~/hooks";
+import {
+  useFeatureFlags,
+  useNavBar,
+  useTranslation,
+  useWalletSelect,
+} from "~/hooks";
 import useGetEarnStrategies from "~/hooks/use-get-earn-strategies";
 import { useStore } from "~/stores";
 import { formatPretty } from "~/utils/formatter";
@@ -47,6 +54,8 @@ function Earn() {
     areStrategiesLoading,
   } = useGetEarnStrategies(userOsmoAddress, isWalletConnected);
 
+  const { onOpenWalletSelect } = useWalletSelect();
+
   const defaultFilters: Filters = useMemo(
     () => ({
       tokenHolder: "all",
@@ -67,28 +76,62 @@ function Earn() {
   }, [earnPage, router]);
 
   return (
-    <div className="flex flex-col gap-10 py-10 pl-8 pr-9">
-      <div className="grid grid-cols-earnpage gap-6 lg:flex lg:flex-col">
-        <div className="flex max-h-[192px] items-end justify-start overflow-hidden rounded-3x4pxlinset bg-osmoverse-850 bg-gradient-earnpage-position-bg px-8 pt-7 pb-4 2xl:justify-between 1.5md:bg-none">
-          <EarnPosition
-            totalBalance={formatPretty(totalBalance)}
-            numberOfPositions={myStrategies.length}
-            isLoading={areQueriesLoading}
-          />
-          {/* <div className="h-full max-h-72 w-0.5 bg-osmoverse-825" />
+    <div className="relative flex flex-col gap-10 py-10 pl-8 pr-9">
+      {!isWalletConnected && (
+        <Image
+          src={"/images/staking-apr-full.svg"}
+          className="absolute right-36 top-16 z-0 -rotate-[75deg] 1.5md:hidden"
+          alt="Staking graphic"
+          width={402}
+          height={286}
+        />
+      )}
+
+      {isWalletConnected ? (
+        <div className="grid grid-cols-earnpage gap-6 lg:flex lg:flex-col">
+          <div className="flex max-h-[192px] items-end justify-start overflow-hidden rounded-3x4pxlinset bg-osmoverse-850 bg-gradient-earnpage-position-bg px-8 pt-7 pb-4 2xl:justify-between 1.5md:bg-none">
+            <EarnPosition
+              totalBalance={formatPretty(totalBalance)}
+              numberOfPositions={myStrategies.length}
+              isLoading={areQueriesLoading}
+            />
+            {/* <div className="h-full max-h-72 w-0.5 bg-osmoverse-825" />
           <EarnAllocation /> */}
-          <p className="ml-auto max-w-[160px] text-right text-body2 font-medium text-osmoverse-200 2xl:hidden">
-            {t("earnPage.lookBelow")}
-          </p>
-          <Image
-            src={"/images/staking-apr-full.svg"}
-            alt="Staking image"
-            width={298}
-            height={212}
-            className="translate-x-8 translate-y-10 -rotate-[75deg] overflow-visible object-cover 2xl:object-contain 1.5md:hidden"
-          />
+            <p className="ml-auto max-w-[160px] text-right text-body2 font-medium text-osmoverse-200 2xl:hidden">
+              {t("earnPage.lookBelow")}
+            </p>
+            <Image
+              src={"/images/staking-apr-full.svg"}
+              alt="Staking image"
+              width={298}
+              height={212}
+              className="translate-x-8 translate-y-10 -rotate-[75deg] overflow-visible object-cover 2xl:object-contain 1.5md:hidden"
+            />
+          </div>
+          <EarnRewards totalUnclaimedRewards={totalUnclaimedRewards} />
         </div>
-        {/* <div className="hidden gap-x-7 rounded-3x4pxlinset bg-osmoverse-850 px-8 pt-7 pb-3 1.5xl:block">
+      ) : (
+        <div className="flex">
+          <div className="z-10 mb-5 flex flex-1 items-end gap-9 xl:flex-col">
+            <div className="flex flex-col gap-7">
+              <h4>Start earning</h4>
+              <p className="body2 text-osmoverse-200 opacity-50">
+                Phasellus libero nunc, sagittis vitae neque eu, ultrices dictum
+                sapien, phasellus egestas quam eu nunc gravida.
+              </p>
+            </div>
+            <Button
+              mode={"primary"}
+              onClick={() => onOpenWalletSelect(accountStore.osmosisChainId)}
+            >
+              {t("connectWallet")}
+            </Button>
+          </div>
+          <div className="flex-1 md:hidden"></div>
+        </div>
+      )}
+
+      {/* <div className="hidden gap-x-7 rounded-3x4pxlinset bg-osmoverse-850 px-8 pt-7 pb-3 1.5xl:block">
           <Tabs>
             <TabButtons>
               <TabButton withTextOpacity textClassName="!text-lg !leading-8">
@@ -112,11 +155,14 @@ function Earn() {
             </TabPanels>
           </Tabs>
         </div> */}
-        <EarnRewards totalUnclaimedRewards={totalUnclaimedRewards} />
-      </div>
+
       {!areStrategiesLoading && strategies ? (
         <FilterProvider defaultFilters={defaultFilters}>
-          <Tabs className="flex flex-col">
+          <Tabs
+            className={classNames("flex flex-col", {
+              "z-10": !isWalletConnected,
+            })}
+          >
             <TabButtons>
               <TabButton
                 withBasePadding
