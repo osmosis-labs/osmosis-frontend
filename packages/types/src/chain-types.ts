5f9aa43ab90d62c97788346c634e9f93e52d9abb
Jon Ator <jonathanator0@gmail.com>
2024-02-22 07:22:10 -0600

    Integrate asset list v2 (#2821)
    * update types
    
    * WIP integrate new asset type
    
    * fix
    
    * WIP integrate new asset list type
    
    * fix build
    
    * fix test
    
    * fix keplr chain tests
    
    * fix assets function
    
    * @JoseRFelix review

--- packages/types/src/chain-types.ts
+++ packages/types/src/chain-types.ts
@@ -2,6 +2,74 @@ import type { ChainInfo as BaseChainInfo } from "@keplr-wallet/types";
 
 import type { AppCurrency, FeeCurrency, StakeCurrency } from "./asset-types";
 
+export interface ChainList {
+  zone: string;
+  chains: Chain[];
+}
+
+export interface Chain {
+  chain_name: string;
+  status: string;
+  network_type: string;
+  pretty_name: string;
+  chain_id: string;
+  description?: string;
+  bech32_prefix: string;
+  bech32_config: Bech32Config;
+  slip44: number;
+  alternative_slip44s?: number[];
+  fees: {
+    fee_tokens: FeeToken[];
+  };
+  staking: {
+    staking_tokens: StakingToken[];
+    lock_duration?: LockDuration;
+  };
+  apis: {
+    rpc: Api[];
+    rest: Api[];
+  };
+  explorers: Explorer[];
+  features: string[];
+}
+
+interface Bech32Config {
+  bech32PrefixAccAddr: string;
+  bech32PrefixAccPub: string;
+  bech32PrefixValAddr: string;
+  bech32PrefixValPub: string;
+  bech32PrefixConsAddr: string;
+  bech32PrefixConsPub: string;
+}
+
+interface FeeToken {
+  denom: string;
+  fixed_min_gas_price?: number;
+  low_gas_price?: number;
+  average_gas_price?: number;
+  high_gas_price?: number;
+  gas_costs?: {
+    cosmos_send: number;
+    ibc_transfer: number;
+  };
+}
+
+interface StakingToken {
+  denom: string;
+}
+
+interface LockDuration {
+  time: string;
+}
+
+interface Api {
+  address: string;
+}
+
+interface Explorer {
+  tx_page: string;
+}
+
 export interface ChainInfo extends BaseChainInfo {
   prettyChainName: string;
 }
