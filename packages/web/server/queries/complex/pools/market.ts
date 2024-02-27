166364ae1bca48e082e8783bd9c830db78377d57
Jon Ator <jonathanator0@gmail.com>
2024-02-27 11:24:06 -0500

    allow query to fail

--- packages/web/server/queries/complex/pools/market.ts
+++ packages/web/server/queries/complex/pools/market.ts
@@ -26,36 +26,21 @@ export function getCachedPoolMarketMetricsMap(): Promise<
     staleWhileRevalidate: 1000 * 60 * 60, // 1 hour
     getFreshValue: async () => {
       const map = new Map<string, PoolMarketMetrics>();
-
       // append fee revenue data to volume data
-      try {
-        const poolsFees = await queryPoolsFees();
-        poolsFees.data.forEach(
-          ({
-            pool_id,
-            volume_24h,
-            volume_7d,
-            fees_spent_24h,
-            fees_spent_7d,
-          }) => {
-            map.set(pool_id, {
-              volume24hUsd: new PricePretty(DEFAULT_VS_CURRENCY, volume_24h),
-              volume7dUsd: new PricePretty(DEFAULT_VS_CURRENCY, volume_7d),
-              feesSpent24hUsd: new PricePretty(
-                DEFAULT_VS_CURRENCY,
-                fees_spent_24h
-              ),
-              feesSpent7dUsd: new PricePretty(
-                DEFAULT_VS_CURRENCY,
-                fees_spent_7d
-              ),
-            });
-          }
-        );
-      } catch (err) {
-        // Return empty map if it fails
-        console.error("Failed to fetch pool metrics", err);
-      }
+      const poolsFees = await queryPoolsFees();
+      poolsFees.data.forEach(
+        ({ pool_id, volume_24h, volume_7d, fees_spent_24h, fees_spent_7d }) => {
+          map.set(pool_id, {
+            volume24hUsd: new PricePretty(DEFAULT_VS_CURRENCY, volume_24h),
+            volume7dUsd: new PricePretty(DEFAULT_VS_CURRENCY, volume_7d),
+            feesSpent24hUsd: new PricePretty(
+              DEFAULT_VS_CURRENCY,
+              fees_spent_24h
+            ),
+            feesSpent7dUsd: new PricePretty(DEFAULT_VS_CURRENCY, fees_spent_7d),
+          });
+        }
+      );
 
       return map;
     },
