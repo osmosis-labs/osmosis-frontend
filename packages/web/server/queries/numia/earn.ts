6791e26632e1de83c560371c0e27eaaf4173bb15
fabry-js <vivaldifabrizio10@gmail.com>
2024-02-29 13:18:54 +0100

    feat: implement new data types

--- packages/web/server/queries/numia/earn.ts
+++ packages/web/server/queries/numia/earn.ts
@@ -6,10 +6,12 @@ import { Asset } from "~/server/queries/complex/assets";
 import { NUMIA_BASE_URL } from ".";
 
 export const EarnStrategyCategories = [
-  "Perps Liquidity",
+  "Lending",
+  "Trading Valut",
   "Staking",
   "Liquid Staking",
-  "Managed Liquidity",
+  "Perp LP",
+  "LP",
 ] as const;
 
 export type EarnStrategyCategory = (typeof EarnStrategyCategories)[number];
@@ -48,7 +50,7 @@ export type RawEarnStrategy = {
   provider: EarnStrategyProvider;
   type: EarnStrategyType;
   lockDuration: number;
-  tvl: number;
+  tvl: RawStrategyTVL;
   apy: number;
   tokenDenoms: EarnStrategyToken[];
   rewardDenoms: EarnStrategyToken[];
@@ -77,7 +79,9 @@ export interface RawStrategyAPY {
 }
 
 export interface RawStrategyTVL {
-  tvl: number;
+  tvlUsd: string;
+  maxTvlUsd?: string;
+  assets: { coinMinimalDenom: string; tvl: string; maxTvl?: string }[];
 }
 
 export interface StrategyAPY {
@@ -85,7 +89,133 @@ export interface StrategyAPY {
 }
 
 export interface StrategyTVL {
-  tvl: PricePretty;
+  tvlUsd: PricePretty;
+  maxTvlUsd?: PricePretty;
+  assets: {
+    coinMinimalDenom: string;
+    tvl: PricePretty;
+    maxTvl?: PricePretty;
+  }[];
+}
+
+/**
+ * Raw response data from the GitHub CMS.
+ */
+export interface RawStrategyCMSData {
+  id: string;
+  name: string;
+  platfrom: string;
+  category: EarnStrategyCategory;
+  type: EarnStrategyType;
+  link: string;
+  contract: string;
+  tvl: string;
+  apr: string;
+  lockDuration: string;
+  riskLevel: number;
+  riskReportUrl: string;
+  startDateTimeUtc: string;
+  unlisted: boolean;
+  disabled: boolean;
+  message: string;
+  depositDenoms: { coinMinimalDenom: string }[];
+  positionDenoms: { coinMinimalDenom: string }[];
+  rewardDenoms: { coinMinimalDenom: string }[];
+  tags: string[];
+}
+
+/**
+ * Processed response data from the GitHub CMS.
+ */
+export interface StrategyCMSData {
+  /**
+   * Unique identifier for the strategy
+   */
+  id: string;
+  /**
+   * Display name for the strategy
+   */
+  name: string;
+  /**
+   * Platform providing the earn strategy.
+   */
+  platfrom: string;
+  /**
+   * Broad category classification for the strategy.
+   *
+   * The currently accepted 'categories' are:
+   * - Lending: The assets are lent out to borrowers.
+   * - Trading Vault: The assets are actively managed by a vault controller. The assets are under complete control of the agent, and can be traded, lent, staked, provided as liquidity, etc.
+   * - Staking: The assets are locked into a crypto platfrom specifically for concensus.
+   * - Liquid Staking: The assets are staked and an economically representative derivative asset is also minted.
+   * - Perp LP: The assets provide liquidity for a perpetual futures contract market.
+   * - LP: The assets provide liquidity for a liquidity pool.
+   */
+  category: EarnStrategyCategory;
+  /**
+   * Further classification of the strategy.
+   */
+  type: EarnStrategyType;
+  /**
+   * URL for user participation interface.
+   */
+  link: string;
+  /**
+   * Primary contract for the strategy
+   */
+  contract: string;
+  /**
+   * Duration assets are locked (ISO 8601).
+   */
+  lockDuration: string;
+  /**
+   * Risk level indicator (0 to 1).
+   */
+  riskLevel: number;
+  /**
+   * URL to risk report on Google Sheets.
+   */
+  riskReportUrl: string;
+  /**
+   * Start date and time (UTC) of the strategy.
+   */
+  startDateTimeUtc: string;
+  /**
+   * Visibility status of the strategy.
+   */
+  unlisted: boolean;
+  /**
+   * Interaction status with the strategy.
+   */
+  disabled: boolean;
+  /**
+   * Important messaging related to the strategy.
+   */
+  message: string;
+  /**
+   * Array describing assets deposited for participation in the strategy.
+   */
+  depositAssets: Asset[];
+  /**
+   * Array describing assets representing a position in the strategy.
+   */
+  positionAssets: Asset[];
+  /**
+   * Array describing rewarded assets for participating in the strategy.
+   */
+  rewardAssets: Asset[];
+  /**
+   * Array of tags associated with the strategy.
+   * The currently accepted tags are:
+   *
+   * - Stablecoin: indicating that the asset(s) required by the strategy is a stablecoin of a world fiat currency.
+   * - Blue Chip: (top 50 mcap) indicating that one or more of the assets required for deposit in the strategy are of a high Market Capitalization--in this case, ranked among the top 50 on CoinGecko.
+   * - Correlated: indicating that all assets that are deposited or represent the position would follow a similar price action due to having a common relative asset. For example:
+   * - - USDC/USDT LP is correlated because both the USDC and USDT prices are meant to follow the same asset (i.e., U.S. Dollar), or
+   * - - Liquid Staking strategies are also considered correlated, because the staked token and the LST are closely related, even though only the LST is accruing relative value. In contrast, dpeositing, say, ETH tokens to mint FRAX tokens is not correlated, because FRAX price follows USD--not ETH.
+   */
+  tags: string[];
+  hasLockingDuration?: boolean;
 }
 
 export interface EarnStrategyBalance {
@@ -110,7 +240,7 @@ export interface EarnStrategy {
   involvedTokens: Asset[];
   rewardTokens: Asset[];
   lockDuration: number;
-  tvl: PricePretty;
+  tvl: StrategyTVL;
   apy: RatePretty;
   risk: number;
   balance: PricePretty;
