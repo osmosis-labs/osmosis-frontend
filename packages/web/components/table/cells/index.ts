5074b38c866b2c07fed505e7724e2602068802a9
Jon Ator <jonathanator0@gmail.com>
2024-02-29 18:04:01 -0500

    delete old pools table

--- packages/web/components/table/cells/index.ts
+++ packages/web/components/table/cells/index.ts
@@ -1,6 +1,5 @@
 export * from "./asset-name";
 export * from "./balance";
-export * from "./metric-loader-cell";
 export * from "./pool-composition";
 export * from "./pool-quick-actions";
 export * from "./price-cell";
