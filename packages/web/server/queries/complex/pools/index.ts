747d4137f5109bf54fcefef8f537ace89d8b9826
Jon Ator <jonathanator0@gmail.com>
2024-03-01 15:49:12 -0500

    get pools from indexer

--- packages/web/server/queries/complex/pools/index.ts
+++ packages/web/server/queries/complex/pools/index.ts
@@ -2,10 +2,10 @@ import { CoinPretty, Dec, PricePretty, RatePretty } from "@keplr-wallet/unit";
 import { z } from "zod";
 
 import { IS_TESTNET } from "~/config/env";
-import { getPoolsFromSidecar } from "~/server/queries/complex/pools/providers/sidecar";
 import { search, SearchSchema } from "~/utils/search";
 
 import { PoolRawResponse } from "../../osmosis";
+import { getPoolsFromImperator } from "./providers/imperator";
 
 const allPooltypes = [
   "concentrated",
@@ -60,7 +60,7 @@ export async function getPool({ poolId }: { poolId: string }): Promise<Pool> {
  *  Params can be used to filter the results by a fuzzy search on the id, type, or coin denoms, as well as a specific id or type. */
 export async function getPools(
   params?: PoolFilter,
-  poolProvider: PoolProvider = getPoolsFromSidecar
+  poolProvider: PoolProvider = getPoolsFromImperator
 ): Promise<Pool[]> {
   let pools = await poolProvider({ poolIds: params?.poolIds });
 
