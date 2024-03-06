0dcbbed2c7f74cc598f08074028dfcef3304a35a
Jon Ator <jonathanator0@gmail.com>
2024-03-06 18:09:46 -0500

    cache batch loaders

--- packages/web/server/queries/complex/assets/price/providers/sidecar.ts
+++ packages/web/server/queries/complex/assets/price/providers/sidecar.ts
@@ -8,11 +8,11 @@ import {
   QUOTE_COIN_MINIMAL_DENOM,
 } from "~/server/queries/sidecar/prices";
 import { EdgeDataLoader } from "~/utils/batching";
-import { DEFAULT_LRU_OPTIONS } from "~/utils/cache";
+import { LARGE_LRU_OPTIONS } from "~/utils/cache";
 
 import { getPriceFromCoinGecko } from "./coingecko";
 
-const sidecarCache = new LRUCache<string, CacheEntry>(DEFAULT_LRU_OPTIONS);
+const sidecarCache = new LRUCache<string, CacheEntry>(LARGE_LRU_OPTIONS);
 
 /** Gets price from SQS query server. Currently only supports prices in USDC with decimals. Falls back to querying CoinGecko if not available.
  *  @throws if there's an issue getting the price. */
@@ -20,36 +20,51 @@ export function getPriceFromSidecar(asset: Asset) {
   return getPriceBatched(asset);
 }
 
-export function getPriceBatched(asset: Asset) {
-  const batchLoader = new EdgeDataLoader(
-    (coinMinimalDenoms: readonly string[]) =>
-      queryPrices(coinMinimalDenoms as string[]).then((priceMap) =>
-        coinMinimalDenoms.map((baseCoinMinimalDenom) => {
-          const price =
-            priceMap[baseCoinMinimalDenom][QUOTE_COIN_MINIMAL_DENOM];
+/** Prevent long-running batch loaders as recommended by `DataLoader` docs. */
+function getBatchLoader() {
+  return cachified({
+    cache: sidecarCache,
+    key: "sidecar-batch-loader",
+    ttl: 1000 * 60 * 10, // 10 minutes
+    getFreshValue: () =>
+      new EdgeDataLoader(
+        (coinMinimalDenoms: readonly string[]) =>
+          queryPrices(coinMinimalDenoms as string[]).then((priceMap) =>
+            coinMinimalDenoms.map((baseCoinMinimalDenom) => {
+              const price =
+                priceMap[baseCoinMinimalDenom][QUOTE_COIN_MINIMAL_DENOM];
 
-          if (price === "no routes were provided" || !price)
-            return new Error(
-              `No SQS price result for ${baseCoinMinimalDenom} and USDC`
-            );
-          else return price;
-        })
+              if (
+                price === "no routes were provided" ||
+                !price ||
+                new Dec(price).isZero()
+              )
+                return new Error(
+                  `No SQS price result for ${baseCoinMinimalDenom} and USDC`
+                );
+              else return price;
+            })
+          ),
+        {
+          // SQS imposes a limit on URI length from its Nginx configuration, so we impose a limit to avoid hitting that limit.
+          maxBatchSize: 30,
+        }
       ),
-    {
-      // SQS imposes a limit on URI length from its Nginx configuration, so we impose a limit to avoid hitting that limit.
-      maxBatchSize: 30,
-    }
-  );
+  });
+}
 
+export function getPriceBatched(asset: Asset) {
   return cachified({
     cache: sidecarCache,
     key: `coingecko-price-${asset.coinMinimalDenom}`,
     ttl: 1000 * 60, // 1 minute
     staleWhileRevalidate: 1000 * 60 * 2, // 2 minutes
     getFreshValue: () =>
-      batchLoader
-        .load(asset.coinMinimalDenom)
-        .then((price) => new Dec(price))
-        .catch(() => getPriceFromCoinGecko(asset)),
+      getBatchLoader().then((loader) =>
+        loader
+          .load(asset.coinMinimalDenom)
+          .then((price) => new Dec(price))
+          .catch(() => getPriceFromCoinGecko(asset))
+      ),
   });
 }
