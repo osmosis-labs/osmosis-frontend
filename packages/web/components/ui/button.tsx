101f42e87584b5522fff70a03b9cf7d5260786ba
Matt Upham <mcupham@gmail.com>
2024-03-11 12:49:39 -0700

    Cleanup

--- packages/web/components/ui/button.tsx
+++ packages/web/components/ui/button.tsx
@@ -37,7 +37,7 @@ const buttonVariants = cva(
       size: {
         default: "h-14 px-6 py-2 rounded-xl",
         sm: "h-6 rounded-md text-caption py-1 px-1.5",
-        md: "h-10 rounded-xl text-caption py-2 px-3",
+        md: "h-10 rounded-xl py-2 px-3",
         icon: "h-10 w-10 rounded-full",
       },
     },
