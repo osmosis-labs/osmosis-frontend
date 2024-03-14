7d3388b9f0849cd1d5ddf71b3f95e8f97ed5d5d5
Jon Ator <jonathanator0@gmail.com>
2024-03-14 18:20:37 -0400

    Add astroport pools from indexer pools provider (#2929)
    * add astroport pools
    
    * add astroport pool ID

--- packages/web/server/queries/complex/pools/env.ts
+++ packages/web/server/queries/complex/pools/env.ts
@@ -2,4 +2,4 @@ import { IS_TESTNET } from "~/config/env";
 
 /** Cosmwasm Code Ids confirmed to be transmuter pools in current env. */
 export const TransmuterPoolCodeIds = IS_TESTNET ? ["3084"] : ["148"];
-export const AstroportPclPoolCodeIds = IS_TESTNET ? ["5005"] : [];
+export const AstroportPclPoolCodeIds = IS_TESTNET ? ["5005"] : ["580"];
