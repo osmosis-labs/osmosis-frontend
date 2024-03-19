d6fca62ebf4648654e377653e0fa3be714fcc4d8
Jon Ator <jonathanator0@gmail.com>
2024-03-19 12:15:55 -0400

    increase slippage further

--- packages/stores/src/account/osmosis/index.ts
+++ packages/stores/src/account/osmosis/index.ts
@@ -582,12 +582,10 @@ export class OsmosisAccountImpl {
     superfluidValidatorAddress?: string,
     baseDeposit?: { currency: Currency; amount: string },
     quoteDeposit?: { currency: Currency; amount: string },
-    maxSlippage = "10",
+    maxSlippage = "15",
     memo: string = "",
     onFulfill?: (tx: DeliverTxResponse) => void
   ) {
-    if (poolId === "1247" || poolId === "1248") maxSlippage = "15";
-
     const queries = this.queries;
 
     const queryPool = queries.queryPools.getPool(poolId);
