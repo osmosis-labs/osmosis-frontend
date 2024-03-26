4cfa77b8b6b88872537f61e5f52468bd8c65f5ab
Roman <roman@osmosis.team>
2024-03-25 21:06:34 -0600

    attempt fix

--- packages/pools/src/router/types.ts
+++ packages/pools/src/router/types.ts
@@ -62,7 +62,7 @@ export type SplitTokenInQuote = Quote & {
   /** In amount after fees paid are subtracted. */
   tokenInFeeAmount?: Int;
   swapFee?: Dec;
-  inBaseOutQuoteSpotPrice?: Int;
+  inBaseOutQuoteSpotPrice?: Dec;
 };
 
 export type Logger = {
