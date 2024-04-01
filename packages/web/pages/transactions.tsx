42d0e0e1f5906383674a23f9b44d5e552c5dde6c
Matt Upham <mcupham@gmail.com>
2024-04-01 09:18:32 -0700

    Clean up row

--- packages/web/pages/transactions.tsx
+++ packages/web/pages/transactions.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 
 import LinkButton from "~/components/buttons/link-button";
+import { Spinner } from "~/components/loaders";
 import { Button } from "~/components/ui/button";
 import { useNavBar } from "~/hooks";
 
@@ -28,9 +29,9 @@ export const Transactions: React.FC = () => {
   });
 
   return (
-    <main className="flex px-8">
+    <main className="flex flex-col px-8">
       <div className="flex w-full justify-between pt-8 pb-4">
-        <h1 className="text-h3 font-h3"> Hello World</h1>
+        <h1 className="text-h3 font-h3">Transactions</h1>
         <div className="flex gap-3">
           <Button
             // TODO update with new variant
@@ -44,6 +45,45 @@ export const Transactions: React.FC = () => {
           >
             Tax Reports &#x2197;
           </Button>
+        </div>
+      </div>
+      <div className="flex flex-col gap-4 px-4 pt-8 pb-3">
+        <div className="text-osmoverse-300">Pending</div>
+        <hr className="text-osmoverse-700" />
+      </div>
+      <div className="w-container m-4 flex h-12 justify-between">
+        <div className="flex items-center gap-4">
+          <Spinner className="h-8 w-8 pb-4 text-wosmongton-500" />
+          <p className="text-osmoverse-100">Swapping</p>
+        </div>
+        <div className="flex gap-4">
+          <Image
+            alt="USDC"
+            src="/tokens/generated/osmo.svg"
+            height={32}
+            width={32}
+          />
+          <div className="flex flex-col text-right text-osmoverse-400">
+            <div className="text-subtitle1">- $100.00</div>
+            <div className="text-body2">10 OSMO</div>
+          </div>
+          <Image
+            alt="right"
+            src="/icons/arrow-right.svg"
+            width={24}
+            height={24}
+            className="text-osmoverse-600"
+          />
+          <Image
+            alt="USDC"
+            src="/tokens/generated/usdc.svg"
+            height={32}
+            width={32}
+          />
+          <div className="flex flex-col text-right text-osmoverse-400">
+            <div className="text-subtitle1">+ $100.00</div>
+            <div className="text-body2">100 USDC</div>
+          </div>
         </div>
       </div>
     </main>
