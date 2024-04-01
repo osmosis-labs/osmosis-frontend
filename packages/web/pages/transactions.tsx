2b0470512af52fcd1e5147305460c5821104212f
Matt Upham <mcupham@gmail.com>
2024-04-01 10:14:02 -0700

    Add dynamic data

--- packages/web/pages/transactions.tsx
+++ packages/web/pages/transactions.tsx
@@ -1,9 +1,87 @@
 import Image from "next/image";
 
+import { Icon } from "~/components/assets";
 import LinkButton from "~/components/buttons/link-button";
 import { Spinner } from "~/components/loaders";
 import { Button } from "~/components/ui/button";
 import { useNavBar } from "~/hooks";
+import { theme } from "~/tailwind.config";
+interface TransactionRowProps {
+  status: "Pending" | "Success" | "Failure";
+}
+
+const TransactionRow = ({ status }: TransactionRowProps) => {
+  return (
+    <div className="w-container m-4 flex h-12 justify-between">
+      <TransactionStatus status={status} />
+      <TransactionDetails />
+    </div>
+  );
+};
+
+interface TransactionStatusProps {
+  status: "Pending" | "Success" | "Failure";
+}
+
+const TransactionStatus = ({ status }: TransactionStatusProps) => {
+  return (
+    <div className="flex items-center gap-4">
+      {status === "Pending" && (
+        <Spinner className="h-8 w-8 pb-4 text-wosmongton-500" />
+      )}
+      {status === "Success" && (
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-osmoverse-825">
+          <Icon id="swap" width={24} height={24} aria-label="swap icon" />
+        </div>
+      )}
+      {status === "Failure" && (
+        <div className="flex h-12 w-12 items-center justify-center rounded-full bg-osmoverse-825">
+          <Icon id="alert-circle" color={theme.colors.rust[500]} />
+        </div>
+      )}
+
+      <p className="text-osmoverse-100">
+        {status === "Pending" && "Swapping"}
+        {status === "Success" && "Swapped"}
+        {status === "Failure" && "Swap failed"}
+      </p>
+    </div>
+  );
+};
+
+const TransactionDetails = () => {
+  return (
+    <div className="flex gap-4">
+      <Image
+        alt="USDC"
+        src="/tokens/generated/osmo.svg"
+        height={32}
+        width={32}
+      />
+      <div className="flex flex-col text-right text-osmoverse-400">
+        <div className="text-subtitle1">- $100.00</div>
+        <div className="text-body2">10 OSMO</div>
+      </div>
+      <Image
+        alt="right"
+        src="/icons/arrow-right.svg"
+        width={24}
+        height={24}
+        className="text-osmoverse-600"
+      />
+      <Image
+        alt="USDC"
+        src="/tokens/generated/usdc.svg"
+        height={32}
+        width={32}
+      />
+      <div className="flex flex-col text-right text-osmoverse-400">
+        <div className="text-subtitle1">+ $100.00</div>
+        <div className="text-body2">100 USDC</div>
+      </div>
+    </div>
+  );
+};
 
 export const Transactions: React.FC = () => {
   useNavBar({
@@ -47,45 +125,34 @@ export const Transactions: React.FC = () => {
           </Button>
         </div>
       </div>
+
+      {/* TODO - parse by date, into groups */}
+
       <div className="flex flex-col gap-4 px-4 pt-8 pb-3">
         <div className="text-osmoverse-300">Pending</div>
         <hr className="text-osmoverse-700" />
       </div>
-      <div className="w-container m-4 flex h-12 justify-between">
-        <div className="flex items-center gap-4">
-          <Spinner className="h-8 w-8 pb-4 text-wosmongton-500" />
-          <p className="text-osmoverse-100">Swapping</p>
-        </div>
-        <div className="flex gap-4">
-          <Image
-            alt="USDC"
-            src="/tokens/generated/osmo.svg"
-            height={32}
-            width={32}
-          />
-          <div className="flex flex-col text-right text-osmoverse-400">
-            <div className="text-subtitle1">- $100.00</div>
-            <div className="text-body2">10 OSMO</div>
-          </div>
-          <Image
-            alt="right"
-            src="/icons/arrow-right.svg"
-            width={24}
-            height={24}
-            className="text-osmoverse-600"
-          />
-          <Image
-            alt="USDC"
-            src="/tokens/generated/usdc.svg"
-            height={32}
-            width={32}
-          />
-          <div className="flex flex-col text-right text-osmoverse-400">
-            <div className="text-subtitle1">+ $100.00</div>
-            <div className="text-body2">100 USDC</div>
-          </div>
-        </div>
+      <TransactionRow status="Pending" />
+
+      <div className="flex flex-col gap-4 px-4 pt-8 pb-3">
+        <div className="text-osmoverse-300">Earlier Today</div>
+        <hr className="text-osmoverse-700" />
       </div>
+      <TransactionRow status="Success" />
+      <TransactionRow status="Failure" />
+
+      <div className="flex flex-col gap-4 px-4 pt-8 pb-3">
+        <div className="text-osmoverse-300">Yesterday</div>
+        <hr className="text-osmoverse-700" />
+      </div>
+      <TransactionRow status="Success" />
+
+      <div className="flex flex-col gap-4 px-4 pt-8 pb-3">
+        <div className="text-osmoverse-300">March 11</div>
+        <hr className="text-osmoverse-700" />
+      </div>
+      <TransactionRow status="Success" />
+      <TransactionRow status="Success" />
     </main>
   );
 };
