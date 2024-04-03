501acb913e409f6bc02e45d07fe1b7a615926d58
Niccolo Raspa <niccoloraspa@gmail.com>
2024-04-03 17:17:28 +0200

    Use stage worker proxy

--- packages/stores/src/queries-external/index.ts
+++ packages/stores/src/queries-external/index.ts
@@ -11,9 +11,9 @@ export * from "./token-data";
 export * from "./token-historical-chart";
 
 export const IMPERATOR_TIMESERIES_DEFAULT_BASEURL =
-  "https://api-osmosis.imperator.co";
+  "https://data-proxy-numia-stage.osmosis-labs.workers.dev";
 export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
-  "https://api-osmosis-chain.imperator.co";
+  "https://data-proxy-numia-stage.osmosis-labs.workers.dev";
 
 /**
  * This domain has a whitelist, so in local development an auth token is required
@@ -21,4 +21,4 @@ export const IMPERATOR_INDEXER_DEFAULT_BASEURL =
 export const COINGECKO_API_DEFAULT_BASEURL =
   "https://coingecko.osmosis.zone/api";
 
-export const NUMIA_INDEXER_BASEURL = "https://public-osmosis-api.numia.xyz";
+export const NUMIA_INDEXER_BASEURL = "https://data-proxy-numia-stage.osmosis-labs.workers.dev";
