1fda5edbebd4df65f0afc8b5b6d021d34c7b1d01
Matt Upham <mcupham@gmail.com>
2024-04-03 09:52:06 -0700

    Add keys to config

---
+++ packages/web/pages/transactions.tsx
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+import { useFeatureFlags } from "~/hooks";
+
+const Transactions = () => {
+  const { transactionsPage, _isInitialized } = useFeatureFlags();
+  console.log("transactionsPage: ", transactionsPage);
+  const router = useRouter();
+
+  useEffect(() => {
+    if (!transactionsPage && _isInitialized) {
+      router.push("/");
+    }
+  }, [transactionsPage, router, _isInitialized]);
+
+  return <div>Transactions Content</div>;
+};
+
+export default Transactions;
