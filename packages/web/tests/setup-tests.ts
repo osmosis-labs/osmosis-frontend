0de54d3d3f51088c1a3f98fd8125d977e5df8884
Jose Felix <jfelx@outlook.com>
2024-04-04 16:30:22 -0400

    test: remove previous fix

--- packages/web/tests/setup-tests.ts
+++ packages/web/tests/setup-tests.ts
@@ -3,13 +3,6 @@ import "@testing-library/jest-dom";
 
 import { server } from "~/tests/msw";
 
-/**
- * Allow re-defining `fetch` in tests
- */
-Object.defineProperty(global, "fetch", {
-  writable: true,
-});
-
 beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
