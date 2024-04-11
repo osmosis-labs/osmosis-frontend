b4c8f07fb522f9bc580b99a9834b2843b0877002
Jose Felix <jfelx@outlook.com>
2024-04-11 02:04:55 -0400

    feat: re-enable browser client

--- packages/web/sentry.client.config.ts
+++ packages/web/sentry.client.config.ts
@@ -20,9 +20,8 @@ Sentry.init({
   replaysSessionSampleRate: 0.1,
 
   // Temporarily disable due to client-side noise coming from extensions, Cosmos Kit, etc.
-  enabled: false,
-  // enabled:
-  //   process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "test",
+  enabled:
+    process.env.NODE_ENV !== "development" && process.env.NODE_ENV !== "test",
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
