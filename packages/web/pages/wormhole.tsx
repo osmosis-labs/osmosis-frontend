8d9a702811d195360958d6b02d2f9151ea45a169
Matt Upham <mcupham@gmail.com>
2024-04-11 17:23:56 -0700

    Cleanup

--- packages/web/pages/wormhole.tsx
+++ packages/web/pages/wormhole.tsx
@@ -22,40 +22,6 @@ const WormholeConnect = dynamic(
   }
 );
 
-// let config: WormholeConnectConfig = {
-//   networks: ["solana", "osmosis"],
-//   rpcs: {
-//     solana:
-//       "https://mainnet.helius-rpc.com/?api-key=f4713222-8bbc-4495-aace-5693e719712e",
-//     wormchain: "https://tncnt-eu-wormchain-main-01.rpc.p2p.world/",
-//   },
-//   tokensConfig: {
-//     W: {
-//       key: "W",
-//       symbol: "W",
-//       nativeChain: "solana",
-//       tokenId: {
-//         chain: "solana",
-//         address: "85VBFQZC9TZkfaptBWjvUw7YbZjy52A6mjtPGjstQAmQ",
-//       },
-//       coinGeckoId: "wormhole",
-//       icon: "https://assets.coingecko.com/coins/images/35087/standard/womrhole_logo_full_color_rgb_2000px_72ppi_fb766ac85a.png?1708688954",
-//       color: "#2894EE",
-//       decimals: {
-//         default: 6,
-//       },
-//       foreignAssets: {
-//         osmosis: {
-//           address:
-//             "ibc/AC6EE43E608B5A7EEE460C960480BC1C3708010E32B2071C429DA259836E10C3",
-//           decimals: 6,
-//         },
-//       },
-//     },
-//   },
-//   tokens: ["W", "SOL", "PYTH"],
-// };
-
 const customTheme: WormholeConnectPartialTheme = {
   mode: "dark",
   background: {
@@ -96,12 +62,17 @@ const customTheme: WormholeConnectPartialTheme = {
 };
 
 const Wormhole: FunctionComponent = () => {
-  const router = useRouter(); // Use Next.js useRouter hook
+  const router = useRouter();
 
-  // Access query parameters via router.query
   const fromNetwork = router.query.from as string;
   const toNetwork = router.query.to as string;
   const token = router.query.token as string;
+
+  console.log("log: ", {
+    fromNetwork,
+    toNetwork,
+    token,
+  });
 
   let config: WormholeConnectConfig = {
     networks: ["solana", "osmosis"],
@@ -137,18 +108,12 @@ const Wormhole: FunctionComponent = () => {
     tokens: ["W", "SOL", "PYTH"],
   };
 
-  // const queryParams = new URLSearchParams(window.location.href.split("?")[1]);
-
   let bridgeDefaults = {
     fromNetwork: "solana",
     toNetwork: "osmosis",
     token: "W",
     requiredNetwork: "osmosis",
   };
-
-  // const fromNetwork = queryParams.get("from");
-  // const toNetwork = queryParams.get("to");
-  // const token = queryParams.get("token");
 
   if (fromNetwork) {
     bridgeDefaults.fromNetwork = fromNetwork;
