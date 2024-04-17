76f304d00a5740a30e3e255f4c51b129a429bf58
Sunny Aggarwal <sunnya97@protonmail.ch>
2024-04-17 17:20:56 -0400

    fix

--- packages/web/components/table/cells/price.tsx
+++ packages/web/components/table/cells/price.tsx
@@ -1,32 +1,12 @@
 import { RatePretty } from "@keplr-wallet/unit";
-import type { CommonPriceChartTimeFrame } from "@osmosis-labs/server";
-import { FunctionComponent, useMemo } from "react";
+import { FunctionComponent } from "react";
 
 import { PriceChange } from "~/components/assets/price-change";
 import { theme } from "~/tailwind.config";
-import { api } from "~/utils/trpc";
 
 export const HistoricalPriceCell: FunctionComponent<{
-  coinDenom: string;
-  timeFrame: CommonPriceChartTimeFrame;
   priceChange24h?: RatePretty;
-}> = ({ coinDenom, priceChange24h, timeFrame = "1D" }) => {
-  const { data: recentPrices } =
-    api.edge.assets.getAssetHistoricalPrice.useQuery(
-      {
-        coinDenom: coinDenom,
-        timeFrame: timeFrame,
-      },
-      {
-        staleTime: 1000 * 30, // 30 secs
-      }
-    );
-
-  const recentPriceCloses = useMemo(
-    () => (recentPrices ? recentPrices.map((p) => p.close) : []),
-    [recentPrices]
-  );
-
+}> = ({ priceChange24h }) => {
   const isBullish = priceChange24h && priceChange24h.toDec().isPositive();
   const isBearish = priceChange24h && priceChange24h.toDec().isNegative();
 
