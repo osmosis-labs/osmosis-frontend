d9dc11b635252933a15d484c89046b2596bc9618
Jon Ator <jonathanator0@gmail.com>
2024-04-17 17:58:45 -0400

    Portfolio page: recent transfers tab (#3100)
    * WIP add recent transfers UI, prepare transaction row
    
    * add chain ID helper to utils
    
    * WIP add recent transfers
    
    * fixes
    
    * fix: withdrawal reactivity issue
    
    * use sig figs
    
    * translate
    
    * fix handling of decimals
    
    * apply font classes
    
    * Update packages/web/localizations/ro.json
    
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>
    
    * add missing translations
    
    ---------
    
    Co-authored-by: Jose Felix <jfelx@outlook.com>
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

---
+++ packages/web/components/transactions/recent-transfers.tsx
@@ -0,0 +1,114 @@
+import { CoinPretty, Dec, DecUtils } from "@keplr-wallet/unit";
+import { makeMinimalAsset } from "@osmosis-labs/utils";
+import { observer } from "mobx-react-lite";
+import Image from "next/image";
+import { FunctionComponent } from "react";
+
+import { AssetLists } from "~/config/generated/asset-lists";
+import { useTranslation, useWalletSelect } from "~/hooks";
+import { useStore } from "~/stores";
+
+import { Spinner } from "../loaders";
+import { TransactionRow } from "./transaction-row";
+import { useRecentTransfers } from "./use-recent-transfers";
+
+export const RecentTransfers: FunctionComponent = observer(() => {
+  const { accountStore } = useStore();
+  const { isLoading: isWalletLoading } = useWalletSelect();
+
+  const account = accountStore.getWallet(accountStore.osmosisChainId);
+
+  if (isWalletLoading) return <Spinner />;
+  if (account?.address)
+    return <UserRecentTransfers address={account.address} />;
+  return <NoTransfersSplash />;
+});
+
+const UserRecentTransfers: FunctionComponent<{ address: string }> = observer(
+  ({ address }) => {
+    const { t } = useTranslation();
+
+    const recentTransfers = useRecentTransfers(address);
+
+    if (recentTransfers.length === 0) return <NoTransfersSplash />;
+
+    return (
+      <div className="flex w-full flex-col gap-2">
+        {recentTransfers.map(({ txHash, status, amount, isWithdraw }) => {
+          const simplifiedStatus =
+            status === "complete"
+              ? "success"
+              : status === "refunded" ||
+                status === "timeout" ||
+                status === "failed"
+              ? "failed"
+              : "pending";
+
+          const coinAmount = amount.split(" ")[0];
+          const coinDenom = amount.split(" ")[1];
+          const asset = AssetLists.flatMap(({ assets }) => assets).find(
+            ({ symbol }) => symbol === coinDenom
+          );
+
+          if (!asset) return null;
+
+          const currency = makeMinimalAsset(asset);
+
+          const pendingText = isWithdraw
+            ? t("assets.historyTable.pendingWithdraw")
+            : t("assets.historyTable.pendingDeposit");
+          const successText = isWithdraw
+            ? t("assets.historyTable.successWithdraw")
+            : t("assets.historyTable.successDeposit");
+          const failedText = isWithdraw
+            ? t("assets.historyTable.failWithdraw")
+            : t("assets.historyTable.failDeposit");
+
+          return (
+            <TransactionRow
+              key={txHash}
+              status={simplifiedStatus}
+              effect={isWithdraw ? "withdraw" : "deposit"}
+              title={{
+                pending: pendingText,
+                success: successText,
+                failed: failedText,
+              }}
+              transfer={{
+                direction: isWithdraw ? "withdraw" : "deposit",
+                amount: new CoinPretty(
+                  currency,
+                  new Dec(coinAmount).mul(
+                    DecUtils.getTenExponentN(currency.coinDecimals)
+                  ) // amount includes decimals
+                ),
+              }}
+            />
+          );
+        })}
+      </div>
+    );
+  }
+);
+
+const NoTransfersSplash: FunctionComponent = () => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="mx-auto my-6 flex max-w-35 flex-col gap-6 text-center">
+      <Image
+        className="mx-auto"
+        src="/images/ion-thumbs-up.svg"
+        alt="ion thumbs up"
+        width="260"
+        height="160"
+      />
+      <div className="flex flex-col gap-2">
+        <h6>{t("transactions.noRecent")}</h6>
+        <p className="body1 text-osmoverse-300">
+          {t("transactions.recentShownHere")}
+        </p>
+      </div>
+    </div>
+  );
+};
