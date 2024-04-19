0e81613fbf506c8cfff596850d002c686c54b38e
Matt Upham <mcupham@gmail.com>
2024-04-19 00:42:53 -0700

    Add connect variant

--- packages/web/components/transactions/no-transactions-splash.tsx
+++ packages/web/components/transactions/no-transactions-splash.tsx
@@ -1,17 +1,25 @@
 import Image from "next/image";
+import React from "react";
 import { FunctionComponent } from "react";
 
+import { Button } from "~/components/ui/button";
 import { useTranslation } from "~/hooks";
+import { useWalletSelect } from "~/hooks/wallet-select";
+import { useStore } from "~/stores";
 
 export const NoTransactionsSplash: FunctionComponent<{
   variant: "transfers" | "transactions" | "connect";
 }> = ({ variant }) => {
+  const { chainStore } = useStore();
+  const osmosisChainId = chainStore.osmosis.chainId;
+  const { onOpenWalletSelect } = useWalletSelect();
+
   const { t } = useTranslation();
 
   const getTitle = () => {
     if (variant === "transfers") return t("transactions.noRecent");
     if (variant === "transactions") return t("transactions.noTransactions");
-    if (variant === "connect") return t("transactions.connect");
+    if (variant === "connect") return t("transactions.connectToSee");
   };
 
   const getBodyText = () => {
@@ -33,6 +41,13 @@ export const NoTransactionsSplash: FunctionComponent<{
         <h6>{getTitle()}</h6>
         <p className="body1 text-osmoverse-300">{getBodyText()}</p>
       </div>
+      {variant === "connect" && (
+        <div className="max-w-56">
+          <Button onClick={() => onOpenWalletSelect(osmosisChainId)} size="md">
+            {t("connectWallet")}
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
