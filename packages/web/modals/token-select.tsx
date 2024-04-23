9705655bda546adce4b399a8fc516205ae74e266
Sunny Aggarwal <sunny@osmosis.team>
2024-04-23 11:11:46 -0400

    Update packages/web/modals/token-select.tsx

--- packages/web/modals/token-select.tsx
+++ packages/web/modals/token-select.tsx
@@ -47,7 +47,7 @@ export const TokenSelectModal: FunctionComponent<
           size="large"
         />
       </div>
-      <ul className="flex max-h-96 flex-col overflow-y-auto md:max-h-64">
+      <ul className="flex max-h-128 flex-col overflow-y-auto md:max-h-64">
         {props.tokens.map((t) => {
           const currency =
             t.token instanceof CoinPretty ? t.token.currency : t.token;
