b14a8e4d6d6b0852486431a72a931aff5217a0a4
Jon Ator <jonathanator0@gmail.com>
2024-04-23 10:07:53 -0400

    Assets & portfolio: reimplement unverified assets behavior (#3142)
    * portfolio unverified assets
    
    * assets page: unverified assets and deposit
    
    * remove unused key
    
    * lint

--- packages/web/components/table/cells/asset.tsx
+++ packages/web/components/table/cells/asset.tsx
@@ -1,41 +1,49 @@
-import classNames from "classnames";
 import Image from "next/image";
 import { FunctionComponent } from "react";
+
+import { Icon } from "~/components/assets";
+import { Tooltip } from "~/components/tooltip";
+import { useTranslation } from "~/hooks";
 
 export const AssetCell: FunctionComponent<
   Partial<{
     coinDenom: string;
     coinName: string;
     coinImageUrl: string;
-    isVerified: boolean;
+    warnUnverified: boolean;
   }>
-> = ({ coinDenom, coinName, coinImageUrl, isVerified = false }) => (
-  <div
-    className={classNames("flex w-44 items-center gap-4 md:gap-1", {
-      "opacity-40": !isVerified,
-    })}
-  >
-    <div className="h-10 w-10 shrink-0">
-      {coinImageUrl && (
-        <Image
-          alt={coinDenom ?? "coin image"}
-          src={coinImageUrl}
-          height={40}
-          width={40}
-        />
+> = ({ coinDenom, coinName, coinImageUrl, warnUnverified = false }) => {
+  const { t } = useTranslation();
+
+  return (
+    <div className="flex w-44 items-center gap-4 md:gap-1">
+      <div className="h-10 w-10 shrink-0">
+        {coinImageUrl && (
+          <Image
+            alt={coinDenom ?? "coin image"}
+            src={coinImageUrl}
+            height={40}
+            width={40}
+          />
+        )}
+      </div>
+      <div className="flex w-full flex-col place-content-center">
+        {coinName && (
+          <div className="subtitle1 overflow-hidden overflow-ellipsis whitespace-nowrap">
+            {coinName}
+          </div>
+        )}
+        {coinDenom && (
+          <span className="body2 md:caption overflow-hidden overflow-ellipsis whitespace-nowrap text-osmoverse-400 md:w-28">
+            {coinDenom}
+          </span>
+        )}
+      </div>
+      {warnUnverified && (
+        <Tooltip content={t("components.selectToken.unverifiedAsset")}>
+          <Icon id="alert-triangle" className="h-5 w-5 text-osmoverse-300" />
+        </Tooltip>
       )}
     </div>
-    <div className="flex w-full flex-col place-content-center">
-      {coinName && (
-        <div className="subtitle1 overflow-hidden overflow-ellipsis whitespace-nowrap">
-          {coinName}
-        </div>
-      )}
-      {coinDenom && (
-        <span className="body2 md:caption overflow-hidden overflow-ellipsis whitespace-nowrap text-osmoverse-400 md:w-28">
-          {coinDenom}
-        </span>
-      )}
-    </div>
-  </div>
-);
+  );
+};
