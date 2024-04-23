b8e84c7bde8d76d9cf5c48d3bafffbf6966a4afd
fabryscript <vivaldifabrizio10@gmail.com>
2024-04-23 11:50:49 +0200

    feat: update and fix tests

--- packages/web/components/chart/__tests__/compress-zeros.spec.ts
+++ packages/web/components/chart/__tests__/compress-zeros.spec.ts
@@ -1,21 +1,21 @@
 import { compressZeros } from "~/components/chart/compress-zeros";
 
 describe("compressZeros function", () => {
-  it("should correctly compress zeros when no currency symbol is present", () => {
+  it("should not compress zeros with and handle the absence of currency symbol", () => {
     expect(compressZeros("123.00", false)).toEqual({
       currencySign: undefined,
       significantDigits: "123",
-      zeros: 2,
-      decimalDigits: "",
+      zeros: 0,
+      decimalDigits: "00",
     });
   });
 
-  it("should correctly compress zeros when currency symbol is present", () => {
+  it("should not compress zeros even if there is a currency symbol", () => {
     expect(compressZeros("$0.00", true)).toEqual({
       currencySign: "$",
       significantDigits: "0",
-      zeros: 2,
-      decimalDigits: "",
+      zeros: 0,
+      decimalDigits: "00",
     });
   });
 
@@ -37,12 +37,12 @@ describe("compressZeros function", () => {
     });
   });
 
-  it("should correctly handle cases with only leading zeros", () => {
+  it("should correctly handle cases with only leading zeros less than the default threshold", () => {
     expect(compressZeros("$00.005", true)).toEqual({
       currencySign: "$",
       significantDigits: "00",
-      zeros: 2,
-      decimalDigits: "5",
+      zeros: 0,
+      decimalDigits: "005",
     });
   });
 
@@ -57,8 +57,32 @@ describe("compressZeros function", () => {
     expect(compressZeros("$0.00", true)).toEqual({
       currencySign: "$",
       significantDigits: "0",
-      zeros: 2,
-      decimalDigits: "",
+      zeros: 0,
+      decimalDigits: "00",
+    });
+  });
+  it("should compress zeros with the default threshold", () => {
+    expect(compressZeros("$0.00000029183", true)).toEqual({
+      currencySign: "$",
+      significantDigits: "0",
+      zeros: 6,
+      decimalDigits: "29183",
+    });
+  });
+  it("should not compress zeros with a different threshold", () => {
+    expect(compressZeros("$1.000000323", true, 8)).toEqual({
+      currencySign: "$",
+      significantDigits: "1",
+      zeros: 0,
+      decimalDigits: "000000323",
+    });
+  });
+  it("should compress zeros with a different threshold", () => {
+    expect(compressZeros("$1.00000000323", true, 5)).toEqual({
+      currencySign: "$",
+      significantDigits: "1",
+      zeros: 8,
+      decimalDigits: "323",
     });
   });
 });
