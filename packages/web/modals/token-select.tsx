ae4b2346b9891e48c1e4efb7feb750c27a766729
Jon Ator <jonathanator0@gmail.com>
2024-04-23 09:51:59 -0400

    increase height of token select

--- packages/web/modals/token-select.tsx
+++ packages/web/modals/token-select.tsx
@@ -47,7 +47,7 @@ export const TokenSelectModal: FunctionComponent<
           size="large"
         />
       </div>
-      <ul className="flex max-h-64 flex-col overflow-y-auto">
+      <ul className="flex max-h-96 flex-col overflow-y-auto md:max-h-64">
         {props.tokens.map((t) => {
           const currency =
             t.token instanceof CoinPretty ? t.token.currency : t.token;
