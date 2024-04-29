450c2fc2eeba21ea3f231c0b094b82dbb6555cb6
Matt Upham <mcupham@gmail.com>
2024-04-29 14:57:07 -0700

    Lint fix

--- packages/server/src/queries/complex/pools/index.ts
+++ packages/server/src/queries/complex/pools/index.ts
@@ -5,8 +5,8 @@ import { z } from "zod";
 import { IS_TESTNET } from "../../../env";
 import { search, SearchSchema } from "../../../utils/search";
 import { PoolRawResponse } from "../../osmosis";
-import { getPoolsFromIndexer } from "./providers/indexer";
 import { getPoolsFromSidecar } from "./providers";
+import { getPoolsFromIndexer } from "./providers/indexer";
 
 const allPooltypes = [
   "concentrated",
