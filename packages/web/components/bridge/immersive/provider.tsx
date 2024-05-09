bddca79d573efc7dff9da462284bc21221815664
Jon Ator <jonathanator0@gmail.com>
2024-05-09 10:33:09 -0400

    add step progress bar

--- packages/web/components/bridge/immersive/provider.tsx
+++ packages/web/components/bridge/immersive/provider.tsx
@@ -1,6 +1,7 @@
 import { Transition } from "@headlessui/react";
 import { FunctionComponent, useState } from "react";
 
+import { StepProgress } from "~/components/stepper/progress-bar";
 import { Button } from "~/components/ui/button";
 import { FiatRampKey } from "~/integrations";
 
@@ -12,10 +13,12 @@ export const ImmersiveBridgeFlow: FunctionComponent<BridgeFlowProvider> = ({
 }) => {
   // TODO: state will be encapsulated in a state hook
   const [isShowing, setIsShowing] = useState(false);
+  const [step, setStep] = useState<0 | 1 | 2 | 3 | 4>(0);
 
   return (
     <Provider
       value={{
+        // globally received events
         startBridge: (direction: "deposit" | "withdraw") => {
           setIsShowing(true);
           console.log("startBridge", direction);
@@ -37,7 +40,7 @@ export const ImmersiveBridgeFlow: FunctionComponent<BridgeFlowProvider> = ({
       {children}
       <Transition
         show={isShowing}
-        className="fixed inset-0 z-[999] flex items-center justify-center bg-black bg-opacity-50"
+        className="fixed inset-0 z-[999] flex items-center justify-center bg-osmoverse-900"
         enter="transition-opacity duration-300"
         enterFrom="opacity-0"
         enterTo="opacity-100"
@@ -45,9 +48,37 @@ export const ImmersiveBridgeFlow: FunctionComponent<BridgeFlowProvider> = ({
         leaveFrom="opacity-100"
         leaveTo="opacity-0"
       >
-        <div className="bg-white rounded p-4 text-center shadow-lg">
-          I will fade in and out
-          <Button onClick={() => setIsShowing(false)}>OK</Button>
+        <div className="flex flex-col gap-10">
+          <StepProgress
+            className="w-full"
+            steps={[
+              { displayLabel: "Asset" },
+              { displayLabel: "Network" },
+              { displayLabel: "Amount" },
+              { displayLabel: "Review" },
+              { displayLabel: "Deposit" },
+            ]}
+            currentStep={step}
+          />
+          <div className="flex gap-2">
+            <Button disabled={step === 0} onClick={() => setStep(0)}>
+              1
+            </Button>
+            <Button disabled={step === 1} onClick={() => setStep(1)}>
+              2
+            </Button>
+            <Button disabled={step === 2} onClick={() => setStep(2)}>
+              3
+            </Button>
+            <Button disabled={step === 3} onClick={() => setStep(3)}>
+              4
+            </Button>
+            <Button disabled={step === 4} onClick={() => setStep(4)}>
+              5
+            </Button>
+          </div>
+          <h6>I will fade in and out</h6>
+          <Button onClick={() => setIsShowing(false)}>Close</Button>
         </div>
       </Transition>
     </Provider>
