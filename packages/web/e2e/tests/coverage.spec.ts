a7e12cb5c96b2317ef1144ffca45f88be7dd8684
yury-dubinin <yury.dubinin@gmail.com>
2024-05-13 17:32:22 +0200

    Added couple of steps

--- packages/web/e2e/tests/coverage.spec.ts
+++ packages/web/e2e/tests/coverage.spec.ts
@@ -1,5 +1,6 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { test } from "playwright-test-coverage";
+import { Locator } from "@playwright/test";
+import { expect, test } from "playwright-test-coverage";
 
 import { SwapPage } from "../pages/swap-page";
 
@@ -7,9 +8,24 @@ test.describe("Test Coverage", () => {
   let swapPage: SwapPage;
 
   test("User should be able to Test Coverage", async ({ page }) => {
+    const exchangeRate: Locator = page.locator(
+      '//span[contains(@class, "subtitle2")]'
+    );
     swapPage = new SwapPage(page);
     await swapPage.goto();
     await swapPage.selectPair("USDC", "USDC.axl");
     await swapPage.flipTokenPair();
+    expect(await exchangeRate.innerText()).toContain("USDC");
+    const assetsTab = page.getByText("Assets");
+    await assetsTab.click();
+    await page.waitForURL("**/assets", { timeout: 15000 });
+    expect(page.url()).toContain("/assets");
+    await page.getByText("Portfolio").click();
+    await page.waitForURL("**/portfolio", { timeout: 15000 });
+    expect(page.url()).toContain("/portfolio");
+    await page.getByText("Deposit").click();
+    expect(
+      await page.getByRole("dialog").locator("//h5").textContent()
+    ).toContainEqual("Deposit");
   });
 });
