8e6888b666502ee8d482161aa29915162f7c4086
yury-dubinin <yury.dubinin@gmail.com>
2024-05-17 17:25:59 +0200

    split swap tests

--- packages/web/e2e/tests/swap.wallet.spec.ts
+++ packages/web/e2e/tests/swap.wallet.spec.ts
@@ -15,12 +15,6 @@ test.describe("Test Swap feature", () => {
   let swapPage: SwapPage;
   let USDC =
     "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4";
-  let USDCa =
-    "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858";
-  let USDTa =
-    "ibc/8242AD24008032E457D2E12D46588FD39FB54FB29680C6C7663D296B383C37C4";
-  let USDT =
-    "ibc/4ABBEF4C8926DDDB320AE5188CFD63267ABBCEFC0583E4AE05D6E5AA2401DDAB";
   let ATOM =
     "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2";
   let TIA =
@@ -93,74 +87,6 @@ test.describe("Test Swap feature", () => {
     expect(msgContentAmount).toContain("sender: " + walletId);
     expect(msgContentAmount).toContain("token_out_denom: uosmo");
     expect(swapPage.isTransactionSuccesful()).toBeTruthy();
-  });
-
-  test("User should be able to swap USDC to USDC.axl", async () => {
-    await swapPage.selectPair("USDC", "USDC.axl");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    await swapPage.swap();
-    // Handle Pop-up page ->
-    const pageApprove = context.waitForEvent("page");
-    const { msgContentAmount } = await swapPage.getWalletMsg(pageApprove);
-    // Handle Pop-up page <-
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDC);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDCa);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
-  });
-
-  test("User should be able to swap USDC.axl to USDC", async () => {
-    await swapPage.selectPair("USDC.axl", "USDC");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    await swapPage.swap();
-    // Handle Pop-up page ->
-    const pageApprove = context.waitForEvent("page");
-    const { msgContentAmount } = await swapPage.getWalletMsg(pageApprove);
-    // Handle Pop-up page <-
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDCa);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDC);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
-  });
-
-  test("User should be able to swap USDT to USDT.axl", async () => {
-    await swapPage.selectPair("USDT", "USDT.axl");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    await swapPage.swap();
-    // Handle Pop-up page ->
-    const pageApprove = context.waitForEvent("page");
-    const { msgContentAmount } = await swapPage.getWalletMsg(pageApprove);
-    // Handle Pop-up page <-
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDT);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDTa);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
-  });
-
-  test("User should be able to swap USDT.axl to USDT", async () => {
-    await swapPage.selectPair("USDT.axl", "USDT");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    await swapPage.swap();
-    // Handle Pop-up page ->
-    const pageApprove = context.waitForEvent("page");
-    const { msgContentAmount } = await swapPage.getWalletMsg(pageApprove);
-    // Handle Pop-up page <-
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDTa);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDT);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
   });
 
   test("User should be able to swap OSMO to USDC", async () => {
