c82572d94c21efc0472e0ccd45b7e803c05dfddc
Jose Felix <jfelx@outlook.com>
2024-05-22 14:55:41 -0400

    test: Add Unit Tests for Bridge Providers and Transfer Status (#3259)
    * combine bridge-related errors
    
    * update error handling in UI
    
    * move poll utility to utils package
    
    * move ethereum deps
    
    * rename transfer status interfaces, cleanup imports
    
    * import
    
    * remove unused procedure
    
    * bridge interface naming, inject env
    
    * reconcile axelar types
    
    * purify env & source token configs
    
    * Remove price query from providers (#3250)
    
    * fetch price in procedure
    
    * don't log price errors from denoms from bridge providers
    
    * removed unused test
    
    * add getTimeoutHeight to context
    
    * move to bridge package (tests broken)
    
    * fix tests
    
    * move transfer status to bridge package from stores package
    
    * update readme
    
    * fix build
    
    * remove stale readme
    
    * remove import
    
    * async axelar import
    
    * @JoseRFelix review
    
    * wip: add unit tests
    
    * test: add initial axelar bridge provider unit tests
    
    * test: add more axelar unit tests
    
    * feat: remove chainlist from bridge providers
    
    * test: add initial skip bridge provider tests
    
    * feat: finish skip unit tests
    
    * feat: add squid provider unit tests
    
    * test: add initial axelar transfer status test
    
    * test: add axelar and skip transfer status unit tests
    
    * test: add tests for explorer url and squid tests
    
    * fix: @jonator feedback. variable name regression
    
    ---------
    
    Co-authored-by: Jon Ator <jonathanator0@gmail.com>

--- packages/utils/src/ibc-utils.ts
+++ packages/utils/src/ibc-utils.ts
@@ -4,16 +4,13 @@ import { sha256 } from "sha.js";
 /**
  *
  * @param path - The path of the IBC transfer. i.e. "channel-2/transfer/channel-1/..." or just the channel: "channel-1"
- * @param coinMinimalDenom Coin denom on source chain.
+ * @param sourceDenom Coin denom on source chain.
  * @returns IBC hash of the coin denom.
  */
-export function makeIBCMinimalDenom(
-  path: string,
-  coinMinimalDenom: string
-): string {
+export function makeIBCMinimalDenom(path: string, sourceDenom: string): string {
   return (
     "ibc/" +
-    Buffer.from(sha256_fn(Buffer.from(`transfer/${path}/${coinMinimalDenom}`)))
+    Buffer.from(sha256_fn(Buffer.from(`transfer/${path}/${sourceDenom}`)))
       .toString("hex")
       .toUpperCase()
   );
