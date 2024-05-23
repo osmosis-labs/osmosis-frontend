8b04b8d7e7a051ed63ffb4d57aeecffee3b3551a
Jon Ator <jonathanator0@gmail.com>
2024-05-23 14:25:19 -0400

    add -f to clean

--- packages/bridge/src/ibc/index.ts
+++ packages/bridge/src/ibc/index.ts
@@ -12,22 +12,22 @@ export class IbcBridgeProvider implements BridgeProvider {
 
   constructor(protected readonly ctx: BridgeProviderContext) {}
 
-  getQuote(params: GetBridgeQuoteParams): Promise<BridgeQuote> {
-    const fromChainId = params.fromChain.chainId;
-    const toChainId = params.toChain.chainId;
-    if (typeof fromChainId !== "string") throw new Error("Invalid fromChainId");
-    if (typeof toChainId !== "string") throw new Error("Invalid toChainId");
+  getQuote(_params: GetBridgeQuoteParams): Promise<BridgeQuote> {
+    // const fromChainId = params.fromChain.chainId;
+    // const toChainId = params.toChain.chainId;
+    // if (typeof fromChainId !== "string") throw new Error("Invalid fromChainId");
+    // if (typeof toChainId !== "string") throw new Error("Invalid toChainId");
 
-    const fromChain = this.ctx.chainList.find(
-      (chain) => chain.chain_id === fromChainId
-    );
-    const toChain = this.ctx.chainList.find(
-      (chain) => chain.chain_id === toChainId
-    );
+    // const fromChain = this.ctx.chainList.find(
+    //   (chain) => chain.chain_id === fromChainId
+    // );
+    // const toChain = this.ctx.chainList.find(
+    //   (chain) => chain.chain_id === toChainId
+    // );
 
-    if (!fromChain || !toChain) {
-      throw new Error("Invalid chainIds");
-    }
+    // if (!fromChain || !toChain) {
+    //   throw new Error("Invalid chainIds");
+    // }
 
     throw new Error("Method not implemented.");
   }
