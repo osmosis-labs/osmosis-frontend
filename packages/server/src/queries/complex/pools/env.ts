f79204313b16229edcc93d5b4796bfe691bc770a
Sunny Aggarwal <sunnya97@protonmail.ch>
2024-05-28 13:13:57 -0400

    bump code id

--- packages/server/src/queries/complex/pools/env.ts
+++ packages/server/src/queries/complex/pools/env.ts
@@ -2,7 +2,7 @@ import { IS_TESTNET } from "../../../env";
 
 /** Cosmwasm Code Ids confirmed to be transmuter pools in current env. */
 const TransmuterPoolCodeIds = IS_TESTNET ? ["3084"] : ["148"];
-const AstroportPclPoolCodeIds = IS_TESTNET ? ["5005"] : ["580"];
+const AstroportPclPoolCodeIds = IS_TESTNET ? ["5005"] : ["773"];
 const WhitewhalePoolCodeIds = IS_TESTNET ? ["?"] : ["503", "641"];
 
 export function getCosmwasmPoolTypeFromCodeId(
