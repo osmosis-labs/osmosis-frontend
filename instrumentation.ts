9e05d08d27e64261e7dadba329978336c2d66add
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2024-05-29 23:20:51 +0200

    Added custom otel exporter

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,5 +1,13 @@
-import { registerOTel } from '@vercel/otel';
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { Resource } from '@opentelemetry/resources'
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
  
-export function register() {
-  registerOTel({ serviceName: 'vercel-datadog-poc' });
-}
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'vercel-datadog-poc',
+  }),
+  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+})
+sdk.start()
