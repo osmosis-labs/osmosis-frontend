b8fcbecbfe8e4ec1d16b2ddc8b43485130b0048a
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2024-05-30 11:10:25 -0600

    Update ibc-overrides.ts

--- packages/web/config/ibc-overrides.ts
+++ packages/web/config/ibc-overrides.ts
@@ -719,6 +719,26 @@ const MainnetIBCAdditionalData: Partial<
     withdrawUrlOverride:
       "https://app.picasso.network/?from=OSMOSIS&to=ETHEREUM",
   },
+  "solana.USDT.pica": {
+    depositUrlOverride: "https://app.picasso.network/?from=ETHEREUM&to=OSMOSIS",
+    withdrawUrlOverride:
+      "https://app.picasso.network/?from=OSMOSIS&to=ETHEREUM",
+  },
+  "edgeSOL.pica": {
+    depositUrlOverride: "https://app.picasso.network/?from=ETHEREUM&to=OSMOSIS",
+    withdrawUrlOverride:
+      "https://app.picasso.network/?from=OSMOSIS&to=ETHEREUM",
+  },
+  "LST.pica": {
+    depositUrlOverride: "https://app.picasso.network/?from=ETHEREUM&to=OSMOSIS",
+    withdrawUrlOverride:
+      "https://app.picasso.network/?from=OSMOSIS&to=ETHEREUM",
+  },
+  "jitoSOL.pica": {
+    depositUrlOverride: "https://app.picasso.network/?from=SOLANA&to=OSMOSIS",
+    withdrawUrlOverride:
+      "https://app.picasso.network/?from=OSMOSIS&to=SOLANA",
+  },
 };
 
 export const IBCAdditionalData: AdditionalData = IS_TESTNET
