5eb316eb14775caac3d63fe9b4aa4ccf262d1d57
Jon Ator <jonathanator0@gmail.com>
2024-05-31 17:09:25 -0400

    fix test

--- packages/tx/src/__tests__/gas.spec.ts
+++ packages/tx/src/__tests__/gas.spec.ts
@@ -778,7 +778,9 @@ describe("getGasFeeAmount", () => {
         "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2",
     });
 
-    expect(gasAmount.denom).toBe("uion");
+    expect(gasAmount.denom).toBe(
+      "ibc/27394FB092D2ECCD56123C74F36E4C1F926001CEADA9CA97EA622B25F41E5EB2"
+    );
     expect(gasAmount.amount).toBe(expectedGasAmount);
     expect(gasAmount.isNeededForTx).toBe(false);
   });
