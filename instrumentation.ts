9dd2adf7d586bbd0c409391f6eb17f1c72abd07c
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2024-06-04 08:58:35 +0200

    Added custom otel library

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,19 +1,19 @@
-// import { NodeSDK } from '@opentelemetry/sdk-node'
-// import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
-// import { Resource } from '@opentelemetry/resources'
-// import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
-// import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
+import { NodeSDK } from '@opentelemetry/sdk-node'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
+import { Resource } from '@opentelemetry/resources'
+import { SEMRESATTRS_SERVICE_NAME } from '@opentelemetry/semantic-conventions'
+import { SimpleSpanProcessor } from '@opentelemetry/sdk-trace-node'
  
-// const sdk = new NodeSDK({
-//   resource: new Resource({
-//     [SEMRESATTRS_SERVICE_NAME]: 'vercel-datadog-poc',
-//   }),
-//   spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
-// })
-// sdk.start()
+const sdk = new NodeSDK({
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'vercel-datadog',
+  }),
+  spanProcessor: new SimpleSpanProcessor(new OTLPTraceExporter()),
+})
+sdk.start()
 
-import { registerOTel } from '@vercel/otel';
+// import { registerOTel } from '@vercel/otel';
  
-export function register() {
-  registerOTel({ serviceName: 'vercel-datadog' });
-}
+// export function register() {
+//   registerOTel({ serviceName: 'vercel-datadog' });
+// }
