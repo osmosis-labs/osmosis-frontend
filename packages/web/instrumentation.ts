3cec956b65fa4e82a81b0c4d5d3d37edd156e78b
Alessandro Lo Manto <alessandro.lo.manto@gmail.com>
2024-06-04 11:05:28 +0200

    Added telemetry configuration

--- packages/web/instrumentation.ts
+++ packages/web/instrumentation.ts
@@ -1,5 +1,22 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
-  registerOTel({ serviceName: "vercel-datadog" });
+  registerOTel({
+    serviceName: "vercel-datadog",
+    instrumentationConfig: {
+      fetch: {
+        propagateContextUrls: [/^https:\/\/sqs\d+/],
+        dontPropagateContextUrls: [/no-propagation\=1/],
+        attributesFromRequestHeaders: {
+          "request.cmd": "X-Cmd",
+        },
+        attributesFromResponseHeaders: {
+          "response.server": "X-Server",
+        },
+      },
+    },
+    attributesFromHeaders: {
+      client: "X-Client",
+    },
+  });
 }
