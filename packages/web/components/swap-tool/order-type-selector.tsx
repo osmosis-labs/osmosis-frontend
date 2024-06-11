f8a73fd6407d744b6ee1113476749b8d6bd26b85
fabryscript <vivaldifabrizio10@gmail.com>
2024-06-12 00:36:20 +0200

    fix: fix panel z-index

--- packages/web/components/swap-tool/order-type-selector.tsx
+++ packages/web/components/swap-tool/order-type-selector.tsx
@@ -65,7 +65,7 @@ export default function OrderTypeSelector() {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <Menu.Items className="absolute right-0 z-20 mt-3 flex w-[280px] origin-top-right flex-col rounded-xl bg-osmoverse-800">
+        <Menu.Items className="absolute right-0 z-50 mt-3 flex w-[280px] origin-top-right flex-col rounded-xl bg-osmoverse-800">
           <div className="flex items-center border-b border-osmoverse-700 py-2 px-4">
             <p className="text-subtitle1 font-semibold">Order Type</p>
           </div>
