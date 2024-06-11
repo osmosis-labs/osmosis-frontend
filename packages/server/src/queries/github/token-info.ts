dc87afe71b39d12ef8d81323e665ee67f1655906
Davide Segullo <davidesegullo@gmail.com>
2024-06-11 09:27:52 -0400

    feat: :sparkles: add new cms config

--- packages/server/src/queries/github/token-info.ts
+++ packages/server/src/queries/github/token-info.ts
@@ -20,13 +20,13 @@ export interface TokenCMSData {
 }
 
 export const getTokenInfo = (denom: string, lang: string) => {
-  const fileName = `${denom.toLowerCase()}_token_info_${lang.toLowerCase()}.json`;
+  const fileName = `${denom.toLowerCase()}_asset_detail_${lang.toLowerCase()}.json`;
 
   if (!CMS_REPOSITORY_PATH)
     throw new Error("Forgot to set CMS_REPOSITORY_PATH env var");
   if (!GITHUB_URL) throw new Error("Forgot to set GITHUB_URL env var");
 
   return githubApi
-    .get<TokenCMSData>(`${CMS_REPOSITORY_PATH}/tokens/${fileName}`)
+    .get<TokenCMSData>(`${CMS_REPOSITORY_PATH}/${fileName}`)
     .then((r) => r.data);
 };
