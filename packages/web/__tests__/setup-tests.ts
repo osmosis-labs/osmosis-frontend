222634d641f3fd889a640f4abe02f22410f28910
Jose Felix <jfelx@outlook.com>
2024-06-17 15:43:21 -0400

    fix: add resize observer polyfill

--- packages/web/__tests__/setup-tests.ts
+++ packages/web/__tests__/setup-tests.ts
@@ -3,8 +3,12 @@ import "@testing-library/jest-dom";
 import "fake-indexeddb/auto";
 import "~/__mocks__/intersection-observer";
 
+import ResizeObserver from "resize-observer-polyfill";
+
 import { server } from "~/__tests__/msw";
 
 beforeAll(() => server.listen({ onUnhandledRequest: "warn" }));
 afterEach(() => server.resetHandlers());
 afterAll(() => server.close());
+
+global.ResizeObserver = ResizeObserver;
