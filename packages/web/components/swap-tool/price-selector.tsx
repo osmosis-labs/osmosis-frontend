c836b3ef7827307db4faaea81fd724605fb7bc74
fabryscript <vivaldifabrizio10@gmail.com>
2024-06-17 13:10:26 +0200

    fix: fix userQuotes undefined handling

--- packages/web/components/swap-tool/price-selector.tsx
+++ packages/web/components/swap-tool/price-selector.tsx
@@ -62,7 +62,7 @@ export default function PriceSelector({
   const { data: userQuotes } = api.edge.assets.getUserAssets.useQuery(
     { osmoAddress: wallet?.address },
     {
-      enabled: !!wallet?.address,
+      enabled: Boolean(wallet?.address),
       select: (data) =>
         data.items
           .map((walletAsset) => {
@@ -100,8 +100,8 @@ export default function PriceSelector({
 
   const userQuotesWithoutBalances = useMemo(
     () =>
-      userQuotes
-        ?.map(({ amount, usdValue, ...props }) => ({ ...props }))
+      (userQuotes ?? [])
+        .map(({ amount, usdValue, ...props }) => ({ ...props }))
         .filter(Boolean) as Asset[],
     [userQuotes]
   );
