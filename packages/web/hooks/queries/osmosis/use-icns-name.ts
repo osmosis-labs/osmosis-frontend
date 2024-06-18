c079c1252da2f7587dfc3c0c635f4589af9574b0
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-06-18 16:45:19 -0700

    Add IS_TESTNET

--- packages/web/hooks/queries/osmosis/use-icns-name.ts
+++ packages/web/hooks/queries/osmosis/use-icns-name.ts
@@ -1,13 +1,14 @@
 import { queryICNSName } from "@osmosis-labs/server";
 import { useQuery } from "@tanstack/react-query";
 
+import { IS_TESTNET } from "~/config";
 import { ChainList } from "~/config/generated/chain-list";
 
 export const useICNSName = ({ address }: { address: string }) => {
   return useQuery({
     queryKey: ["icns-name", address],
     queryFn: () => queryICNSName({ address, chainList: ChainList }),
-    enabled: Boolean(address) && typeof address === "string",
+    enabled: !IS_TESTNET && Boolean(address) && typeof address === "string",
     select: ({ data: { names, primary_name } }) => {
       return {
         names: names,
