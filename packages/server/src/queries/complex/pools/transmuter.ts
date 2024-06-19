ae3c9668d8033ebbeea578b366a40928eeb10a39
Sunny Aggarwal <sunnya97@protonmail.ch>
2024-06-19 16:51:45 -0400

    try

--- packages/server/src/queries/complex/pools/transmuter.ts
+++ packages/server/src/queries/complex/pools/transmuter.ts
@@ -40,6 +40,37 @@ export async function getCachedTransmuterTotalPoolLiquidity(
         chainList,
       });
 
+      
+
+      normalized_pool_liquidity = total_pool_liquidity.map((asset) => {
+        const coin = captureIfError(() =>
+          getAsset({ assetLists, anyDenom: asset.denom })
+        );
+
+        if (coin) {
+          return {
+            denom: coin.denom,
+            amount: new Dec(asset.amount).mul(new Dec(coin.coinDecimals)).toString(),
+          };
+        }
+
+
+        return {
+            denom: string;
+            amount: string;
+        }
+      }
+
+      for (const coin of total_pool_liquidity) {
+        const asset = captureIfError(() =>
+          getAsset({ assetLists, anyDenom: coin.denom })
+        );
+
+        if (asset) {
+
+        }
+      }
+
       const totalLiquidity = total_pool_liquidity.reduce((acc, asset) => {
         return acc.add(new Dec(asset.amount));
       }, new Dec(0));
