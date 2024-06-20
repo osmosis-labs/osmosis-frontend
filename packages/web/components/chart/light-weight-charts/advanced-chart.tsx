c8a81ac12dc9f69eecc12879de982e59070b9cb7
Davide Segullo <davidesegullo@gmail.com>
2024-06-20 15:27:09 +0200

    feat: :sparkles: add trading view custom datafeed

--- packages/web/components/chart/light-weight-charts/advanced-chart.tsx
+++ packages/web/components/chart/light-weight-charts/advanced-chart.tsx
@@ -1,12 +1,22 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
-import { UDFCompatibleDatafeed } from "~/public/datafeeds/udf";
 import {
   ChartingLibraryWidgetOptions,
+  DatafeedConfiguration,
+  ErrorCallback,
+  HistoryCallback,
+  IBasicDataFeed,
+  IChartingLibraryWidget,
+  LibrarySymbolInfo,
+  OnReadyCallback,
   ResolutionString,
+  ResolveCallback,
+  SearchSymbolsCallback,
+  Timezone,
   widget,
 } from "~/public/tradingview";
 import { theme } from "~/tailwind.config";
+import { trpcHelpers } from "~/utils/helpers";
 
 const themeOptions: Partial<ChartingLibraryWidgetOptions> = {
   theme: "dark",
@@ -47,41 +57,200 @@ const themeOptions: Partial<ChartingLibraryWidgetOptions> = {
   },
 };
 
-export const AdvancedChart = (props: Partial<ChartingLibraryWidgetOptions>) => {
-  const chartContainerRef =
-    useRef<HTMLDivElement>() as React.MutableRefObject<HTMLInputElement>;
+const configurationData: DatafeedConfiguration = {
+  supported_resolutions: ["60", "1D", "1W", "1M", "12M"] as ResolutionString[],
+  exchanges: [
+    {
+      value: "dYdX", // `exchange` argument for the `searchSymbols` method, if a user selects this exchange
+      name: "dYdX", // filter name
+      desc: "dYdX v4 exchange", // full exchange name displayed in the filter popup
+    },
+  ],
+  symbols_types: [
+    {
+      name: "crypto",
+      value: "crypto", // `symbolType` argument for the `searchSymbols` method, if a user selects this symbol type
+    },
+  ],
+};
 
-  useEffect(() => {
+const getHistoricalDatafeed = (): IBasicDataFeed => ({
+  onReady(callback: OnReadyCallback) {
+    setTimeout(() => callback(configurationData), 0);
+  },
+  searchSymbols: (
+    _: string,
+    __: string,
+    ___: string,
+    onResult: SearchSymbolsCallback
+  ) => {
+    onResult([]);
+  },
+  resolveSymbol: async (
+    denom: string,
+    onResult: ResolveCallback,
+    onError: ErrorCallback
+  ) => {
+    try {
+      const asset = await trpcHelpers.edge.assets.getUserAsset.fetch({
+        findMinDenomOrSymbol: denom,
+      });
+
+      const pricescale = 10 ** asset.coinDecimals;
+
+      const symbolInfo: LibrarySymbolInfo = {
+        ticker: asset.coinName,
+        name: asset.coinDenom,
+        description: asset.coinName,
+        type: "crypto",
+        exchange: "Osmosis",
+        listed_exchange: "Osmosis",
+        has_intraday: true,
+        has_daily: true,
+        minmov: 1,
+        pricescale,
+        session: "24x7",
+        /* intraday_multipliers: ["5", "60", "720", "1440", "1440"], */
+        supported_resolutions: configurationData.supported_resolutions!,
+        data_status: "endofday",
+        timezone: Intl.DateTimeFormat().resolvedOptions().timeZone as Timezone,
+        format: "price",
+      };
+
+      setTimeout(() => onResult(symbolInfo), 0);
+    } catch (error) {
+      setTimeout(() => onError((error as Error).message), 0);
+    }
+  },
+
+  getBars: async (
+    symbolInfo: LibrarySymbolInfo,
+    resolution: ResolutionString,
+    periodParams: {
+      countBack: number;
+      from: number;
+      to: number;
+      firstDataRequest: boolean;
+    },
+    onResult: HistoryCallback,
+    onError: ErrorCallback
+  ) => {
+    try {
+      if (!symbolInfo) return;
+
+      const { countBack, from, to, firstDataRequest } = periodParams;
+
+      const customTimeFrame = {
+        timeFrame: 5,
+      };
+
+      switch (resolution) {
+        case "60":
+          customTimeFrame.timeFrame = 5;
+          break;
+        case "1D":
+          customTimeFrame.timeFrame = 60;
+          break;
+        case "1W":
+          customTimeFrame.timeFrame = 720;
+          break;
+        case "1M":
+          customTimeFrame.timeFrame = 1440;
+          break;
+        case "12M":
+          customTimeFrame.timeFrame = 1440;
+          break;
+      }
+
+      const bars = await trpcHelpers.edge.assets.getAssetHistoricalPrice.fetch({
+        coinDenom: symbolInfo.name,
+        timeFrame: {
+          custom: customTimeFrame,
+        },
+      });
+
+      if (bars.length === 0 || bars.length < countBack) {
+        onResult([], {
+          noData: true,
+        });
+
+        return;
+      }
+
+      if (firstDataRequest) {
+        onResult(
+          bars.map((bar) => ({
+            ...bar,
+            time: bar.time * 1000,
+          })),
+          {
+            noData: false,
+          }
+        );
+      }
+    } catch (error) {
+      onError((error as Error).message);
+    }
+  },
+
+  subscribeBars() {},
+
+  unsubscribeBars: () => {},
+});
+
+type AdvancedChartProps = Omit<
+  Partial<ChartingLibraryWidgetOptions>,
+  "symbol"
+> & {
+  coinDenom: string;
+};
+
+export const AdvancedChart = (props: AdvancedChartProps) => {
+  const [container, setContainer] = useState<HTMLDivElement | null>(null);
+  const chart = useRef<IChartingLibraryWidget>();
+
+  if (container && chart.current === undefined) {
     const widgetOptions: ChartingLibraryWidgetOptions = {
-      symbol: props.symbol,
-      // BEWARE: no trailing slash is expected in feed URL
-      datafeed: new UDFCompatibleDatafeed(
-        "https://demo_feed.tradingview.com",
-        undefined,
-        {
-          maxResponseLength: 1000,
-          expectedOrder: "latestFirst",
-        }
-      ),
-      interval: props.interval as ResolutionString,
-      container: chartContainerRef.current,
+      symbol: props.coinDenom,
+      datafeed: getHistoricalDatafeed(),
+      interval: "1d" as ResolutionString,
+      container,
       library_path: "/tradingview/",
       locale: "en",
-      enabled_features: ["use_localstorage_for_settings"],
-      fullscreen: props.fullscreen,
+      debug: true,
+      disabled_features: [
+        "header_symbol_search",
+        "header_compare",
+        "symbol_search_hot_key",
+        "symbol_info",
+        "go_to_date",
+        "timeframes_toolbar",
+        "header_quick_search",
+        "header_symbol_search",
+        "symbol_search_hot_key",
+        "compare_symbol_search_spread_operators",
+        "studies_symbol_search_spread_operators",
+      ],
+      enabled_features: [
+        "remove_library_container_border",
+        "hide_last_na_study_output",
+        "dont_show_boolean_study_arguments",
+        "hide_left_toolbar_by_default",
+      ],
       autosize: true,
       ...themeOptions,
+      ...props,
     };
 
-    // @ts-ignore
-    const tvWidget = (window.tvWidget = new widget(widgetOptions));
+    chart.current = new widget(widgetOptions);
+  }
 
-    console.log(widgetOptions);
-
+  useEffect(() => {
     return () => {
-      tvWidget.remove();
+      chart.current?.remove();
+      chart.current = undefined;
     };
-  }, [props]);
+  }, []);
 
-  return <div className="relative h-full" ref={chartContainerRef} />;
+  return <div className="relative h-full" ref={setContainer} />;
 };
