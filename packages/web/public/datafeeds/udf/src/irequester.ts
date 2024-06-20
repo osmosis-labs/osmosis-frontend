394f42d001f34b15bd77d3b171ef4f048e896011
Davide Segullo <davidesegullo@gmail.com>
2024-06-20 10:53:05 +0200

    feat: :heavy_plus_sign: add datafeeds udf

--- packages/web/public/datafeeds/udf/src/irequester.ts
+++ packages/web/public/datafeeds/udf/src/irequester.ts
@@ -1,7 +1,19 @@
-import { RequestParams, UdfErrorResponse, UdfResponse } from './helpers';
+import { RequestParams, UdfErrorResponse, UdfResponse } from "./helpers";
 
 export interface IRequester {
-	sendRequest<T extends UdfResponse>(datafeedUrl: string, urlPath: string, params?: RequestParams): Promise<T | UdfErrorResponse>;
-	sendRequest<T>(datafeedUrl: string, urlPath: string, params?: RequestParams): Promise<T>;
-	sendRequest<T>(datafeedUrl: string, urlPath: string, params?: RequestParams): Promise<T>;
+  sendRequest<T extends UdfResponse>(
+    datafeedUrl: string,
+    urlPath: string,
+    params?: RequestParams
+  ): Promise<T | UdfErrorResponse>;
+  sendRequest<T>(
+    datafeedUrl: string,
+    urlPath: string,
+    params?: RequestParams
+  ): Promise<T>;
+  sendRequest<T>(
+    datafeedUrl: string,
+    urlPath: string,
+    params?: RequestParams
+  ): Promise<T>;
 }
