ab264867511a2079b66ba5d34e667f4a763564c9
fabryscript <vivaldifabrizio10@gmail.com>
2024-06-20 12:40:19 +0200

    feat: add URL-based tab system

--- packages/web/components/transactions/transaction-content.tsx
+++ packages/web/components/transactions/transaction-content.tsx
@@ -1,3 +1,4 @@
+import { Tab } from "@headlessui/react";
 import { FormattedTransaction } from "@osmosis-labs/server";
 import {
   AccountStoreWallet,
@@ -5,15 +6,20 @@ import {
   CosmwasmAccount,
   OsmosisAccount,
 } from "@osmosis-labs/stores";
+import classNames from "classnames";
 import { useRouter } from "next/router";
+import { parseAsStringLiteral, useQueryState } from "nuqs";
 
 import { BackToTopButton } from "~/components/buttons/back-to-top-button";
 import { Spinner } from "~/components/loaders";
 import { NoTransactionsSplash } from "~/components/transactions/no-transactions-splash";
+import { OrderHistory } from "~/components/transactions/order-history";
 import { TransactionButtons } from "~/components/transactions/transaction-buttons";
 import { TransactionsPaginaton } from "~/components/transactions/transaction-pagination";
 import { TransactionRows } from "~/components/transactions/transaction-rows";
 import { useTranslation } from "~/hooks";
+
+const TX_PAGE_TABS = ["history", "orders"] as const;
 
 export const TransactionContent = ({
   setSelectedTransactionHash,
@@ -46,6 +52,11 @@ export const TransactionContent = ({
 
   const router = useRouter();
 
+  const [tab, setTab] = useQueryState(
+    "tab",
+    parseAsStringLiteral(TX_PAGE_TABS).withDefault("history")
+  );
+
   const showTransactionContent =
     wallet &&
     wallet.isWalletConnected &&
@@ -68,40 +79,74 @@ export const TransactionContent = ({
         <TransactionButtons open={open} address={address} />
       </div>
 
-      <div className="-mx-4 flex flex-col">
-        {showConnectWallet ? (
-          <NoTransactionsSplash variant="connect" />
-        ) : showTransactionContent ? (
-          <TransactionRows
-            transactions={transactions}
-            setSelectedTransactionHash={setSelectedTransactionHash}
-            selectedTransactionHash={selectedTransactionHash}
-            setOpen={setOpen}
-            open={open}
-          />
-        ) : isLoading ? (
-          <Spinner className="self-center" />
-        ) : transactions.length === 0 ? (
-          <NoTransactionsSplash variant="transactions" />
-        ) : null}
-      </div>
-      <div className="py-6">
-        {showPagination && (
-          <TransactionsPaginaton
-            showPrevious={+page > 0}
-            showNext={hasNextPage}
-            previousHref={{
-              pathname: router.pathname,
-              query: { ...router.query, page: Math.max(0, +page - 1) },
-            }}
-            nextHref={{
-              pathname: router.pathname,
-              query: { ...router.query, page: +page + 1 },
-            }}
-          />
-        )}
-      </div>
-      <BackToTopButton />
+      <Tab.Group
+        manual
+        selectedIndex={TX_PAGE_TABS.indexOf(tab)}
+        onChange={(idx) => setTab(TX_PAGE_TABS[idx])}
+      >
+        <Tab.List className="flex items-center gap-8">
+          {TX_PAGE_TABS.map((defaultTab) => (
+            <Tab key={defaultTab}>
+              <h5
+                className={classNames({
+                  "text-osmoverse-500": defaultTab !== tab,
+                })}
+              >
+                {/**
+                 * TEMP
+                 *
+                 * We will use i18n keys here
+                 */}
+                {defaultTab.slice(0, 1).toUpperCase()}
+                {defaultTab.slice(1)}
+              </h5>
+            </Tab>
+          ))}
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel>
+            <>
+              <div className="-mx-4 flex flex-col">
+                {showConnectWallet ? (
+                  <NoTransactionsSplash variant="connect" />
+                ) : showTransactionContent ? (
+                  <TransactionRows
+                    transactions={transactions}
+                    setSelectedTransactionHash={setSelectedTransactionHash}
+                    selectedTransactionHash={selectedTransactionHash}
+                    setOpen={setOpen}
+                    open={open}
+                  />
+                ) : isLoading ? (
+                  <Spinner className="self-center" />
+                ) : transactions.length === 0 ? (
+                  <NoTransactionsSplash variant="transactions" />
+                ) : null}
+              </div>
+              <div className="py-6">
+                {showPagination && (
+                  <TransactionsPaginaton
+                    showPrevious={+page > 0}
+                    showNext={hasNextPage}
+                    previousHref={{
+                      pathname: router.pathname,
+                      query: { ...router.query, page: Math.max(0, +page - 1) },
+                    }}
+                    nextHref={{
+                      pathname: router.pathname,
+                      query: { ...router.query, page: +page + 1 },
+                    }}
+                  />
+                )}
+              </div>
+              <BackToTopButton />
+            </>
+          </Tab.Panel>
+          <Tab.Panel>
+            <OrderHistory />
+          </Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
     </div>
   );
 };
