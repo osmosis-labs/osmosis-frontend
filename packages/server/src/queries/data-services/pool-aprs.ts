1e8b809e2e57c4685db54f310f3fe0ce6c0f1831
Davide Segullo <davidesegullo@gmail.com>
2024-06-21 11:00:49 +0200

    feat: :sparkles: add queryPoolAprsRange service

--- packages/server/src/queries/data-services/pool-aprs.ts
+++ packages/server/src/queries/data-services/pool-aprs.ts
@@ -2,17 +2,28 @@ import { apiClient } from "@osmosis-labs/utils";
 
 import { NUMIA_BASE_URL } from "../../env";
 
-type PoolApr = {
+type PoolApr<T = number> = {
   pool_id: string;
-  swap_fees: number;
-  superfluid: number;
-  osmosis: number;
-  boost: number;
-  total_apr: number;
+  swap_fees: T;
+  superfluid: T;
+  osmosis: T;
+  boost: T;
+  total_apr: T;
 };
 
 /** Queries numia for a breakdown of APRs per pool. */
 export function queryPoolAprs(): Promise<PoolApr[]> {
   const url = new URL("/pools_apr", NUMIA_BASE_URL);
+  return apiClient(url.toString());
+}
+
+type Range = {
+  lower: number;
+  upper: number;
+};
+
+/** Queries numia for a breakdown of APRs per pool. */
+export function queryPoolAprsRange(): Promise<PoolApr<Range>[]> {
+  const url = new URL("/pools_apr_range", NUMIA_BASE_URL);
   return apiClient(url.toString());
 }
