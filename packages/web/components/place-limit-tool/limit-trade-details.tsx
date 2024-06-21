b7a4d91332a8c5f687604e64e2062774cddc03ef
Connor Barr <crnbarr@gmail.com>
2024-06-21 15:30:54 +0100

    feat: altered trade details loading state

--- packages/web/components/place-limit-tool/limit-trade-details.tsx
+++ packages/web/components/place-limit-tool/limit-trade-details.tsx
@@ -4,7 +4,7 @@ import classNames from "classnames";
 import React, { FC, memo, useMemo, useState } from "react";
 
 import { Icon } from "~/components/assets";
-import { SkeletonLoader } from "~/components/loaders/skeleton-loader";
+import { Spinner } from "~/components/loaders";
 import { useTranslation } from "~/hooks";
 import { PlaceLimitState } from "~/hooks/limit-orders";
 import { formatPretty } from "~/utils/formatter";
@@ -34,76 +34,82 @@ export const LimitTradeDetails: FC<LimitTradeDetailsProps> = memo(
       return swapState.isMakerFeeLoading;
     }, [swapState.isMakerFeeLoading]);
 
+    const shouldDisplayInfo = useMemo(() => {
+      return !isLoading && displayInfo;
+    }, [isLoading, displayInfo]);
+
     return (
-      <SkeletonLoader isLoaded={!isLoading}>
-        <div className="flex w-full flex-col">
-          <div
-            className="flex w-full cursor-pointer items-center justify-between"
-            onClick={() => setDisplayInfo(!displayInfo)}
-          >
-            <div className="text-subtitle1">
-              {t("place-limit.tradeDetails")}
-            </div>
-            <div className="flex items-center justify-end text-body2 text-osmoverse-300">
+      <div className="flex w-full flex-col">
+        <div
+          className="flex w-full cursor-pointer items-center justify-between"
+          onClick={() => setDisplayInfo(!displayInfo && !isLoading)}
+        >
+          <div className="text-subtitle1">{t("place-limit.tradeDetails")}</div>
+          <div className="flex items-center justify-end text-body2 text-osmoverse-300">
+            {isLoading ? (
+              <div className="flex items-center text-body2 text-osmoverse-400">
+                <Spinner className="mr-2" /> Estimating
+              </div>
+            ) : (
               <Icon
                 id="chevron-down"
                 className={classNames(
                   "ml-3 text-osmoverse-400 transition-transform",
                   {
-                    "rotate-180": displayInfo,
+                    "rotate-180": shouldDisplayInfo,
                   }
                 )}
                 height={12}
                 width={12}
               />
+            )}
+          </div>
+        </div>
+        <div
+          className={classNames(
+            "flex w-full flex-col items-start overflow-hidden transition-all",
+            {
+              "h-0": !shouldDisplayInfo,
+              "h-[105px]": shouldDisplayInfo,
+            }
+          )}
+        >
+          <div className="flex w-full items-center justify-between py-3 text-body2 text-osmoverse-300">
+            <div>
+              Total fees when filled ( ~
+              {formatPretty(swapState.makerFee.mul(new Dec(100)), {
+                maxDecimals: 2,
+                minimumFractionDigits: 2,
+              })}
+              %)
+            </div>
+            <div>
+              ~
+              {formatPretty(makerFeeFiat, {
+                maxDecimals: 2,
+                minimumFractionDigits: 2,
+              })}
             </div>
           </div>
-          <div
-            className={classNames(
-              "flex w-full flex-col items-start overflow-hidden transition-all",
-              {
-                "h-0": !displayInfo,
-                "h-[105px]": displayInfo,
-              }
-            )}
-          >
-            <div className="flex w-full items-center justify-between py-3 text-body2 text-osmoverse-300">
-              <div>
-                Total fees when filled ( ~
-                {formatPretty(swapState.makerFee.mul(new Dec(100)), {
-                  maxDecimals: 2,
-                  minimumFractionDigits: 2,
-                })}
-                %)
-              </div>
-              <div>
-                ~
-                {formatPretty(makerFeeFiat, {
-                  maxDecimals: 2,
-                  minimumFractionDigits: 2,
-                })}
-              </div>
-            </div>
-            <hr className="my-2 w-full text-osmoverse-700" />
-            <div className="flex w-full items-center justify-between py-3 text-body2 text-osmoverse-300">
-              <div>Receive</div>
-              <div>
-                ~
-                {formatPretty(swapState.expectedFiatAmountOut, {
-                  maxDecimals: 2,
-                  minimumFractionDigits: 2,
-                })}{" "}
-                (
-                {formatPretty(swapState.expectedTokenAmountOut, {
-                  maxDecimals: 2,
-                  minimumFractionDigits: 2,
-                })}
-                )
-              </div>
+          <hr className="my-2 w-full text-osmoverse-700" />
+          <div className="flex w-full items-center justify-between py-3 text-body2 text-osmoverse-300">
+            <div>Receive</div>
+            <div>
+              ~
+              {formatPretty(swapState.expectedFiatAmountOut, {
+                maxDecimals: 2,
+                minimumFractionDigits: 2,
+              })}{" "}
+              (
+              {formatPretty(swapState.expectedTokenAmountOut, {
+                maxDecimals: 2,
+                minimumFractionDigits: 2,
+              })}
+              )
             </div>
           </div>
         </div>
-      </SkeletonLoader>
+      </div>
     );
   }
 );
