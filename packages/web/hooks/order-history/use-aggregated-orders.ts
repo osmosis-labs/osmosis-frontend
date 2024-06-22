36bf38c44a7346c87fe11b9b1c165332ceab64fe
fabryscript <vivaldifabrizio10@gmail.com>
2024-06-22 13:33:56 +0200

    fix: fix memory leak

--- packages/web/hooks/order-history/use-aggregated-orders.ts
+++ packages/web/hooks/order-history/use-aggregated-orders.ts
@@ -1,24 +1,29 @@
 import { getAssetFromAssetList } from "@osmosis-labs/utils";
+import { useMemo } from "react";
 
 import { Order } from "~/components/complex/orders-history";
 import { AssetLists } from "~/config/generated/asset-lists";
 
 export function useAggregatedOrders({ orders }: { orders: Order[] }) {
-  const aggregated: Order[] = orders.map((order) => {
-    return {
-      ...order,
-      baseDenom:
-        getAssetFromAssetList({
-          assetLists: AssetLists,
-          coinMinimalDenom: order.baseDenom,
-        })?.symbol ?? order.baseDenom,
-      quoteDenom:
-        getAssetFromAssetList({
-          assetLists: AssetLists,
-          coinMinimalDenom: order.quoteDenom,
-        })?.symbol ?? order.quoteDenom,
-    };
-  });
+  const aggregated: Order[] = useMemo(
+    () =>
+      orders.map((order) => {
+        return {
+          ...order,
+          baseDenom:
+            getAssetFromAssetList({
+              assetLists: AssetLists,
+              coinMinimalDenom: order.baseDenom,
+            })?.symbol ?? order.baseDenom,
+          quoteDenom:
+            getAssetFromAssetList({
+              assetLists: AssetLists,
+              coinMinimalDenom: order.quoteDenom,
+            })?.symbol ?? order.quoteDenom,
+        };
+      }),
+    [orders]
+  );
 
   return {
     orders: aggregated,
