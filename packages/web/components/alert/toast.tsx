af97a3c130ce233d3f47d9e613e46f85d30ee80f
Jon Ator <jonathanator0@gmail.com>
2024-06-27 17:30:27 -0400

    show toasts during transfer

--- packages/web/components/alert/toast.tsx
+++ packages/web/components/alert/toast.tsx
@@ -1,40 +1,40 @@
 import Image from "next/image";
 import React, { FunctionComponent } from "react";
-import { toast, ToastOptions } from "react-toastify";
+import { toast, ToastOptions as ReactToastifyOptions } from "react-toastify";
 
 import { Alert, ToastType } from "~/components/alert";
 import { Icon } from "~/components/assets";
 import { t } from "~/hooks";
 
+export type ToastOptions = Partial<ReactToastifyOptions>;
+
 export function displayToast(
   alert: Alert,
   type: ToastType,
-  toastOptions?: Partial<ToastOptions>
+  toastOptions?: ToastOptions
 ) {
   toastOptions = {
-    ...{
-      position: "top-right",
-      autoClose: 7000,
-      hideProgressBar: true,
-      closeOnClick: false,
-      pauseOnHover: true,
-      draggable: false,
-      progress: undefined,
-      pauseOnFocusLoss: false,
-      closeButton: ({ closeToast }) => (
-        <button
-          onClick={closeToast}
-          className="absolute -left-2 -top-2 h-6 w-6 hover:opacity-75 md:top-2 md:h-5 md:w-5"
-        >
-          <Image
-            alt="close"
-            src="/icons/close-circle.svg"
-            height={32}
-            width={32}
-          />
-        </button>
-      ),
-    },
+    position: "top-right",
+    autoClose: type === ToastType.LOADING ? 2000 : 7000,
+    hideProgressBar: true,
+    closeOnClick: false,
+    pauseOnHover: true,
+    draggable: false,
+    progress: undefined,
+    pauseOnFocusLoss: false,
+    closeButton: ({ closeToast }) => (
+      <button
+        onClick={closeToast}
+        className="absolute -left-2 -top-2 h-6 w-6 hover:opacity-75 md:top-2 md:h-5 md:w-5"
+      >
+        <Image
+          alt="close"
+          src="/icons/close-circle.svg"
+          height={32}
+          width={32}
+        />
+      </button>
+    ),
     ...(toastOptions ?? {}),
   };
 
@@ -46,7 +46,7 @@ export function displayToast(
       toast(<ErrorToast {...alert} />, toastOptions);
       break;
     case ToastType.LOADING:
-      toast(<LoadingToast {...alert} />, { ...toastOptions, autoClose: 2000 });
+      toast(<LoadingToast {...alert} />, toastOptions);
       break;
     case ToastType.ONE_CLICK_TRADING:
       toast(<OneClickTradingToast {...alert} />, toastOptions);
@@ -58,6 +58,8 @@ const LoadingToast: FunctionComponent<Alert> = ({
   titleTranslationKey,
   captionTranslationKey,
   captionElement,
+  learnMoreUrl,
+  learnMoreUrlCaption,
 }) => (
   <div className="flex items-center gap-3 md:gap-2">
     <div className="flex h-8 w-8 shrink-0 animate-spin items-center">
@@ -77,6 +79,18 @@ const LoadingToast: FunctionComponent<Alert> = ({
             ? t(captionTranslationKey)
             : t(...captionTranslationKey)}
         </p>
+      )}
+      {learnMoreUrl && learnMoreUrlCaption && (
+        <a
+          target="__blank"
+          href={learnMoreUrl}
+          className="inline cursor-pointer text-sm hover:opacity-75 md:text-xs"
+        >
+          {t(learnMoreUrlCaption ?? "Learn more")}
+          <div className="mb-0.75 ml-2 inline-block">
+            <Icon aria-label="link" id="external-link" height={12} width={12} />
+          </div>
+        </a>
       )}
     </div>
   </div>
--- packages/web/stores/transfer-history.ts
+++ packages/web/stores/transfer-history.ts
@@ -14,6 +14,9 @@ import {
   toJS,
 } from "mobx";
 import { computedFn } from "mobx-utils";
+import { toast } from "react-toastify";
+
+import { displayToast, ToastType } from "~/components/alert";
 
 /** Persistable data enough to identify a tx. */
 type TxSnapshot = {
@@ -39,6 +42,13 @@ export class TransferHistoryStore implements TransferStatusReceiver {
   protected snapshots: TxSnapshot[] = [];
   @observable
   private isRestoredFromLocalStorage = false;
+
+  /**
+   * Since we can't control how many times a status provider
+   * will call `receiveNewTxStatus`, we need to track which
+   * tx statuses have already been resolved to avoid duplicity in UI.
+   */
+  private readonly _resolvedTxStatusKeys = new Set<string>();
 
   constructor(
     protected readonly onAccountTransferSuccess: (
@@ -121,6 +131,19 @@ export class TransferHistoryStore implements TransferStatusReceiver {
       prefixedKey.slice(statusSource.keyPrefix.length)
     );
 
+    setTimeout(() => {
+      displayToast(
+        {
+          titleTranslationKey: isWithdraw
+            ? "transfer.pendingWithdraw"
+            : "transfer.pendingDeposit",
+          captionTranslationKey: amount,
+        },
+        ToastType.LOADING,
+        { toastId: prefixedKey, autoClose: false }
+      );
+    }, 500);
+
     this.snapshots.push({
       createdAtMs: Date.now(),
       prefixedKey,
@@ -150,13 +173,64 @@ export class TransferHistoryStore implements TransferStatusReceiver {
       return;
     }
 
-    // update balances if successful
-    if (status === "success") {
-      this.onAccountTransferSuccess(snapshot.accountAddress);
-    }
-
+    // set updates
     snapshot.status = status;
     snapshot.reason = reason;
+
+    switch (status) {
+      case "pending":
+        displayToast(
+          {
+            titleTranslationKey: snapshot.isWithdraw
+              ? "transfer.pendingWithdraw"
+              : "transfer.pendingDeposit",
+          },
+          ToastType.LOADING,
+          { toastId: prefixedKey, autoClose: false }
+        );
+        break;
+      case "success":
+        if (this._resolvedTxStatusKeys.has(prefixedKey)) break;
+        displayToast(
+          {
+            titleTranslationKey: snapshot.isWithdraw
+              ? "transfer.completedWithdraw"
+              : "transfer.completedDeposit",
+            captionTranslationKey: snapshot.amount,
+          },
+          ToastType.SUCCESS
+        );
+        this.onAccountTransferSuccess(snapshot.accountAddress);
+        this._resolvedTxStatusKeys.add(prefixedKey);
+        toast.dismiss(prefixedKey);
+        break;
+      case "failed":
+        if (this._resolvedTxStatusKeys.has(prefixedKey)) break;
+        displayToast(
+          {
+            titleTranslationKey: snapshot.isWithdraw
+              ? "transfer.failWithdraw"
+              : "transfer.failDeposit",
+            captionTranslationKey: snapshot.amount,
+          },
+          ToastType.ERROR
+        );
+        this._resolvedTxStatusKeys.add(prefixedKey);
+        toast.dismiss(prefixedKey);
+        break;
+      case "connection-error":
+        if (this._resolvedTxStatusKeys.has(prefixedKey)) break;
+        displayToast(
+          {
+            titleTranslationKey: "transfer.connectionError",
+            captionTranslationKey: snapshot.amount,
+          },
+          ToastType.ERROR
+        );
+        this._resolvedTxStatusKeys.add(prefixedKey);
+        toast.dismiss(prefixedKey);
+        break;
+    }
   }
 
   /** Use persisted tx snapshots to resume Tx monitoring after browser first loads.
@@ -174,10 +248,14 @@ export class TransferHistoryStore implements TransferStatusReceiver {
         snapshot.prefixedKey.startsWith(source.keyPrefix)
       );
 
-      // start receiving tx status updates again
-      statusSource?.trackTxStatus(
-        snapshot.prefixedKey.slice(statusSource.keyPrefix.length)
-      );
+      // start receiving tx status updates again for snapshots that were still pending
+      if (snapshot.status === "pending" && statusSource) {
+        statusSource.trackTxStatus(
+          snapshot.prefixedKey.slice(statusSource.keyPrefix.length)
+        );
+      } else {
+        this._resolvedTxStatusKeys.add(snapshot.prefixedKey);
+      }
 
       runInAction(() => {
         this.snapshots.push(snapshot);
