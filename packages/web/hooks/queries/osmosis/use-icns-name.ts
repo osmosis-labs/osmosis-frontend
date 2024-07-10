59f7b4eba1871eb46ffeee9a3dde76d3736d3967
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-07-10 14:57:26 -0700

    Revert "update enabled variable (#3469)"
    This reverts commit b28f8b87a16700ebe68bec55555e17bb166d85e0.

--- packages/web/hooks/queries/osmosis/use-icns-name.ts
+++ packages/web/hooks/queries/osmosis/use-icns-name.ts
@@ -1,17 +1,13 @@
 import { queryICNSName } from "@osmosis-labs/server";
 import { useQuery } from "@tanstack/react-query";
 
-import { IS_TESTNET } from "~/config";
 import { ChainList } from "~/config/generated/chain-list";
 
 export const useICNSName = ({ address }: { address: string }) => {
-  const enabled =
-    !!IS_TESTNET && Boolean(address) && typeof address === "string";
-
   return useQuery({
     queryKey: ["icns-name", address],
     queryFn: () => queryICNSName({ address, chainList: ChainList }),
-    enabled,
+    enabled: Boolean(address) && typeof address === "string",
     select: ({ data: { names, primary_name } }) => {
       return {
         names: names,
