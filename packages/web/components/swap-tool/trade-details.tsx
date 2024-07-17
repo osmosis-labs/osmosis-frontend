b0331b4bc011150e772cd5302895059b30bc0462
Sunny Aggarwal <sunnya97@protonmail.ch>
2024-07-17 14:02:30 -0400

    test

--- packages/web/components/swap-tool/trade-details.tsx
+++ packages/web/components/swap-tool/trade-details.tsx
@@ -226,28 +226,6 @@ export const TradeDetails = ({
                   }
                 />
                 <hr className="my-2 w-full text-osmoverse-700" />
-                {outAmountLessSlippage &&
-                  outFiatAmountLessSlippage &&
-                  swapState?.toAsset && (
-                    <RecapRow
-                      left={t("limitOrders.receiveEstimated")}
-                      right={
-                        <span>
-                          <span className="text-osmoverse-100">
-                            {formatPretty(outAmountLessSlippage, {
-                              maxDecimals: 8,
-                            })}{" "}
-                            {swapState?.toAsset.coinDenom}
-                          </span>{" "}
-                          {outFiatAmountLessSlippage && (
-                            <span className="text-osmoverse-300">
-                              (~{formatPretty(outFiatAmountLessSlippage)})
-                            </span>
-                          )}
-                        </span>
-                      }
-                    />
-                  )}
                 <span className="subtitle1 py-3 text-white-full">
                   {t("limitOrders.moreDetails")}
                 </span>
@@ -317,6 +295,28 @@ export const TradeDetails = ({
                     }
                   />
                 )}
+                {outAmountLessSlippage &&
+                  outFiatAmountLessSlippage &&
+                  swapState?.toAsset && (
+                    <RecapRow
+                      left={t("limitOrders.receiveEstimated")}
+                      right={
+                        <span>
+                          <span className="text-osmoverse-100">
+                            {formatPretty(outAmountLessSlippage, {
+                              maxDecimals: 8,
+                            })}{" "}
+                            {swapState?.toAsset.coinDenom}
+                          </span>{" "}
+                          {outFiatAmountLessSlippage && (
+                            <span className="text-osmoverse-300">
+                              (~{formatPretty(outFiatAmountLessSlippage)})
+                            </span>
+                          )}
+                        </span>
+                      }
+                    />
+                  )}
                 <div className="flex w-full">
                   <RecapRow
                     left={t("limitOrders.swapRoute")}
