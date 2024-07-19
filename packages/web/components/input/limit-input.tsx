3eaa534a9fcc08b0a2e6b0aa19344789d75ee9a1
fabryscript <vivaldifabrizio10@gmail.com>
2024-07-19 16:59:32 +0200

    Revert "feat: limit fiat input to two digits"
    This reverts commit ae1e677c879daad620f5792ee5f504f945a96838.

--- packages/web/components/input/limit-input.tsx
+++ packages/web/components/input/limit-input.tsx
@@ -116,13 +116,7 @@ export const LimitInput: FC<LimitInputProps> = ({
   const setFiatAmountSafe = useCallback(
     (value?: string) => {
       if (!value) {
-        setMarketAmount("");
         return setFiatAmount("");
-      }
-
-      // Limit decimal input to two digits
-      if (value.split(".")[1]?.length > 2) {
-        return;
       }
 
       const updatedValue = transformAmount(value);
@@ -195,7 +189,6 @@ export const LimitInput: FC<LimitInputProps> = ({
 
     setTokenAmountSafe(tokenValue ? tokenValue.toString() : undefined);
   }, [price, fiatAmount, setTokenAmountSafe, focused]);
-
   return (
     <div className="relative h-[108px]">
       {(["fiat", "token"] as ("fiat" | "token")[]).map((inputType) => (
