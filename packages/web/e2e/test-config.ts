24ade93f4fe90d305c6744e4d55acd2458e10222
yury-dubinin <yury.dubinin@gmail.com>
2024-07-20 11:03:17 +0200

    use secrets

--- packages/web/e2e/test-config.ts
+++ packages/web/e2e/test-config.ts
@@ -1,8 +1,8 @@
 export class TestConfig {
   getProxyConfig() {
     const server = process.env.TEST_PROXY ?? "http://138.68.112.16:8888";
-    const username = process.env.TEST_PROXY_USERNAME ?? "osmo";
-    const password = process.env.TEST_PROXY_PASSWORD ?? "tlnyproxy88";
+    const username = process.env.TEST_PROXY_USERNAME;
+    const password = process.env.TEST_PROXY_PASSWORD;
 
     console.info("Get Test Proxy configuration for server: " + server);
 
