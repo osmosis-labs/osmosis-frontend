c17284b4cf54368f7d241c9c6cee41b248d6d518
fabryscript <vivaldifabrizio10@gmail.com>
2024-07-23 13:20:32 +0200

    feat: create generic disclaimer tooltip

---
+++ packages/web/components/tooltip/generic-disclaimer.tsx
@@ -0,0 +1,30 @@
+import { PropsWithChildren, ReactNode } from "react";
+
+import { Tooltip } from "~/components/tooltip";
+
+export function GenericDisclaimer({
+  body,
+  children,
+  title,
+  disabled,
+}: PropsWithChildren<
+  Partial<{ title: ReactNode; body: ReactNode; disabled: boolean }>
+>) {
+  return (
+    <Tooltip
+      disabled={disabled}
+      rootClassNames="!p-0 w-[280px] !border-0 !rounded-none !bg-transparent"
+      content={
+        <div className="relative flex items-start gap-3 rounded-xl border border-[#39383D] bg-osmoverse-1000 p-3">
+          <div className="flex flex-col gap-1">
+            <span className="caption">{title}</span>
+            <span className="caption text-osmoverse-300">{body}</span>
+          </div>
+        </div>
+      }
+      className="text-wosmongton-300"
+    >
+      <div className="w full">{children}</div>
+    </Tooltip>
+  );
+}
