f492c2bceadc5d3fbb3aa33368341d5256c48b4b
yury-dubinin <yury.dubinin@gmail.com>
2024-07-26 15:03:44 +0200

    Updated stable swap tests

--- packages/web/e2e/tests/swap.stables-extra.spec.ts
+++ packages/web/e2e/tests/swap.stables-extra.spec.ts
@@ -2,25 +2,22 @@
 import { BrowserContext, chromium, expect, test } from "@playwright/test";
 import process from "process";
 
-import { TestConfig } from "~/e2e/test-config";
-import { UnzipExtension } from "~/e2e/unzip-extension";
-
 import { SwapPage } from "../pages/swap-page";
 import { WalletPage } from "../pages/wallet-page";
+import { TestConfig } from "../test-config";
+import { UnzipExtension } from "../unzip-extension";
 
-test.describe("Test Swap Stables feature", () => {
+test.describe("Test Swap Stables Extra feature", () => {
   let context: BrowserContext;
   const walletId =
     process.env.WALLET_ID_S ?? "osmo1dkmsds5j6q9l9lv4dkhas68767tlqfx8ls5j0c";
   const privateKey = process.env.PRIVATE_KEY_S ?? "private_key_s";
   const password = process.env.PASSWORD ?? "TestPassword2024.";
   let swapPage: SwapPage;
-  let USDC =
-    "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4";
-  let USDCa =
-    "ibc/D189335C6E4A68B513C10AB227BF1C1D38C746766278BA3EEB4FB14124F1D858";
-  let allUSDT =
-    "factory/osmo1em6xs47hd82806f5cxgyufguxrrc7l0aqx7nzzptjuqgswczk8csavdxek/alloyed/allUSDT";
+  let USDTa =
+    "ibc/8242AD24008032E457D2E12D46588FD39FB54FB29680C6C7663D296B383C37C4";
+  let kavaUSDT =
+    "ibc/4ABBEF4C8926DDDB320AE5188CFD63267ABBCEFC0583E4AE05D6E5AA2401DDAB";
 
   test.beforeAll(async () => {
     const pathToExtension = new UnzipExtension().getPathToExtension();
@@ -51,61 +48,31 @@ test.describe("Test Swap Stables feature", () => {
     await context.close();
   });
 
-  test("User should be able to swap USDC to USDC.eth.axl", async () => {
+  test("User should be able to swap USDT.eth.axl to USDT.kava", async () => {
     await swapPage.goto();
-    await swapPage.selectPair("USDC", "USDC.eth.axl");
+    await swapPage.selectPair("USDT.eth.axl", "USDT.kava");
     await swapPage.enterAmount("0.1");
     await swapPage.showSwapInfo();
     const { msgContentAmount } = await swapPage.swapAndGetWalletMsg(context);
     expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDC);
+    expect(msgContentAmount).toContain("denom: " + USDTa);
     expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDCa);
+    expect(msgContentAmount).toContain("token_out_denom: " + kavaUSDT);
     expect(swapPage.isTransactionBroadcasted(10));
     expect(swapPage.isTransactionSuccesful(10));
     expect(swapPage.getTransactionUrl()).toBeTruthy();
   });
 
-  test("User should be able to swap USDC.eth.axl to USDC", async () => {
+  test("User should be able to swap USDT.kava to USDT.eth.axl", async () => {
     await swapPage.goto();
-    await swapPage.selectPair("USDC.eth.axl", "USDC");
+    await swapPage.selectPair("USDT.kava", "USDT.eth.axl");
     await swapPage.enterAmount("0.1");
     await swapPage.showSwapInfo();
     const { msgContentAmount } = await swapPage.swapAndGetWalletMsg(context);
     expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDCa);
+    expect(msgContentAmount).toContain("denom: " + kavaUSDT);
     expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain("token_out_denom: " + USDC);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
-    expect(swapPage.getTransactionUrl()).toBeTruthy();
-  });
-
-  test("User should be able to swap USDT to USDC", async () => {
-    await swapPage.goto();
-    await swapPage.selectPair("USDT", "USDC");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    const { msgContentAmount } = await swapPage.swapAndGetWalletMsg(context);
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain(USDC);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain(allUSDT);
-    expect(swapPage.isTransactionBroadcasted(10));
-    expect(swapPage.isTransactionSuccesful(10));
-    expect(swapPage.getTransactionUrl()).toBeTruthy();
-  });
-
-  test("User should be able to swap USDC to USDT", async () => {
-    await swapPage.goto();
-    await swapPage.selectPair("USDC", "USDT");
-    await swapPage.enterAmount("0.1");
-    await swapPage.showSwapInfo();
-    const { msgContentAmount } = await swapPage.swapAndGetWalletMsg(context);
-    expect(msgContentAmount).toBeTruthy();
-    expect(msgContentAmount).toContain("denom: " + USDC);
-    expect(msgContentAmount).toContain("sender: " + walletId);
-    expect(msgContentAmount).toContain(allUSDT);
+    expect(msgContentAmount).toContain("token_out_denom: " + USDTa);
     expect(swapPage.isTransactionBroadcasted(10));
     expect(swapPage.isTransactionSuccesful(10));
     expect(swapPage.getTransactionUrl()).toBeTruthy();
