691ebdeeaca0087b8c0d7c46064810f131d005a4
Jon Ator <jonathanator0@gmail.com>
2024-07-26 07:53:02 -0400

    handle non-standard status result

--- packages/server/src/queries/cosmos/rpc-status.ts
+++ packages/server/src/queries/cosmos/rpc-status.ts
@@ -1,46 +1,48 @@
 import { apiClient } from "@osmosis-labs/utils";
 
+export type Status = {
+  node_info: {
+    protocol_version: {
+      p2p: string;
+      block: string;
+      app: string;
+    };
+    id: string;
+    listen_addr: string;
+    network: string;
+    version: string;
+    channels: string;
+    moniker: string;
+    other: {
+      tx_index: "on" | "off";
+      rpc_address: string;
+    };
+  };
+  sync_info: {
+    latest_block_hash: string;
+    latest_app_hash: string;
+    latest_block_height: string;
+    latest_block_time: string;
+    earliest_block_hash: string;
+    earliest_app_hash: string;
+    earliest_block_height: string;
+    earliest_block_time: string;
+    catching_up: boolean;
+  };
+  validator_info: {
+    address: string;
+    pub_key: {
+      type: string;
+      value: string;
+    };
+    voting_power: string;
+  };
+};
+
 export type QueryStatusResponse = {
   jsonrpc: "2.0";
   id: number;
-  result: {
-    node_info: {
-      protocol_version: {
-        p2p: string;
-        block: string;
-        app: string;
-      };
-      id: string;
-      listen_addr: string;
-      network: string;
-      version: string;
-      channels: string;
-      moniker: string;
-      other: {
-        tx_index: "on" | "off";
-        rpc_address: string;
-      };
-    };
-    sync_info: {
-      latest_block_hash: string;
-      latest_app_hash: string;
-      latest_block_height: string;
-      latest_block_time: string;
-      earliest_block_hash: string;
-      earliest_app_hash: string;
-      earliest_block_height: string;
-      earliest_block_time: string;
-      catching_up: boolean;
-    };
-    validator_info: {
-      address: string;
-      pub_key: {
-        type: string;
-        value: string;
-      };
-      voting_power: string;
-    };
-  };
+  result: Status;
 };
 
 export async function queryRPCStatus({
@@ -48,5 +50,19 @@ export async function queryRPCStatus({
 }: {
   restUrl: string;
 }): Promise<QueryStatusResponse> {
-  return apiClient<QueryStatusResponse>(restUrl + "/status");
+  const data = await apiClient<QueryStatusResponse | Status>(
+    restUrl + "/status"
+  );
+
+  // some chains return a nonstandard response that does not include the jsonrpc field
+  // but rather just the status object
+  if ("jsonrpc" in data) {
+    return data as QueryStatusResponse;
+  } else {
+    return {
+      jsonrpc: "2.0",
+      id: 1,
+      result: data as Status,
+    };
+  }
 }
