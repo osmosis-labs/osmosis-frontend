33bba8eb3ac2ec900069258f7ab5ae53b0d226d6
Jon Ator <jonathanator0@gmail.com>
2024-07-26 13:40:04 -0400

    (Deposit/Withdraw) Fix initial chain race condition (#3608)
    * don't return results if loading
    
    * change strategy
    
    * fix dep

--- packages/web/components/bridge/amount-screen.tsx
+++ packages/web/components/bridge/amount-screen.tsx
@@ -508,7 +508,7 @@ export const AmountScreen = observer(
     useEffect(() => {
       const chain = direction === "deposit" ? toChain : fromChain;
       const setChain = direction === "deposit" ? setToChain : setFromChain;
-      if (isNil(chain) && !isNil(osmosisChain)) {
+      if (isNil(chain) && !isNil(osmosisChain) && !isLoadingSupportedAssets) {
         setChain({
           chainId: osmosisChain.chain_id,
           chainName: osmosisChain.chain_name,
@@ -519,7 +519,15 @@ export const AmountScreen = observer(
           bech32Prefix: osmosisChain.bech32_prefix,
         });
       }
-    }, [direction, fromChain, osmosisChain, setFromChain, setToChain, toChain]);
+    }, [
+      direction,
+      fromChain,
+      toChain,
+      osmosisChain,
+      isLoadingSupportedAssets,
+      setFromChain,
+      setToChain,
+    ]);
 
     /**
      * Set the initial chain based on the direction.
@@ -532,7 +540,8 @@ export const AmountScreen = observer(
       if (
         isNil(chain) &&
         !isNil(supportedChains) &&
-        supportedChains.length > 0
+        supportedChains.length > 0 &&
+        !isLoadingSupportedAssets
       ) {
         const firstChain = supportedChains[0];
         setChain(firstChain);
@@ -540,11 +549,12 @@ export const AmountScreen = observer(
     }, [
       direction,
       fromChain,
+      toChain,
+      supportedChains,
+      isLoadingSupportedAssets,
+      osmosisWalletConnected,
       setFromChain,
       setToChain,
-      supportedChains,
-      toChain,
-      osmosisWalletConnected,
     ]);
 
     const onChangeCryptoInput = useCallback(
