86f6dc140436ad16b4edc20331a876e668e487d3
Dev Ojha <ValarDragon@users.noreply.github.com>
2024-08-01 19:43:50 +0200

    Delete notifi code to save bundle size (#3643)
    * Delete notifi code to save bundle size
    
    This deletes the Notifi code, saving us almost 10% of bundle size. (2.4MB compressed -> 2.2MB compressed)
    
    * Remove feature flags, and from package.json
    
    * remove localizations
    
    * lint
    
    ---------
    
    Co-authored-by: Jon Ator <jonathanator0@gmail.com>

--- packages/web/hooks/use-feature-flags.ts
+++ packages/web/hooks/use-feature-flags.ts
@@ -7,8 +7,6 @@ import { useWindowSize } from "~/hooks";
 export type AvailableFlags =
   | "staking"
   | "swapsAdBanner"
-  | "notifications"
-  | "mobileNotifications"
   | "tokenInfo"
   | "sidebarOsmoChangeAndChart"
   | "multiBridgeProviders"
@@ -38,7 +36,6 @@ type ModifiedFlags =
 const defaultFlags: Record<ModifiedFlags, boolean> = {
   staking: true,
   swapsAdBanner: true,
-  notifications: true,
   tokenInfo: true,
   sidebarOsmoChangeAndChart: true,
   multiBridgeProviders: true,
@@ -82,9 +79,6 @@ export const useFeatureFlags = () => {
   return {
     ...launchdarklyFlags,
     ...(isDevModeWithoutClientID ? defaultFlags : {}),
-    notifications: isMobile
-      ? launchdarklyFlags.mobileNotifications
-      : launchdarklyFlags.notifications,
     portfolioPageAndNewAssetsPage:
       // don't want to use either on mobile
       // as this flag bundles the 2 pages,
