d7abb8e68813212c3d023fc3beefa21c09897e8e
Davide Segullo <davidesegullo@gmail.com>
2024-08-01 18:38:33 +0200

    fix: :lipstick: fix trade widget overlap with sidebar

--- packages/web/components/layouts/main.tsx
+++ packages/web/components/layouts/main.tsx
@@ -40,7 +40,7 @@ export const MainLayout = observer(
             <OsmosisFullLogo onClick={() => router.push("/")} />
           </div>
         )}
-        <div className="fixed inset-y-0 z-40 flex w-sidebar flex-col overflow-y-auto overflow-x-hidden px-2 py-6 md:hidden">
+        <div className="fixed inset-y-0 z-40 flex w-sidebar flex-col overflow-y-auto overflow-x-hidden px-2 py-6 xl:bg-osmoverse-1000 md:hidden">
           {showBlockLogo && (
             <div className="z-50 mx-auto ml-3 w-sidebar grow-0">
               <OsmosisFullLogo onClick={() => router.push("/")} />
