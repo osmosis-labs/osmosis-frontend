d7984dfd30f174453ae772e0e88dff77e7bb9dd9
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-08-02 11:03:09 -0700

    Add temp tabs

--- packages/web/components/complex/portfolio/allocation.tsx
+++ packages/web/components/complex/portfolio/allocation.tsx
@@ -7,9 +7,82 @@ type AllocationOptions = "all" | "assets" | "available";
 
 import { PricePretty } from "@keplr-wallet/unit";
 import { DEFAULT_VS_CURRENCY } from "@osmosis-labs/server";
+import classNames from "classnames";
+import { FunctionComponent, useMemo } from "react";
 
 import { displayFiatPrice } from "~/components/transactions/transaction-utils";
 import { useTranslation } from "~/hooks";
+import { useTranslation } from "~/hooks";
+
+export enum SwapToolTab {
+  SWAP = "swap",
+  BUY = "buy",
+  SELL = "sell",
+}
+
+export interface SwapToolTabsProps {
+  setTab: (tab: SwapToolTab) => void;
+  activeTab: SwapToolTab;
+}
+
+/**
+ * Component for swapping between tabs on the swap modal.
+ * Has three tabs:
+ * - Buy
+ * - Sell
+ * - Swap
+ */
+export const SwapToolTabs: FunctionComponent<SwapToolTabsProps> = ({
+  setTab,
+  activeTab,
+}) => {
+  const { t } = useTranslation();
+
+  const tabs = useMemo(
+    () => [
+      {
+        label: t("portfolio.buy"),
+        value: SwapToolTab.BUY,
+      },
+      {
+        label: t("limitOrders.sell"),
+        value: SwapToolTab.SELL,
+      },
+      {
+        label: t("swap.title"),
+        value: SwapToolTab.SWAP,
+      },
+    ],
+    [t]
+  );
+
+  return (
+    <div className="flex w-max items-center rounded-3xl border border-osmoverse-700">
+      {tabs.map((tab) => {
+        const isActive = activeTab === tab.value;
+        return (
+          <button
+            key={`swap-tab-${tab.value}`}
+            onClick={() => setTab(tab.value)}
+            className={classNames("rounded-3xl px-4 py-3 transition-colors", {
+              "hover:bg-osmoverse-850": !isActive,
+              "bg-wosmongton-100": isActive,
+            })}
+          >
+            <p
+              className={classNames("font-semibold", {
+                "text-wosmongton-100": !isActive,
+                "text-osmoverse-900": isActive,
+              })}
+            >
+              {tab.label}
+            </p>
+          </button>
+        );
+      })}
+    </div>
+  );
+};
 
 export const Allocation: FunctionComponent<{
   allocation?: AllocationResponse;
@@ -34,6 +107,10 @@ export const Allocation: FunctionComponent<{
     <div className="flex w-1/2 flex-col">
       <div className="text-h6">Allocation</div>
       <div className="my-2">
+        <SwapToolTabs
+          setTab={(tab) => setSelectedOption(tab)}
+          activeTab={selectedOption}
+        />
         <RadioWithOptions
           mode="secondary"
           variant="small"
