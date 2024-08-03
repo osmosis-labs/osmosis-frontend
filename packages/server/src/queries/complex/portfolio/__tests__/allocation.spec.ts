3e96ac3866244bddd085d1dc34de1658b713c8c8
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-08-02 17:58:43 -0700

    Update tests

--- packages/server/src/queries/complex/portfolio/__tests__/allocation.spec.ts
+++ packages/server/src/queries/complex/portfolio/__tests__/allocation.spec.ts
@@ -1,115 +1,74 @@
-import { Dec, PricePretty, RatePretty } from "@keplr-wallet/unit";
-
 import { AllocationResponse } from "../../../data-services/allocation";
-import { DEFAULT_VS_CURRENCY } from "../../assets/config";
 import { getAll } from "../allocation";
 
 const MOCK_DATA: AllocationResponse = {
   categories: {
     "in-locks": {
-      capitalization: "0.000000000000000000",
+      capitalization: "5.000000000000000000",
       is_best_effort: false,
     },
     pooled: {
-      capitalization: "0.000000000000000000",
+      capitalization: "5.000000000000000000",
       is_best_effort: false,
     },
     staked: {
-      capitalization: "84.612314471448660006",
+      capitalization: "5.000000000000000000",
       is_best_effort: false,
     },
     "total-assets": {
-      capitalization: "85.602001371755321819",
+      capitalization: "60.000000000000000000",
       account_coins_result: [
         {
           coin: {
             denom: "factory/osmo1pfyxruwvtwk00y8z06dh2lqjdj82ldvy74wzm3/WOSMO",
-            amount: "200395428",
+            amount: "789",
           },
-          cap_value: "0.028184368652420535",
+          cap_value: "10.000000000000000000",
         },
         {
           coin: {
             denom:
               "factory/osmo1rckme96ptawr4zwexxj5g5gej9s2dmud8r2t9j0k0prn5mch5g4snzzwjv/sail",
-            amount: "315296168",
+            amount: "456",
           },
-          cap_value: "0.028539893083828982",
+          cap_value: "20.000000000000000000",
         },
         {
           coin: {
             denom:
               "ibc/7ED954CFFFC06EE8419387F3FC688837FF64EF264DE14219935F724EEEDBF8D3",
-            amount: "15746",
+            amount: "123",
           },
-          cap_value: "0.009551106711133061",
-        },
-        {
-          coin: {
-            denom:
-              "ibc/884EBC228DFCE8F1304D917A712AA9611427A6C1ECC3179B2E91D7468FB091A2",
-            amount: "127891131120",
-          },
-          cap_value: "0.006316292675404908",
-        },
-        {
-          coin: {
-            denom: "uosmo",
-            amount: "211159602",
-          },
-          cap_value: "85.529409710632534333",
+          cap_value: "30.000000000000000000",
         },
       ],
       is_best_effort: false,
     },
     "unclaimed-rewards": {
-      capitalization: "0.000000000000000000",
+      capitalization: "5.000000000000000000",
       is_best_effort: false,
     },
     unstaking: {
-      capitalization: "0.000000000000000000",
+      capitalization: "5.000000000000000000",
       is_best_effort: false,
     },
     "user-balances": {
-      capitalization: "0.989686900306661813",
+      capitalization: "10.000000000000000000",
       account_coins_result: [
         {
           coin: {
             denom: "factory/osmo1pfyxruwvtwk00y8z06dh2lqjdj82ldvy74wzm3/WOSMO",
-            amount: "200395428",
+            amount: "789",
           },
-          cap_value: "0.028184368652420535",
+          cap_value: "10.000000000000000000",
         },
         {
           coin: {
             denom:
               "factory/osmo1rckme96ptawr4zwexxj5g5gej9s2dmud8r2t9j0k0prn5mch5g4snzzwjv/sail",
-            amount: "315296168",
+            amount: "456",
           },
-          cap_value: "0.028539893083828982",
-        },
-        {
-          coin: {
-            denom:
-              "ibc/7ED954CFFFC06EE8419387F3FC688837FF64EF264DE14219935F724EEEDBF8D3",
-            amount: "15746",
-          },
-          cap_value: "0.009551106711133061",
-        },
-        {
-          coin: {
-            denom:
-              "ibc/884EBC228DFCE8F1304D917A712AA9611427A6C1ECC3179B2E91D7468FB091A2",
-            amount: "127891131120",
-          },
-          cap_value: "0.006316292675404908",
-        },
-        {
-          coin: {
-            denom: "uosmo",
-            amount: "2264174",
-          },
-          cap_value: "0.917095239183874327",
+          cap_value: "20.000000000000000000",
         },
       ],
       is_best_effort: false,
@@ -120,33 +79,37 @@ const MOCK_DATA: AllocationResponse = {
 describe("Allocation Functions", () => {
   describe("getAll", () => {
     it("should calculate the correct allocation percentages and fiat values", () => {
-      const result = getAll(MOCK_DATA.categories);
+      const result = getAll(MOCK_DATA.categories).map((allocation) => ({
+        ...allocation,
+        percentage: allocation.percentage.toString(),
+        fiatValue: allocation.fiatValue.toString(),
+      }));
 
       expect(result).toEqual([
         {
           key: "available",
-          percentage: new RatePretty(new Dec(100).quo(new Dec(1500))),
-          fiatValue: new PricePretty(DEFAULT_VS_CURRENCY, new Dec(100)),
+          percentage: "33.333%",
+          fiatValue: "$10",
         },
         {
           key: "staked",
-          percentage: new RatePretty(new Dec(200).quo(new Dec(1500))),
-          fiatValue: new PricePretty(DEFAULT_VS_CURRENCY, new Dec(200)),
+          percentage: "16.666%",
+          fiatValue: "$5",
         },
         {
           key: "unstaking",
-          percentage: new RatePretty(new Dec(300).quo(new Dec(1500))),
-          fiatValue: new PricePretty(DEFAULT_VS_CURRENCY, new Dec(300)),
+          percentage: "16.666%",
+          fiatValue: "$5",
         },
         {
           key: "unclaimedRewards",
-          percentage: new RatePretty(new Dec(400).quo(new Dec(1500))),
-          fiatValue: new PricePretty(DEFAULT_VS_CURRENCY, new Dec(400)),
+          percentage: "16.666%",
+          fiatValue: "$5",
         },
         {
           key: "pooled",
-          percentage: new RatePretty(new Dec(500).quo(new Dec(1500))),
-          fiatValue: new PricePretty(DEFAULT_VS_CURRENCY, new Dec(500)),
+          percentage: "16.666%",
+          fiatValue: "$5",
         },
       ]);
     });
