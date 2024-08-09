adc5c7d5f03f6d4074e2774f000d27922a94d528
Jose Felix <jfelx@outlook.com>
2024-08-09 19:57:56 -0400

    feat: add instrumentation config

--- packages/web/instrumentation.ts
+++ packages/web/instrumentation.ts
@@ -3,5 +3,10 @@ import { registerOTel } from "@vercel/otel";
 export function register() {
   registerOTel({
     serviceName: process.env.NEXT_PUBLIC_VERCEL_URL ?? "osmosis-frontend",
+    instrumentationConfig: {
+      fetch: {
+        propagateContextUrls: ["*"],
+      },
+    },
   });
 }
