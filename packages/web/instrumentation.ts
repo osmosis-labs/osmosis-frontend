0fa3d8eaa28cae01a4dc0697429795b571174b96
Jose Felix <jfelx@outlook.com>
2024-08-13 14:53:42 -0400

    temp: add log

--- packages/web/instrumentation.ts
+++ packages/web/instrumentation.ts
@@ -1,6 +1,10 @@
 import { registerOTel } from "@vercel/otel";
 
 export function register() {
+  console.log(
+    "registering otel",
+    process.env.NEXT_PUBLIC_VERCEL_URL ?? "osmosis-frontend"
+  );
   registerOTel({
     serviceName: process.env.NEXT_PUBLIC_VERCEL_URL ?? "osmosis-frontend",
     instrumentationConfig: {
