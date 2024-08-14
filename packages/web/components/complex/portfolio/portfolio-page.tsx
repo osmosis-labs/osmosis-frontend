377732f4a26c20f411af4c86684a78cbbc83e117
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-08-14 12:48:18 -0700

    Set up 1440px screen size

--- packages/web/components/complex/portfolio/portfolio-page.tsx
+++ packages/web/components/complex/portfolio/portfolio-page.tsx
@@ -8,7 +8,6 @@ import { Allocation } from "~/components/complex/portfolio/allocation";
 import { AssetsOverview } from "~/components/complex/portfolio/assets-overview";
 import { UserPositionsSection } from "~/components/complex/portfolio/user-positions";
 import { UserZeroBalanceTableSplash } from "~/components/complex/portfolio/user-zero-balance-table-splash";
-import { WalletDisconnectedSplash } from "~/components/complex/portfolio/wallet-disconnected-splash";
 import { Spinner } from "~/components/loaders";
 import { AssetBalancesTable } from "~/components/table/asset-balances";
 import { RecentActivity } from "~/components/transactions/recent-activity/recent-activity";
@@ -71,101 +70,96 @@ export const PortfolioPage: FunctionComponent = () => {
 
   const { logEvent } = useAmplitudeAnalytics();
 
+  const isWalletConnected =
+    wallet && wallet.isWalletConnected && wallet.address;
+
   return (
-    <main
+    <div
       className={classNames(
-        "mx-auto flex w-full max-w-container flex-col p-8 pt-4 md:p-4",
+        "mx-auto flex w-full max-w-container flex-row gap-12 p-8 pt-4 1.5xl:flex-col md:p-4",
         {
           "bg-osmoverse-900": !featureFlags.limitOrders,
         }
       )}
     >
-      <section className="flex py-3" ref={overviewRef}>
-        <AssetsOverview
-          totalValue={
-            totalValueData || new PricePretty(DEFAULT_VS_CURRENCY, new Dec(0))
-          }
-          isTotalValueFetched={isTotalValueFetched}
-        />
-      </section>
+      <main className="flex w-[752px] min-w-[752px] flex-col">
+        <section className="flex py-3" ref={overviewRef}>
+          <AssetsOverview
+            totalValue={
+              totalValueData || new PricePretty(DEFAULT_VS_CURRENCY, new Dec(0))
+            }
+            isTotalValueFetched={isTotalValueFetched}
+          />
+        </section>
+        <section className="w-full py-3">
+          <TabGroup>
+            <TabList className="-mx-4 flex" ref={tabsRef}>
+              <Tab
+                disabled={userHasNoAssets}
+                className="py-3 px-4 disabled:opacity-80"
+                onClick={() => {
+                  logEvent([
+                    EventName.Portfolio.tabClicked,
+                    { section: "Your assets" },
+                  ]);
+                }}
+              >
+                {({ selected }) => (
+                  <h6 className={!selected ? "text-osmoverse-500" : undefined}>
+                    {t("portfolio.yourAssets")}
+                  </h6>
+                )}
+              </Tab>
+              <Tab
+                disabled={userHasNoAssets}
+                className="py-3 px-4 disabled:opacity-80 "
+                onClick={() => {
+                  logEvent([
+                    EventName.Portfolio.tabClicked,
+                    { section: "Your positions" },
+                  ]);
+                }}
+              >
+                {({ selected }) => (
+                  <h6 className={!selected ? "text-osmoverse-500" : undefined}>
+                    {t("portfolio.yourPositions")}
+                  </h6>
+                )}
+              </Tab>
+            </TabList>
+            {!isTotalValueFetched ? (
+              <div className="mx-auto my-6 w-fit">
+                <Spinner />
+              </div>
+            ) : userHasNoAssets ? (
+              <UserZeroBalanceTableSplash />
+            ) : (
+              <TabPanels>
+                <TabPanel>
+                  <AssetBalancesTable
+                    tableTopPadding={overviewHeight + tabsHeight}
+                  />
+                </TabPanel>
+                <TabPanel>
+                  <UserPositionsSection address={wallet.address} />
+                </TabPanel>
+              </TabPanels>
+            )}
+          </TabGroup>
+        </section>
+      </main>
 
-      {wallet && wallet.isWalletConnected && wallet.address ? (
-        <>
-          <section className="w-full py-3">
-            <TabGroup>
-              <TabList className="-mx-4 flex" ref={tabsRef}>
-                <Tab
-                  disabled={userHasNoAssets}
-                  className="py-3 px-4 disabled:opacity-80"
-                  onClick={() => {
-                    logEvent([
-                      EventName.Portfolio.tabClicked,
-                      { section: "Your assets" },
-                    ]);
-                  }}
-                >
-                  {({ selected }) => (
-                    <h6
-                      className={!selected ? "text-osmoverse-500" : undefined}
-                    >
-                      {t("portfolio.yourAssets")}
-                    </h6>
-                  )}
-                </Tab>
-                <Tab
-                  disabled={userHasNoAssets}
-                  className="py-3 px-4 disabled:opacity-80 "
-                  onClick={() => {
-                    logEvent([
-                      EventName.Portfolio.tabClicked,
-                      { section: "Your positions" },
-                    ]);
-                  }}
-                >
-                  {({ selected }) => (
-                    <h6
-                      className={!selected ? "text-osmoverse-500" : undefined}
-                    >
-                      {t("portfolio.yourPositions")}
-                    </h6>
-                  )}
-                </Tab>
-              </TabList>
-              {!isTotalValueFetched ? (
-                <div className="mx-auto my-6 w-fit">
-                  <Spinner />
-                </div>
-              ) : userHasNoAssets ? (
-                <UserZeroBalanceTableSplash />
-              ) : (
-                <TabPanels>
-                  <TabPanel>
-                    <AssetBalancesTable
-                      tableTopPadding={overviewHeight + tabsHeight}
-                    />
-                  </TabPanel>
-                  <TabPanel>
-                    <UserPositionsSection address={wallet.address} />
-                  </TabPanel>
-                </TabPanels>
-              )}
-            </TabGroup>
-          </section>
-          <aside className="flex w-full gap-16">
-            <div className="flex grow flex-col">
-              <RecentActivity />
-            </div>
-            <div className="min-w-80 flex w-80 flex-col">
-              {featureFlags.cypherCard && <CypherCard />}
-              {!isLoadingAllocation && !userHasNoAssets && (
-                <Allocation allocation={allocation} />
-              )}
-            </div>
-          </aside>
-        </>
-      ) : isWalletLoading ? null : (
-        <WalletDisconnectedSplash />
-      )}
-    </main>
+      <aside className="flex w-[320px] min-w-[320px] flex-col">
+        {featureFlags.cypherCard && <CypherCard />}
+        {!isLoadingAllocation && !userHasNoAssets && (
+          <Allocation allocation={allocation} />
+        )}
+        <RecentActivity />
+      </aside>
+    </div>
   );
 };
+
+//  isWalletLoading ? null : (
+//   <WalletDisconnectedSplash />
+// )}
