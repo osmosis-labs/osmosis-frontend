cb0541df95319be0b57f1031be25a2b467dc451a
Jose Felix <jfelx@outlook.com>
2024-08-14 22:48:35 -0400

    feat: remove remaining sentry calls

--- packages/web/utils/error.ts
+++ packages/web/utils/error.ts
@@ -1,6 +1,12 @@
+import { context, trace } from "@opentelemetry/api";
+
 export function captureError(e: any) {
+  const activeSpan = trace.getSpan(context.active());
   if (e instanceof Error) {
-    // Sentry.captureException(e);
+    if (activeSpan) {
+      // Reuse the existing active span
+      activeSpan.recordException(e);
+    }
     if (process.env.NODE_ENV === "development") console.warn("Captured:", e);
   } else if (process.env.NODE_ENV === "development") {
     console.warn("Did not capture non-Error:", e);
