3129374c8770385a33dff1ff62962f5c5fb872e3
Jose Felix <jfelx@outlook.com>
2024-08-16 15:33:22 -0400

    improvement: clearer name

--- packages/web/utils/service-name.ts
+++ packages/web/utils/service-name.ts
@@ -3,7 +3,7 @@ export function getOpentelemetryServiceName(): string {
   return process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL?.includes(
     "stage."
   )
-    ? "stage" // If it does, return "stage"
-    : process.env.NEXT_PUBLIC_VERCEL_ENV ?? // Otherwise, return the Vercel environment (production, preview, or development)
+    ? "osmosis-frontend-stage" // If it does, return "stage"
+    : `osmosis-frontend-${process.env.NEXT_PUBLIC_VERCEL_ENV}` ?? // Otherwise, return the Vercel environment (production, preview, or development)
         "fallback-osmosis-frontend-service-name";
 }
