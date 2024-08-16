a9cbef86d795d4a18b106979d08b754d4c06d832
Jose Felix <jfelx@outlook.com>
2024-08-15 23:29:50 -0400

    feat: use vercel project production url

--- packages/web/utils/service-name.ts
+++ packages/web/utils/service-name.ts
@@ -1,6 +1,6 @@
 export function getOpentelemetryServiceName(): string {
   return (
-    process.env.NEXT_PUBLIC_VERCEL_URL ??
+    process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL ??
     "fallback-osmosis-frontend-service-name"
   );
 }
