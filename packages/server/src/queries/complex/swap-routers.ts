39aa44f1bf63404a8f3698030d90a256985f138a
Roman <roman@osmosis.team>
2024-08-17 19:02:49 -0700

    remove tfm router (#3753)

--- packages/server/src/queries/complex/swap-routers.ts
+++ packages/server/src/queries/complex/swap-routers.ts
@@ -1,36 +1,16 @@
 import type { TokenOutGivenInRouter } from "@osmosis-labs/pools";
-import { AssetList, Chain } from "@osmosis-labs/types";
+import { Chain } from "@osmosis-labs/types";
 import { z } from "zod";
 
-import { SIDECAR_BASE_URL, TFM_BASE_URL } from "../../env";
+import { SIDECAR_BASE_URL } from "../../env";
 import { routeTokenOutGivenIn } from "../complex/pools/route-token-out-given-in";
 import { OsmosisSidecarRemoteRouter } from "../sidecar/router";
-import { TfmRemoteRouter } from "../tfm/router";
-import { calcAssetValue } from "./assets";
 
 export const availableRoutersSchema = z.enum(["tfm", "sidecar", "legacy"]);
 export type RouterKey = z.infer<typeof availableRoutersSchema>;
 
-export function getRouters(
-  osmosisChainId: string,
-  assetLists: AssetList[],
-  chainList: Chain[]
-) {
+export function getRouters(chainList: Chain[]) {
   return [
-    {
-      name: "tfm",
-      router: new TfmRemoteRouter(
-        osmosisChainId,
-        TFM_BASE_URL ?? "https://api.tfm.com",
-        (coinMinimalDenom, amount) =>
-          calcAssetValue({
-            assetLists,
-            chainList,
-            anyDenom: coinMinimalDenom,
-            amount,
-          })
-      ),
-    },
     {
       name: "sidecar",
       router: new OsmosisSidecarRemoteRouter(
