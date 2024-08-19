31ae48cf22d0c9378f7596ab8ee4e424eb594af0
Jose Felix <jfelx@outlook.com>
2024-08-19 14:47:49 -0400

    improvement: remove comment

--- packages/web/utils/service-name.ts
+++ packages/web/utils/service-name.ts
@@ -9,7 +9,6 @@ export function getOpentelemetryServiceName(): string {
     gitIndex !== -1 &&
     urlParts?.[gitIndex + 1] === "stage";
 
-  // Check if the production URL contains "stage."
   return isStage
     ? "osmosis-frontend-stage" // If it does, return "stage"
     : `osmosis-frontend-${process.env.NEXT_PUBLIC_VERCEL_ENV}` ?? // Otherwise, return the Vercel environment (production, preview, or development)
