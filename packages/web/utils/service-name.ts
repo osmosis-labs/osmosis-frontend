dd41870838470ed2c18e966d1834ff97a7f7a39f
Jose Felix <jfelx@outlook.com>
2024-08-19 14:44:52 -0400

    feat: match stage environment

--- packages/web/utils/service-name.ts
+++ packages/web/utils/service-name.ts
@@ -1,8 +1,16 @@
 export function getOpentelemetryServiceName(): string {
+  const url = process.env.NEXT_PUBLIC_VERCEL_BRANCH_URL;
+  const urlParts = url?.split("-");
+
+  // Find the index of 'git' and check if the next part is 'stage'
+  const gitIndex = urlParts?.indexOf("git");
+  const isStage =
+    gitIndex !== undefined &&
+    gitIndex !== -1 &&
+    urlParts?.[gitIndex + 1] === "stage";
+
   // Check if the production URL contains "stage."
-  return process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL?.includes(
-    "stage."
-  )
+  return isStage
     ? "osmosis-frontend-stage" // If it does, return "stage"
     : `osmosis-frontend-${process.env.NEXT_PUBLIC_VERCEL_ENV}` ?? // Otherwise, return the Vercel environment (production, preview, or development)
         "fallback-osmosis-frontend-service-name";
