79fcae4acce3b12b42c99383353c28cafeac1ed6
Connor Barr <crnbarr@gmail.com>
2024-09-05 15:42:35 +0100

    feat: removed pool restriction on orderbooks

--- packages/server/src/queries/complex/orderbooks/pools.ts
+++ packages/server/src/queries/complex/orderbooks/pools.ts
@@ -22,16 +22,14 @@ export function getOrderbookPools() {
     ttl: 1000 * 60 * 60, // 1 hour
     getFreshValue: () =>
       queryCanonicalOrderbooks().then(async (data) => {
-        return data
-          .filter((o) => o.pool_id < 2065)
-          .map((orderbook) => {
-            return {
-              baseDenom: orderbook.base,
-              quoteDenom: orderbook.quote,
-              contractAddress: orderbook.contract_address,
-              poolId: orderbook.pool_id.toString(),
-            };
-          }) as Orderbook[];
+        return data.map((orderbook) => {
+          return {
+            baseDenom: orderbook.base,
+            quoteDenom: orderbook.quote,
+            contractAddress: orderbook.contract_address,
+            poolId: orderbook.pool_id.toString(),
+          };
+        }) as Orderbook[];
       }),
   });
 }
