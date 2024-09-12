12fda21ac3ea2df1e32781bb931e4e57869b89d0
Matt Upham <30577966+mattupham@users.noreply.github.com>
2024-09-11 19:07:54 -0700

    Clean up comments

--- packages/web/components/table/asset-balances.tsx
+++ packages/web/components/table/asset-balances.tsx
@@ -237,6 +237,7 @@ export const AssetBalancesTable: FunctionComponent<{
 
   const table = useReactTable({
     data: filteredAssetsData,
+    // @ts-expect-error
     columns: collapsedColumns,
     manualSorting: true,
     manualFiltering: true,
