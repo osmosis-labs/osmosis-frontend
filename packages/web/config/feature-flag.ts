52acd196fc6da4b34bd98926300442cefe499b28
JohnnyWyles <97029546+JohnnyWyles@users.noreply.github.com>
2024-10-08 13:57:58 +0100

    Revert "Add Transmuter Contract Codes"
    This reverts commit c216e0a374e9c2904735e5f6efe926da8e66b332.

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -4,7 +4,7 @@ import { IS_TESTNET } from "./env";
 export const BlacklistedPoolIds: string[] = ["895"];
 
 /** Cosmwasm Code Ids confirmed to be transmuter pools in current env. */
-export const TransmuterPoolCodeIds = IS_TESTNET ? ["3084"] : ["148","814","867","996"];
+export const TransmuterPoolCodeIds = IS_TESTNET ? ["3084"] : ["148"];
 
 export const RecommendedSwapDenoms = [
   "OSMO",
