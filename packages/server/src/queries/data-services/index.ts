d2dec6ffca1486a500f91108e9d09dfa36b02008
Jon Ator <jonathanator0@gmail.com>
2024-10-16 10:48:40 -0400

    remove mcap query client

--- packages/server/src/queries/data-services/index.ts
+++ packages/server/src/queries/data-services/index.ts
@@ -2,7 +2,6 @@ export * from "../sidecar/allocation";
 export * from "./earn";
 export * from "./filtered-pools";
 export * from "./historical-limit-orders";
-export * from "./market-cap";
 export * from "./pool-aprs";
 export * from "./pools-fees";
 export * from "./portfolio-over-time";
