e9809c892b23d54f6f94ca59179ea1b856fa2809
Greg Nagy <greg@dhamma.works>
2024-11-03 20:30:44 +0100

    feat: dust to osmo swap flow implementation
    Reusing the existing infrastructure for swaps encoded in the useSwap
    seemed to be the favorable direction.
    
    The main trick to it is to wrap the hooks into lightweight components
    and handle the batching through the component interactions.
    
    (implmeneting `useDustToOsmo` from scratch would have been possible but
    it would have needed more testing and it would have duplicated at least
    some of the existing functionality — eg handling 1 click trading)

--- packages/web/components/complex/dust-to-osmo/dust-to-osmo.tsx
+++ packages/web/components/complex/dust-to-osmo/dust-to-osmo.tsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
+import { Dec } from "@keplr-wallet/unit";
+import { useMutation, UseMutationResult } from "@tanstack/react-query";
+import { useCallback, useEffect, useState } from "react";
 
 import { Icon } from "~/components/assets";
 import { Button } from "~/components/ui/button";
+import { DefaultSlippage } from "~/config/swap";
 import { useTranslation, useWalletSelect } from "~/hooks";
+import { useSwap } from "~/hooks/use-swap";
 import { useStore } from "~/stores";
 import { HideDustUserSetting } from "~/stores/user-settings";
 import { api } from "~/utils/trpc";
+
+const maxSlippage = new Dec(DefaultSlippage);
 
 export function DustToOsmo() {
   const { t } = useTranslation();
@@ -14,9 +20,30 @@ export function DustToOsmo() {
   const account = accountStore.getWallet(accountStore.osmosisChainId);
   const { isLoading: isLoadingWallet } = useWalletSelect();
 
-  const [loadDustAssets, setLoadDustAssets] = useState(false);
+  const [convertDust, setConvertDust] = useState(false);
 
-  const { data: dustAssets, isLoading: isLoadingDust } =
+  const [isSendingSwapTxs, setIsSendingSwapTxs] = useState(0);
+
+  const onSendSwapTxStatus = useCallback(
+    (status: UseMutationResult["status"]) => {
+      switch (status) {
+        case "loading":
+          setIsSendingSwapTxs((prev) => prev + 1);
+          break;
+        case "error":
+        case "success":
+          // We are being cheerfully neglectful with error handling here
+          setIsSendingSwapTxs((prev) => prev - 1);
+          break;
+        case "idle":
+        default:
+          break;
+      }
+    },
+    []
+  );
+
+  const { data: dustAssets, isSuccess: isSuccessDust } =
     api.edge.assets.getUserDustAssets.useQuery(
       {
         userOsmoAddress: account?.address ?? "",
@@ -25,8 +52,7 @@ export function DustToOsmo() {
         dustThreshold: HideDustUserSetting.DUST_THRESHOLD,
       },
       {
-        enabled:
-          !isLoadingWallet && Boolean(account?.address) && loadDustAssets,
+        enabled: !isLoadingWallet && Boolean(account?.address) && convertDust,
         cacheTime: 0,
       }
     );
@@ -37,19 +63,71 @@ export function DustToOsmo() {
         className="gap-2 !border !border-osmoverse-700 !py-2 !px-4 !text-wosmongton-200"
         variant="outline"
         size="lg-full"
-        isLoading={isLoadingDust}
-        onClick={() => setLoadDustAssets(true)}
+        isLoading={convertDust && isSendingSwapTxs > 0}
+        onClick={() => setConvertDust(true)}
       >
         <Icon id="dust-broom" className="h-6 w-6" />
         {t("dustToOsmo.mainButton")}
       </Button>
-      {dustAssets && dustAssets.length > 0 && (
-        <div className="flex flex-col gap-2">
-          {dustAssets.map((asset) => (
-            <div key={asset.coinDenom}>{asset.coinDenom}</div>
-          ))}
-        </div>
-      )}
+      {isSuccessDust &&
+        dustAssets.map((dustAsset) => (
+          <SwapHandler
+            key={dustAsset.coinDenom}
+            fromDenom={dustAsset.coinDenom}
+            amount={dustAsset.amount.toDec().toString()}
+            onSendSwapTxStatus={onSendSwapTxStatus}
+          />
+        ))}
     </>
   );
+}
+
+function SwapHandler({
+  fromDenom,
+  amount,
+  onSendSwapTxStatus,
+}: {
+  fromDenom: string;
+  amount: string;
+  onSendSwapTxStatus: (status: UseMutationResult["status"]) => void;
+}) {
+  const {
+    inAmountInput: { setAmount: setInAmount },
+    isReadyToSwap,
+    sendTradeTokenInTx,
+  } = useSwap({
+    initialFromDenom: fromDenom,
+    initialToDenom: "OSMO",
+    useQueryParams: false,
+    maxSlippage,
+    quoteType: "out-given-in",
+    inputDebounceMs: 0,
+  });
+
+  useEffect(() => {
+    setInAmount(amount);
+  }, [setInAmount, amount]);
+
+  const {
+    mutate: sendSwapTx,
+    isIdle: isSendSwapTxIdle,
+    status: sendSwapTxStatus,
+  } = useMutation(sendTradeTokenInTx);
+
+  useEffect(() => {
+    if (isReadyToSwap && isSendSwapTxIdle) {
+      sendSwapTx();
+    }
+  }, [isReadyToSwap, isSendSwapTxIdle, sendSwapTx]);
+
+  useEffect(() => {
+    if (sendSwapTxStatus !== "idle") {
+      // useSwap needs a bit of time to be ready so we send loading first
+      onSendSwapTxStatus("loading");
+    } else {
+      onSendSwapTxStatus(sendSwapTxStatus);
+    }
+  }, [sendSwapTxStatus, onSendSwapTxStatus]);
+
+  return <></>;
 }
