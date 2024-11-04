1861bac8d2e4c956310989609b6a8fde215b45cb
Jon Ator <jonathanator0@gmail.com>
2024-11-04 12:19:40 -0500

    fix server tests

--- packages/server/src/queries/complex/portfolio/__tests__/assets.spec.ts
+++ packages/server/src/queries/complex/portfolio/__tests__/assets.spec.ts
@@ -78,169 +78,180 @@ const MOCK_DATA: PortfolioAssetsResponse = {
   },
 };
 
-describe("Allocation Functions", () => {
-  describe("getAll", () => {
-    it("should calculate the correct allocation percentages and fiat values", () => {
-      const result = getAllocations(MOCK_DATA.categories).map((allocation) => ({
-        ...allocation,
-        percentage: allocation.percentage.toString(),
-        fiatValue: allocation.fiatValue.toString(),
-      }));
+describe("getAll", () => {
+  it("should calculate the correct allocation percentages and fiat values", () => {
+    const result = getAllocations(MOCK_DATA.categories).map((allocation) => ({
+      ...allocation,
+      percentage: allocation.percentage.toString(),
+      fiatValue: allocation.fiatValue.toString(),
+    }));
 
-      expect(result).toEqual([
-        {
-          key: "available",
-          percentage: "33.333%",
-          fiatValue: "$10",
-        },
-        {
-          key: "staked",
-          percentage: "16.666%",
-          fiatValue: "$5",
-        },
-        {
-          key: "unstaking",
-          percentage: "16.666%",
-          fiatValue: "$5",
-        },
-        {
-          key: "unclaimedRewards",
-          percentage: "16.666%",
-          fiatValue: "$5",
-        },
-        {
-          key: "pooled",
-          percentage: "16.666%",
-          fiatValue: "$5",
-        },
-      ]);
-    });
+    expect(result).toEqual([
+      {
+        key: "available",
+        percentage: "33.333%",
+        fiatValue: "$10",
+      },
+      {
+        key: "staked",
+        percentage: "16.666%",
+        fiatValue: "$5",
+      },
+      {
+        key: "unstaking",
+        percentage: "16.666%",
+        fiatValue: "$5",
+      },
+      {
+        key: "unclaimedRewards",
+        percentage: "16.666%",
+        fiatValue: "$5",
+      },
+      {
+        key: "pooled",
+        percentage: "16.666%",
+        fiatValue: "$5",
+      },
+    ]);
+  });
+});
+
+describe("calculatePercentAndFiatValues", () => {
+  it("should calculate the correct asset percentages and fiat values - Total Assets", () => {
+    const result = calculatePercentAndFiatValues(
+      MOCK_DATA.categories,
+      assetLists,
+      "total-assets",
+      5
+    ).map((allocation) => ({
+      ...allocation,
+      percentage: allocation.percentage.toString(),
+      fiatValue: allocation.fiatValue.toString(),
+    }));
+
+    delete result[0].asset;
+    delete result[1].asset;
+    delete result[2].asset;
+    delete result[3].asset;
+
+    expect(result).toEqual([
+      {
+        fiatValue: "$30",
+        percentage: "50%",
+        key: "CTK",
+      },
+      {
+        key: "SAIL",
+        percentage: "33.333%",
+        fiatValue: "$20",
+      },
+      {
+        fiatValue: "$10",
+        key: "WOSMO",
+        percentage: "16.666%",
+      },
+      {
+        key: "Other",
+        percentage: "0%",
+        fiatValue: "$0",
+      },
+    ]);
   });
 
-  describe("calculatePercentAndFiatValues", () => {
-    it("should calculate the correct asset percentages and fiat values - Total Assets", () => {
-      const result = calculatePercentAndFiatValues(
-        MOCK_DATA.categories,
-        assetLists,
-        "total-assets",
-        5
-      ).map((allocation) => ({
-        ...allocation,
-        percentage: allocation.percentage.toString(),
-        fiatValue: allocation.fiatValue.toString(),
-      }));
+  it("should calculate the correct asset percentages and fiat values - User Balances", () => {
+    const result = calculatePercentAndFiatValues(
+      MOCK_DATA.categories,
+      assetLists,
+      "user-balances",
+      5
+    ).map((allocation) => ({
+      ...allocation,
+      percentage: allocation.percentage.toString(),
+      fiatValue: allocation.fiatValue.toString(),
+    }));
 
-      expect(result).toEqual([
-        {
-          fiatValue: "$30",
-          percentage: "50%",
-          key: "CTK",
-        },
-        {
-          key: "SAIL",
-          percentage: "33.333%",
-          fiatValue: "$20",
-        },
-        {
-          fiatValue: "$10",
-          key: "WOSMO",
-          percentage: "16.666%",
-        },
-        {
-          key: "Other",
-          percentage: "0%",
-          fiatValue: "$0",
-        },
-      ]);
-    });
+    delete result[0].asset;
+    delete result[1].asset;
+    delete result[2].asset;
 
-    it("should calculate the correct asset percentages and fiat values - User Balances", () => {
-      const result = calculatePercentAndFiatValues(
-        MOCK_DATA.categories,
-        assetLists,
-        "user-balances",
-        5
-      ).map((allocation) => ({
-        ...allocation,
-        percentage: allocation.percentage.toString(),
-        fiatValue: allocation.fiatValue.toString(),
-      }));
+    expect(result).toEqual([
+      {
+        fiatValue: "$20",
+        key: "SAIL",
+        percentage: "200%",
+      },
+      {
+        key: "WOSMO",
+        percentage: "100%",
+        fiatValue: "$10",
+      },
+      {
+        key: "Other",
+        percentage: "0%",
+        fiatValue: "$0",
+      },
+    ]);
+  });
 
-      expect(result).toEqual([
-        {
-          fiatValue: "$20",
-          key: "SAIL",
-          percentage: "200%",
-        },
-        {
-          key: "WOSMO",
-          percentage: "100%",
-          fiatValue: "$10",
-        },
-        {
-          key: "Other",
-          percentage: "0%",
-          fiatValue: "$0",
-        },
-      ]);
-    });
-
-    it("should handle zero balances correctly", () => {
-      const zeroBalanceData = {
-        ...MOCK_DATA.categories,
-        "total-assets": {
-          capitalization: "0",
-          account_coins_result: [
-            {
-              coin: {
-                denom:
-                  "factory/osmo1pfyxruwvtwk00y8z06dh2lqjdj82ldvy74wzm3/WOSMO",
-                amount: "0",
-              },
-              cap_value: "0",
+  it("should handle zero balances correctly", () => {
+    const zeroBalanceData = {
+      ...MOCK_DATA.categories,
+      "total-assets": {
+        capitalization: "0",
+        account_coins_result: [
+          {
+            coin: {
+              denom:
+                "factory/osmo1pfyxruwvtwk00y8z06dh2lqjdj82ldvy74wzm3/WOSMO",
+              amount: "0",
             },
-            {
-              coin: {
-                denom:
-                  "factory/osmo1rckme96ptawr4zwexxj5g5gej9s2dmud8r2t9j0k0prn5mch5g4snzzwjv/sail",
-                amount: "0",
-              },
-              cap_value: "0",
+            cap_value: "0",
+          },
+          {
+            coin: {
+              denom:
+                "factory/osmo1rckme96ptawr4zwexxj5g5gej9s2dmud8r2t9j0k0prn5mch5g4snzzwjv/sail",
+              amount: "0",
             },
-          ],
-          is_best_effort: false,
-        },
-      };
+            cap_value: "0",
+          },
+        ],
+        is_best_effort: false,
+      },
+    };
 
-      const result = calculatePercentAndFiatValues(
-        zeroBalanceData,
-        assetLists,
-        "total-assets",
-        5
-      ).map((allocation) => ({
-        ...allocation,
-        percentage: allocation.percentage.toString(),
-        fiatValue: allocation.fiatValue.toString(),
-      }));
+    const result = calculatePercentAndFiatValues(
+      zeroBalanceData,
+      assetLists,
+      "total-assets",
+      5
+    ).map((allocation) => ({
+      ...allocation,
+      percentage: allocation.percentage.toString(),
+      fiatValue: allocation.fiatValue.toString(),
+    }));
 
-      expect(result).toEqual([
-        {
-          key: "WOSMO",
-          percentage: "0%",
-          fiatValue: "$0",
-        },
-        {
-          key: "SAIL",
-          percentage: "0%",
-          fiatValue: "$0",
-        },
-        {
-          key: "Other",
-          percentage: "0%",
-          fiatValue: "$0",
-        },
-      ]);
-    });
+    delete result[0].asset;
+    delete result[1].asset;
+    delete result[2].asset;
+
+    expect(result).toEqual([
+      {
+        key: "WOSMO",
+        percentage: "0%",
+        fiatValue: "$0",
+      },
+      {
+        key: "SAIL",
+        percentage: "0%",
+        fiatValue: "$0",
+      },
+      {
+        key: "Other",
+        percentage: "0%",
+        fiatValue: "$0",
+      },
+    ]);
   });
 });
 
@@ -249,7 +260,7 @@ describe("Has Asset Variants", () => {
     const userCoinMinimalDenoms = [
       {
         denom:
-          "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4", // OSMO
+          "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4", // USDC
         amount: "0.2",
       },
       {
@@ -265,14 +276,13 @@ describe("Has Asset Variants", () => {
     ];
 
     const result = checkAssetVariants(userCoinMinimalDenoms, assetLists);
-    expect(result.length).toBe(1);
+    expect(result.length).toBe(2);
   });
 
   it("should return false when there are no asset variants", () => {
     const userCoinMinimalDenoms = [
       {
-        denom:
-          "ibc/498A0751C798A0D9A389AA3691123DADA57DAA4FE165D5C75894505B876BA6E4", // OSMO
+        denom: "uosmo", // OSMO
         amount: "0.2",
       },
       {
