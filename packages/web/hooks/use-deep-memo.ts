4afcbfee0c169189bd30a66c766846b5ad04b496
Greg Nagy <greg@dhamma.works>
2024-11-16 14:40:43 +0100

    feat: add useDeepMemo to use deep equal when memoizing
    lodash.isEqual was another option for deep equality checks but
    fast-deep-equal is lot smaller and more performant

---
+++ packages/web/hooks/use-deep-memo.ts
@@ -0,0 +1,24 @@
+import equal from "fast-deep-equal";
+import { DependencyList, useRef } from "react";
+
+/**
+ * A custom hook for `useMemo` that uses deep comparison on the dependencies.
+ *
+ * @param factory - A function that produces the memoized value.
+ * @param dependencies - The dependency array to be deeply compared.
+ * @returns The memoized value.
+ */
+export function useDeepMemo<T>(
+  factory: () => T,
+  dependencies: DependencyList
+): T {
+  const dependenciesRef = useRef<DependencyList>();
+  const memoizedValueRef = useRef<T>();
+
+  if (!equal(dependenciesRef.current, dependencies)) {
+    dependenciesRef.current = dependencies;
+    memoizedValueRef.current = factory();
+  }
+
+  return memoizedValueRef.current!;
+}
