ba336373529a2e2f107432925dcd9dcfb6b01599
yurydubinin <62520712+yury-dubinin@users.noreply.github.com>
2024-11-28 12:43:27 +0100

    fixed config issue

--- packages/e2e/tests/portfolio.wallet.spec.ts
+++ packages/e2e/tests/portfolio.wallet.spec.ts
@@ -25,7 +25,7 @@ test.describe("Test Portfolio feature", () => {
     // Launch Chrome with a Keplr wallet extension
     context = await chromium.launchPersistentContext(
       "",
-      new TestConfig().getBrowserExtensionConfig(true, pathToExtension)
+      new TestConfig().getBrowserExtensionConfig(false, pathToExtension)
     );
     // Get all new pages (including Extension) in the context and wait
     const emptyPage = context.pages()[0];
