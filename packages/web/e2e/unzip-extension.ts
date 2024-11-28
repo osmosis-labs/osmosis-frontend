a63c9b816115f064fee1e5022c09208defcc4abc
yurydubinin <62520712+yury-dubinin@users.noreply.github.com>
2024-11-28 10:23:57 +0100

    Update Keplr

--- packages/web/e2e/unzip-extension.ts
+++ packages/web/e2e/unzip-extension.ts
@@ -3,25 +3,13 @@ import path from "path";
 
 export class UnzipExtension {
   getPathToExtension() {
-    console.log("Unzip Wallet Extension before tests.");
+    console.log("Unzip Keplr Wallet Extension before tests.");
     // Unzip keplr-extension-manifest
     const pathToZip = path.join(
       __dirname,
-      "./keplr-extension-manifest-v3-v0.12.125.zip"
+      "./keplr-extension-manifest-v3-v0.12.156.zip"
     );
     const pathToExtension = path.join(__dirname, "./keplr-extension-manifest");
-    decompress(pathToZip, pathToExtension);
-    return pathToExtension;
-  }
-
-  getPathToMetaMaskExtension() {
-    console.log("Unzip Wallet Extension before tests.");
-    // Unzip metamask-extension-manifest
-    const pathToZip = path.join(__dirname, "./metamask-chrome-12.1.1.zip");
-    const pathToExtension = path.join(
-      __dirname,
-      "./metamask-extension-manifest"
-    );
     decompress(pathToZip, pathToExtension);
     return pathToExtension;
   }
