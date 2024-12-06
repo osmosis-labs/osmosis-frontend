d679e49dccc760448d2d065e2dbbcb7d8cd2de94
Jon Ator <jonathanator0@gmail.com>
2024-12-05 20:36:36 -0500

    clean up env

--- packages/server/src/queries/blockaid/index.ts
+++
@@ -1,9 +0,0 @@
-import { BLOCKAID_API_KEY } from "../../env";
-
-export * from "./transaction";
-
-export const blockaidAuthHeaders: HeadersInit | undefined = BLOCKAID_API_KEY
-  ? {
-      "x-api-key": BLOCKAID_API_KEY,
-    }
-  : undefined;
--- packages/server/src/queries/data-services/token-pair-historical-chart.ts
+++ packages/server/src/queries/data-services/token-pair-historical-chart.ts
@@ -1,6 +1,6 @@
 import { apiClient } from "@osmosis-labs/utils";
 
-import { TIMESERIES_DATA_URL } from "../../env";
+import { HISTORICAL_DATA_URL } from "../../env";
 
 export interface TokenPairHistoricalPrice {
   close: number;
@@ -33,7 +33,7 @@ export async function queryTokenPairHistoricalChart(
   // collect params
   const url = new URL(
     `/pairs/v1/historical/${poolId}/chart?asset_in=${quoteCoinMinimalDenom}&asset_out=${baseCoinMinimalDenom}&range=${priceRange}&asset_type=denom`,
-    TIMESERIES_DATA_URL
+    HISTORICAL_DATA_URL
   );
   try {
     const response = await apiClient<
