54386427289d8a7db3f53a836193331fba818d43
Enrico Barbieri <enrico@osmosis.team>
2024-12-06 01:30:47 +0100

    refactor: :recycle: Move getBaseUrl to url utils file
    getBaseUrl is now needed outside trpc

--- packages/web/utils/trpc.ts
+++ packages/web/utils/trpc.ts
@@ -27,12 +27,7 @@ import {
   constructEdgeUrlPathname,
   EdgeRouterKey,
 } from "~/utils/trpc-edge";
-
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
-};
+import { getBaseUrl } from "~/utils/url";
 
 /**
  * Create a minimal local router to avoid importing
--- packages/web/utils/url.ts
+++ packages/web/utils/url.ts
@@ -1,3 +1,9 @@
+export const getBaseUrl = (absolute = false) => {
+  if (!absolute && typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+  return `http://localhost:${process.env.PORT ?? 3000}`; // dev SSR should use localhost
+};
+
 export function removeQueryParam(key: string) {
   if (!key) return;
   if (typeof window === "undefined") return;
