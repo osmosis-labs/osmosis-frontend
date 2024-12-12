24cbea517122e17da86d50a3b97647419c06c31e
Jose Felix <jfelx@outlook.com>
2024-12-12 18:36:57 -0400

    (Formatting) Lint fix (#4014)

--- packages/web/config/feature-flag.ts
+++ packages/web/config/feature-flag.ts
@@ -3,10 +3,10 @@ import { IS_TESTNET } from "./env";
 /** Blacklists pools out at the query level. Marks them as non existant. */
 export const BlacklistedPoolIds: string[] = ["895"];
 
-/** 
-* Cosmwasm Code Ids confirmed to be transmuter pools in current env. 
-* @deprecated use packages/server/src/queries/complex/pools/env.ts 
-*/
+/**
+ * Cosmwasm Code Ids confirmed to be transmuter pools in current env.
+ * @deprecated use packages/server/src/queries/complex/pools/env.ts
+ */
 export const TransmuterPoolCodeIds = IS_TESTNET ? ["3084"] : ["148"];
 
 export const RecommendedSwapDenoms = [
