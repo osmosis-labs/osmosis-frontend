61314b9eafb100ff0df0509e10738869f76303fc
Davide Segullo <davidesegullo@gmail.com>
2024-12-18 16:23:19 +0100

    feat: :sparkles: add missing Assets.pageViewed event (#4022)

--- packages/web/pages/assets/index.tsx
+++ packages/web/pages/assets/index.tsx
@@ -1,11 +1,19 @@
 import type { NextPage } from "next";
 
 import { AssetsInfoTable } from "~/components/table/asset-info";
+import { EventName } from "~/config";
+import { useAmplitudeAnalytics } from "~/hooks";
 
-const Assets: NextPage = () => (
-  <main className="mx-auto max-w-container p-8 pt-4 md:p-4">
-    <AssetsInfoTable tableTopPadding={16} />
-  </main>
-);
+const Assets: NextPage = () => {
+  useAmplitudeAnalytics({
+    onLoadEvent: [EventName.Assets.pageViewed],
+  });
+
+  return (
+    <main className="mx-auto max-w-container p-8 pt-4 md:p-4">
+      <AssetsInfoTable tableTopPadding={16} />
+    </main>
+  );
+};
 
 export default Assets;
