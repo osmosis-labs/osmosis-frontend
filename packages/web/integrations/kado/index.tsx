e344a1adf3ccc48e44188a7205d5933554fcb042
JeremyParish69 <95667791+JeremyParish69@users.noreply.github.com>
2025-06-24 13:01:18 -0600

    Update index.tsx

--- packages/web/integrations/kado/index.tsx
+++ packages/web/integrations/kado/index.tsx
@@ -7,8 +7,8 @@ import { useStore } from "~/stores";
 
 /** Assumed wallet connected */
 export const Kado: FunctionComponent<
-  Pick<ModalBaseProps, "isOpen" | "onRequestClose">
-> = observer(() => {
+  { assetKey: string } & Pick<ModalBaseProps, "isOpen" | "onRequestClose">
+> = observer(({ assetKey }) => {
   const { chainStore, accountStore } = useStore();
 
   const account = accountStore.getWallet(chainStore.osmosis.chainId);
@@ -17,7 +17,8 @@ export const Kado: FunctionComponent<
 
   return (
     <iframe
-      src={`https://widget.swapped.com/`}
+      src={`https://app.kado.money/?
+      onPayCurrency=USD&onPayAmount=200&onRevCurrency=OSMO&offPayCurrency=${assetKey}&offRevCurrency=USD&network=OSMOSIS&onToAddress=${account?.address}&offFromAddress=X&cryptoList=OSMO,USDC,wBTC&networkList=OSMOSIS&apiKey=67a2aaad-a2a5-4412-be40-e3a70aa7b53d&product=BUY`}
       width="420"
       height="700"
       allow="clipboard-write; payment; accelerometer; gyroscope; camera; geolocation; autoplay; fullscreen;"
