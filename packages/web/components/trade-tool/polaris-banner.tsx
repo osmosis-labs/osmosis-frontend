cd06e26f44d941ffeab550bf970d030e176166a2
fabryscript <vivaldifabrizio10@gmail.com>
2025-07-14 13:03:41 +0200

    feat: add polaris banner

---
+++ packages/web/components/trade-tool/polaris-banner.tsx
@@ -0,0 +1,78 @@
+import Image from "next/image";
+import Link from "next/link";
+
+import { Icon } from "~/components/assets/icon";
+import { useSwap } from "~/hooks/use-swap";
+
+export const PolarisBanner = ({ onClose }: { onClose: () => void }) => {
+  const { fromAsset } = useSwap();
+
+  if (!fromAsset) return null;
+
+  return (
+    <div className="relative overflow-hidden flex flex-col my-3 gap-2 bg-[#653026] p-5 rounded-3xl">
+      <h6 className="inline-flex items-center gap-1 text-[#FFFFFF70] font-semibold">
+        Trade your
+        <div className="px-1">
+          <Image
+            src={fromAsset?.coinImageUrl ?? ""}
+            alt={fromAsset?.coinDenom ?? ""}
+            width={24}
+            height={24}
+            className="rounded-full"
+          />
+        </div>
+        <span className="text-[#F8F6F2]">{fromAsset?.coinDenom ?? ""}</span>
+        with
+      </h6>
+      <h6 className="inline-flex items-center gap-1 text-[#FFFFFF70] font-semibold">
+        <div className="px-1">
+          <Image
+            src="/tokens/generated/sol.svg"
+            alt="SOL"
+            width={24}
+            height={24}
+          />
+        </div>
+        <span className="text-[#F8F6F2]">SOL,</span>
+        <div className="px-1">
+          <Image src="/logos/hype-logo.png" alt="HYPE" width={24} height={24} />
+        </div>
+        <span className="text-[#F8F6F2]">HYPE,</span>
+      </h6>
+      <h6 className="inline-flex items-center gap-1 text-[#FFFFFF70] font-semibold">
+        <div className="px-1">
+          <Image
+            src="/logos/polaris-banner-logos.png"
+            alt="GOOGL - 1TSLA - AAPL logos"
+            width={52}
+            height={24}
+          />
+        </div>
+        <span>and so much more.</span>
+      </h6>
+      <Link
+        href="https://polaris.app"
+        target="_blank"
+        className="py-3 px-4 bg-[#F99E53] rounded-3xl mt-6 flex items-center gap-1 text-[#0A0F0D] max-w-[260px] hover:bg-[#F78425] transition-colors"
+      >
+        <span className="subtitle1">Trade anything on Polaris</span>
+        <Icon id="arrow-up-right" width={16} height={16} className="mb-1" />
+      </Link>
+      <Image
+        src="/images/polaris-compass.png"
+        alt=""
+        width={131}
+        height={189}
+        className="absolute right-0 top-2"
+      />
+      <button
+        type="button"
+        className="rounded-full absolute top-3 right-3 z-10 h-8 w-8 flex items-center justify-center bg-osmoverse-alpha-850 backdrop-blur-xl"
+        onClick={onClose}
+      >
+        <Icon id="close" width={16} height={16} className="text-[#F99E53]" />
+      </button>
+    </div>
+  );
+};
