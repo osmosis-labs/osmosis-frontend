aac97f1ad4e5eb097f1a3b6e9f3b26e7b1eb28bc
JohnnyWyles <johnny.wyles@osmosis.team>
2025-10-11 14:35:51 +0100

    Add more messages to one click trading

--- packages/types/src/one-click-trading-types.ts
+++ packages/types/src/one-click-trading-types.ts
@@ -1,12 +1,23 @@
 import type { PricePretty } from "@osmosis-labs/unit";
 
 export type AvailableOneClickTradingMessages =
+  // Trading - Swaps
   | "/osmosis.poolmanager.v1beta1.MsgSwapExactAmountIn"
   | "/osmosis.poolmanager.v1beta1.MsgSplitRouteSwapExactAmountIn"
   | "/osmosis.poolmanager.v1beta1.MsgSwapExactAmountOut"
   | "/osmosis.poolmanager.v1beta1.MsgSplitRouteSwapExactAmountOut"
+  // LP Management - Concentrated Liquidity
   | "/osmosis.concentratedliquidity.v1beta1.MsgWithdrawPosition"
-  | "/osmosis.valsetpref.v1beta1.MsgSetValidatorSetPreference";
+  | "/osmosis.concentratedliquidity.v1beta1.MsgCollectSpreadRewards"
+  | "/osmosis.concentratedliquidity.v1beta1.MsgCollectIncentives"
+  | "/osmosis.concentratedliquidity.v1beta1.MsgAddToPosition"
+  | "/osmosis.concentratedliquidity.v1beta1.MsgCreatePosition"
+  // LP Management - GAMM Pools
+  | "/osmosis.gamm.v1beta1.MsgJoinPool"
+  | "/osmosis.gamm.v1beta1.MsgExitPool"
+  // Staking Reward Management
+  | "/osmosis.valsetpref.v1beta1.MsgSetValidatorSetPreference"
+  | "/osmosis.valsetpref.v1beta1.MsgWithdrawDelegationRewards";
 
 export type OneClickTradingResetPeriods = "day" | "week" | "month" | "year";
 export interface OneClickTradingTimeLimit {
