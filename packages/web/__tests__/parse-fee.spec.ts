d54c3264dec2898d406d181e90f8a4c76df34800
Jason Banfield <jason.banfield.za@gmail.com>
2025-10-14 21:34:44 +0200

    Run prettier on web package

--- packages/web/__tests__/parse-fee.spec.ts
+++ packages/web/__tests__/parse-fee.spec.ts
@@ -9,7 +9,10 @@ describe("extractFeeDetailsFromError", () => {
     ["fee of 1,234 USD", { amount: "1234", currency: "USD" }],
     ["cost $12,345,678.90 EUR", { amount: "12345678.90", currency: "EUR" }],
     ["fee of 16.36 usd", { amount: "16.36", currency: "USD" }],
-    ["Network reports a fee of 0.000123 BTC.", { amount: "0.000123", currency: "BTC" }],
+    [
+      "Network reports a fee of 0.000123 BTC.",
+      { amount: "0.000123", currency: "BTC" },
+    ],
     ["This route costs $10 USDT", { amount: "10", currency: "USDT" }],
   ])("parses valid formats: %s", (msg, expected) => {
     expect(extractFeeDetailsFromError(String(msg))).toEqual(expected);
