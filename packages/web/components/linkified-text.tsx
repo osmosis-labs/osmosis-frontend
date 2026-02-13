24c1706a866a88c80c356eb888021b322bd55c28
JohnnyWyles <johnny.wyles@osmosis.team>
2026-02-13 15:51:10 +0000

    import order

--- packages/web/components/linkified-text.tsx
+++ packages/web/components/linkified-text.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, Fragment } from "react";
+import { Fragment, FunctionComponent } from "react";
 
 const URL_REGEX =
   /(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s]{2,}|www\.[a-zA-Z0-9]+\.[^\s]{2,})/gi;
