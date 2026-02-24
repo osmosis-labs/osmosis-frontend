a6e4f2bc5d937c059f886d45220e8f88fedafa60
JohnnyWyles <johnny.wyles@osmosis.team>
2026-02-24 16:29:58 +0000

    revert

--- packages/trpc/src/__tests_e2e__/swap-router.spec.ts
+++ packages/trpc/src/__tests_e2e__/swap-router.spec.ts
@@ -385,8 +385,8 @@ it("Sidecar — Should return valid quote for medium volume token", async () => 
   });
 
   // Desired price is 10% of the total fiat value locked in the pool
-  const desiredPrice = mediumVolumePool.totalFiatValueLocked
-    .toDec()
+  const desiredPrice = mediumVolumePool
+    .totalFiatValueLocked!.toDec()
     .mul(new Dec(0.1));
 
   // Token in amount is the desired price divided by the token price
@@ -444,8 +444,8 @@ it("Sidecar — Should return valid quote for low volume token", async () => {
   });
 
   // Desired price is 10% of the total fiat value locked in the pool
-  const desiredPrice = lowVolumeTokenPool.totalFiatValueLocked
-    .toDec()
+  const desiredPrice = lowVolumeTokenPool
+    .totalFiatValueLocked!.toDec()
     .mul(new Dec(0.1));
 
   // Token in amount is the desired price divided by the token price
