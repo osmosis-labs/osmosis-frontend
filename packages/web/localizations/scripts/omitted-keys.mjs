b161fb60f916d05157d60ecc408f7b08124ed63d
JohnnyWyles <johnny.wyles@osmosis.team>
2026-04-22 09:27:22 +0100

    Revert exception and add localisations

--- packages/web/localizations/scripts/omitted-keys.mjs
+++ packages/web/localizations/scripts/omitted-keys.mjs
@@ -1,9 +1,6 @@
 /** Add key paths here to skip them in localizations tests. */
 export const omittedKeyPaths = [
   "assets.categories",
-  "components.table.orderbook",
-  "components.table.astroport",
-  "components.table.whitewhale",
   "limitOrders.historyTable.columns",
   "transfer.nomic.estimatedTime",
 ];
