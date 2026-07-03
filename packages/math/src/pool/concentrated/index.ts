1752f274e7dcff1328fa2beb8ecd99510c2e4152
JohnnyWyles <johnny.wyles@osmosis.team>
2026-07-03 21:41:39 +0100

    style(math): sort concentrated index exports

--- packages/math/src/pool/concentrated/index.ts
+++ packages/math/src/pool/concentrated/index.ts
@@ -1,6 +1,6 @@
 export * from "./const";
 export * from "./math";
+export * from "./position-value";
 export * from "./quotes";
 export * from "./tick";
 export * from "./types";
-export * from "./position-value";
